c47da2447649106748dbc9e8722556ac34c4b3e0
R. Kukuh <rkukuh@gmail.com>
2018-08-22 17:08:38 +0700

    Code styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,11 +32,11 @@
     <!-- begin::Body -->
     <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark
                  m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
-
         <!-- begin:: Page -->
         <div class="m-grid m-grid--hor m-grid--root m-page">
-            <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-1" id="m_login"
-                style="background-image: url(../../../assets/metronic/app/media/img//bg/bg-1.jpg);">
+            <div id="m_login"
+                    class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-1"
+                    style="background-image: url(../../../assets/metronic/app/media/img//bg/bg-1.jpg);">
                 <div class="m-grid__item m-grid__item--fluid m-login__wrapper">
                     <div class="m-login__container">
                         <div class="m-login__logo">
@@ -46,53 +46,62 @@
                         </div>
                         <div class="m-login__signin">
                             <div class="m-login__head">
-                                <h3 class="m-login__title">Sign In To Admin</h3>
+                                <h3 class="m-login__title">Login Form</h3>
                             </div>
 
-                            <form class="m-login__form m-form" method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                                {{ csrf_field() }}
+                            <form method="POST" action="{{ route('login') }}" class="m-login__form m-form">
+
+                                @csrf
+
                                 <div class="form-group m-form__group">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder="Email"
-                                        value="{{ old('email') }}" required autofocus>
+                                    <input type="email" id="email" name="email" placeholder="Email"
+                                            class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" autofocus>
                                 </div>
                                 <div class="form-group m-form__group">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password"
-                                        name="password" required> @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span> @endif
+                                    <input type="password" id="password" name="password" placeholder="Password"
+                                            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
                                 </div>
                                 <div class="row m-login__form-sub">
                                     <div class="col m--align-left m-login__form-left">
                                         <label class="m-checkbox  m-checkbox--light">
-                                <input type="checkbox" name="remember"> Remember me
-                                <span></span>
-                                </label>
+                                            <input type="checkbox" name="remember"> Remember me
+                                            <span></span>
+                                        </label>
                                     </div>
                                     <div class="col m--align-right m-login__form-right">
-                                        <a href="javascript:;" id="m_login_forget_password" class="m-link">Forget Password ?</a>
+                                        <a href="javascript:;" id="m_login_forget_password" class="m-link">
+                                            Forget Password ?
+                                        </a>
                                     </div>
                                 </div>
                                 <div class="m-login__form-action">
-                                    <button class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primary" type="submit"
-                                        class="btn btn-primary">
-                                        {{ __('Login') }}
+                                    <button type="submit"
+                                            class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">
+                                            {{ __('Login') }}
                                     </button>
                                 </div>
                             </form>
                         </div>
                         <div class="m-login__forget-password">
                             <div class="m-login__head">
-                                <h3 class="m-login__title">Forgotten Password ?</h3>
-                                <div class="m-login__desc">Enter your email to reset your password:</div>
+                                <h3 class="m-login__title">
+                                    Forgotten Password ?
+                                </h3>
+                                <div class="m-login__desc">
+                                    Enter your email to reset your password:
+                                </div>
                             </div>
                             <form class="m-login__form m-form" action="">
                                 <div class="form-group m-form__group">
-                                    <input class="form-control m-input" type="text" placeholder="Email" name="email" id="m_email" autocomplete="off">
+                                    <input type="text" id="m_email" name="email" class="form-control m-input" placeholder="Email" autocomplete="off">
                                 </div>
                                 <div class="m-login__form-action">
-                                    <button id="m_login_forget_password_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">Request</button>&nbsp;&nbsp;
-                                    <button id="m_login_forget_password_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">Cancel</button>
+                                    <button id="m_login_forget_password_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">
+                                        Request
+                                    </button>
+                                    <button id="m_login_forget_password_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">
+                                        Cancel
+                                    </button>
                                 </div>
                             </form>
                         </div>
@@ -110,7 +119,6 @@
         <!--begin::Page Snippets -->
         <script type="text/javascript" src="{{ asset('assets/metronic/snippets/custom/pages/user/login.js')}}"></script>
         <!--end::Page Snippets -->
-
     </body>
     <!-- end::Body -->
 </html>
