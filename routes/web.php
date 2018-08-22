692139d18e8ec8fb7756e1bf47b1295a297db917
R. Kukuh <rkukuh@gmail.com>
2018-08-22 16:39:19 +0700

    Remove duplicate route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', function () {
     return view('auth.login');
 });
-
-Route::get('/home', function () {
-    return view('home');
-});
