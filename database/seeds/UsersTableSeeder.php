c98c01b0abff14c088ec15fd8df52e664843ab1f
R. Kukuh <rkukuh@gmail.com>
2018-09-04 15:21:27 +0700

    Populate seeder data

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +14,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'name'      => 'Super Admin',
+            'email'     => 'admin@madewow.com',
+            'password'  => Hash::make('admin'),
+        ]);
+
+        $user->assignRole(
+            Role::where('name', 'admin')->first()
+        );
     }
 }
