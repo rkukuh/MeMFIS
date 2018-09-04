631e07bc10a4bbb222d231ad9952c9ad17ca1c3f
R. Kukuh <rkukuh@gmail.com>
2018-09-04 14:27:03 +0700

    Add uuid column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
