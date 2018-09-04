7ab1d90025728b0b758556f12ca792eab467d73f
R. Kukuh <rkukuh@gmail.com>
2018-09-04 15:18:53 +0700

    Populate seeder data

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RolesTableSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'customer']);
     }
 }
