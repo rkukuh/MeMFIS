ad90ae56d63909cc01c0752e02b01433dac8c04e
R. Kukuh <rkukuh@gmail.com>
2018-09-04 15:35:21 +0700

    Call seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
