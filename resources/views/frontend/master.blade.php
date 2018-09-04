d53526c2c42eff08cd8597ea4d5b9d8c00a5b463
Audhy <audhy.vk01@gmail.com>
2018-09-04 08:27:24 +0700

    repair typo

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -63,7 +63,7 @@
 <!-- end::Quick Nav -->	
     	<!--begin::Base Scripts -->        
     	    	<script src="{{asset('assets/metronic/vendors/base/vendors.bundle.js')}}" type="text/javascript"></script>
-		    	<script src="assets/metronic/demo/default/base/scripts.bundle.js')}}" type="text/javascript"></script>
+		    	<script src="{{asset('assets/metronic/demo/default/base/scripts.bundle.js')}}" type="text/javascript"></script>
 				<!--end::Base Scripts -->   
 
          
@@ -76,7 +76,7 @@
         
                     
         <!--begin::Page Snippets --> 
-                <script src="{{asset('assets/metronic/app/js/dashboard.js" type="text/javascript')}}"></script>
+                <script src="{{asset('assets/metronic/app/js/dashboard.js')}}" type="text/javascript')}}"></script>
                 <!--end::Page Snippets -->   
                 @stack('footer-scripts')
             </body>
