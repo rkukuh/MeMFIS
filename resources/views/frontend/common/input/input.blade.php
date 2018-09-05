16063a35a3bfd39dd05c266caf823896597af687
Audhy <audhy.vk01@gmail.com>
2018-09-05 14:46:36 +0700

    repair component

--- resources/views/frontend/common/input/input.blade.php
+++ resources/views/frontend/common/input/input.blade.php
@@ -1,4 +1,4 @@
-<input type="{{ $type or 'text' }}" class="form-control  m-input {{ $class or '' }}" name="{{ $name or '' }}" id="{{ $name or '' }}" placeholder="{{ $name or '' }}">
+<input type="{{ $type or 'text' }}" class="form-control  m-input {{ $class or '' }}" name="{{ $name or '' }}" id="{{ $name or '' }}" placeholder="{{ $placeholder or $name }}">
 <div class="form-control-feedback text-danger" id="{{ $name or '' }}-error"></div>
 <span class="m-form__help">
 {{ $text or '' }}
--- resources/views/frontend/common/input/radio.blade.php
+++ resources/views/frontend/common/input/radio.blade.php
@@ -1,4 +1,4 @@
 <label class="m-radio m-radio--state-{{ $color or 'primary' }} {{ $class or '' }}">
-<input type="radio" name="{{ $name or '' }}" value="{{ $value or '' }}"> {{ $Text or '' }}
+<input type="radio" name="{{ $name or '' }}" value="{{ $value or '' }}"> {{ $text or '' }}
 <span></span>
 </label>
