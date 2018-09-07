b6a617fc0190ec4bc97da478cd0a3d2e79a8b3b2
Audhy <audhy.vk01@gmail.com>
2018-09-07 10:13:46 +0700

    item

---
+++ public/js/item.js
@@ -0,0 +1,337 @@
+var Category = {
+    init: function() {
+        $(".m_datatable").mDatatable({
+            data: {
+                type: "remote",
+                source: {
+                    read: {
+                        // sample GET method
+                        method: "GET",
+                        url: "/getitem",
+                        map: function(raw) {
+                            // sample data mapping
+                            var dataSet = raw;
+                            if (typeof raw.data !== "undefined") {
+                                dataSet = raw.data;
+                            }
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: "default",
+                class: "",
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: { input: $("#generalSearch") },
+            toolbar: {
+                items: {
+                    pagination: { pageSizeSelect: [5, 10, 20, 30, 50, 100] }
+                }
+            },
+            columns: [
+                // {
+                //     field: "id",
+                //     title: "#",
+                //     sortable: !1,
+                //     width: 40
+                // },
+                {
+                    field: "code",
+                    title: "Code",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 60
+                },
+                {
+                    field: "name",
+                    title: "Category",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "barcode",
+                    title: "Barcode",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "id_category",
+                    title: "Category",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "description",
+                    title: "Description",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: "active",
+                    title: "Active",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "isppn",
+                    title: "IsPPn",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "isstock",
+                    title: "IsStock",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "accountcode",
+                    title: "AccountCode",
+                    sortable: "asc",
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: "Actions",
+                    width: 110,
+                    title: "Actions",
+                    sortable: !1,
+                    overflow: "visible",
+                    template: function(t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_customer" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill show" title="Details" data-id=' +
+                            t.id +
+                            '>\t\t\t\t\t\t\t<i class="la la-search"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t' +
+                            '<button data-toggle="modal" data-target="#modal_customer" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-id=' +
+                            t.id +
+                            '>\t\t\t\t\t\t\t<i class="la la-pencil"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t' +
+                            '\t\t\t\t\t\t    \t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill  delete" href="#" data-id=' +
+                            t.id +
+                            ' title="Delete"><i class="la la-trash"></i> </a>\t\t\t\t\t\t    \t'
+                        );
+                    }
+                }
+            ]
+        });
+
+        var simpan = $(".modal-footer").on("click", ".add", function() {
+            var name = $("input[name=name]").val();
+            $("#simpan").text("Simpan");
+            var registerForm = $("#CustomerForm");
+            var formData = registerForm.serialize();
+            // var tes = $("input[name=name]").val();
+            $("#name-error").html("");
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                },
+                type: "post",
+                url: "/category",
+                data: {
+                    _token: $("input[name=_token]").val(),
+                    name: name
+                },
+                success: function(data) {
+                    console.log(data);
+                    if (data.errors) {
+                        if (data.errors.name) {
+                            $("#name-error").html(data.errors.name[0]);
+                            document.getElementById("name").value = name;
+                        }
+                        // if(data.errors.email){
+                        //     $( '#email-error' ).html( data.errors.email[0] );
+                        // }
+                    } else {
+                        $("#modal_customer").modal("hide");
+                        toastr.success("Berhasil Disimpan.", "Sukses!!", {
+                            timeOut: 5000
+                        });
+                        var table = $(".m_datatable").mDatatable();
+                        table.originalDataSet = [];
+                        table.reload();
+                    }
+                }
+            });
+        });
+
+        var edit = $(".m_datatable").on("click", ".edit", function() {
+            $("#button").show();
+            var triggerid = $(this).data("id");
+            $("#simpan").text("Perbarui");
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                },
+                type: "get",
+                url: "/category/" + triggerid + "/edit",
+                success: function(data) {
+                    document.getElementById("name").value = data.name;
+                    document.getElementById("id").value = data.id;
+                    $(".btn-success").addClass("update");
+                    $(".btn-success").removeClass("add");
+                },
+                error: function(jqXhr, json, errorThrown) {
+                    // this are default for ajax errors
+                    var errors = jqXhr.responseJSON;
+                    var errorsHtml = "";
+                    $.each(errors["errors"], function(index, value) {
+                        $("#kategori-error").html(value);
+                    });
+                }
+            });
+        });
+
+        var update = $(".modal-footer").on("click", ".update", function() {
+            var name = $("input[name=name]").val();
+            $("#name-error").html("");
+            $("#button").show();
+            var triggerid = $("input[name=id]").val();
+            $("#simpan").text("Perbarui");
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                },
+                type: "put",
+                url: "/category/" + triggerid,
+                data: {
+                    _token: $("input[name=_token]").val(),
+                    name: name
+                },
+                success: function(data) {
+                    console.log(data);
+                    if (data.errors) {
+                        if (data.errors.name) {
+                            $("#name-error").html(data.errors.name[0]);
+                            document.getElementById("name").value = name;
+                        }
+                        // if(data.errors.email){
+                        //     $( '#email-error' ).html( data.errors.email[0] );
+                        // }
+                    } else {
+                        $("#modal_customer").modal("hide");
+                        toastr.success("Berhasil Disimpan.", "Sukses!!", {
+                            timeOut: 5000
+                        });
+                        var table = $(".m_datatable").mDatatable();
+                        table.originalDataSet = [];
+                        table.reload();
+                    }
+                }
+            });
+        });
+
+        var show = $(".m_datatable").on("click", ".show", function() {
+            $("#button").hide();
+            var triggerid = $(this).data("id");
+            $("#simpan").text("Perbarui");
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                },
+                type: "get",
+                url: "/category/" + triggerid,
+                success: function(data) {
+                    document.getElementById("TitleModalCustomer").innerHTML =
+                        "Detail Customer #ID-" + triggerid;
+
+                    document.getElementById("name").value = data.name;
+                    document.getElementById("name").readOnly = true;
+                    //   document.getElementById('id').value=data.id;
+                    //   $('.btn-success').removeClass('simpan');
+                    //   $('.btn-success').addClass('update');
+                },
+                error: function(jqXhr, json, errorThrown) {
+                    // this are default for ajax errors
+                    var errors = jqXhr.responseJSON;
+                    var errorsHtml = "";
+                    $.each(errors["errors"], function(index, value) {
+                        $("#kategori-error").html(value);
+                    });
+                }
+            });
+        });
+
+
+        var remove = $(".m_datatable").on("click", ".delete", function() {
+            var triggerid = $(this).data("id");
+            swal({
+                title: "Are you sure?",
+                text: "You will not be able to recover this imaginary file!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, keep it"
+            }).then(result => {
+                if (result.value) {
+                    $.ajax({
+                        headers: {
+                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                "content"
+                            )
+                        },
+                        type: "DELETE",
+                        url: "/category/" + triggerid + "",
+                        success: function(data) {
+                            toastr.success(
+                                "Data Berhasil Dihapus.",
+                                "Sukses!!!",
+                                { timeOut: 5000 }
+                            );
+                            var table = $(".m_datatable").mDatatable();
+                            table.originalDataSet = [];
+                            table.reload();
+                        },
+                        error: function(jqXhr, json, errorThrown) {
+                            // this are default for ajax errors
+                            var errors = jqXhr.responseJSON;
+                            var errorsHtml = "";
+                            $.each(errors["errors"], function(index, value) {
+                                $("#delete-error").html(value);
+                            });
+                        }
+                    });
+                    swal(
+                        "Deleted!",
+                        "Your imaginary file has been deleted.",
+                        "success"
+                    );
+                } else {
+                    swal(
+                        "Cancelled",
+                        "Your imaginary file is safe :)",
+                        "error"
+                    );
+                }
+            });
+        });
+
+        $('#modal_customer').on('hidden.bs.modal', function(e) {
+            $(this).find('#CustomerForm')[0].reset();
+            $("#name-error").html("");
+
+          });
+
+    }
+};
+
+
+jQuery(document).ready(function() {
+    Category.init();
+});
---
+++ resources/views/frontend/item/modal.blade.php
@@ -0,0 +1,171 @@
+<!-- begin::Modal-->
+<div class="modal fade" id="modal_customer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="TitleModalCustomer">Item</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <!--begin::Form -->
+                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="CustomerForm">
+                            <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                                <div class="m-portlet__body">
+                                    <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Code *
+                                        </label>
+
+                                    @component('frontend.common.input.input')
+                                        @slot('text', 'Code')
+                                        @slot('name', 'code')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Name *
+                                        </label>
+                                        <br>
+                                        @component('frontend.common.input.input')
+                                        @slot('text', 'Name')
+                                        @slot('name', 'name')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    </div>
+                                    <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Barcode *
+                                        </label>
+
+                                    @component('frontend.common.input.input')
+                                        @slot('text', 'Barcode')
+                                        @slot('name', 'barcode')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Category *
+                                        </label>
+                                        <br>
+                                        @component('frontend.common.input.select')
+                                            @slot('text', 'Category')
+                                            @slot('name', 'category')
+                                            @slot('id', 'm_select2_1')
+                                            @slot('style', 'width:100%')
+                                        @endcomponent
+
+                                    </div>
+                                    </div>
+                                    <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Description *
+                                        </label>
+
+                                    @component('frontend.common.input.textarea')
+                                        @slot('name', 'description')
+                                        @slot('rows', '3')
+                                        @slot('text', 'Description')
+                                    @endcomponent
+
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        Active *
+                                        </label>
+                                        <br>
+                                        @component('frontend.common.input.input')
+                                        @slot('text', 'Active')
+                                        @slot('name', 'active')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    </div>
+                                    <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        IsPPn *
+                                        </label>
+
+                                        @component('frontend.common.input.input')
+                                        @slot('text', 'IsPPn')
+                                        @slot('name', 'isppn')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        IsStock *
+                                        </label>
+                                        <br>
+                                        @component('frontend.common.input.input')
+                                        @slot('text', 'IsStock')
+                                        @slot('name', 'isstock')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    </div>
+                                    <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        xPicture *
+                                        </label>
+
+                                        @component('frontend.common.input.upload')
+                                        @slot('text', 'xPicture')
+                                        @slot('name', 'xpicture')
+                                    @endcomponent
+
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                        AccountCode *
+                                        </label>
+                                        <br>
+                                        @component('frontend.common.input.input')
+                                        @slot('text', 'AccountCode')
+                                        @slot('name', 'accountcode')
+                                        @slot('type', 'text')
+                                    @endcomponent
+
+                                    </div>
+                                    </div>
+                                    
+                                    
+                                </div>
+
+                    <div class="modal-footer">
+                          @component('frontend.common.buttons.close')
+                            @slot('color', 'secondary')
+                            @slot('size', 'md')
+                            @slot('data_dismiss', 'modal')
+                          @endcomponent
+
+                          @component('frontend.common.buttons.submit')
+                            @slot('color', 'success')
+                            @slot('size', 'md')
+                            @slot('class', 'add')
+                        
+                          @endcomponent
+
+                    </div>
+                    </div>
+                            </form>
+                </div>
+                </div>
+                </div>
+                <!--end::Modal-->
