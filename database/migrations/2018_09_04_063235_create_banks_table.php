aed7de4230a5585b7664fbf79707c4c1710b1707
R. Kukuh <rkukuh@gmail.com>
2018-09-10 14:48:03 +0700

    FIX: Missing uuid column

--- database/migrations/2018_09_04_063235_create_banks_table.php
+++ database/migrations/2018_09_04_063235_create_banks_table.php
@@ -15,6 +15,7 @@ class CreateBanksTable extends Migration
     {
         Schema::create('banks', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
             $table->string('abbr');
             $table->string('name');
             $table->timestamps();
