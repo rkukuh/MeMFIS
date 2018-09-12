9999f3fff3ba5e70e542704be7cce48a4a18a5e0
R. Kukuh <rkukuh@gmail.com>
2018-09-12 15:52:32 +0700

    Add columns

--- database/migrations/2018_09_12_083619_create_phones_table.php
+++ database/migrations/2018_09_12_083619_create_phones_table.php
@@ -15,7 +15,20 @@ class CreatePhonesTable extends Migration
     {
         Schema::create('phones', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('number');
+            $table->string('ext');
+            $table->unsignedInteger('type_id');
+            $table->boolean('is_primary');
+            $table->integer('phoneable_id');
+            $table->string('phoneable_type');
             $table->timestamps();
+            $table->softDeletes();
+
+            // $table->foreign('type_id')
+            //       ->references('id')->on('types')
+            //       ->onUpdate('cascade')
+            //       ->onDelete('restrict');
         });
     }
 
