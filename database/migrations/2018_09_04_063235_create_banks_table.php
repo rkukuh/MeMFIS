a04667b17a1d718334ef566e7a8d5b9c1c556473
R. Kukuh <rkukuh@gmail.com>
2018-09-12 13:05:06 +0700

    Change 'abbr' attribute to 'code'

--- database/migrations/2018_09_04_063235_create_banks_table.php
+++ database/migrations/2018_09_04_063235_create_banks_table.php
@@ -16,7 +16,7 @@ class CreateBanksTable extends Migration
         Schema::create('banks', function (Blueprint $table) {
             $table->increments('id');
             $table->char('uuid', 36)->unique();
-            $table->string('abbr');
+            $table->string('code');
             $table->string('name');
             $table->timestamps();
             $table->softDeletes();
