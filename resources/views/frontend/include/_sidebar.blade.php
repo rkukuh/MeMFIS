b0ae03283e8723bbab0cff70886a157059c97f5a
Audhy <audhy.vk01@gmail.com>
2018-09-12 14:30:07 +0700

    remove auditable sidebar menu

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -116,15 +116,6 @@
             </span>
         </a>
         </li>      
-
-        <li class="m-menu__item" aria-haspopup="true"  data-menu-submenu-toggle="hover">
-        <a  href="/audit" class="m-menu__link m-menu__toggle">
-            <i class="m-menu__link-icon flaticon-list-3"></i>
-            <span class="m-menu__link-text">
-            Audit
-            </span>
-        </a>
-        </li>      
         <li class="m-menu__item" aria-haspopup="true"  data-menu-submenu-toggle="hover">
         <a  href="" class="m-menu__link m-menu__toggle">
             <i class="m-menu__link-icon flaticon-user"></i>
