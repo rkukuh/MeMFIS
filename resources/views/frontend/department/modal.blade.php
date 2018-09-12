7ee968b19145de283bd4787d130f8f7c4ab2a2d6
Audhy <audhy.vk01@gmail.com>
2018-09-12 14:29:52 +0700

    repair modal

--- resources/views/frontend/department/modal.blade.php
+++ resources/views/frontend/department/modal.blade.php
@@ -1,9 +1,9 @@
 <!--begin::Modal-->
-<div class="modal fade" id="modal_customer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal_department" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="TitleModalCustomer">Bank</h5>
+                        <h5 class="modal-title" id="TitleModalDepartment">Bank</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
@@ -11,13 +11,13 @@
                     <div class="modal-body">
 
                         <!--begin::Form-->
-                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="CustomerForm">
+                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="DepartmentForm">
                             <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
                                 <div class="m-portlet__body">
                                     <div class="form-group m-form__group row ">
                                     <div class="col-sm-6 col-md-6 col-lg-6">
                                         <label class="form-control-label">
-                                        Abbr *
+                                        Department *
                                         </label>
 
                                     @component('frontend.common.input.input')
--- resources/views/frontend/email/modal.blade.php
+++ resources/views/frontend/email/modal.blade.php
@@ -1,9 +1,9 @@
 <!--begin::Modal-->
-<div class="modal fade" id="modal_customer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal_email" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="TitleModalCustomer">Email</h5>
+                        <h5 class="modal-title" id="TitleModalEmail">Email</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
@@ -11,13 +11,13 @@
                     <div class="modal-body">
 
                         <!--begin::Form-->
-                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="CustomerForm">
+                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="EmailForm">
                             <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
                                 <div class="m-portlet__body">
                                     <div class="form-group m-form__group row ">
                                     <div class="col-sm-12 col-md-12 col-lg-12">
                                     <label class="form-control-label">
-                                        Eanil
+                                        Email
                                         </label>
 
                                       <div id="m_repeater_1a">                    
@@ -62,41 +62,8 @@
                                                 </div>
                                             </div>
                                         </div>
-
-                                        <!-- <label class="form-control-label">
-                                        Name *
-                                        </label>
-
-                                        
-
-                                    @component('frontend.common.input.input')
-                                        @slot('text', 'Name')
-                                        @slot('name', 'name')
-                                        @slot('type', 'text')
-                                    @endcomponent -->
-
                                     </div>
-                                    <!-- <div class="col-sm-6 col-md-6 col-lg-6"> -->
-                                        <!-- <label class="form-control-label">
-                                        type *
-                                        </label>
-                                        <br>
-                                    @component('frontend.common.input.radio')
-                                        @slot('text', 'Work')
-                                        @slot('name', 'account_no')
-                                        @slot('value', 'work')
-                                    @endcomponent
-                                    <br>
-                                    @component('frontend.common.input.radio')
-                                        @slot('text', 'Personal')
-                                        @slot('name', 'account_no')
-                                        @slot('value', 'personal')
-                                    @endcomponent -->
-
-                                    <!-- </div> -->
-                                    </div>
-                                    
-                                    
+                                    </div>                                    
                                 </div>
 
                     <div class="modal-footer">
