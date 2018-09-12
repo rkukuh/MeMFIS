6c223e75fc06432acdb0b67565a41977a259a945
R. Kukuh <rkukuh@gmail.com>
2018-09-12 20:53:17 +0700

    Add columns

--- database/migrations/2018_09_12_134432_create_emails_table.php
+++ database/migrations/2018_09_12_134432_create_emails_table.php
@@ -15,7 +15,19 @@ class CreateEmailsTable extends Migration
     {
         Schema::create('emails', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('address');
+            $table->unsignedInteger('type_id');
+            $table->boolean('is_primary');
+            $table->integer('emailable_id');
+            $table->string('emailable_type');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('type_id')
+                  ->references('id')->on('types')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');
         });
     }
 
