98bfa83149ff2e631a426cd00236962b63ffaac8
R. Kukuh <rkukuh@gmail.com>
2018-09-12 16:21:18 +0700

    Populate seed data

--- database/seeds/TypesTableSeeder.php
+++ database/seeds/TypesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use PharIo\Manifest\Type;
 use Illuminate\Database\Seeder;
 
 class TypesTableSeeder extends Seeder
@@ -11,6 +12,40 @@ class TypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /** PHONE */
+
+        Type::create([
+            'name' => 'Work',
+            'of'  => 'phone',
+        ]);
+
+        Type::create([
+            'name' => 'Personal',
+            'of'  => 'phone',
+        ]);
+
+        /** EMAIL */
+
+        Type::create([
+            'name' => 'Work',
+            'of'  => 'email',
+        ]);
+
+        Type::create([
+            'name' => 'Personal',
+            'of'  => 'email',
+        ]);
+
+        /** FAX */
+
+        Type::create([
+            'name' => 'Work',
+            'of'  => 'fax',
+        ]);
+
+        Type::create([
+            'name' => 'Personal',
+            'of'  => 'fax',
+        ]);
     }
 }
