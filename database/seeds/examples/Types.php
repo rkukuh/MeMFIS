f18b1cc20987ecb413855ee3a23bce3ce472a9e1
R. Kukuh <rkukuh@gmail.com>
2018-09-12 16:23:24 +0700

    Populate example data

--- database/seeds/examples/Types.php
+++ database/seeds/examples/Types.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class Types extends Seeder
 {
@@ -11,6 +12,6 @@ class Types extends Seeder
      */
     public function run()
     {
-        //
+        factory(Type::class, 10)->create();
     }
 }
