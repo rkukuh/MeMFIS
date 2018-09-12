13a5d32a1e67f2f6943b7af60e497dc99c2536f5
R. Kukuh <rkukuh@gmail.com>
2018-09-12 10:59:25 +0700

    Move some routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 Auth::routes();
 
+Route::view('/home', 'frontend.home');
+Route::get('/home', 'HomeController@index')->name('home');
+
 require_once('admin.php');
 require_once('frontend.php');
 
