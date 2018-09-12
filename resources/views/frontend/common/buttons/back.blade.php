2ac9d92952c6d4420ec75243c78ed2e19a5bbbe8
Audhy <audhy.vk01@gmail.com>
2018-09-12 14:26:37 +0700

    back component

---
+++ resources/views/frontend/common/buttons/back.blade.php
@@ -0,0 +1,14 @@
+
+
+
+<a class="btn btn-{{ $color or 'default' }} btn-{{ $size or 'md' }} {{ $class or '' }}" 
+    href="{{ $href or '' }}"
+    style="{{ $style or '' }}">
+    <span>
+    <i class="la la-undo"></i>
+    </span>
+    {{ $text or 'Back' }}
+</a>
+
+
+
