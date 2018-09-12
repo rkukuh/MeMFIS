ad86f3a94fb766d43a1a70a93d43958c9048719f
R. Kukuh <rkukuh@gmail.com>
2018-09-12 16:24:49 +0700

    Define local scopes

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\MemfisModel;
+use Illuminate\Database\Eloquent\Builder;
 
 class Type extends MemfisModel
 {
@@ -10,4 +11,39 @@ class Type extends MemfisModel
         'name',
         'of',
     ];
+
+    /******************************************* SCOPE *******************************************/
+
+    /**
+     * Scope a query to only include category of phone.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfPhone(Builder $query)
+    {
+        return $query->where('of', 'phone');
+    }
+
+    /**
+     * Scope a query to only include category of email.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfEmail(Builder $query)
+    {
+        return $query->where('of', 'email');
+    }
+
+    /**
+     * Scope a query to only include category of fax.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfFax(Builder $query)
+    {
+        return $query->where('of', 'fax');
+    }
 }
