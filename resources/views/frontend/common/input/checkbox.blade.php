4d942b85d0b5410b8791860fe72726eb2d49ed1e
Audhy <audhy.vk01@gmail.com>
2018-09-13 11:27:04 +0700

    add value and element to component

--- resources/views/frontend/common/input/checkbox.blade.php
+++ resources/views/frontend/common/input/checkbox.blade.php
@@ -1,5 +1,5 @@
 <label class="m-checkbox m-checkbox--{{ $color or 'primary' }} {{ $class or '' }}">
-<input type="checkbox" name="{{ $name or '' }}" style="{{$style or ''}}">{{ $text or '' }} 
+<input type="checkbox" name="{{ $name or '' }}" style="{{$style or ''}}" {{$editable or ''}} {{$value or ''}}>{{ $text or '' }} 
 <span></span>
 
 </label>
