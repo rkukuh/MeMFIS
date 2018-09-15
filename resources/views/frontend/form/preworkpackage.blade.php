2446b6504e050a7d1c26281f6d5a714e833218bf
Audhy <audhy.vk01@gmail.com>
2018-09-15 16:06:12 +0700

    add form

---
+++ resources/views/frontend/form/preworkpackage.blade.php
@@ -0,0 +1,216 @@
+<!-- inputan dari task card -->
+<html>
+    <head>
+    </head>
+    <body>
+        <table  width="100%" style="margin: 0px;">
+            <tr>
+                <th colspan="8" height="60" >
+                <center>
+                    <img src="img/LogoMMF.png" alt="logo" height="100%" style="margin-bottom:-20px">
+                    <h4>CALCULATION MHRS PROJECT C07 CHECK B737-36N PK-NAU <br>NAM AIR</h4>
+                </center>
+                </th>
+            </tr>
+        </table>      
+        <table  width="100%" border="1px" style="margin: 0px;border-collapse: collapse;">
+            <tr>
+                <th colspan="2" width="52%"><center>
+                   WORK SCOPE</center> 
+                </th>
+                <th width="12%"><center>
+                   MHRS</center>
+                </th>
+                <th width="12%"><center>
+                   MATERIAL</center> 
+                </th>
+                <th width="12%"><center>
+                   TOOLS</center> 
+                </th>
+                <th width="12%"><center>
+                   TAT <br>(Turn Around Time)</center> 
+                </th>
+            </tr>
+        </table>
+        <table  width="100%" border="1px" style="margin: 0px;border-collapse: collapse;">
+            <tr>
+                <td width="12%"> A. Routine</td>
+                <td width="40%">Basic C07 (414 Tasks)</td>
+                <td width="12%">258.87</td>
+                <td width="12%">.....</td>
+                <td width="12%">.....</td>
+                <td width="12%">.....</td>
+            </tr>
+            <tr >
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>B. AD/SB </td>
+                <td>AD/SB (31 Task)</td>
+                <td>979.40</td>
+                <td>.....</td>
+                <td>.....</td>
+                <td >.....</td>
+            </tr>
+            <tr>
+                <td height="15"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>C. STC</td>
+                <td>STC Items (35 Tasks)</td>
+                <td>107.00</td>
+                <td>.....</td>
+                <td>.....</td>
+                <td >.....</td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>D. HTC</td>
+                <td>Hard Time Component items (33 Tasks)</td>
+                <td>43.10</td>
+                <td>.....</td>
+                <td>.....</td>
+                <td >.....</td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+
+            </tr>
+            <tr>
+                <td>E. Access</td>
+                <td>Access Panel</td>
+                <td>60.99</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>F. Additional</td>
+                <td>EA Non AD (30 Tasks)</td>
+                <td>34.30</td>
+                <td>.....</td>
+                <td>....</td>
+                <td>....</td>                
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>>>>TOTAL</td>
+                <td><b>1483.66</b></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td ><b>ADDITIONAL</b></td>
+                <td>EST Defect Finding</td>
+                <td>370.92</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td height="15"> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> </td>
+                <td>>>>TOTAL</td>
+                <td>370.92</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td > </td>
+                <td>>>>SUB TOTAL</td>
+                <td>1854.58</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+        </table>       
+    </body>
+</html>
