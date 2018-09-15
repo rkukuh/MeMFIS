f440beb8e7cb14b7ed866e6f9dba7715bcc6c81e
Audhy <audhy.vk01@gmail.com>
2018-09-15 16:08:43 +0700

    add request form

--- app/Http/Requests/Frontend/DepartmentStore.php
+++ app/Http/Requests/Frontend/DepartmentStore.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Frontend;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class DepartmentStore extends FormRequest
 {
