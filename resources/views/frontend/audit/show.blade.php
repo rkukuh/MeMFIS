98d4f6c8bbb869bab149e01451a6d89d5fe38b95
Audhy <audhy.vk01@gmail.com>
2018-09-15 16:08:33 +0700

    repair

--- resources/views/frontend/audit/show.blade.php
+++ resources/views/frontend/audit/show.blade.php
@@ -7,7 +7,7 @@
   <div class="d-flex align-items-center">
     <div class="mr-auto">
       <h3 class="m-subheader__title m-subheader__title--separator">
-        Audit
+        Auditable
       </h3>
       <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
         <li class="m-nav__item m-nav__item--home">
@@ -25,15 +25,6 @@
             </span>
           </a>
         </li>
-        -
-        </li>
-        <li class="m-nav__item">
-          <a href="" class="m-nav__link">
-            <span class="m-nav__link-text">
-              Detail
-            </span>
-          </a>
-        </li>
       </ul>
     </div>
   </div>
@@ -43,75 +34,67 @@
   <div class="row">
     <div class="col-lg-12">
       <!--begin::Portlet-->
-
-        <!--begin::Form-->
-        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="myForm">                
-        <div class="m-portlet">
-        	<div class="m-portlet__head">
-        		<div class="m-portlet__head-caption">
-        			<div class="m-portlet__head-title">
-        				<h3 class="m-portlet__head-text">
-        					Audit Detail
-        				</h3>
-        			</div>
-        		</div>
-        	</div>
-        	<div class="m-portlet__body">
-        		<ul class="nav nav-tabs  m-tabs-line m-tabs-line--primary" role="tablist">
-            <li class="nav-item m-tabs__item">
-                                            <a class="nav-link m-tabs__link active" data-toggle="tab" href="#general" role="tab"><i class="la la-book"></i> General</a>
-                                        </li>
-                                        <li class="nav-item m-tabs__item">
-                                            <a class="nav-link m-tabs__link" data-toggle="tab" href="#address" role="tab"><i class="la la-map"></i> Address</a>
-                                        </li>
-                                        <li class="nav-item m-tabs__item">
-                                            <a class="nav-link m-tabs__link" data-toggle="tab" href="#contact" role="tab"><i class="la la-phone"></i> Contact</a>
-                                        </li>
-                                        <li class="nav-item m-tabs__item">
-                                            <a class="nav-link m-tabs__link" href="/audit" ><i class="la la-info-circle"></i> Auditable</a>
-                                        </li>
-                </ul>                        
-                <div class="tab-content">
-                <div class="tab-pane active" id="general" role="tabpanel">
-                                        @include('frontend.customer.tabs-show.general')
-                                        </div>
-                                        <div class="tab-pane" id="address" role="tabpanel">
-                                        @include('frontend.customer.tabs-show.address')                            
-                                        </div>
-                                        <div class="tab-pane" id="contact" role="tabpanel">
-                                            @include('frontend.customer.tabs-show.contact')                            
-                                        </div>
-                </div>      
-        	</div>
-          <div class="m-portlet__foot m-portlet__no-border m-portlet__foot--fit">
-            <div class="m-form__actions m-form__actions--solid">
-              <div class="row">
-                <div class="col-lg-6">
-                @component('frontend.common.buttons.back')
-                            @slot('color', 'primary')
-                            @slot('size', 'md')
-                            @slot('href', '/customer')
-                @endcomponent
-
-                <!-- <a class="btn btn-success" href="">
-                Back to Pokemon
-                </a> -->
-                  
-                </div>
-              </div>
+      <div class="m-portlet">
+        <div class="m-portlet__head">
+          <div class="m-portlet__head-caption">
+            <div class="m-portlet__head-title">
+              <span class="m-portlet__head-icon m--hide">
+                <i class="la la-gear"></i>
+              </span>
+              <h3 class="m-portlet__head-text">
+                Audit Table
+              </h3>
             </div>
           </div>
-        </form>
-        
+        </div>
+
+
+     
+            <div class="m-portlet m-portlet--mobile">
+
+            <div class="m-portlet__body">
+                <!--begin: Search Form -->
+                <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                    <div class="row align-items-center">
+                        <div class="col-xl-8 order-2 order-xl-1">
+                            <div class="form-group m-form__group row align-items-center">
+                                <div class="col-md-4">
+                                    <div class="m-input-icon m-input-icon--left">
+                                        <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
+                                        <span class="m-input-icon__icon m-input-icon__icon--left">
+                                            <span><i class="la la-search"></i></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!--end: Search Form -->
+                
+
+                <!--begin: Datatable -->
+                <div class="m_datatable" id="scrolling_both"></div>
+                <!--end: Datatable -->
+            </div>
+        </div>
+
+
         <!--end::Form-->
       </div>
       <!--end::Portlet-->
     </div>
   </div>
 </div>
-
 @endsection
 
+<!-- @section('script')
+<script src="{{ asset('js/ajax.js')}}"></script>
+@endsection -->
 @push('footer-scripts')
-
+<script> 
+  var tes = "<?php echo "Auth::user()->role" ?>";
+</script> 
+<script src="{{ asset('js/audit.js')}}"></script>
 @endpush  
