b38267ac2d411d6e9ed60279ca98dadb18d6be44
R. Kukuh <rkukuh@gmail.com>
2018-09-16 17:16:20 +0700

    Refactor

--- resources/views/frontend/supplier/tabs-create/general.blade.php
+++ resources/views/frontend/supplier/tabs-create/general.blade.php
@@ -1,106 +1,103 @@
 <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                            <label class="form-control-label">
-                                            Name *
-                                            </label>
-
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Name')
-                                            @slot('name', 'name')
-                                            @slot('type', 'text')
-                                        @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                    <label class="form-control-label">
-                                        NPWP
-                                        </label>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'NPWP')
-                                            @slot('name', 'npwp')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        NPPKP
-                                        </label>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'NPPKP')
-                                            @slot('name', 'nppkp')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        ToP
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'ToP')
-                                            @slot('name', 'top')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                            Barcode
-                                        </label>                            
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Barcode')
-                                            @slot('name', 'barcode')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Contact Person
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Contact Person')
-                                            @slot('name', 'contactperson')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Contact Person Job Position
-                                            </label>
-                                            
-                                            @component('frontend.common.input.input')
-                                            @slot('text', 'Contact Person Job Position')
-                                            @slot('name', 'contactpersonjobposition')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Active *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.checkbox')
-                                        @slot('text', 'Active')
-                                        @slot('name', 'active')
-                                    @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        AccountCode *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'AccountCode')
-                                            @slot('name', 'accountcode')
-                                            @slot('type', 'text')
-                                        @endcomponent
-                                    </div>
-                                </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Name *
+        </label>
+        @component('frontend.common.input.input')
+            @slot('text', 'Name')
+            @slot('name', 'name')
+            @slot('type', 'text')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            NPWP
+        </label>
+        @component('frontend.common.input.input')
+            @slot('text', 'NPWP')
+            @slot('name', 'npwp')
+            @slot('type', 'text')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            NPPKP
+        </label>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('text', 'NPPKP')
+            @slot('name', 'nppkp')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            ToP
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('text', 'ToP')
+            @slot('name', 'top')
+            @slot('type', 'text')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Barcode
+        </label>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('text', 'Barcode')
+            @slot('name', 'barcode')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Contact Person
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('text', 'Contact Person')
+            @slot('name', 'contactperson')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Contact Person Job Position
+        </label>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('name', 'contactpersonjobposition')
+            @slot('text', 'Contact Person Job Position')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Active *
+        </label>
+        <br>
+        @component('frontend.common.input.checkbox')
+            @slot('text', 'Active')
+            @slot('name', 'active')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            AccountCode *
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('text', 'AccountCode')
+            @slot('name', 'accountcode')
+        @endcomponent
+    </div>
+</div>
--- resources/views/frontend/supplier/tabs-show/general.blade.php
+++ resources/views/frontend/supplier/tabs-show/general.blade.php
@@ -1,125 +1,122 @@
 <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                            <label class="form-control-label">
-                                            Name *
-                                            </label>
-
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Name')
-                                            @slot('name', 'name')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                    <label class="form-control-label">
-                                        NPWP
-                                        </label>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'NPWP')
-                                            @slot('name', 'npwp')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        NPPKP
-                                        </label>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'NPPKP')
-                                            @slot('name', 'nppkp')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        ToP
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'ToP')
-                                            @slot('name', 'top')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                            Barcode
-                                        </label>                            
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Barcode')
-                                            @slot('name', 'barcode')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Contact Person
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'Contact Person')
-                                            @slot('name', 'contactperson')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Contact Person Job Position
-                                            </label>
-                                            
-                                            @component('frontend.common.input.input')
-                                            @slot('text', 'Contact Person Job Position')
-                                            @slot('name', 'contactpersonjobposition')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Active *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.checkbox')
-                                        @slot('text', 'Active')
-                                        @slot('name', 'active')
-                                        @slot('color', 'disabled')
-                                        @slot('editable', 'disabled')
-                                        @slot('value', 'checked')
-                                    @endcomponent
-                                    </div>
-                                </div>
-                                <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        AccountCode *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                            @slot('text', 'AccountCode')
-                                            @slot('name', 'accountcode')
-                                            @slot('type', 'text')
-                                            @slot('value', 'text')
-                                            @slot('editable', 'readonly')
-                                        @endcomponent
-                                    </div>
-                                </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Name *
+        </label>
+        @component('frontend.common.input.input')
+            @slot('text', 'Name')
+            @slot('name', 'name')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            NPWP
+        </label>
+        @component('frontend.common.input.input')
+            @slot('text', 'NPWP')
+            @slot('name', 'npwp')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            NPPKP
+        </label>
+        @component('frontend.common.input.input')
+            @slot('text', 'NPPKP')
+            @slot('name', 'nppkp')
+            @slot('type', 'text')
+            @slot('value', 'text')
+        @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            ToP
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('text', 'ToP')
+            @slot('name', 'top')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Barcode
+        </label>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('text', 'Barcode')
+            @slot('name', 'barcode')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Contact Person
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('text', 'Contact Person')
+            @slot('name', 'contactperson')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Contact Person Job Position
+        </label>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('editable', 'readonly')
+            @slot('text', 'Contact Person Job Position')
+            @slot('name', 'contactpersonjobposition')
+        @endcomponent
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            Active *
+        </label>
+        <br>
+        @component('frontend.common.input.checkbox')
+            @slot('text', 'Active')
+            @slot('name', 'active')
+            @slot('value', 'checked')
+            @slot('color', 'disabled')
+            @slot('editable', 'disabled')
+        @endcomponent
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            AccountCode *
+        </label>
+        <br>
+        @component('frontend.common.input.input')
+            @slot('type', 'text')
+            @slot('value', 'text')
+            @slot('text', 'AccountCode')
+            @slot('name', 'accountcode')
+            @slot('editable', 'readonly')
+        @endcomponent
+    </div>
+</div>
