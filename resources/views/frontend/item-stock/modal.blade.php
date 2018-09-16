cb329fa19ceefd22bf2837f693ab739c043812c5
R. Kukuh <rkukuh@gmail.com>
2018-09-16 16:51:47 +0700

    Refactor

--- resources/views/frontend/item-stock/modal.blade.php
+++ resources/views/frontend/item-stock/modal.blade.php
@@ -1,100 +1,88 @@
-<!-- begin::Modal-->
-<div class="modal fade" id="modal_itemstock" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="TitleModalCustomer">Item Stock</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+<div class="modal fade" id="modal_itemstock" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="TitleModalCustomer">Item Stock</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="CustomerForm">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                    <div class="m-portlet__body">
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Code *
+                                </label>
+                                @component('frontend.common.input.input')
+                                    @slot('text', 'Code')
+                                    @slot('name', 'code')
+                                    @slot('type', 'text')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Warehouse *
+                                </label>
+                                <br>
+                                @component('frontend.common.input.select')
+                                    @slot('text', 'Warehouse')
+                                    @slot('name', 'warehouse')
+                                    @slot('id', 'm_select2_1')
+                                    @slot('style', 'width:100%')
+                                @endcomponent
+
+                                @component('frontend.common.buttons.create-new')
+                                    @slot('size', 'sm')
+                                    @slot('text', 'add warehouse')
+                                    @slot('data_target', '#modal_werehouse')
+                                @endcomponent
+                            </div>
+                        </div>
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Max *
+                                </label>
+                                @component('frontend.common.input.input')
+                                    @slot('text', 'Max')
+                                    @slot('name', 'max')
+                                    @slot('type', 'number')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Min *
+                                </label>
+                                <br>
+                                @component('frontend.common.input.input')
+                                    @slot('text', 'Min')
+                                    @slot('name', 'min')
+                                    @slot('type', 'number')
+                                @endcomponent
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-
-                        <!--begin::Form -->
-                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="CustomerForm">
-                            <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
-                                <div class="m-portlet__body">
-                                    <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Code *
-                                        </label>
-
-                                    @component('frontend.common.input.input')
-                                        @slot('text', 'Code')
-                                        @slot('name', 'code')
-                                        @slot('type', 'text')
-                                    @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Warehouse *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.select')
-                                            @slot('text', 'Warehouse')
-                                            @slot('name', 'warehouse')
-                                            @slot('id', 'm_select2_1')
-                                            @slot('style', 'width:100%')
-                                        @endcomponent
-                                        @component('frontend.common.buttons.create-new')
-                                        @slot('text', 'add warehouse')
-                                        @slot('size', 'sm')
-                                        @slot('data_target', '#modal_werehouse')            
-                                        @endcomponent
-
-                                    </div>
-                                    </div>
-                                    <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Max *
-                                        </label>
-
-                                    @component('frontend.common.input.input')
-                                        @slot('text', 'Max')
-                                        @slot('name', 'max')
-                                        @slot('type', 'number')
-                                    @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Min *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                        @slot('text', 'Min')
-                                        @slot('name', 'min')
-                                        @slot('type', 'number')
-                                    @endcomponent
-
-                                    </div>
-                                    </div>
-                                    
-                                    
-                                    
-                                </div>
-
                     <div class="modal-footer">
-                          @component('frontend.common.buttons.close')
-                            @slot('color', 'secondary')
+                        @component('frontend.common.buttons.close')
                             @slot('size', 'md')
+                            @slot('color', 'secondary')
                             @slot('data_dismiss', 'modal')
-                          @endcomponent
+                        @endcomponent
 
-                          @component('frontend.common.buttons.submit')
-                            @slot('color', 'success')
+                        @component('frontend.common.buttons.submit')
                             @slot('size', 'md')
                             @slot('class', 'add')
-                        
-                          @endcomponent
+                            @slot('color', 'success')
+                        @endcomponent
+                    </div>
+                </div>
+            </form>
 
-                    </div>
-                    </div>
-                            </form>
-                </div>
-                </div>
-                </div>
-                <!--end::Modal-->
+        </div>
+    </div>
+</div>
