d66daf6a23c2988470915ee39e13f35624484757
R. Kukuh <rkukuh@gmail.com>
2018-09-16 13:09:04 +0700

    Beautify

--- resources/views/frontend/audit/index.blade.php
+++ resources/views/frontend/audit/index.blade.php
@@ -1,101 +1,97 @@
 @extends('frontend.master')
 
 @section('content')
-
-<!-- BEGIN: Subheader -->
-<div class="m-subheader ">
-  <div class="d-flex align-items-center">
-    <div class="mr-auto">
-      <h3 class="m-subheader__title m-subheader__title--separator">
-        Auditable
-      </h3>
-      <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-        <li class="m-nav__item m-nav__item--home">
-          <a href="" class="m-nav__link m-nav__link--icon">
-            <i class="m-nav__link-icon la la-home"></i>
-          </a>
-        </li>
-        <li class="m-nav__separator">
-          -
-        </li>
-        <li class="m-nav__item">
-          <a href="/audit" class="m-nav__link">
-            <span class="m-nav__link-text">
-              Audit
-            </span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-<!-- END: Subheader -->
-<div class="m-content">
-  <div class="row">
-    <div class="col-lg-12">
-      <!--begin::Portlet-->
-      <div class="m-portlet">
-        <div class="m-portlet__head">
-          <div class="m-portlet__head-caption">
-            <div class="m-portlet__head-title">
-              <span class="m-portlet__head-icon m--hide">
-                <i class="la la-gear"></i>
-              </span>
-              <h3 class="m-portlet__head-text">
-                Audit Table
-              </h3>
+    <!-- BEGIN: Subheader -->
+    <div class="m-subheader ">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Auditable
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="/audit" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Auditable
+                            </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
-          </div>
         </div>
+    </div>
+    <!-- END: Subheader -->
 
-
-     
-            <div class="m-portlet m-portlet--mobile">
-
-            <div class="m-portlet__body">
-                <!--begin: Search Form -->
-                <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                    <div class="row align-items-center">
-                        <div class="col-xl-8 order-2 order-xl-1">
-                            <div class="form-group m-form__group row align-items-center">
-                                <div class="col-md-4">
-                                    <div class="m-input-icon m-input-icon--left">
-                                        <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                        <span class="m-input-icon__icon m-input-icon__icon--left">
-                                            <span><i class="la la-search"></i></span>
-                                        </span>
-                                    </div>
-                                </div>
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--begin::Portlet-->
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+                                <h3 class="m-portlet__head-text">
+                                    Audit Datalist
+                                </h3>
                             </div>
                         </div>
-
                     </div>
-                </div>
-                <!--end: Search Form -->
-                @include('frontend.customer.modal')
-                
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <!--begin: Search Form -->
+                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                <div class="row align-items-center">
+                                    <div class="col-xl-8 order-2 order-xl-1">
+                                        <div class="form-group m-form__group row align-items-center">
+                                            <div class="col-md-4">
+                                                <div class="m-input-icon m-input-icon--left">
+                                                    <input type="text" class="form-control m-input"
+                                                        placeholder="Search..." id="generalSearch">
+                                                    <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                        <span><i class="la la-search"></i></span>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                <!--begin: Datatable -->
-                <div class="m_datatable" id="scrolling_both"></div>
-                <!--end: Datatable -->
+                                </div>
+                            </div>
+                            <!--end: Search Form -->
+                            @include('frontend.customer.modal')
+
+                            <!--begin: Datatable -->
+                            <div class="m_datatable" id="scrolling_both"></div>
+                            <!--end: Datatable -->
+                        </div>
+                    </div>
+                    <!--end::Form-->
+                </div>
+                <!--end::Portlet-->
             </div>
         </div>
-
-
-        <!--end::Form-->
-      </div>
-      <!--end::Portlet-->
     </div>
-  </div>
-</div>
 @endsection
 
-<!-- @section('script')
-<script src="{{ asset('js/ajax.js')}}"></script>
-@endsection -->
+@section('script')
+    <script src="{{ asset('js/ajax.js')}}"></script>
+@endsection
+
 @push('footer-scripts')
-<script> 
-  var tes = "<?php echo Auth::user()->id ?>";
-</script> 
-<script src="{{ asset('js/audit.js')}}"></script>
-@endpush  
+    <script>
+        var tes = "<?php echo Auth::user()->id; ?>";
+    </script>
+
+    <script src="{{ asset('js/audit.js')}}"></script>
+@endpush
