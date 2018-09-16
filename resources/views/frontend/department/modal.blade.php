4172869e768fae0c5f89ffe49f3401d751e15879
R. Kukuh <rkukuh@gmail.com>
2018-09-16 15:35:59 +0700

    Refactor

--- resources/views/frontend/department/modal.blade.php
+++ resources/views/frontend/department/modal.blade.php
@@ -1,68 +1,59 @@
-<!--begin::Modal-->
-<div class="modal fade" id="modal_department" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="TitleModalDepartment">Bank</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+<div class="modal fade" id="modal_department" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="TitleModalDepartment">Bank</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="DepartmentForm">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                    <div class="m-portlet__body">
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Department *
+                                </label>
+                                @component('frontend.common.input.input')
+                                    @slot('name', 'name')
+                                    @slot('type', 'text')
+                                    @slot('text', 'Department Name')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Parent *
+                                </label>
+                                <br>
+                                @component('frontend.common.input.select')
+                                    @slot('text', 'Parent')
+                                    @slot('parent_id', 'name')
+                                    @slot('id', 'm_select2_1')
+                                    @slot('style', 'width:100%')
+                                @endcomponent
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-
-                        <!--begin::Form-->
-                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="DepartmentForm">
-                            <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
-                                <div class="m-portlet__body">
-                                    <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Department *
-                                        </label>
-
-                                    @component('frontend.common.input.input')
-                                        @slot('text', 'Department Name')
-                                        @slot('name', 'name')
-                                        @slot('type', 'text')
-                                    @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Parent *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.select')
-                                        @slot('text', 'Parent')
-                                        @slot('parent_id', 'name')
-                                        @slot('id', 'm_select2_1')
-                                        @slot('style', 'width:100%')
-                                    @endcomponent
-
-                                    </div>
-                                    </div>
-                                    
-                                    
-                                </div>
-
                     <div class="modal-footer">
-                          @component('frontend.common.buttons.close')
+                        @component('frontend.common.buttons.close')
+                            @slot('size', 'md')
                             @slot('color', 'secondary')
-                            @slot('size', 'md')
                             @slot('data_dismiss', 'modal')
-                          @endcomponent
+                        @endcomponent
 
-                          @component('frontend.common.buttons.submit')
-                            @slot('color', 'success')
+                        @component('frontend.common.buttons.submit')
                             @slot('size', 'md')
+                            @slot('color', 'success')
                             @slot('class', 'add')
-                        
-                          @endcomponent
+                        @endcomponent
+                    </div>
+                </div>
+            </form>
 
-                    </div>
-                    </div>
-                            </form>
-                </div>
-                </div>
-                </div>
-                <!--end::Modal-->
+        </div>
+    </div>
+</div>
