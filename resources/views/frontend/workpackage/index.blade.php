a4281076e604e01d4cd35ccef0f6e06d38f81190
R. Kukuh <rkukuh@gmail.com>
2018-09-16 17:33:38 +0700

    Refactor

--- resources/views/frontend/workpackage/index.blade.php
+++ resources/views/frontend/workpackage/index.blade.php
@@ -1,114 +1,108 @@
 @extends('frontend.master')
 
 @section('content')
-
-<!-- BEGIN: Subheader -->
-<div class="m-subheader ">
-  <div class="d-flex align-items-center">
-    <div class="mr-auto">
-      <h3 class="m-subheader__title m-subheader__title--separator">
-        Workpackage
-      </h3>
-      <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-        <li class="m-nav__item m-nav__item--home">
-          <a href="" class="m-nav__link m-nav__link--icon">
-            <i class="m-nav__link-icon la la-home"></i>
-          </a>
-        </li>
-        <li class="m-nav__separator">
-          -
-        </li>
-        <li class="m-nav__item">
-          <a href="/workpackage" class="m-nav__link">
-            <span class="m-nav__link-text">
-                Workpackage
-            </span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-<!-- END: Subheader -->
-<div class="m-content">
-  <div class="row">
-    <div class="col-lg-12">
-      <!--begin::Portlet-->
-      <div class="m-portlet">
-        <div class="m-portlet__head">
-          <div class="m-portlet__head-caption">
-            <div class="m-portlet__head-title">
-              <span class="m-portlet__head-icon m--hide">
-                <i class="la la-gear"></i>
-              </span>
-              <h3 class="m-portlet__head-text">
-                Form Workpackage
-              </h3>
+    <!-- BEGIN: Subheader -->
+    <div class="m-subheader ">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Workpackage
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="/workpackage" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Workpackage
+                            </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
-          </div>
         </div>
+    </div>
+    <!-- END: Subheader -->
 
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--begin::Portlet-->
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+                                <h3 class="m-portlet__head-text">
+                                    Form Workpackage
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <!--begin: Search Form -->
+                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                <div class="row align-items-center">
+                                    <div class="col-xl-8 order-2 order-xl-1">
+                                        <div class="form-group m-form__group row align-items-center">
+                                            <div class="col-md-4">
+                                                <div class="m-input-icon m-input-icon--left">
+                                                    <input type="text" class="form-control m-input" placeholder="Search..."
+                                                        id="generalSearch">
+                                                    <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                        <span><i class="la la-search"></i></span>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 order-1 order-xl-2 m--align-right">
+                                        @component('frontend.common.buttons.create-new')
+                                            @slot('size', 'md')
+                                            @slot('color', 'primary')
+                                            @slot('text', 'Add Task Card')
+                                            @slot('data_target', '#modal_workpackage')
+                                        @endcomponent
 
-     
-            <div class="m-portlet m-portlet--mobile">
-
-            <div class="m-portlet__body">
-                <!--begin: Search Form -->
-                <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                    <div class="row align-items-center">
-                        <div class="col-xl-8 order-2 order-xl-1">
-                            <div class="form-group m-form__group row align-items-center">
-                                <div class="col-md-4">
-                                    <div class="m-input-icon m-input-icon--left">
-                                        <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                        <span class="m-input-icon__icon m-input-icon__icon--left">
-                                            <span><i class="la la-search"></i></span>
-                                        </span>
+                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-                        @component('frontend.common.buttons.create-new')
-                            @slot('text', 'Add Task Card')
-                            @slot('color', 'primary')
-                            @slot('size', 'md')
-                            @slot('data_target', '#modal_workpackage')
-                        @endcomponent
+                            <!--end: Search Form -->
 
-                            <div class="m-separator m-separator--dashed d-xl-none"></div>					
+                            @include('frontend.tp.modal')
+                            @include('frontend.workpackage.modal')
+
+                            <!--begin: Datatable -->
+                            <div class="m_datatable" id="scrolling_both"></div>
+                            <!--end: Datatable -->
                         </div>
                     </div>
+                    <!--end::Form-->
                 </div>
-                <!--end: Search Form -->
-                @include('frontend.workpackage.modal')
-                @include('frontend.tp.modal')
-
-
-                <!--begin: Datatable -->
-                <div class="m_datatable" id="scrolling_both"></div>
-                <!--end: Datatable -->
+                <!--end::Portlet-->
             </div>
         </div>
-
-
-        <!--end::Form-->
-      </div>
-      <!--end::Portlet-->
     </div>
-  </div>
-</div>
-
 @endsection
 
-<!-- @section('script')
-<script src="{{ asset('js/ajax.js')}}"></script>
-@endsection -->
+@section('script')
+    {{-- <script src="{{ asset('js/ajax.js')}}"></script> --}}
+@endsection
+
 @push('footer-scripts')
-<script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}" type="text/javascript"></script>
-<!-- <script src="{{ asset('assets/metronic//demo/default/custom/crud/forms/widgets/typeahead.js')}}" type="text/javascript"></script> -->
-<script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/select2.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
+    {{-- <script src="{{ asset('assets/metronic//demo/default/custom/crud/forms/widgets/typeahead.js')}}"></script> --}}
+    <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/select2.js')}}"></script>
 
-<script src="{{ asset('js/workpackage.js')}}"></script>
-
-@endpush  
+    <script src="{{ asset('js/workpackage.js')}}"></script>
+@endpush
--- resources/views/frontend/workpackage/modal.blade.php
+++ resources/views/frontend/workpackage/modal.blade.php
@@ -1,98 +1,87 @@
-<!-- begin::Modal-->
-<div class="modal fade" id="modal_workpackage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="TitleModalWorkpackage">Workpackage</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+<div class="modal fade" id="modal_workpackage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="TitleModalWorkpackage">Workpackage</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="kWorkpackage">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                    <div class="m-portlet__body">
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Code *
+                                </label>
+                                @component('frontend.common.input.input')
+                                    @slot('text', 'Code')
+                                    @slot('name', 'code')
+                                    @slot('type', 'text')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Name *
+                                </label>
+                                <br>
+                                @component('frontend.common.input.input')
+                                    @slot('text', 'Name')
+                                    @slot('name', 'name')
+                                    @slot('type', 'text')
+                                @endcomponent
+                            </div>
+                        </div>
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    TaskcardPackage *
+                                </label>
+                                @component('frontend.common.input.select')
+                                    @slot('text', 'Tp')
+                                    @slot('name', 'tp')
+                                    @slot('id', 'm_select2_1')
+                                    @slot('style', 'width:100%')
+                                @endcomponent
+
+                                @component('frontend.common.buttons.create-new')
+                                    @slot('size', 'sm')
+                                    @slot('text', 'add TP')
+                                    @slot('data_target', '#modal_tp')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Upload TaskCard *
+                                </label>
+                                <br>
+                                @component('frontend.common.input.upload')
+                                    @slot('text', 'file')
+                                    @slot('name', 'file')
+                                @endcomponent
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-
-                        <!--begin::Form -->
-                        <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="kWorkpackage">
-                            <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
-                                <div class="m-portlet__body">
-                                    <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Code *
-                                        </label>
-
-                                    @component('frontend.common.input.input')
-                                        @slot('text', 'Code')
-                                        @slot('name', 'code')
-                                        @slot('type', 'text')
-                                    @endcomponent
-
-                                    </div>
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Name *
-                                        </label>
-                                        <br>
-                                        @component('frontend.common.input.input')
-                                        @slot('text', 'Name')
-                                        @slot('name', 'name')
-                                        @slot('type', 'text')
-                                    @endcomponent
-
-                                    </div>
-                                    </div>
-                                    <div class="form-group m-form__group row ">
-                                    <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        TaskcardPackage *
-                                        </label>
-                                        @component('frontend.common.input.select')
-                                            @slot('text', 'Tp')
-                                            @slot('name', 'tp')
-                                            @slot('id', 'm_select2_1')
-                                            @slot('style', 'width:100%')
-                                        @endcomponent
-                                        @component('frontend.common.buttons.create-new')
-                                        @slot('text', 'add TP')
-                                        @slot('size', 'sm')
-                                        @slot('data_target', '#modal_tp')            
-                                        @endcomponent
-
-
-                                    </div>
-                                    <!-- <div class="col-sm-6 col-md-6 col-lg-6">
-                                        <label class="form-control-label">
-                                        Upload TaskCard *
-                                        </label>
-                                        <br>
-                                    @component('frontend.common.input.upload')
-                                        @slot('text', 'file')
-                                        @slot('name', 'file')
-                                    @endcomponent
-
-                                    </div> -->
-                                    </div>
-                                    
-                                    
-                                </div>
-
                     <div class="modal-footer">
-                          @component('frontend.common.buttons.close')
-                            @slot('color', 'secondary')
+                        @component('frontend.common.buttons.close')
                             @slot('size', 'md')
+                            @slot('color', 'secondary')
                             @slot('data_dismiss', 'modal')
-                          @endcomponent
+                        @endcomponent
 
-                          @component('frontend.common.buttons.submit')
-                            @slot('color', 'success')
+                        @component('frontend.common.buttons.submit')
                             @slot('size', 'md')
                             @slot('class', 'add')
-                        
-                          @endcomponent
+                            @slot('color', 'success')
+                        @endcomponent
+                    </div>
+                </div>
+            </form>
 
-                    </div>
-                    </div>
-                            </form>
-                </div>
-                </div>
-                </div>
-                <!--end::Modal-->
+        </div>
+    </div>
+</div>
