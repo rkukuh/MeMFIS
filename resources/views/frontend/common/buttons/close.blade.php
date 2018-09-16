dc61dd06fd9c572bf10964f9545a59bb776ba9a5
R. Kukuh <rkukuh@gmail.com>
2018-09-16 14:27:00 +0700

    Refactor

--- resources/views/frontend/common/buttons/close.blade.php
+++ resources/views/frontend/common/buttons/close.blade.php
@@ -1,15 +1,17 @@
-<button type="{{ $type or 'button' }}"
-        name="{{ $name or 'create' }}"
-        value="{{ $value or '' }}"
-        class="btn
-               btn-{{ $color or 'default' }}
-               btn-{{ $size or 'md' }}
-               {{ $class or '' }} "
-        style="{{ $style or '' }}"
-        data-dismiss="{{ $data_dismiss = 'modal' }}">
+<button
+    type="{{ $type or 'button' }}"
+    id="{{ $id or '' }}"
+    name="{{ $name or 'create' }}"
+    value="{{ $value or '' }}"
+    class="btn
+           btn-{{ $color or 'default' }}
+           btn-{{ $size or 'md' }}
+           {{ $class or '' }}"
+    style="{{ $style or '' }}"
+    data-dismiss="{{ $data_dismiss or 'modal' }}">
 
-        <span>
-            <i class="la la-{{ $icon or 'close' }}"></i>
-            <span>{{ $text or 'Close' }}</span>
-        </span>
+    <span>
+        <i class="la la-{{ $icon or 'close' }}"></i>
+        <span>{{ $text or 'Close' }}</span>
+    </span>
 </button>
