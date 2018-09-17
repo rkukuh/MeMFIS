48843f3c9736ff296f8ee617fe7ab983b14a120c
Audhy <audhy.vk01@gmail.com>
2018-09-17 11:30:15 +0700

    refactor

--- resources/views/frontend/supplier/tabs-create/general.blade.php
+++ resources/views/frontend/supplier/tabs-create/general.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group m-form__group row ">
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
-            Name
-        @component('frontend.common.label.optional')
+            Code
+        @component('frontend.common.label.required')
         @endcomponent
         </label>
         @component('frontend.common.input.input')
@@ -12,19 +12,31 @@
         @endcomponent
     </div>
     <div class="col-sm-6 col-md-6 col-lg-6">
-        <label class="form-control-label">
-            NPWP
-        @component('frontend.common.label.optional')
-        @endcomponent
-        </label>
-        @component('frontend.common.input.input')
-            @slot('text', 'NPWP')
-            @slot('name', 'npwp')
-            @slot('type', 'text')
-        @endcomponent
+            <label class="form-control-label">
+                Name
+            @component('frontend.common.label.optional')
+            @endcomponent
+            </label>
+            @component('frontend.common.input.input')
+                @slot('text', 'Name')
+                @slot('name', 'name')
+                @slot('type', 'text')
+            @endcomponent
+        </div>
     </div>
-</div>
 <div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    NPWP
+                @component('frontend.common.label.optional')
+                @endcomponent
+                </label>
+                @component('frontend.common.input.input')
+                    @slot('text', 'NPWP')
+                    @slot('name', 'npwp')
+                    @slot('type', 'text')
+                @endcomponent
+    </div>
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
             NPPKP
@@ -37,21 +49,21 @@
             @slot('name', 'nppkp')
         @endcomponent
     </div>
-    <div class="col-sm-6 col-md-6 col-lg-6">
-        <label class="form-control-label">
-            ToP
-        @component('frontend.common.label.required')
-        @endcomponent
-        </label>
-        <br>
-        @component('frontend.common.input.input')
-            @slot('text', 'ToP')
-            @slot('name', 'top')
-            @slot('type', 'text')
-        @endcomponent
-    </div>
 </div>
 <div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    ToP
+                @component('frontend.common.label.required')
+                @endcomponent
+                </label>
+                <br>
+                @component('frontend.common.input.input')
+                    @slot('text', 'ToP')
+                    @slot('name', 'top')
+                    @slot('type', 'text')
+                @endcomponent
+    </div>        
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
             Barcode
@@ -64,21 +76,21 @@
             @slot('name', 'barcode')
         @endcomponent
     </div>
-    <div class="col-sm-6 col-md-6 col-lg-6">
-        <label class="form-control-label">
-            Contact Person
-        @component('frontend.common.label.optional')
-        @endcomponent
-        </label>
-        <br>
-        @component('frontend.common.input.input')
-            @slot('type', 'text')
-            @slot('text', 'Contact Person')
-            @slot('name', 'contactperson')
-        @endcomponent
-    </div>
 </div>
 <div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    Contact Person
+                @component('frontend.common.label.optional')
+                @endcomponent
+                </label>
+                <br>
+                @component('frontend.common.input.input')
+                    @slot('type', 'text')
+                    @slot('text', 'Contact Person')
+                    @slot('name', 'contactperson')
+                @endcomponent
+    </div>        
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
             Contact Person Job Position
@@ -91,20 +103,20 @@
             @slot('text', 'Contact Person Job Position')
         @endcomponent
     </div>
-    <div class="col-sm-6 col-md-6 col-lg-6">
-        <label class="form-control-label">
-            Active
-        @component('frontend.common.label.optional')
-        @endcomponent
-        </label>
-        <br>
-        @component('frontend.common.input.checkbox')
-            @slot('text', 'Active')
-            @slot('name', 'active')
-        @endcomponent
-    </div>
 </div>
 <div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    Active
+                @component('frontend.common.label.optional')
+                @endcomponent
+                </label>
+                <br>
+                @component('frontend.common.input.checkbox')
+                    @slot('text', 'Active')
+                    @slot('name', 'active')
+                @endcomponent
+    </div>        
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
             AccountCode
