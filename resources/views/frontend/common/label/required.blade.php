f301a8fb3aed496fca5ebee670f0637f91664b7c
Audhy <audhy.vk01@gmail.com>
2018-09-17 10:21:40 +0700

    component label

---
+++ resources/views/frontend/common/label/required.blade.php
@@ -0,0 +1,7 @@
+<{{ $html_tag or 'span' }} class="{{$class or ''}}"
+                       style="font-weight: {{ $font_weight or 'bold' }};
+                              color:{{ $color or 'red' }};">
+
+    {{ $text or '*' }}
+
+</{{ $html_tag or 'span' }}>
