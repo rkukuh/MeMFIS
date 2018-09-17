6760ad6f55e320dbd480124cf03dd898d552ba7f
Audhy <audhy.vk01@gmail.com>
2018-09-17 09:13:05 +0700

    Change "Form XXX" to "XXX Datalist" in all index views

--- resources/views/frontend/workpackage/index.blade.php
+++ resources/views/frontend/workpackage/index.blade.php
@@ -42,7 +42,7 @@
                                     <i class="la la-gear"></i>
                                 </span>
                                 <h3 class="m-portlet__head-text">
-                                    Form Workpackage
+                                    Workpackage Datalist
                                 </h3>
                             </div>
                         </div>
@@ -79,8 +79,8 @@
                             </div>
                             <!--end: Search Form -->
 
-                            @include('frontend.tp.modal')
                             @include('frontend.workpackage.modal')
+                            @include('frontend.taskcardpackage.modal')
 
                             <!--begin: Datatable -->
                             <div class="m_datatable" id="scrolling_both"></div>
