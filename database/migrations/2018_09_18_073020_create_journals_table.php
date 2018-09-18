40e34da0009ecedbc5984ed8adeebff0e49e950b
Audhy <audhy.vk01@gmail.com>
2018-09-18 07:37:11 +0700

    migration journals

---
+++ database/migrations/2018_09_18_073020_create_journals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJournalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('code',50);
+            $table->string('name')->nullable();
+            $table->integer('type')->nullable();
+            $table->integer('level')->nullable();
+            $table->string('deccription')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journals');
+    }
+}
