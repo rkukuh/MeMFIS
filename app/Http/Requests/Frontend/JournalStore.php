169ede2af4fb9357bf8744227605183c633ff722
Audhy <audhy.vk01@gmail.com>
2018-09-18 09:53:40 +0700

    request form journal

--- app/Http/Requests/Frontend/JournalStore.php
+++ app/Http/Requests/Frontend/JournalStore.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Frontend;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class JournalStore extends FormRequest
 {
@@ -24,6 +26,7 @@ class JournalStore extends FormRequest
     public function rules()
     {
         return [
+            'code' => 'required|numeric',
             // 'name' => 'required|min:3|max:50|regex:/^[\pL\s\-]+$/u',
         ];
     }
