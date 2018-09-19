6b3e534131afa79f7d28bf579cb6f2ff4e3523a4
Audhy <audhy.vk01@gmail.com>
2018-09-19 08:10:09 +0700

    refactor

--- resources/views/frontend/taskcardpackage/modal.blade.php
+++ resources/views/frontend/taskcardpackage/modal.blade.php
@@ -18,10 +18,9 @@
                                 @component('frontend.common.label.required')
                                 @endcomponent
                                 </label>
-                                @component('frontend.common.input.input')
+                                @component('frontend.common.input.text')
                                     @slot('text', 'Code')
                                     @slot('name', 'code')
-                                    @slot('type', 'text')
                                 @endcomponent
                             </div>
                             <div class="col-sm-6 col-md-6 col-lg-6">
@@ -31,10 +30,9 @@
                                 @endcomponent
                                 </label>
                                 <br>
-                                @component('frontend.common.input.input')
+                                @component('frontend.common.input.text')
                                     @slot('text', 'Name')
                                     @slot('name', 'name')
-                                    @slot('type', 'text')
                                 @endcomponent
                             </div>
                         </div>
