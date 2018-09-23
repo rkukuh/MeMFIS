39379b14bb53f1a89d0a6451840677230f2c8104
R. Kukuh <rkukuh@gmail.com>
2018-09-23 15:34:52 +0700

    Implements renamed rotue

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -124,7 +124,7 @@
                 </a>
             </li>
             <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
-                <a href="{{ route('frontend.taskcardpackage.index') }}" class="m-menu__link m-menu__toggle">
+                <a href="{{ route('frontend.taskcard-package.index') }}" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
                         Task Card Package
