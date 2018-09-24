6cbddbfe9f31ab83f5e8c8598696789ec8e31b90
Audhy <audhy.vk01@gmail.com>
2018-09-24 15:09:50 +0700

    add migration table

---
+++ database/migrations/2018_09_24_141009_create_items_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('code',50);
+            $table->string('name');
+            $table->string('barcode');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->text('description');
+            $table->integer('active');
+            $table->integer('isppn');
+            $table->integer('istock');
+            $table->string('xpicture');
+            $table->unsignedInteger('account_code')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')
+            ->references('id')->on('categories')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+            $table->foreign('account_code')
+            ->references('id')->on('journals')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');          
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
