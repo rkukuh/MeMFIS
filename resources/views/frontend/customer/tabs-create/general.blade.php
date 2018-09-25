6a70049596c0c535f61d9f4e5a272c2f7205f748
Audhy <audhy.vk01@gmail.com>
2018-09-25 15:52:07 +0700

    make display: none

--- resources/views/frontend/customer/tabs-create/general.blade.php
+++ resources/views/frontend/customer/tabs-create/general.blade.php
@@ -117,7 +117,7 @@
             @slot('style', 'width:100%')
         @endcomponent
     </div>
-    {{-- <div class="col-sm-6 col-md-6 col-lg-6">
+    <div class="col-sm-6 col-md-6 col-lg-6" style="display: none">
         <label class="form-control-label">
             Leveling @include('frontend.common.label.optional')
         </label>
@@ -126,9 +126,9 @@
             @slot('text', 'Leveling')
             @slot('name', 'leveling')
         @endcomponent
-    </div> --}}
+    </div>
 </div>
-{{-- <div class="form-group m-form__group row ">
+<div class="form-group m-form__group row" style="display: none">
     <div class="col-sm-6 col-md-6 col-lg-6">
         <label class="form-control-label">
             xType @include('frontend.common.label.optional')
@@ -172,4 +172,4 @@
             xType
         </span>
     </div>
-</div> --}}
+</div>
