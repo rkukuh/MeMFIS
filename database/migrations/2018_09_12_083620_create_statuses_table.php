64bf619c12b378bc6edb70d7b2f1cb5d6035cb8d
R. Kukuh <rkukuh@gmail.com>
2018-09-28 21:25:03 +0700

    Create migration to hold Status master data

---
+++ database/migrations/2018_09_12_083620_create_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('name');
+            $table->string('of');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
