050f52d532ed8bea8100b846120423a07469a25e
R. Kukuh <rkukuh@gmail.com>
2018-09-29 15:33:09 +0700

    Refactor: Department and Job Title migrations

--- database/migrations/2018_09_12_141129_create_job_titles_table.php
+++ database/migrations/2018_09_12_141129_create_job_titles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePositionsTable extends Migration
+class CreateJobTitlesTable extends Migration
 {
     /**
      * Run the migrations.
