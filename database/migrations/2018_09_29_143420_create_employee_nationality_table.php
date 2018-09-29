6a5266b663ebd740ab3431ecd6dfb1403e8a42e8
R. Kukuh <rkukuh@gmail.com>
2018-09-29 14:37:19 +0700

    Create M-M migration for Employee and Nationality

---
+++ database/migrations/2018_09_29_143420_create_employee_nationality_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeNationalityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_nationality', function (Blueprint $table) {
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('nationality_id');
+            $table->timestamps();
+
+            $table->foreign('employee_id')
+                    ->references('id')->on('employees')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('nationality_id')
+                    ->references('id')->on('nationalities')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_nationality');
+    }
+}
