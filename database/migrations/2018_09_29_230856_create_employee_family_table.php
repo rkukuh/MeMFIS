71f1b45fef82ff65b6236363b566fcba64e86fed
R. Kukuh <rkukuh@gmail.com>
2018-09-29 23:10:58 +0700

    Create M-M migration for Employee and their Family, if any

---
+++ database/migrations/2018_09_29_230856_create_employee_family_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeFamilyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_family', function (Blueprint $table) {
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('family_id');
+            $table->unsignedInteger('relationship_type');
+            $table->timestamps();
+
+            $table->foreign('employee_id')
+                    ->references('id')->on('employees')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('family_id')
+                    ->references('id')->on('families')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('relationship_type')
+                  ->references('id')->on('types')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_family');
+    }
+}
