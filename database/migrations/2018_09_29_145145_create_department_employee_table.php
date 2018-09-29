1aaf11fa9bf275a847db3e2cc8995694c17018d9
R. Kukuh <rkukuh@gmail.com>
2018-09-29 14:54:31 +0700

    Create M-M migration for Department and Employee

---
+++ database/migrations/2018_09_29_145145_create_department_employee_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_employee', function (Blueprint $table) {
+            $table->unsignedInteger('department_id');
+            $table->unsignedInteger('employee_id');
+            $table->timestamp('joined_at')->nullable();
+            $table->timestamp('left_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('department_id')
+                    ->references('id')->on('departments')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('employee_id')
+                    ->references('id')->on('employees')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_employee');
+    }
+}
