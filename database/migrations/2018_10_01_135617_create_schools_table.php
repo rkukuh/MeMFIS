f5fea0c1b7e3946a3b6bb07e21d70612b9e53a19
R. Kukuh <rkukuh@gmail.com>
2018-10-01 13:59:42 +0700

    Create migration for School

---
+++ database/migrations/2018_10_01_135617_create_schools_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('name');
+            $table->unsignedInteger('degree')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('degree')
+                    ->references('id')->on('types')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+}
