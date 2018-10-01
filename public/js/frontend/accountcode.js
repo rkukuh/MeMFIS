494ae79f528227b0bbbf25e0534b0cea78eda716
R. Kukuh <rkukuh@gmail.com>
2018-10-01 16:57:31 +0700

    Use single-quote instead

--- public/js/frontend/accountcode.js
+++ public/js/frontend/accountcode.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     bank = function () {
-        var select = document.getElementById("m_select2_3a");
+        var select = document.getElementById('m_select2_3a');
 
         $.ajax({
-            url: "/accountcode/",
-            type: "GET",
-            dataType: "json",
+            url: '/accountcode/',
+            type: 'GET',
+            dataType: 'json',
             success: function (data) {
                 var angka = 1;
 
@@ -14,14 +14,14 @@ $(document).ready(function () {
                 $.each(data, function (key, value) {
                     if (angka == 1) {
                         $('select[name="accountcode"]').append(
-                            "<option> Select a Accountcode</option>"
+                            '<option> Select a Accountcode</option>'
                         );
 
                         angka = 0;
                     }
 
                     $('select[name="accountcode"]').append(
-                        '<option value="' + key + '">' + value + "</option>"
+                        '<option value="' + key + '">' + value + '</option>'
                     );
                 });
             }
