76c47063b8514fcebf80ddaa69d0f6cf3b55d6a0
R. Kukuh <rkukuh@gmail.com>
2018-10-01 14:09:56 +0700

    Create M-M migration for Employee and School

---
+++ database/migrations/2018_10_01_140759_create_employee_school_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeSchoolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_school', function (Blueprint $table) {
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('school_id');
+            $table->timestamp('start_at');
+            $table->timestamp('graduated_at');
+            $table->timestamps();
+
+            $table->foreign('employee_id')
+                    ->references('id')->on('employees')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('school_id')
+                    ->references('id')->on('schools')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_school');
+    }
+}
