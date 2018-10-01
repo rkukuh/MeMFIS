6e7decb2bad0e4b890343b6f906311923575f1fa
R. Kukuh <rkukuh@gmail.com>
2018-10-01 16:31:42 +0700

    Refactor

--- public/js/frontend/address.js
+++ public/js/frontend/address.js
@@ -1,37 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var select = document.getElementById("m_select2_1");
 
     $.ajax({
         url: "/addres/country/",
         type: "GET",
         dataType: "json",
-        success: function(data) {
+        success: function (data) {
             var angka = 1;
+
             $('select[name="country"]').empty();
-            $.each(data, function(key, value) {
+
+            $.each(data, function (key, value) {
                 select.options[select.options.length] = new Option(value, key);
             });
         }
     });
 });
-$(document).ready(function() {
-    $('select[name="country"]').on("change", function() {
+
+$(document).ready(function () {
+    $('select[name="country"]').on("change", function () {
         var stateID = $(this).val();
+
         if (stateID) {
             $.ajax({
                 url: "/addres/city/" + stateID,
                 type: "GET",
                 dataType: "json",
-                success: function(data) {
+                success: function (data) {
                     var angka = 1;
+
                     $('select[name="city"]').empty();
-                    $.each(data, function(key, value) {
+
+                    $.each(data, function (key, value) {
                         if (angka == 1) {
                             $('select[name="city"]').append(
                                 "<option> Select a City</option>"
                             );
+
                             angka = 0;
                         }
+
                         $('select[name="city"]').append(
                             '<option value="' + key + '">' + value + "</option>"
                         );
