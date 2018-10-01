73623a98a1fded2970fa88d4246a619b343b45e9
R. Kukuh <rkukuh@gmail.com>
2018-10-01 16:29:43 +0700

    Refactor

--- public/js/frontend/accountcode.js
+++ public/js/frontend/accountcode.js
@@ -1,21 +1,25 @@
-$(document).ready(function() {
-    bank = function() {
+$(document).ready(function () {
+    bank = function () {
         var select = document.getElementById("m_select2_3a");
 
         $.ajax({
             url: "/accountcode/",
             type: "GET",
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 var angka = 1;
+
                 $('select[name="accountcode"]').empty();
-                $.each(data, function(key, value) {
+
+                $.each(data, function (key, value) {
                     if (angka == 1) {
                         $('select[name="accountcode"]').append(
                             "<option> Select a Accountcode</option>"
                         );
+
                         angka = 0;
                     }
+
                     $('select[name="accountcode"]').append(
                         '<option value="' + key + '">' + value + "</option>"
                     );
@@ -23,5 +27,6 @@ $(document).ready(function() {
             }
         });
     };
+
     bank();
 });
