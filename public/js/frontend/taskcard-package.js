1cc90acaef9ca21dbc800877018dd3ea891946f8
Audhy <audhy.vk01@gmail.com>
2018-10-02 09:04:11 +0700

    fixxing

--- public/js/frontend/taskcard-package.js
+++ public/js/frontend/taskcard-package.js
@@ -78,7 +78,7 @@ var TaskcardPackage = {
                     overflow: 'visible',
                     template: function (t, e, i) {
                         return (
-                            '<button data-toggle="modal" data-target="#modal_customer" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill show" title="Details" data-id="' +
+                            '<button data-toggle="modal" data-target="#modal_customer" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill show" title="Details" data-id=' +
                             t.id +
                             '>\t\t\t\t\t\t\t<i class="la la-search"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t'
                         );
