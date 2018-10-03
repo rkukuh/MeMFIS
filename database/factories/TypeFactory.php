cf166d6f069790bd60b0f2648c7d85ab840ef875
R. Kukuh <rkukuh@gmail.com>
2018-10-03 20:57:32 +0700

    Add more factory states

--- database/factories/TypeFactory.php
+++ database/factories/TypeFactory.php
@@ -8,14 +8,24 @@ $factory->define(Type::class, function (Faker $faker) {
     return [
         'name' => 'Type #' . $faker->unixTime(),
         'of'   => $faker->randomElement([
+            'arc',
+            'fax',
             'phone',
             'email',
-            'fax'
+            'journal',
+            'regulator',
+            'capability',
+            'eligibility',
         ])
     ];
 
 });
 
+$factory->state(Type::class, 'arc', ['of' => 'arc']);
+$factory->state(Type::class, 'fax', ['of' => 'fax']);
 $factory->state(Type::class, 'phone', ['of' => 'phone']);
 $factory->state(Type::class, 'email', ['of' => 'email']);
-$factory->state(Type::class, 'fax', ['of' => 'fax']);
+$factory->state(Type::class, 'journal', ['of' => 'journal']);
+$factory->state(Type::class, 'regulator', ['of' => 'regulator']);
+$factory->state(Type::class, 'capability', ['of' => 'capability']);
+$factory->state(Type::class, 'eligibility', ['of' => 'eligibility']);
