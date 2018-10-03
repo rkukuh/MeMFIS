f143a84fd06d85817cc1fc49366ca09371b801ee
R. Kukuh <rkukuh@gmail.com>
2018-10-03 23:51:28 +0700

    Use shortcode instead

--- routes/component.php
+++ routes/component.php
@@ -2,12 +2,12 @@
 
 Route::name('component.')->group(function () {
 
-    Route::get('update-button', function(){
-        return View::make('frontend.common.buttons.update')->render();
+    Route::get('/update-button', function(){
+        return view('frontend.common.buttons.update');
     });
 
-    Route::get('submit-button', function(){
-        return View::make('frontend.common.buttons.submit')->render();
+    Route::get('/submit-button', function(){
+        return view('frontend.common.buttons.submit');
     });
 
     Route::get('get-category', 'FillComboxController@category')->name('get-category');
