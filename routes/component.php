a7e4f8cf05797a15b608d54cef5336367c1a4973
R. Kukuh <rkukuh@gmail.com>
2018-10-03 23:55:00 +0700

    Use shortcut instead

--- routes/component.php
+++ routes/component.php
@@ -2,13 +2,8 @@
 
 Route::name('component.')->group(function () {
 
-    Route::get('/update-button', function(){
-        return view('frontend.common.buttons.update');
-    })->name('button-update');
-
-    Route::get('/submit-button', function(){
-        return view('frontend.common.buttons.submit');
-    })->name('button-submit');
+    Route::view('/update-button', 'frontend.common.buttons.update')->name('button-update');
+    Route::view('/submit-button', 'frontend.common.buttons.submit')->name('button-submit');
 
     Route::get('get-category', 'FillComboxController@category')->name('get-category');
     Route::get('get-accountcode', 'FillComboxController@accountcode')->name('get-accountcode');
