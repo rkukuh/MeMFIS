c33f38358482bae39625398d382661fea9800585
R. Kukuh <rkukuh@gmail.com>
2018-10-03 23:47:14 +0700

    Wrap componnet's route into a named group

--- routes/component.php
+++ routes/component.php
@@ -1,16 +1,16 @@
 <?php
 
-Route::get('update-button', function(){
-    return View::make('frontend.common.buttons.update')
-    ->render();
+Route::name('component.')->group(function () {
+
+    Route::get('update-button', function(){
+        return View::make('frontend.common.buttons.update')->render();
+    });
+
+    Route::get('submit-button', function(){
+        return View::make('frontend.common.buttons.submit')->render();
+    });
+
+    Route::get('get-category', 'FillComboxController@category')->name('get-category');
+    Route::get('get-accountcode', 'FillComboxController@accountcode')->name('get-accountcode');
 
 });
-
-Route::get('submit-button', function(){
-    return View::make('frontend.common.buttons.submit')
-    ->render();
-
-});
-
-Route::get('get-accountcode', 'FillComboxController@accountcode')->name('get-accountcode');
-Route::get('get-category', 'FillComboxController@category')->name('get-category');
