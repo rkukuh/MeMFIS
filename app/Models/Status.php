30ed7e0148cd2bb746208711dc56d61d9dddb3e0
R. Kukuh <rkukuh@gmail.com>
2018-10-04 14:00:23 +0700

    Define local scopes

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -12,4 +12,39 @@ class Status extends MemfisModel
         'of',
         'description',
     ];
+
+    /******************************************* SCOPE *******************************************/
+
+    /**
+     * Scope a query to only include category of customer's component repair.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfCustomerComponentRepair(Builder $query)
+    {
+        return $query->where('of', 'customer-component-repair');
+    }
+
+    /**
+     * Scope a query to only include category of employment.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfEmployment(Builder $query)
+    {
+        return $query->where('of', 'employment');
+    }
+
+    /**
+     * Scope a query to only include category of marital.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfMarital(Builder $query)
+    {
+        return $query->where('of', 'marital');
+    }
 }
