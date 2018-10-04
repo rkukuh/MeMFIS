c37b39f4fae3a30883b774592d60daa0c6288725
R. Kukuh <rkukuh@gmail.com>
2018-10-04 14:04:41 +0700

    Define attributes

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -5,11 +5,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Status::class, function (Faker $faker) {
 
+    $name = 'Status-test #' . $faker->unixTime();
+
     return [
-        'name' => 'Status #' . $faker->unixTime(),
+        'code' => str_slug($name),
+        'name' => $name,
         'of'   => $faker->randomElement([
             'marital',
             'employment',
+            'customer-component-repair',
         ])
     ];
 
@@ -17,3 +21,4 @@ $factory->define(Status::class, function (Faker $faker) {
 
 $factory->state(Status::class, 'marital', ['of' => 'marital']);
 $factory->state(Status::class, 'employment', ['of' => 'employment']);
+$factory->state(Status::class, 'customer-component-repair', ['of' => 'customer-component-repair']);
