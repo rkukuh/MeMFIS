a331e783a0a786cb5f417fdaaf734605c6093338
Audhy <audhy.vk01@gmail.com>
2018-10-04 13:02:46 +0700

    typo

--- routes/component.php
+++ routes/component.php
@@ -5,7 +5,8 @@ Route::name('component.')->group(function () {
     Route::view('/update-button', 'frontend.common.buttons.update')->name('button-update');
     Route::view('/submit-button', 'frontend.common.buttons.submit')->name('button-submit');
 
-    Route::get('get-category', 'FillComboxController@category')->name('get-category');
-    Route::get('get-accountcode', 'FillComboxController@accountcode')->name('get-accountcode');
+    Route::get('get-category', 'Frontend\FillComboxController@category')->name('get-category');
+    Route::get('get-accountcode', 'Frontend\FillComboxController@accountcode')->name('get-accountcode');    
 
 });
+
