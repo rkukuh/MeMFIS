5b2d731ccfbcd66ee556ec4e62936f8e777826b7
Audhy <audhy.vk01@gmail.com>
2018-10-05 07:32:12 +0700

    refactor journal

--- resources/views/frontend/journal/modal.blade.php
+++ resources/views/frontend/journal/modal.blade.php
@@ -56,10 +56,11 @@
                                     Level @include('frontend.common.label.optional')
                                 </label>
 
-                                @component('frontend.common.input.number')
+                                @component('frontend.common.input.text')
                                     @slot('value', '1')
                                     @slot('text', 'Level')
                                     @slot('name', 'level')
+                                    @slot('class', 'number')
                                     @slot('id_error', 'code')
                                 @endcomponent
                             </div>
