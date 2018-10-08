ff2a3fcb606b6bfe31ac8118a01a9a00bce20902
R. Kukuh <rkukuh@gmail.com>
2018-10-08 12:32:32 +0700

    Get currency data for combobox

--- app/Http/Controllers/Frontend/FillComboxController.php
+++ app/Http/Controllers/Frontend/FillComboxController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Models\Journal;
 use App\Models\Category;
+use App\Models\Currency;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,11 +15,11 @@ class FillComboxController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function accountcode()
+    public function accountCodes()
     {
-        $accountCode = Journal::pluck('code', 'id');
+        $accountCodes = Journal::pluck('code', 'id');
 
-        return json_encode($accountCode);
+        return json_encode($accountCodes);
 
     }
 
@@ -27,11 +28,24 @@ class FillComboxController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function category()
+    public function categories()
     {
-        $category = Category::pluck('name', 'id');
-        
-        return json_encode($category);
+        $categories = Category::pluck('name', 'id');
+
+        return json_encode($categories);
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function currencies()
+    {
+        $currencies = Currency::pluck('name', 'id');
+
+        return json_encode($currencies);
 
     }
 }
