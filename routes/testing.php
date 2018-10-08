e03ff4b7bbe81ab12393b959af5c43d117d40eab
Audhy <audhy.vk01@gmail.com>
2018-10-08 09:40:45 +0700

    testing maps

--- routes/testing.php
+++ routes/testing.php
@@ -10,6 +10,7 @@ Route::name('testing.')->group(function () {
 
         Route::resource('/testing', 'Frontend\TestingController');
         Route::get('/metronic', 'Frontend\TestingController@metronic')->name('metronic');
+        Route::view('/maps', 'frontend\testing\maps')->name('maps');
 
 
     });
