7107d39d71a5ff422745cec6c98af89c0d3eae61
R. Kukuh <rkukuh@gmail.com>
2018-10-09 12:43:41 +0700

    Use trait for Media Library

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use App\MemfisModel;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Item extends MemfisModel
+class Item extends MemfisModel implements HasMedia
 {
-    //
+    use HasMediaTrait;
 }
