a37d30fd308ede140a120a47cd88626d2b0a0e35
R. Kukuh <rkukuh@gmail.com>
2018-10-09 12:56:22 +0700

    Define fillable attributes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,10 @@ use App\MemfisModel;
 
 class Category extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'account_code',
+    ];
 }
