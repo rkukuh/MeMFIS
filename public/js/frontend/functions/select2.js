926a58a03e5bda4b74c9ca447563b46fc6762e0b
Audhy <audhy.vk01@gmail.com>
2018-10-09 10:00:40 +0700

    add new select2 unit

--- public/js/frontend/functions/select2.js
+++ public/js/frontend/functions/select2.js
@@ -20,6 +20,8 @@ let Select2 = {
             placeholder: 'Select a Type'
         }), $('#unit, #unit_validate').select2({
             placeholder: 'Select a Unit'
+        }), $('#unit2, #unit2_validate').select2({
+            placeholder: 'Select a Unit'
         }), $('#currency, #currency_validate').select2({
             placeholder: 'Select a Currency'
         }), $('#category, #category_validate').select2({
