491f43003cfa5fbd418ba55cd7da51d5d4e492f6
Audhy <audhy.vk01@gmail.com>
2018-10-09 20:54:04 +0700

    add storage select2

--- public/js/frontend/functions/fill-combobox.js
+++ public/js/frontend/functions/fill-combobox.js
@@ -185,3 +185,34 @@ $(document).ready(function () {
 
     currencies();
 });
+
+$(document).ready(function () {
+    storages = function () {
+        $.ajax({
+            url: '/get-storages/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka7 = 1;
+
+                $('select[name="storage"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka7 == 1) {
+                        $('select[name="storage"]').append(
+                            '<option> Select a Storage</option>'
+                        );
+
+                        angka7 = 0;
+                    }
+
+                    $('select[name="storage"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    storages();
+});
