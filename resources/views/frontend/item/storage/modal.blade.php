3ad1a1a7e8495d9d052787a1306edc81ce17d387
Audhy <audhy.vk01@gmail.com>
2018-10-09 19:05:30 +0700

    FIX: buttom footer

--- resources/views/frontend/item/storage/modal.blade.php
+++ resources/views/frontend/item/storage/modal.blade.php
@@ -66,7 +66,9 @@
                     <div class="modal-footer">
                         <div class="flex">
                             <div class="action-buttons">
-                                @include('frontend.common.buttons.submit')
+                                    @component('frontend.common.buttons.submit')
+                                        @slot('class', 'add-stock')
+                                    @endcomponent
                                 @include('frontend.common.buttons.reset')
                                 @include('frontend.common.buttons.close')
                             </div>
