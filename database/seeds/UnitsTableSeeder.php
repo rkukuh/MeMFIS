06fa1f24ce672e96c6158ae0bde5b37a75ad3ef3
R. Kukuh <rkukuh@gmail.com>
2018-10-09 11:06:25 +0700

    WIP

--- database/seeds/UnitsTableSeeder.php
+++ database/seeds/UnitsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Unit;
 use Illuminate\Database\Seeder;
 
 class UnitsTableSeeder extends Seeder
@@ -11,6 +12,32 @@ class UnitsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /** WEIGHT */
+
+        Unit::create([
+            'name' => 'Kilogram',
+            'symbol' => 'kg',
+            'type_id' => Type::ofUnit()->where('code', 'weight')->first()->id,
+        ]);
+
+        Unit::create([
+            'name' => 'Ton',
+            'symbol' => 'ton',
+            'type_id' => Type::ofUnit()->where('code', 'weight')->first()->id,
+        ]);
+
+        /** DIMENSION */
+
+        Unit::create([
+            'name' => 'Centimeter',
+            'symbol' => 'cm',
+            'type_id' => Type::ofUnit()->where('code', 'dimension')->first()->id,
+        ]);
+
+        Unit::create([
+            'name' => 'Meter',
+            'symbol' => 'm',
+            'type_id' => Type::ofUnit()->where('code', 'dimension')->first()->id,
+        ]);
     }
 }
