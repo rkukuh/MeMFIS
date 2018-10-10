a58b0ff63ec09af5e1f606aa491ed59eb6031ed3
R. Kukuh <rkukuh@gmail.com>
2018-10-10 15:20:09 +0700

    Define attributes

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -5,8 +5,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
 
+    $sequence = $faker->unixTime();
+
     return [
-        //
+        'code' => 'IT-EX-' . $sequence,
+        'name' => 'Item Example #' . $sequence,
     ];
 
 });
