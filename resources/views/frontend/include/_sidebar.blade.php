24cd92e57f98c81a77deae752d6c945d453aec6e
Audhy <audhy.vk01@gmail.com>
2018-10-10 13:55:51 +0700

    sidebar update sub menu item

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -33,21 +33,39 @@
                         </span>
                     </a>
                 </li>
-                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
-                    <a href="{{ route('frontend.category-item.index') }}" class="m-menu__link m-menu__toggle">
-                        <i class="m-menu__link-icon flaticon-list-3"></i>
-                        <span class="m-menu__link-text">
-                            Category
-                        </span>
-                    </a>
-                </li>
-                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
-                    <a href="{{ route('frontend.item.index') }}" class="m-menu__link m-menu__toggle">
-                        <i class="m-menu__link-icon flaticon-list-3"></i>
-                        <span class="m-menu__link-text">
+                <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  data-menu-submenu-toggle="hover">
+                        <a  href="#" class="m-menu__link m-menu__toggle">
+                          <i class="m-menu__link-icon flaticon-list-3"></i>
+                          <span class="m-menu__link-text">
                             Item
-                        </span>
-                    </a>
+                          </span>
+                          <i class="m-menu__ver-arrow la la-angle-right"></i>
+                        </a>
+                        <div class="m-menu__submenu ">
+                          <span class="m-menu__arrow"></span>
+                          <ul class="m-menu__subnav">
+                            <li class="m-menu__item " aria-haspopup="true" >
+                              <a  href="{{ route('frontend.item.index') }}" class="m-menu__link ">
+                                <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                                  <span></span>
+                                </i>
+                                <span class="m-menu__link-text">
+                                    Items
+                                </span>
+                              </a>
+                            </li>
+                            <li class="m-menu__item " aria-haspopup="true" >
+                              <a  href="{{ route('frontend.category-item.index') }}" class="m-menu__link ">
+                                <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                                  <span></span>
+                                </i>
+                                <span class="m-menu__link-text">
+                                  Categories
+                                </span>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
                 </li>
                 <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
                     <a href="{{ route('frontend.storage.index') }}" class="m-menu__link m-menu__toggle">
