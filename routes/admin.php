ead03122643c1cd87ea61da0904cf57614a64645
R. Kukuh <rkukuh@gmail.com>
2018-10-10 15:05:25 +0700

    Define admin route for Item

--- routes/admin.php
+++ routes/admin.php
@@ -24,6 +24,7 @@ Route::name('admin.')->group(function () {
         /** MASTER */
 
         Route::resource('unit', 'UnitController');
+        Route::resource('item', 'ItemController');
         Route::resource('department', 'DepartmentController');
 
         /** FINANCE */
