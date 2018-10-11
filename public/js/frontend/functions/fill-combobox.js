fc57e765d4f46b1891060d251fb6531962ab9ea0
Audhy <audhy.vk01@gmail.com>
2018-10-11 09:18:17 +0700

    add type function

--- public/js/frontend/functions/fill-combobox.js
+++ public/js/frontend/functions/fill-combobox.js
@@ -216,3 +216,31 @@ $(document).ready(function () {
 
     storages();
 });
+
+$(document).ready(function () {
+    type = function () {
+        $.ajax({
+            url: '/type',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka8 = 1;
+
+                $('select[name="type"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka8 == 1) {
+                        $('select[name="type"]').append(
+                            '<option> Select a Type</option>'
+                        );
+                        angka8 = 0;
+                    }
+                    $('select[name="type"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+    type();
+});
