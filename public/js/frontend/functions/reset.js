48a1bf49a58cf3c6d59460fcbd4b4aaf1605556d
Audhy <audhy.vk01@gmail.com>
2018-10-11 10:08:53 +0700

    FIX: doesn't work reset form, manualy reset

--- public/js/frontend/functions/reset.js
+++ public/js/frontend/functions/reset.js
@@ -1,17 +1,25 @@
 var item_reset = function () {
-    var frm = document.getElementsByName('item-form')[0];
-    frm.reset();
+    document.getElementById('code').value = "";
+    document.getElementById('name').value = "";
+    document.getElementById('description').value = "";
+    document.getElementById('barcode').value = "";
+    document.getElementById('ppn').value = "";
     $("#accountcode2").select2('val', 'All');
     $("#category").select2('val', 'All');
     $('input[type=file]').val("");
-    // document.getElementById('largeImage-label').innerHTML = '';
-
+    $('input[type=checkbox]').prop('checked',false);
 }
 
 var uom_reset = function () {
-    var frm = document.getElementsByName('item-form')[0];
-    frm.reset();
+    document.getElementById('qty').value = "";
+    document.getElementById('qty2').value = "";
     $("#unit").select2('val', 'All');
     $("#unit2").select2('val', 'All');
 
+}
+
+var minmaxstock_reset = function () {
+    $("#storage").select2('val', 'All');
+    document.getElementById('min').value = "";
+    document.getElementById('max').value = "";
 }
