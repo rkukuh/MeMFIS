ef74f95051412006ac47249ae83a0ee1f3522cf1
Audhy <audhy.vk01@gmail.com>
2018-10-12 15:30:46 +0700

    new component

---
+++ resources/views/frontend/common/input/hidden.blade.php
@@ -0,0 +1,18 @@
+<input
+    type="hidden"
+    id="{{ $id or $name }}"
+    name="{{ $name or '' }}"
+    class="form-control m-input
+           {{ $class or '' }}"
+    style="{{$style or ''}}"
+    value="{{$value or ''}}"
+    placeholder="{{ $placeholder or '' }}"
+    {{$editable or ''}}>
+
+<div class="form-control-feedback text-danger" id="{{ $id_error or '' }}-error"></div>
+
+<span class="m-form__help">
+    {{ $help_text or '' }}
+</span>
+
+{{-- refactor --}}
