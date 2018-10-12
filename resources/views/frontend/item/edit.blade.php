c06c2ffbc20e9ea2970517c15e2081cdb15059f0
Audhy <audhy.vk01@gmail.com>
2018-10-12 21:37:21 +0700

    open command

--- resources/views/frontend/item/edit.blade.php
+++ resources/views/frontend/item/edit.blade.php
@@ -334,7 +334,7 @@
     <script src="{{ asset('js/frontend/functions/reset.js')}}"></script>
     <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
     <script src="{{ asset('js/frontend/functions/select2.js')}}"></script>
-    {{-- <script src="{{ asset('js/frontend/functions/fill-combobox.js')}}"></script> --}}
+    <script src="{{ asset('js/frontend/functions/fill-combobox.js')}}"></script>
 
     <script src="{{ asset('js/frontend/functions/number.js')}}"></script>
     <script src="{{ asset('js/frontend/item/edit.js') }}"></script>
