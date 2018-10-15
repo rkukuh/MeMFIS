97eba4d7c1f49c18f6bfe4a4798eca86808565db
R. Kukuh <rkukuh@gmail.com>
2018-10-15 16:16:13 +0700

    WIP: M-M relationship Employee and General License

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\MemfisModel;
+use App\Models\Pivots\GeneralLicense;
 
 class Employee extends MemfisModel
 {
@@ -15,4 +16,31 @@ class Employee extends MemfisModel
         'gender',
         'hired_at',
     ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * Many-to-Many: An employee may have zero or many general license.
+     *
+     * This function will retrieve the general licenses of an employee.
+     * See: GeneralLicense's employees() method for the inverse
+     *
+     * @return mixed
+     */
+    public function general_licenses()
+    {
+        return $this->belongsToMany(GeneralLicense::class)
+                    ->as('general_license')
+                    ->withPivot(
+                        'code',
+                        'aviation_degree',
+                        'aviation_degree_code',
+                        'exam_no',
+                        'exam_date',
+                        'attendance_no',
+                        'issued_at',
+                        'revoke_at'
+                    )
+                    ->withTimestamps();
+    }
 }
