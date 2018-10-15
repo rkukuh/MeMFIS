6c941bd6b1000b7c23fc74fda5a5adc7092325ea
R. Kukuh <rkukuh@gmail.com>
2018-10-15 16:10:19 +0700

    FIX: Incorrect class imports

--- app/Models/Pivots/GeneralLicense.php
+++ app/Models/Pivots/GeneralLicense.php
@@ -2,9 +2,9 @@
 
 namespace App\Models\Pivots;
 
-use App\MemfisModel;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class GeneralLicense extends MemfisModel
+class GeneralLicense extends Pivot
 {
     //
 }
