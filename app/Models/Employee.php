5a22c5b45fd05ec03eedf2569e4c3fc6c33518b4
R. Kukuh <rkukuh@gmail.com>
2018-10-15 16:01:09 +0700

    Define fillable attributes

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,13 @@ use App\MemfisModel;
 
 class Employee extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'code',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'dob',
+        'gender',
+        'hired_at',
+    ];
 }
