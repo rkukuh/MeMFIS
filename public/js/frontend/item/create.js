60c7e5b4b8740fa562316d4ae94bc26ef324a293
Audhy <audhy.vk01@gmail.com>
2018-10-15 15:04:25 +0700

    call component

--- public/js/frontend/item/create.js
+++ public/js/frontend/item/create.js
@@ -92,6 +92,7 @@ let Item = {
                         toastr.success('Data berhasil disimpan.', 'Sukses', {
                             timeOut: 5000
                         });
+                        update_item_button();
                         // photo();
                     }
                 }
@@ -164,6 +165,90 @@ let Item = {
                             alert(err);
                         }
                     });
+            });
+        });
+
+        $('.footer').on('click', '.edit-item', function () {
+
+            if ($('#category :selected').length > 0) {
+                var selectedcategories = [];
+                $('#category :selected').each(function (i, selected) {
+                    selectedcategories[i] = $(selected).val();
+                });
+            }
+
+
+            if (document.getElementById("isstock").checked) {
+                isstock = 1;
+            } else {
+                isstock = 0;
+            }
+
+            if (document.getElementById("isppn").checked) {
+                isppn = 1;
+            } else {
+                isppn = 0;
+            }
+            let accountcode2 = $('#accountcode2').val();
+            let code = $('input[name=code]').val();
+            let name = $('input[name=name]').val();
+            let description = $('#description').val();
+            let barcode = $('input[name=barcode]').val();
+            let ppn = $('input[name=ppn]').val();
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                type: 'PUT',
+                url: '/item/'+code+'/update',
+                data: {
+                    _token: $('input[name=_token]').val(),
+                    code: code,
+                    name: name,
+                    barcode: barcode,
+                    ppn: ppn,
+                    description: description,
+                    accountcode: accountcode2,
+                    selectedcategories: selectedcategories
+
+                },
+                success: function (data) {
+                    if (data.errors) {
+                        if (data.errors.code) {
+                            $('#code-error').html(data.errors.code[0]);
+
+                        }
+
+                        if (data.errors.name) {
+                            $('#name-error').html(data.errors.name[0]);
+
+                        }
+
+                        document.getElementById('code').value = code;
+                        document.getElementById('name').value = name;
+                        document.getElementById('description').value = description;
+                        document.getElementById('barcode').value = barcode;
+                        document.getElementById('accountcode2').value = accountcode2;
+
+                    } else {
+
+                        $('input[type=file]').val("");
+                        $('#code-error').html('');
+                        $('#name-error').html('');
+                        $('#description-error').html('');
+                        $('#barcode-error').html('');
+                        // document.getElementById('item-uom').removeAttribute('disabled');
+                        // document.getElementById('item-minmaxstock').removeAttribute('disabled');
+                        $('#item-storage').html(code);
+                        $('#item-unit').html();
+                        // item_reset();
+                        toastr.success('Data berhasil disimpan.', 'Sukses', {
+                            timeOut: 5000
+                        });
+                        // location.reload();
+                        // photo();
+                    }
+                }
             });
         });
 
