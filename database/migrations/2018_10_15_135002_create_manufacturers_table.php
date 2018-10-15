6ee96bdbf94a7bfa74d8da53b9f5bf7c29c004a5
R. Kukuh <rkukuh@gmail.com>
2018-10-15 13:56:43 +0700

    Add columns

--- database/migrations/2018_10_15_135002_create_manufacturers_table.php
+++ database/migrations/2018_10_15_135002_create_manufacturers_table.php
@@ -15,7 +15,14 @@ class CreateManufacturersTable extends Migration
     {
         Schema::create('manufacturers', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('code');
+            $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('code');
+            $table->index('name');
         });
     }
 
