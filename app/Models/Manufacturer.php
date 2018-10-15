2a452d3b9652632331cb10108a6fd7644d452995
R. Kukuh <rkukuh@gmail.com>
2018-10-15 13:56:52 +0700

    Define fillable's columns

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use App\MemfisModel;
+use App\Scopes\OrderByColumn;
 
 class Manufacturer extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'code',
+        'name',
+    ];
+
+    /******************************************* BOOT ********************************************/
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new OrderByColumn('name'));
+    }
 }
