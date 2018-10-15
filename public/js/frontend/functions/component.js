6bccb69d91d758c456ef7481659a6177f73f4a7a
Audhy <audhy.vk01@gmail.com>
2018-10-15 08:34:16 +0700

    component js

---
+++ public/js/frontend/functions/component.js
@@ -0,0 +1,17 @@
+var update_button = function () {
+    $('.btn-success').removeClass('add');
+    $('.btn-success').addClass('update');
+    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save Changes</span></span>");
+}
+
+var save_button = function () {
+    if ($('.update')[0]) {
+        $('.btn-success').removeClass('update');
+        $('.btn-success').addClass('add');
+        $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save New</span></span>");
+    }
+}
+
+let close = $('.modal-footer').on('click', '.clse', function () {
+    save_button();
+});
