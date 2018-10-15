67d5af92df0297839ae12e1f09ec1030a0ce24e3
R. Kukuh <rkukuh@gmail.com>
2018-10-15 12:19:46 +0700

    Remove unused 'id' column in transactional table

--- database/migrations/2018_09_24_142857_create_item_storage_table.php
+++ database/migrations/2018_09_24_142857_create_item_storage_table.php
@@ -14,7 +14,6 @@ class CreateItemStorageTable extends Migration
     public function up()
     {
         Schema::create('item_storage', function (Blueprint $table) {
-            $table->increments('id');
             $table->unsignedInteger('item_id');
             $table->unsignedInteger('storage_id');
             $table->double('min', 8, 2);
