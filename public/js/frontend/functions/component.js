53526186736714b5b620e3b06408063e2bf6f1f5
Audhy <audhy.vk01@gmail.com>
2018-10-15 15:02:50 +0700

    add new component

--- public/js/frontend/functions/component.js
+++ public/js/frontend/functions/component.js
@@ -15,3 +15,10 @@ var save_button = function () {
 let close = $('.modal-footer').on('click', '.clse', function () {
     save_button();
 });
+
+var update_item_button = function () {
+    $('.btn-success').removeClass('add');
+    $('.btn-success').addClass('edit-item');
+    $('.btn-success').removeClass('add-item');
+    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save Changes</span></span>");
+}
