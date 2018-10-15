412de27d64d5cf4bf93c6be6313bcbbc21356efc
R. Kukuh <rkukuh@gmail.com>
2018-10-15 14:11:33 +0700

    Populate seeder data

--- database/seeds/AircraftsTableSeeder.php
+++ database/seeds/AircraftsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Manufacturer;
 
 class AircraftsTableSeeder extends Seeder
 {
@@ -11,6 +12,36 @@ class AircraftsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $boeing = Manufacturer::where('code', 'boeing')->first();
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-200',
+            'name' => 'B737-200',
+        ]);
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-300',
+            'name' => 'B737-300',
+        ]);
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-400',
+            'name' => 'B737-400',
+        ]);
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-500',
+            'name' => 'B737-500',
+        ]);
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-800NG',
+            'name' => 'B737-800NG',
+        ]);
+
+        $boeing->aircrafts()->create([
+            'code' => 'B737-900ER',
+            'name' => 'B737-900ER',
+        ]);
     }
 }
