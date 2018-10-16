1cc8f8786966e17a15c56dc430fc53642bc9dccb
R. Kukuh <rkukuh@gmail.com>
2018-10-16 13:41:04 +0700

    Populate intial data

--- database/seeds/LicensesTableSeeder.php
+++ database/seeds/LicensesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\License;
 use Illuminate\Database\Seeder;
 
 class LicensesTableSeeder extends Seeder
@@ -11,6 +12,28 @@ class LicensesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        License::create([
+            'code' => 'general-license',
+            'name' => 'General License',
+            'regulator' => Type::ofRegulator()->where('code', 'dgca')->first()->id,
+        ]);
+
+        License::create([
+            'code' => 'amel-dgca',
+            'name' => 'AME License (by DGCA)',
+            'regulator' => Type::ofRegulator()->where('code', 'dgca')->first()->id,
+        ]);
+
+        License::create([
+            'code' => 'amel-easa',
+            'name' => 'AME License (by EASA)',
+            'regulator' => Type::ofRegulator()->where('code', 'easa')->first()->id,
+        ]);
+
+        License::create([
+            'code' => 'amel-faa',
+            'name' => 'AME License (by FAA)',
+            'regulator' => Type::ofRegulator()->where('code', 'faa')->first()->id,
+        ]);
     }
 }
