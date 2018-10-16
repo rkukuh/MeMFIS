1593fdd9956ce38bac5d7be13bad7198eddf50ea
R. Kukuh <rkukuh@gmail.com>
2018-10-16 14:28:10 +0700

    WIP: Dummy data

--- database/seeds/dummies/CertifiedStaff.php
+++ database/seeds/dummies/CertifiedStaff.php
@@ -17,11 +17,20 @@ class CertifiedStaff extends Seeder
     {
         /** Create EMPLOYEE */
 
-        //
+        $aldrin = Employee::create([
+            'code' => 'MMF-00900',
+            'first_name' => 'Aldrin',
+            'last_name' => 'Collins',
+        ]);
 
         /** Assign EMPLOYEE to GENERAL LICENSE */
 
-        //
+        $general_license = License::where('code', 'general-license')->first();
+
+        $aldrin->licenses()->attach($general_license, [
+            'code' => 'E/I.3475',
+            'issued_at' => '2011-08-15',
+        ]);
 
         /** Assign EMPLOYEE to AME LICENSE */
 
