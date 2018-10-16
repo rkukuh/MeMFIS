35044f683b3fbb951b7fc89e2ced8edbb6cd4d92
Audhy <audhy.vk01@gmail.com>
2018-10-16 21:49:47 +0700

    add group

--- routes/component.php
+++ routes/component.php
@@ -5,14 +5,22 @@ Route::name('component.')->group(function () {
     Route::view('/update-button', 'frontend.common.buttons.update')->name('button-update');
     Route::view('/submit-button', 'frontend.common.buttons.submit')->name('button-submit');
 
+
+    Route::group([
+
+        'middleware'    => 'auth',
+        'namespace'     => 'Frontend'
+
+    ], function () {
+
     Route::get('/addres/city/{id}','AddresController@City')->name('city');
     Route::get('/addres/country','AddresController@Country')->name('country');
 
-    Route::get('get-units', 'Frontend\FillComboxController@units')->name('get-units');    
-    Route::get('get-currencies', 'Frontend\FillComboxController@currencies')->name('get-currencies');    
-    Route::get('get-storages-combobox', 'Frontend\FillComboxController@storages')->name('get-storages');    
-    Route::get('get-categories-item', 'Frontend\FillComboxController@categories')->name('get-categories');
-    Route::get('get-accountcodes', 'Frontend\FillComboxController@accountCodes')->name('get-accountcodes');    
-
+    Route::get('get-units', 'FillComboxController@units')->name('get-units');    
+    Route::get('get-currencies', 'FillComboxController@currencies')->name('get-currencies');    
+    Route::get('get-storages-combobox', 'FillComboxController@storages')->name('get-storages');    
+    Route::get('get-categories-item', 'FillComboxController@categories')->name('get-categories');
+    Route::get('get-accountcodes', 'FillComboxController@accountCodes')->name('get-accountcodes');    
+    });
 });
 
