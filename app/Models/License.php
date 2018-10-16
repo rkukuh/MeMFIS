a574d995e51414bc53844b5ba629dc6c4227c15f
R. Kukuh <rkukuh@gmail.com>
2018-10-16 14:59:34 +0700

    Define 1-M relationship between License and General License

--- app/Models/License.php
+++ app/Models/License.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\MemfisModel;
 use App\Scopes\OrderByColumn;
+use App\Models\Pivots\GeneralLicense;
 
 class License extends MemfisModel
 {
@@ -46,6 +47,28 @@ class License extends MemfisModel
                         'issued_at',
                         'valid_until',
                         'revoke_at'
+                    )
+                    ->withTimestamps();
+    }
+
+    /**
+     * One-to-Many: A license may have zero or many general license.
+     *
+     * This function will retrieve all the general licenses of a license.
+     * See: General License's license() method for the inverse
+     *
+     * @return mixed
+     */
+    public function general_licenses()
+    {
+        return $this->hasMany(GeneralLicense::class)
+                    ->as('general_license')
+                    ->withPivot(
+                        'aviation_degree',
+                        'aviation_degree_code',
+                        'exam_no',
+                        'exam_date',
+                        'attendance_no'
                     )
                     ->withTimestamps();
     }
--- app/Models/Pivots/GeneralLicense.php
+++ app/Models/Pivots/GeneralLicense.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class GeneralLicense extends Pivot
 {
-    //
+    protected $fillable = [
+        'aviation_degree',
+        'aviation_degree_code',
+        'exam_no',
+        'exam_date',
+        'attendance_no',
+    ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-to-Many: A license may have zero or many general license.
+     *
+     * This function will retrieve the license of a general license.
+     * See: General License's license() method for the inverse
+     *
+     * @return mixed
+     */
+    public function license()
+    {
+        return $this->belongsTo(License::class)
+                    ->as('general_license')
+                    ->withPivot(
+                        'aviation_degree',
+                        'aviation_degree_code',
+                        'exam_no',
+                        'exam_date',
+                        'attendance_no'
+                    )
+                    ->withTimestamps();
+    }
 }
