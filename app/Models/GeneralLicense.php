432335eb1c3102e01f4a0a77a303b0b2ae426478
R. Kukuh <rkukuh@gmail.com>
2018-10-16 15:53:20 +0700

    Remove unused relationships

--- app/Models/GeneralLicense.php
+++ app/Models/GeneralLicense.php
@@ -16,25 +16,5 @@ class GeneralLicense extends Pivot
 
     /*************************************** RELATIONSHIP ****************************************/
 
-    /**
-     * One-to-Many: A license may have zero or many general license.
-     *
-     * This function will retrieve the license of a general license.
-     * See: General License's license() method for the inverse
-     *
-     * @return mixed
-     */
-    public function license()
-    {
-        return $this->belongsTo(License::class)
-                    ->as('general_license')
-                    ->withPivot(
-                        'aviation_degree',
-                        'aviation_degree_code',
-                        'exam_no',
-                        'exam_date',
-                        'attendance_no'
-                    )
-                    ->withTimestamps();
-    }
+    //
 }
