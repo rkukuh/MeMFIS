bd27aeb85151fb868a6d667b75c8e7e2a45ca77c
R. Kukuh <rkukuh@gmail.com>
2018-10-16 13:35:28 +0700

    Define attributes to become faker's value

--- database/factories/LicenseFactory.php
+++ database/factories/LicenseFactory.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Models\Type;
 use App\Models\License;
 use Faker\Generator as Faker;
 
 $factory->define(License::class, function (Faker $faker) {
 
+    $sequence = $faker->unixTime();
+
     return [
-        //
+        'code' => 'LIC-' . $sequence,
+        'name' => 'License Example #' . $sequence,
+        'regulator' => Type::ofRegulator()->get()->random()->id
     ];
 
 });
