b1b14379903048757c022e92159f8867c3455b3e
R. Kukuh <rkukuh@gmail.com>
2018-10-16 13:48:37 +0700

    Create dummy data seeder

---
+++ database/seeds/dummies/CertifiedStaff.php
@@ -0,0 +1,20 @@
+<?php
+
+use Carbon\Carbon;
+use App\Models\Type;
+use App\Models\License;
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+
+class CertifiedStaff extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
