cf8c810e993207746cd2ec4f00bfc0070a3fd1da
Audhy <audhy.vk01@gmail.com>
2018-10-16 19:14:28 +0700

    refactor

--- routes/component.php
+++ routes/component.php
@@ -5,11 +5,14 @@ Route::name('component.')->group(function () {
     Route::view('/update-button', 'frontend.common.buttons.update')->name('button-update');
     Route::view('/submit-button', 'frontend.common.buttons.submit')->name('button-submit');
 
-    Route::get('get-categories-item', 'Frontend\FillComboxController@categories')->name('get-categories');
-    Route::get('get-accountcodes', 'Frontend\FillComboxController@accountCodes')->name('get-accountcodes');    
+    Route::get('/addres/city/{id}','AddresController@City')->name('city');
+    Route::get('/addres/country','AddresController@Country')->name('country');
+
     Route::get('get-units', 'Frontend\FillComboxController@units')->name('get-units');    
     Route::get('get-currencies', 'Frontend\FillComboxController@currencies')->name('get-currencies');    
     Route::get('get-storages-combobox', 'Frontend\FillComboxController@storages')->name('get-storages');    
+    Route::get('get-categories-item', 'Frontend\FillComboxController@categories')->name('get-categories');
+    Route::get('get-accountcodes', 'Frontend\FillComboxController@accountCodes')->name('get-accountcodes');    
 
 });
 
