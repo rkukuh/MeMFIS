8765679a46806dc26b448dc14dc0529102840a9a
Audhy <audhy.vk01@gmail.com>
2018-10-16 15:55:57 +0700

    unused

--- public/js/frontend/item/edit.js
+++ public/js/frontend/item/edit.js
@@ -245,15 +245,81 @@ let Item = {
                         toastr.success('Data berhasil disimpan.', 'Sukses', {
                             timeOut: 5000
                         });
-                        location.reload();
+                        // location.reload();
                         // photo();
                     }
                 }
             });
         });
 
+        $(function () {
 
+            // klik();
+            let inputFile = $('#myInput');
+            let button = $('#myButton');
+            let buttonSubmit = $('#add-item');
+            let filesContainer = $('#myFiles');
+            let files = [];
 
+            inputFile.change(function () {
+                let newFiles = [];
+                for (let index = 0; index < inputFile[0].files.length; index++) {
+                    let file = inputFile[0].files[index];
+                    newFiles.push(file);
+                    files.push(file);
+                }
+
+                newFiles.forEach(file => {
+                    let fileElement = $(`<p>${file.name}</p>`);
+                    fileElement.data('fileData', file);
+                    filesContainer.append(fileElement);
+
+                    fileElement.click(function (event) {
+                        let fileElement = $(event.target);
+                        let indexToRemove = files.indexOf(fileElement.data('fileData'));
+                        fileElement.remove();
+                        files.splice(indexToRemove, 1);
+                    });
+                });
+            });
+
+            button.click(function () {
+                inputFile.click();
+            });
+            $('.footer').on('click', '.edit-item', function () {
+                let formData = new FormData();
+                let code = $('input[name=code]').val();
+                formData.append('code', code);
+
+                let z = 0;
+                files.forEach(file => {
+                    formData.append('file' + z, file);
+                    z++;
+                });
+
+                // console.log('Sending...');
+
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+
+                        url: '/post-photos',
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        type: 'POST',
+                        success: function (data) {
+                            if (data.uploaded == true) {
+                                // alert('sukses');
+                            }
+                        },
+                        error: function (err) {
+                            alert(err);
+                        }
+                    });
+            });
+        });
 
         let simpan2 = $('.modal-footer').on('click', '.add-uom', function () {
             let code = $('input[name=code]').val();
