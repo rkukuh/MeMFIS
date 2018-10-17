be3dcef4c6b14c90786ba4519cf173ca1e13026f
R. Kukuh <rkukuh@gmail.com>
2018-10-17 15:44:03 +0700

    Define model factory states

--- database/factories/TypeFactory.php
+++ database/factories/TypeFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Type::class, function (Faker $faker) {
 
-    $name = 'Type-test #' . $faker->unixTime();
+    $name = 'Type Example #' . $faker->unixTime();
 
     return [
         'code' => str_slug($name),
@@ -13,12 +13,15 @@ $factory->define(Type::class, function (Faker $faker) {
         'of'   => $faker->randomElement([
             'arc',
             'fax',
+            'unit',
             'phone',
             'email',
             'journal',
             'regulator',
             'capability',
             'eligibility',
+            'formal-school-degree',
+            'aviation-school-degree',
         ]),
     ];
 
@@ -26,9 +29,12 @@ $factory->define(Type::class, function (Faker $faker) {
 
 $factory->state(Type::class, 'arc', ['of' => 'arc']);
 $factory->state(Type::class, 'fax', ['of' => 'fax']);
+$factory->state(Type::class, 'unit', ['of' => 'unit']);
 $factory->state(Type::class, 'phone', ['of' => 'phone']);
 $factory->state(Type::class, 'email', ['of' => 'email']);
 $factory->state(Type::class, 'journal', ['of' => 'journal']);
 $factory->state(Type::class, 'regulator', ['of' => 'regulator']);
 $factory->state(Type::class, 'capability', ['of' => 'capability']);
 $factory->state(Type::class, 'eligibility', ['of' => 'eligibility']);
+$factory->state(Type::class, 'formal-school-degree', ['of' => 'formal-school-degree']);
+$factory->state(Type::class, 'aviation-school-degree', ['of' => 'aviation-school-degree']);
