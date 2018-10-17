93e780696f9189901a8bf79a01d4f082687bd60d
R. Kukuh <rkukuh@gmail.com>
2018-10-17 15:40:25 +0700

    Define fillable columns

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use App\MemfisModel;
+use Illuminate\Database\Eloquent\Builder;
 
 class Level extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'code',
+        'name',
+        'of',
+        'score',
+        'description',
+    ];
 }
