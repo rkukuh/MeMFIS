c1c49a625421818ed6055a34f7f7f3a29426d72f
Audhy <audhy.vk01@gmail.com>
2018-10-17 13:48:21 +0700

    change id of form

--- resources/views/frontend/employees/employee/modal/education.blade.php
+++ resources/views/frontend/employees/employee/modal/education.blade.php
@@ -11,7 +11,7 @@
             <div class="modal-body">
 
                 <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="EducationForm">
-                    <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="id_education" id="id_education">
                     <div class="m-portlet__body">
                         <div class="form-group m-form__group row ">
                             <div class="col-sm-6 col-md-6 col-lg-6">
