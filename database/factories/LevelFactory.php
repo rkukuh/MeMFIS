315266d3c56ae00ba270552bcb78d0c38e8d0d4a
R. Kukuh <rkukuh@gmail.com>
2018-10-17 15:47:04 +0700

    Define logic

--- database/factories/LevelFactory.php
+++ database/factories/LevelFactory.php
@@ -5,8 +5,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Level::class, function (Faker $faker) {
 
+    $name = 'Level Example #' . $faker->unixTime();
+
     return [
-        //
+        'code' => str_slug($name),
+        'name' => $name,
+        'of'   => $faker->randomElement([
+            'language',
+        ]),
+        'score' => $faker->randomDigitNotNull() * 10
     ];
 
 });
