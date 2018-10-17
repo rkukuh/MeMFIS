67504ab46c1269572def1fbe023aef53a5ff8139
R. Kukuh <rkukuh@gmail.com>
2018-10-17 21:36:21 +0700

    WIP: Dummy for certified staff

--- database/seeds/dummies/CertifiedStaff.php
+++ database/seeds/dummies/CertifiedStaff.php
@@ -5,6 +5,7 @@ use App\Models\Type;
 use App\Models\License;
 use App\Models\Employee;
 use Illuminate\Database\Seeder;
+use App\Models\Pivots\EmployeeLicense;
 
 class CertifiedStaff extends Seeder
 {
@@ -32,17 +33,31 @@ class CertifiedStaff extends Seeder
             'issued_at' => Carbon::createFromFormat('Y-m-d', '2011-08-15'),
         ]);
 
-        $aldrin->licenses()->attach($general_license, [
+        $ap_3475 = $aldrin->licenses()->attach($general_license, [
             'code' => 'A/P.3475',
             'issued_at' => Carbon::createFromFormat('Y-m-d', '2011-08-15'),
         ]);
 
         /** Assign GENERAL LICENSE to AVIATION DEGREE */
 
-        $degree_a1 = Type::ofAviationSchoolDegree()->where('code', 'a1')->first();
-        $degree_a4 = Type::ofAviationSchoolDegree()->where('code', 'a4')->first();
+        $ei_3475 = EmployeeLicense::whereHas('employee', function ($query) use ($aldrin) {
+                        return $query->where('employee_id', $aldrin->id);
+                    })
+                    ->where('code', 'E/I.3475')
+                    ->first();
+
         $degree_c2 = Type::ofAviationSchoolDegree()->where('code', 'c2')->first();
         $degree_c4 = Type::ofAviationSchoolDegree()->where('code', 'c4')->first();
+
+        EmployeeLicense::whereHas('employee', function ($query) use ($aldrin) {
+                            $query->where('employee_id', $aldrin->id);
+                        })
+                        ->find($ei_3475->id)
+                        ->general_license_details()
+                        ->save([
+                            $degree_c2,
+                            $degree_c4,
+                        ]);
 
         /** Assign EMPLOYEE to AME LICENSE */
 
