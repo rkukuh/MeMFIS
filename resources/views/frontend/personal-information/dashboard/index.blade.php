ece3794c8491d4546520889309f5bd25af15b57c
Audhy <audhy.vk01@gmail.com>
2018-10-17 11:01:42 +0700

    -

---
+++ resources/views/frontend/personal-information/dashboard/index.blade.php
@@ -0,0 +1,37 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader ">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Dashboard
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
+    <script src="{{ asset('js/frontend/employee/employee-history.js')}}"></script>
+@endpush
