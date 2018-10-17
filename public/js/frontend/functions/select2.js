902f3aff1b574e13c68431f1309fc3bcb7aa139a
Audhy <audhy.vk01@gmail.com>
2018-10-17 13:46:15 +0700

    makeid of name select2

--- public/js/frontend/functions/select2.js
+++ public/js/frontend/functions/select2.js
@@ -22,6 +22,24 @@ let Select2 = {
             placeholder: 'Select a Type'
         }), $('#name2, #name2_validate').select2({
             placeholder: 'Select a Name'
+        }), $('#name3, #name3_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name4, #name4_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name5, #name5_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name6, #name6_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name7, #name7_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name8, #name8_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name9, #name9_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name10, #name10_validate').select2({
+            placeholder: 'Select a Name'
+        }), $('#name11, #name11_validate').select2({
+            placeholder: 'Select a Name'
         }), $('#certification, #certification_validate').select2({
             placeholder: 'Select a Certification'
         }), $('#school, #school_validate').select2({
