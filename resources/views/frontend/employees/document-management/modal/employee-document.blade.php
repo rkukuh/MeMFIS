cdb1eb149b9b41222659dc07406413d556d29d96
Audhy <audhy.vk01@gmail.com>
2018-10-17 09:53:12 +0700

    add label on title modal

--- resources/views/frontend/employees/document-management/modal/employee-document.blade.php
+++ resources/views/frontend/employees/document-management/modal/employee-document.blade.php
@@ -2,6 +2,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
+                @include('frontend.common.label.create-new')
                 <h5 class="modal-title" id="TitleModalEmployeeDocument">Employee Document</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
