52b26180bc4b9736522b72bbe455756458c3dd16
Audhy <audhy.vk01@gmail.com>
2018-10-17 13:27:51 +0700

    add new fill

--- public/js/frontend/functions/fill-combobox.js
+++ public/js/frontend/functions/fill-combobox.js
@@ -61,6 +61,37 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+    certification = function () {
+        $.ajax({
+            url: '/get-categories-item/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka3 = 1;
+
+                $('select[name="certification"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka3 == 1) {
+                        $('select[name="certification"]').append(
+                            '<option> Select a Name</option>'
+                        );
+
+                        angka3 = 0;
+                    }
+
+                    $('select[name="certification"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    certification();
+});
+
+$(document).ready(function () {
     name2 = function () {
         $.ajax({
             url: '/get-categories-item/',
