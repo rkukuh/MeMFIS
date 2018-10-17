f9755e46af90926b59efeb5278c19f61ca1c4535
R. Kukuh <rkukuh@gmail.com>
2018-10-17 15:33:17 +0700

    Indexing

--- database/migrations/2018_09_12_083619_create_statuses_table.php
+++ database/migrations/2018_09_12_083619_create_statuses_table.php
@@ -23,6 +23,7 @@ class CreateStatusesTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
+            $table->index('code');
             $table->index('name');
         });
     }
