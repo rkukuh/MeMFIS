f5a59493585e8417ec95f9e29ab3f9ab43126eeb
Audhy <audhy.vk01@gmail.com>
2018-10-17 13:21:43 +0700

    add new fill

--- public/js/frontend/functions/fill-combobox.js
+++ public/js/frontend/functions/fill-combobox.js
@@ -60,7 +60,67 @@ $(document).ready(function () {
     accountcode_category();
 });
 
+$(document).ready(function () {
+    name2 = function () {
+        $.ajax({
+            url: '/get-categories-item/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka3 = 1;
 
+                $('select[name="name"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka3 == 1) {
+                        $('select[name="name"]').append(
+                            '<option> Select a Name</option>'
+                        );
+
+                        angka3 = 0;
+                    }
+
+                    $('select[name="name"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    name2();
+});
+
+$(document).ready(function () {
+    school = function () {
+        $.ajax({
+            url: '/get-categories-item/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka3 = 1;
+
+                $('select[name="school"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka3 == 1) {
+                        $('select[name="school"]').append(
+                            '<option> Select a school</option>'
+                        );
+
+                        angka3 = 0;
+                    }
+
+                    $('select[name="school"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    school();
+});
 
 $(document).ready(function () {
     category = function () {
