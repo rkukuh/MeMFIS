be0b23a90a86c09952a480ebac70ea57933a853a
Audhy <audhy.vk01@gmail.com>
2018-10-18 11:09:45 +0700

    profil view

--- resources/views/frontend/personal-information/basic-information/include/my-detail.blade.php
+++ resources/views/frontend/personal-information/basic-information/include/my-detail.blade.php
@@ -1,26 +1,153 @@
 <div class="col-lg-12 col-md-12 col-sm-12">
-        <div class="row align-items-center">
-                <div class="col-xl-8 order-2 order-xl-1">
-                    <div class="form-group m-form__group row align-items-center">
-                        <div class="col-md-4">
-                            <div class="m-input-icon m-input-icon--left">
-                                <input type="text" class="form-control m-input" placeholder="Search..."
-                                    id="generalSearch">
-                                <span class="m-input-icon__icon m-input-icon__icon--left">
-                                    <span><i class="la la-search"></i></span>
-                                </span>
-                            </div>
+{{-- <div class="m-content">
+        <div class="row"> --}}
+            <div class="m-content">
+                <div class="row">
+                    <table>
+                        <tr>
+                            <td width="20%">
+                                    <img src="{{asset('assets/metronic/app/media/img/users/user4.jpg')}}" height="130px">
+                            </td>
+                            <td width="70%">
+                                    <h5>Name :</h5> 
+                                    <p>xxxxxx </p> 
+                                    <h5>Nik :</h5> 
+                                    <p>xxxx </p> 
+        
+                            </td>
+                        </tr>
+                    </table>
+                        {{-- <div class="col-sm-3 col-md-3 col-lg-3">
                         </div>
-                    </div>
-                </div>
-                <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-                    {{-- @component('frontend.common.buttons.create-new')
-                        @slot('text', '')
-                        @slot('data_target', '#modal_')
-                    @endcomponent --}}
-
-                    <div class="m-separator m-separator--dashed d-xl-none"></div>
+                        <div class="col-sm-9 col-md-9 col-lg-9">
+                        </div> --}}
+                        {{-- <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Date of Birth </h5> 
+                            <p>March 17th, 1984 </p> 
+                            <h5>Joined Date </h5> 
+                            <p>August 3rd, 2005 </p> 
+                        </div>                    
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Gender </h5> 
+                            <p>Male </p> 
+                        </div>                             --}}
                 </div>
             </div>
-        <div class="m_datatable_employee_hitory" id="scrolling_both"></div>
-    </div>
+            <div class="m-portlet m-portlet--bordered m-portlet--rounded m-portlet--unair">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Personal Information
+                            </h3>
+                        </div>			
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                    <div class="row">
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Driving License No. </h5> 
+                            <p> - </p> 
+                            <h5>Nationality </h5> 
+                            <p>Canadian. </p> 
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Other ID </h5> 
+                            <p>294-38-3535 </p> 
+                            <h5>Marital Status </h5> 
+                            <p>Married </p> 
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Date of Birth </h5> 
+                            <p>March 17th, 1984 </p> 
+                            <h5>Joined Date </h5> 
+                            <p>August 3rd, 2005 </p> 
+                        </div>                    
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <h5>Gender </h5> 
+                            <p>Male </p> 
+                        </div>                            
+                    </div>
+                </div>
+            </div>	
+            <div class="m-portlet m-portlet--bordered m-portlet--rounded m-portlet--unair">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Contact Information
+                            </h3>
+                        </div>			
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                        <div class="m-portlet__body">
+                                <div class="row">
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Driving License No. </h5> 
+                                        <p> - </p> 
+                                        <h5>Nationality </h5> 
+                                        <p>Canadian. </p> 
+                                    </div>
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Other ID </h5> 
+                                        <p>294-38-3535 </p> 
+                                        <h5>Marital Status </h5> 
+                                        <p>Married </p> 
+                                    </div>
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Date of Birth </h5> 
+                                        <p>March 17th, 1984 </p> 
+                                        <h5>Joined Date </h5> 
+                                        <p>August 3rd, 2005 </p> 
+                                    </div>                    
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Gender </h5> 
+                                        <p>Male </p> 
+                                    </div>                            
+                                </div>
+                            </div>
+                </div>
+            </div>	
+            <div class="m-portlet m-portlet--bordered m-portlet--rounded m-portlet--unair">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Job Details
+                            </h3>
+                        </div>			
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                        <div class="m-portlet__body">
+                                <div class="row">
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Driving License No. </h5> 
+                                        <p> - </p> 
+                                        <h5>Nationality </h5> 
+                                        <p>Canadian. </p> 
+                                    </div>
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Other ID </h5> 
+                                        <p>294-38-3535 </p> 
+                                        <h5>Marital Status </h5> 
+                                        <p>Married </p> 
+                                    </div>
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Date of Birth </h5> 
+                                        <p>March 17th, 1984 </p> 
+                                        <h5>Joined Date </h5> 
+                                        <p>August 3rd, 2005 </p> 
+                                    </div>                    
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <h5>Gender </h5> 
+                                        <p>Male </p> 
+                                    </div>                            
+                                </div>
+                            </div>
+                </div>
+            </div>	
+        {{-- </div>
+    </div> --}}
+</div>
