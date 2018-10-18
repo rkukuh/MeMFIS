54663c9d1c9fa40605aa6d2218ee8f089f263cf7
Audhy <audhy.vk01@gmail.com>
2018-10-18 15:44:56 +0700

    update controller employee

--- app/Http/Controllers/Frontend/EmployeeController.php
+++ app/Http/Controllers/Frontend/EmployeeController.php
@@ -141,6 +141,7 @@ class EmployeeController extends Controller
             'first_name' => $request->first_name,
             'middle_name' => $request->middle_name,
             'last_name' => $request->last_name,
+            'gender' => $request->gender,
             'dob' => $request->dob,
             'hired_at' => $request->hired_at,
         ]);
@@ -179,7 +180,17 @@ class EmployeeController extends Controller
      */
     public function update(EmployeeUpdate $request, Employee $employee)
     {
-        //
+        // dd($request->all());
+        $employee->code = $request->code;
+        $employee->first_name = $request->first_name;
+        $employee->middle_name = $request->middle_name;
+        $employee->last_name = $request->last_name;
+        $employee->gender = $request->gender;
+        $employee->dob = $request->dob;
+        $employee->hired_at = $request->hired_at;
+        $employee->save();
+
+        return response()->json($employee);
     }
 
     /**
