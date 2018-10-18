34e3b05325d0d218455416d80e22f6b051a74522
Audhy <audhy.vk01@gmail.com>
2018-10-18 15:44:45 +0700

    form request update

--- app/Http/Requests/Frontend/EmployeeUpdate.php
+++ app/Http/Requests/Frontend/EmployeeUpdate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Frontend;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class EmployeeUpdate extends FormRequest
 {
@@ -13,7 +15,7 @@ class EmployeeUpdate extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,12 @@ class EmployeeUpdate extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => 'required',
+            'first_name' => 'required',
         ];
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()]));
     }
 }
