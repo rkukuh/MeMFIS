a2518a420e492d7541929adf0300bb3d142b59a0
ThamrinPH <husni.sosnet@gmail.com>
2018-10-18 09:37:08 +0700

    coba commit heheheehhe windows only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Schema; // windows only
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); // windows only
     }
 
     /**
