17da59c8b09c6fc861b4a0648a82e74367f0acad
R. Kukuh <rkukuh@gmail.com>
2018-10-18 13:30:20 +0700

    Remove unused testing routes

--- routes/testing.php
+++ routes/testing.php
@@ -16,31 +16,6 @@ Route::name('testing.')->group(function () {
         Route::get('/metronic', 'Frontend\TestingController@metronic')->name('metronic');
         Route::post('/testing-photos','Frontend\TestingController@postPhotos')->name('testing-photos');
 
-        Route::get('/certified-staff/{staffId}', function ($staffId) {
-
-            /** Get a specific license of a given employee and license's number */
-
-            $result = App\Models\Pivots\EmployeeLicense::with('employee', 'license')
-                        ->whereHas('employee', function ($query) use ($staffId) {
-                            $query->where('employee_id', $staffId);
-                        })
-                        ->where('code', 'E/I.3475') // license's number, omittable
-                        ->get();
-
-            return $result;
-        });
-
-        Route::get('/employee-license-staff/{staffId}', function ($staffId) {
-
-            $ei_3475 = App\Models\Pivots\EmployeeLicense::where('code', 'E/I.3475')->first();
-
-            return App\Models\Pivots\EmployeeLicense::whereHas('employee', function ($query) use ($staffId) {
-                        $query->where('employee_id', $staffId);
-                    })
-                    ->find($ei_3475->id) // with ID = a Model returned, without ID = an array returned
-                    ->employee;
-        });
-
     });
 
 });
