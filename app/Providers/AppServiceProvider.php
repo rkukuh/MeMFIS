5dc74d377fde0ed71deb71aca3b119b6f1886c45
R. Kukuh <rkukuh@gmail.com>
2018-10-18 12:19:59 +0700

    FIX: Index Lengths & MySQL / MariaDB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\Schema; // windows only
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191); // windows only
+        Schema::defaultStringLength(191);
     }
 
     /**
