9c4ff34abbac33256e5cfc4bfcbbc04258735a9c
Audhy <audhy.vk01@gmail.com>
2018-10-19 10:59:04 +0700

    edit format

--- public/js/frontend/functions/datepicker.js
+++ public/js/frontend/functions/datepicker.js
@@ -30,31 +30,37 @@ var BootstrapDatepicker = function () {
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#graduated_at, #graduated_at_validate").datepicker({
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#exam_date, #exam_date_validate").datepicker({
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#issued_at, #issued_at_validate").datepicker({
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#valid_until, #valid_until_validate").datepicker({
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#revoke_at, #revoke_at_validate").datepicker({
                 rtl: mUtil.isRTL(),
                 todayHighlight: !0,
                 orientation: "bottom left",
+                format: "yyyy-mm-dd",
                 templates: t
             }), $("#m_datepicker_1_modal").datepicker({
                 rtl: mUtil.isRTL(),
