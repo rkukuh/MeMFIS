9806d6fc4d4a427bbb9e77fb272735087c7e08c6
R. Kukuh <rkukuh@gmail.com>
2018-10-22 11:22:23 +0700

    Use actual small size logo

--- public/js/frontend/item/index.js
+++ public/js/frontend/item/index.js
@@ -64,7 +64,7 @@ let Item = {
                     overflow: 'visible',
                     template: function (t, e, i) {
                         return (
-                            '<img src="/img/LogoMMF.png" width="30px">'
+                            '<img src="/img/LogoMMF-30x13.png"'
                         );
                     }
                 },
