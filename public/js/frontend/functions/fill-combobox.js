7ec2b528440f79615a050803ef3bab73d181aec3
Audhy <audhy.vk01@gmail.com>
2018-10-22 16:31:24 +0700

    move function to separate js

--- public/js/frontend/functions/fill-combobox.js
+++ public/js/frontend/functions/fill-combobox.js
@@ -29,36 +29,6 @@ $(document).ready(function () {
     accountcode_category();
 });
 
-$(document).ready(function () {
-    units2 = function () {
-        $.ajax({
-            url: '/get-units/',
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                let angka5 = 1;
-
-                $('select[name="unit2"]').empty();
-
-                $.each(data, function (key, value) {
-                    if (angka5 == 1) {
-                        $('select[name="unit2"]').append(
-                            '<option> Select a Unit</option>'
-                        );
-
-                        angka5 = 0;
-                    }
-
-                    $('select[name="unit2"]').append(
-                        '<option value="' + key + '">' + value + '</option>'
-                    );
-                });
-            }
-        });
-    };
-
-    units2();
-});
 
 
 
--- public/js/frontend/functions/fill-combobox/unit.js
+++ public/js/frontend/functions/fill-combobox/unit.js
@@ -28,3 +28,34 @@ $(document).ready(function () {
 
     unit();
 });
+
+$(document).ready(function () {
+    units2 = function () {
+        $.ajax({
+            url: '/get-units/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let angka5 = 1;
+
+                $('select[name="unit2"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (angka5 == 1) {
+                        $('select[name="unit2"]').append(
+                            '<option> Select a Unit</option>'
+                        );
+
+                        angka5 = 0;
+                    }
+
+                    $('select[name="unit2"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    units2();
+});
