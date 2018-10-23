6f3e1ba38770b7b54d6fff211c3caeddcd1215a6
Audhy <audhy.vk01@gmail.com>
2018-10-23 15:11:23 +0700

    refactor numeric

--- public/js/frontend/functions/number.js
+++ public/js/frontend/functions/number.js
@@ -1,13 +1,24 @@
-$('input.number').keyup(function(event) {
+// $('input.number').keyup(function(event) {
 
-    // skip for arrow keys
-    if(event.which >= 37 && event.which <= 40) return;
+//     // skip for arrow keys
+//     if(event.which >= 37 && event.which <= 40) return;
 
-    // format number
-        $(this).val(function(index, value) {
-        return value
-        .replace(/\D/g, "")
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-        ;
-        });
+//     // format number
+//         $(this).val(function(index, value) {
+//         // var n = value;
+//         return value.toLocaleString(en);
+//         // var parts = value.toString().split(".");
+//         // parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//         // return parts.join(".");        // return value
+//         // .replace(/\D/g, "")
+//         // .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+//         // ;
+//         });
+// });
+
+$("input.number").on('keyup', function(evt){
+    if (evt.which != 110 ){//not a fullstop
+        var n = parseFloat($(this).val().replace(/\,/g,''),10);
+        $(this).val(n.toLocaleString());
+    }
 });
