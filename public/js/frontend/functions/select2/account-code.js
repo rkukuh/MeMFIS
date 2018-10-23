dcc224146b18fdb6502789446cdc2ec151375acb
Audhy <audhy.vk01@gmail.com>
2018-10-23 13:09:46 +0700

    disable select2 accountcode2

--- public/js/frontend/functions/select2/account-code.js
+++ public/js/frontend/functions/select2/account-code.js
@@ -2,8 +2,8 @@ let AccountCodeSelect2 = {
     init: function () {
         $('#accountcode, #accountcode_validate').select2({
             placeholder: 'Select a Account Code'
-        }), $('#accountcode2, #accountcode2_validate').select2({
-            placeholder: 'Select a Account Code'
+        // }), $('#accountcode2, #accountcode2_validate').select2({
+        //     placeholder: 'Select a Account Code'
         }), $('#accountcode3, #accountcode3_validate').select2({
             placeholder: 'Select a Account Code'
         });
