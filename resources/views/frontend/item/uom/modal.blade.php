9a713b6460c4ce6a9ad9a187c7a197c9cb88b785
Audhy <audhy.vk01@gmail.com>
2018-10-24 15:07:06 +0700

    rename qty to quantity

--- resources/views/frontend/item/uom/modal.blade.php
+++ resources/views/frontend/item/uom/modal.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group m-form__group row ">
                             <div class="col-sm-6 col-md-6 col-lg-6">
                                     <label class="form-control-label">
-                                        Qty @include('frontend.common.label.required')
+                                        Quantity @include('frontend.common.label.required')
                                     </label>
 
                                     @component('frontend.common.input.numeric')
