011895a8b4c497c0e9946656cd72a45ca3152d05
R. Kukuh <rkukuh@gmail.com>
2018-10-24 11:14:47 +0700

    Add columns for initial unit

--- database/migrations/2018_09_24_141009_create_items_table.php
+++ database/migrations/2018_09_24_141009_create_items_table.php
@@ -18,17 +18,24 @@ class CreateItemsTable extends Migration
             $table->char('uuid', 36)->unique();
             $table->string('code');
             $table->string('name');
+            $table->unsignedInteger('unit_id');
+            $table->double('unit_quantity', 8, 2);
             $table->string('barcode')->nullable();
-            $table->text('description')->nullable();
             $table->boolean('is_ppn')->nullable();
             $table->integer('ppn_amount')->nullable();
             $table->boolean('is_stock')->nullable();
             $table->unsignedInteger('account_code')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
             $table->foreign('account_code')
                   ->references('id')->on('journals')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');
+
+            $table->foreign('unit_id')
+                  ->references('id')->on('units')
                   ->onUpdate('cascade')
                   ->onDelete('restrict');
 
