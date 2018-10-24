70285e377d214e838b92f69feee4470aa083b78d
Audhy <audhy.vk01@gmail.com>
2018-10-24 15:05:34 +0700

    command unised

--- public/js/frontend/functions/fill-combobox/unit.js
+++ public/js/frontend/functions/fill-combobox/unit.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
-    unit = function () {
-        $.ajax({
-            url: '/get-units/',
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                let angka4 = 1;
+// $(document).ready(function () {
+//     unit = function () {
+//         $.ajax({
+//             url: '/get-units/',
+//             type: 'GET',
+//             dataType: 'json',
+//             success: function (data) {
+//                 let angka4 = 1;
 
-                $('select[name="unit"]').empty();
+//                 $('select[name="unit"]').empty();
 
-                $.each(data, function (key, value) {
-                    if (angka4 == 1) {
-                        $('select[name="unit"]').append(
-                            '<option> Select a Unit</option>'
-                        );
+//                 $.each(data, function (key, value) {
+//                     if (angka4 == 1) {
+//                         $('select[name="unit"]').append(
+//                             '<option> Select a Unit</option>'
+//                         );
 
-                        angka4 = 0;
-                    }
+//                         angka4 = 0;
+//                     }
 
-                    $('select[name="unit"]').append(
-                        '<option value="' + key + '">' + value + '</option>'
-                    );
-                });
-            }
-        });
-    };
+//                     $('select[name="unit"]').append(
+//                         '<option value="' + key + '">' + value + '</option>'
+//                     );
+//                 });
+//             }
+//         });
+//     };
 
-    unit();
-});
+//     unit();
+// });
 
 $(document).ready(function () {
     units2 = function () {
