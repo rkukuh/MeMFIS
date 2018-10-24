50e1aba44e3874bbda7fdb4188a2787f7719cf66
Audhy <audhy.vk01@gmail.com>
2018-10-24 15:06:52 +0700

    component edit select2

---
+++ resources/views/frontend/common/input/edit-select2.blade.php
@@ -0,0 +1,20 @@
+<select id="category" name="category"  class="form-control m-select2">
+        <option value="">
+            &mdash; Select Category &mdash;
+        </option>
+        @if($parameter1 or ''->isEmpty())
+        {{-- @foreach("{{$parameter2 or ''}}" as $category)
+            <option value="{{$category->id}}">
+                {{$category->name}}
+            </option>
+        @endforeach  --}}
+        {{-- @else
+        @foreach("{{$parameter2 or ''}}" as $aKey => $aSport)
+            @foreach("{{$parameter1 or ''}}" as $aItemKey => $aItemSport)
+                <option value="{{$aSport->id}}" @if($aSport->id == $aItemSport->id)selected="selected"@endif>{{$aSport->name}}</option>
+            @endforeach
+        @endforeach --}}
+        @endif
+</select>
+
+{{-- $category_items --}}
