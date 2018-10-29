f1818b74515df83991fae83eef0c0e5f21ea2280
R. Kukuh <rkukuh@gmail.com>
2018-10-29 14:23:12 +0700

    Refactor: Trim codes

--- public/js/frontend/item/create.js
+++ public/js/frontend/item/create.js
@@ -2,7 +2,7 @@ let Item = {
     init: function () {
         $(document).ready(function () {
             $('.btn-success').removeClass('add');
-            document.getElementById('isppn').onchange = function () {
+            document.getElementById('is_ppn').onchange = function () {
                 document.getElementById('ppn').disabled = !this.checked;
             };
         });
@@ -20,28 +20,26 @@ let Item = {
                 });
             }
 
-            if (document.getElementById("isstock").checked) {
-                isstock = 1;
+            if (document.getElementById("is_stock").checked) {
+                is_stock = 1;
             } else {
-                isstock = 0;
+                is_stock = 0;
             }
 
-            if (document.getElementById("isppn").checked) {
-                isppn = 1;
+            if (document.getElementById("is_ppn").checked) {
+                is_ppn = 1;
             } else {
-                isppn = 0;
+                is_ppn = 0;
             }
 
-            let accountcode = $('#accountcode').val();
             let code = $('input[name=code]').val();
             let name = $('input[name=name]').val();
-            let quantity = $('input[name=quantity]').val();
-
-            let description = $('#description').val();
             let category = $('#category').val();
+            let description = $('#description').val();
+            let quantity = $('input[name=quantity]').val();
             let unit = $('#unit').val();
-            let barcode = $('input[name=barcode]').val();
-            let ppn = $('input[name=ppn]').val();
+            let ppn_amount = $('input[name=ppn_amount]').val();
+            let account_code = $('#account_code').val();
 
             $.ajax({
                 headers: {
@@ -53,17 +51,14 @@ let Item = {
                     _token: $('input[name=_token]').val(),
                     code: code,
                     name: name,
-                    barcode: barcode,
+                    category: category,
+                    description: description,
                     quantity: quantity,
                     unit: unit,
-                    isstock: isstock,
-                    isppn: isppn,
-                    ppn: ppn,
-                    description: description,
-                    accountcode: accountcode,
-                    selectedtags: selectedtags,
-                    category: category
-
+                    is_stock: is_stock,
+                    is_ppn: is_ppn,
+                    ppn_amount: ppn_amount,
+                    account_code: account_code,
                 },
                 success: function (data) {
                     if (data.errors) {
@@ -73,6 +68,10 @@ let Item = {
 
                         if (data.errors.name) {
                             $('#name-error').html(data.errors.name[0]);
+                        }
+
+                        if (data.errors.category) {
+                            $('#category-error').html(data.errors.category[0]);
                         }
 
                         if (data.errors.quantity) {
@@ -83,201 +82,29 @@ let Item = {
                             $('#unit-error').html(data.errors.unit[0]);
                         }
 
-                        if (data.errors.category) {
-                            $('#category-error').html(data.errors.category[0]);
-                        }
-
                         document.getElementById('code').value = code;
                         document.getElementById('name').value = name;
                         document.getElementById('description').value = description;
-                        document.getElementById('barcode').value = barcode;
-                        document.getElementById('accountcode').value = accountcode;
+                        document.getElementById('account_code').value = account_code;
 
                     } else {
-                        $('input[type=file]').val("");
                         $('#code-error').html('');
                         $('#name-error').html('');
                         $('#description-error').html('');
-                        $('#barcode-error').html('');
+
                         document.getElementById('item-uom').removeAttribute('disabled');
                         document.getElementById('item-minmaxstock').removeAttribute('disabled');
+
                         $('#item-storage').html(code);
                         $('#item-unit').html(code);
-                        // item_reset();
+
                         toastr.success('Data berhasil disimpan.', 'Sukses', {
                             timeOut: 5000
                         });
+
                         update_item_button();
+
                         window.location.href = '/item/' + data.uuid + '/edit';
-                    }
-                }
-            });
-        });
-
-        $(function () {
-            let inputFile = $('#myInput');
-            let button = $('#myButton');
-            let buttonSubmit = $('#add-item');
-            let filesContainer = $('#myFiles');
-            let files = [];
-
-            inputFile.change(function () {
-                let newFiles = [];
-
-                for (let index = 0; index < inputFile[0].files.length; index++) {
-                    let file = inputFile[0].files[index];
-
-                    newFiles.push(file);
-                    files.push(file);
-                }
-
-                newFiles.forEach(file => {
-                    let fileElement = $(`<p>${file.name}</p>`);
-
-                    fileElement.data('fileData', file);
-                    filesContainer.append(fileElement);
-
-                    fileElement.click(function (event) {
-                        let fileElement = $(event.target);
-                        let indexToRemove = files.indexOf(fileElement.data('fileData'));
-
-                        fileElement.remove();
-                        files.splice(indexToRemove, 1);
-                    });
-                });
-            });
-
-            button.click(function () {
-                inputFile.click();
-            });
-
-            $('.footer').on('click', '.add-item', function () {
-                let formData = new FormData();
-                let code = $('input[name=code]').val();
-                formData.append('code', code);
-
-                let z = 0;
-                files.forEach(file => {
-                    formData.append('file' + z, file);
-                    z++;
-                });
-
-                $.ajax({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-
-                    url: '/post-photos',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
-                    type: 'POST',
-                    success: function (data) {
-                        if (data.uploaded == true) {
-                            //
-                        }
-                    },
-                    error: function (err) {
-                        alert(err);
-                    }
-                });
-            });
-        });
-
-        let simpan = $('.modal-footer').on('click', '.add-journal', function () {
-            $('#simpan').text('Simpan');
-
-            let type = $('#type').val();
-            let level = $('#level').val();
-            let registerForm = $('#CustomerForm');
-            let code = $('input[name=code-journal]').val();
-            let name = $('input[name=name-journal]').val();
-            let formData = registerForm.serialize();
-            let description = $('#description-journal').val();
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type: 'post',
-                url: '/journal',
-                data: {
-                    _token: $('input[name=_token]').val(),
-                    code: code,
-                    name: name,
-                    type: type,
-                    level: level,
-                    description: description
-                },
-                success: function (data) {
-                    if (data.errors) {
-                        if (data.errors.code) {
-                            $('#code-error').html(data.errors.code[0]);
-
-                            document.getElementById('code-journal').value = code;
-                            document.getElementById('name-journal').value = name;
-                            document.getElementById('type').value = type;
-                            document.getElementById('level').value = level;
-                            document.getElementById('description-journal').value = description;
-                        }
-
-                        if (data.errors.name) {
-                            $('#name-error').html(data.errors.name[0]);
-
-                            document.getElementById('code-journal').value = code;
-                            document.getElementById('name-journal').value = name;
-                            document.getElementById('type').value = type;
-                            document.getElementById('level').value = level;
-                            document.getElementById('description-journal').value = description;
-                        }
-
-                        if (data.errors.type) {
-                            $('#type-error').html(data.errors.type[0]);
-
-                            document.getElementById('code-journal').value = code;
-                            document.getElementById('name-journal').value = name;
-                            document.getElementById('type').value = type;
-                            document.getElementById('level').value = level;
-                            document.getElementById('description-journal').value = description;
-                        }
-
-                        if (data.errors.level) {
-                            $('#level-error').html(data.errors.level[0]);
-
-                            document.getElementById('code-journal').value = code;
-                            document.getElementById('name-journal').value = name;
-                            document.getElementById('type').value = type;
-                            document.getElementById('level').value = level;
-                            document.getElementById('description-journal').value = description;
-                        }
-
-                        if (data.errors.description) {
-                            $('#description-error').html(data.errors.description[0]);
-
-                            document.getElementById('code-journal').value = code;
-                            document.getElementById('name-journal').value = name;
-                            document.getElementById('type').value = type;
-                            document.getElementById('level').value = level;
-                            document.getElementById('description-journal').value = description;
-                        }
-                    } else {
-                        accountcode();
-                        $('#modal_journal').modal('hide');
-
-                        toastr.success('Data berhasil disimpan.', 'Sukses', {
-                            timeOut: 5000
-                        });
-
-                        $('#code-error').html('');
-                        $('#name-error').html('');
-                        $('#type-error').html('');
-                        $('#level-error').html('');
-                        $('#description-error').html('');
-
-                        let table = $('.m_datatable').mDatatable();
-
-                        table.originalDataSet = [];
-                        table.reload();
                     }
                 }
             });
