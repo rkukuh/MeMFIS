16c145c9c6eefeea35c9042d2755bcb9232625fd
R. Kukuh <rkukuh@gmail.com>
2018-10-29 14:24:42 +0700

    Delete unused js file

--- public/js/frontend/item/qty.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#qty').on('blur', function() {
-        let qty = $('#qty').val();
-        let convert = accounting.formatNumber(qty, 2, ".", ",");
-        document.getElementById('qty').value = convert;
-    });
-}); //end ready
