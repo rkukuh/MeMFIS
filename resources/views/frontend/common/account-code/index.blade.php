e191819481d0e7fdb1d7b9161c6d4be2622c7c61
R. Kukuh <rkukuh@gmail.com>
2018-11-01 12:22:37 +0700

    Update component

--- resources/views/frontend/common/account-code/index.blade.php
+++ resources/views/frontend/common/account-code/index.blade.php
@@ -1,12 +1,16 @@
-<div style="background-color: beige; padding: 15px;">
-    <div class="search-journal" id="search-journal">{{ $text or 'Search the account code' }}</div>
+<div style="background-color: {{ $background_color or 'beige' }};
+            padding: {{ $padding or '15' }}px;">
+
+    <div class="search-journal" id="search-journal">
+        {{ $text or 'Search account code' }}
+    </div>
 
     @component('frontend.common.account-code.button-create')
         @slot('size', 'sm')
         @slot('text', 'Search')
-        @slot('style', 'margin-top: -23px')
-        @slot('class', 'pull-right')
         @slot('icon', 'search')
+        @slot('class', 'pull-right')
+        @slot('style', 'margin-top: -23px')
         @slot('data_target', '#modal_account_code')
     @endcomponent
 </div>
@@ -14,6 +18,6 @@
 @include('frontend.common.account-code.modal')
 
 @push('footer-scripts')
-    <script src="{{ asset('assets/metronic/vendors/custom/datatables/datatables.bundle.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/metronic/vendors/custom/datatables/datatables.bundle.js') }}"></script>
     <script src="{{ asset('js/frontend/item/account-code.js') }}"></script>
 @endpush
