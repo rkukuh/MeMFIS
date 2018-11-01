920e84286f582788cb7dff285f7e46f249926843
R. Kukuh <rkukuh@gmail.com>
2018-11-01 20:34:07 +0700

    Refactor

--- resources/views/frontend/item/edit.blade.php
+++ resources/views/frontend/item/edit.blade.php
@@ -17,7 +17,7 @@
                         -
                     </li>
                     <li class="m-nav__item">
-                        <a href="{{route('frontend.item.index')}}" class="m-nav__link">
+                        <a href="{{ route('frontend.item.index') }}" class="m-nav__link">
                             <span class="m-nav__link-text">
                                 Material
                             </span>
