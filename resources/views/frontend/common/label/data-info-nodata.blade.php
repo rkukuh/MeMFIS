672900263670bfa0594ecdadd9e8e1a56d12804d
R. Kukuh <rkukuh@gmail.com>
2018-11-01 15:14:48 +0700

    Create new component

---
+++ resources/views/frontend/common/label/data-info-nodata.blade.php
@@ -0,0 +1,7 @@
+<{{ $element or 'div'}}
+    style="background-color: {{ $background_color or 'whitesmoke' }};
+    padding: {{ $padding or '5px 15px' }};">
+
+    {{ $text or '-' }}
+
+</{{ $element or 'div'}}>
