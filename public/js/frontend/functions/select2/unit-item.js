b387d83f8b8b743506c07bf657bc9fd97d5bab01
audhy <audhy.vk01@gmail.com>
2018-11-03 17:41:12 +0700

    make select2

---
+++ public/js/frontend/functions/select2/unit-item.js
@@ -0,0 +1,13 @@
+let UnitItemSelect2 = {
+    init: function () {
+        $('#unit, #unit_validate').select2({
+            placeholder: 'Select a Unit'
+        }), $('#unit_item, #unit_item_validate').select2({
+            placeholder: 'Select a Unit'
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    UnitItemSelect2.init();
+});
