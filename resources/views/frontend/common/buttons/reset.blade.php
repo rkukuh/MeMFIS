f20894c819b75d285dd5c20e75e826079a2a2bce
R. Kukuh <rkukuh@gmail.com>
2018-11-03 20:58:52 +0700

    Update components

--- resources/views/frontend/common/buttons/reset.blade.php
+++ resources/views/frontend/common/buttons/reset.blade.php
@@ -1,6 +1,6 @@
 <button
-    type="{{ $type or 'button' }}"
     id="{{ $id or '' }}"
+    type="{{ $type or 'reset' }}"
     name="{{ $name or 'reset' }}"
     value="{{ $value or '' }}"
     class="btn
@@ -8,10 +8,12 @@
            btn-{{ $size or 'md' }}
            {{ $class or '' }} reset"
     style="{{ $style or '' }}"
-    >
+    target="{{ $target or '' }}"
+>
 
     <span>
         <i class="fa {{ $icon or 'fa-undo' }}"></i>
+
         <span>{{ $text or 'Reset' }}</span>
     </span>
 </button>
--- resources/views/frontend/common/buttons/submit.blade.php
+++ resources/views/frontend/common/buttons/submit.blade.php
@@ -1,17 +1,19 @@
 <button
-    type="{{ $type or 'reset' }}"
     id="{{ $id or '' }}"
+    type="{{ $type or 'submit' }}"
     name="{{ $name or 'submit' }}"
+    value="{{ $value or '' }}"
     class="btn
            btn-{{ $color or 'success' }}
            btn-{{ $size or 'md' }}
                {{ $class or '' }} add"
     style="{{ $style or '' }}"
-    value="{{ $value or '' }}"
-    target="{{ $target or '' }}">
+    target="{{ $target or '' }}"
+>
 
     <span>
         <i class="fa {{ $icon or 'fa-save' }}"></i>
+
         <span>{{ $text or 'Save New' }}</span>
     </span>
 </button>
