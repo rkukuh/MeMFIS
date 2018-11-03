9adcb86eacb08ab6e58ee6385f7bc68ad136b4c4
R. Kukuh <rkukuh@gmail.com>
2018-11-03 15:28:59 +0700

    Refactor

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -1,11 +1,11 @@
-<button class="m-aside-left-close  m-aside-left-close--skin-dark" id="m_aside_left_close_btn">
+<button class="m-aside-left-close m-aside-left-close--skin-dark" id="m_aside_left_close_btn">
     <i class="la la-close"></i>
 </button>
 
-<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark">
-    <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark" m-menu-vertical="1"
+<div id="m_aside_left" class="m-grid__item	m-aside-left m-aside-left--skin-dark">
+    <div id="m_ver_menu" class="m-aside-menu m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark" m-menu-vertical="1"
          m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
-        <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow">
+        <ul class="m-menu__nav m-menu__nav--dropdown-submenu-arrow">
             <li class="m-menu__section">
                 <h4 class="m-menu__section-text">
                     Menu Utama
@@ -34,7 +34,7 @@
                     </span>
                 </a>
             </li>
-            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
+            <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
                 <a href="#" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
@@ -68,7 +68,7 @@
                     </ul>
                 </div>
             </li>
-            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
+            <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
                 <a href="#" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
@@ -142,7 +142,7 @@
                     </ul>
                 </div>
             </li>
-            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
+            <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
                 <a href="#" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
