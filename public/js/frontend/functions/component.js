6f4e8bbbd9102a61a5bf333a7049ae359a20e68b
R. Kukuh <rkukuh@gmail.com>
2018-11-03 20:56:30 +0700

    Use 'let' instead of 'var'

--- public/js/frontend/functions/component.js
+++ public/js/frontend/functions/component.js
@@ -1,38 +1,24 @@
-var update_button = function () {
+let update_button = function () {
     $('.btn-success').removeClass('add');
     $('.btn-success').addClass('update');
-    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save Changes</span></span>");
-}
+    $('.btn-success').html("<span><i class='fa fa-save'></i><span>Save Changes</span></span>");
+};
 
-var save_button = function () {
+let save_button = function () {
     if ($('.update')[0]) {
         $('.btn-success').removeClass('update');
         $('.btn-success').addClass('add');
-        $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save New</span></span>");
+        $('.btn-success').html("<span><i class='fa fa-save'></i><span>Save New</span></span>");
     }
-}
+};
 
-let close = $('.modal-footer').on('click', '.clse', function () {
+let close = $('.modal-footer').on('click', '.close', function () {
     save_button();
 });
 
-var update_item_button = function () {
+let update_item_button = function () {
     $('.btn-success').removeClass('add');
     $('.btn-success').addClass('edit-item');
     $('.btn-success').removeClass('add-item');
-    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save Changes</span></span>");
-}
-
-var save_general_license_button = function () {
-    $('.btn-success').removeClass('add');
-    $('.btn-success').removeClass('update-general-license');
-    $('.btn-success').addClass('add-general-license');
-    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save New</span></span>");
-}
-
-var update_general_license_button = function () {
-    $('.btn-success').removeClass('update');
-    $('.btn-success').removeClass('add-general-license');
-    $('.btn-success').addClass('update-general-license');
-    $('.btn-success').html("<span><i class='fa fa-save'></i><span> Save Changes</span></span>");
+    $('.btn-success').html("<span><i class='fa fa-save'></i><span>Save Changes</span></span>");
 }
