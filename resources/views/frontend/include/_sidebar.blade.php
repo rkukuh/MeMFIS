e605ee0f7b2dca3516497da966cf838e4b3d26eb
R. Kukuh <rkukuh@gmail.com>
2018-11-03 20:18:53 +0700

    Show sidebar menu for employee

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -68,7 +68,7 @@
                     </ul>
                 </div>
             </li>
-            <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover" style="display: none;">
+            <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
                 <a href="#" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
