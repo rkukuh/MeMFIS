2b48f9079b4ad59c570b1994eb8993bd2bbba3f0
R. Kukuh <rkukuh@gmail.com>
2018-11-05 16:57:17 +0700

    Refactor

--- resources/views/frontend/common/account-code/button-create.blade.php
+++ resources/views/frontend/common/account-code/button-create.blade.php
@@ -1,6 +1,6 @@
 <button
-    type="{{ $type or 'button' }}"
     id="{{ $id or '' }}"
+    type="{{ $type or 'button' }}"
     name="{{ $name or 'create' }}"
     value="{{ $value or '' }}"
     class="btn btn-primary
@@ -12,10 +12,11 @@
     data-toggle="{{ $data_toggle or 'modal' }}"
     data-target="{{ $data_target or '#' }}"
     {{ $attribute or '' }}
-    >
+>
 
     <span>
         <i class="la la-{{ $icon or 'plus'}}"></i>
+
         <span>{{ $text or 'Add' }}</span>
     </span>
 </button>
