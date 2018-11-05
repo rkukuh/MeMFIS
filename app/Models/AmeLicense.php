7104c8cd6a1f15833c5ea2517963242cad33d03d
R. Kukuh <rkukuh@gmail.com>
2018-11-05 20:57:51 +0700

    Define 1-M relationship between Employee-License (pivot) and AMEL

--- app/Models/AmeLicense.php
+++ app/Models/AmeLicense.php
@@ -10,4 +10,19 @@ class AmeLicense extends MemfisModel
         'aircraft_id',
         'type_id',
     ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-to-Many: An AME license may have zero or many aircraft rating.
+     *
+     * This function will retrieve the header of an AME license.
+     * See: Employee License's ame_licenses() method for the inverse
+     *
+     * @return mixed
+     */
+    public function header()
+    {
+        return $this->belongsTo(EmployeeLicense::class, 'employee_license_id');
+    }
 }
