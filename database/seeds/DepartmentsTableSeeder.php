2177af39243a72295db54bb0b145fd31d2e0f10b
R. Kukuh <rkukuh@gmail.com>
2018-11-05 12:37:11 +0700

    Populate department

--- database/seeds/DepartmentsTableSeeder.php
+++ database/seeds/DepartmentsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Department;
 use Illuminate\Database\Seeder;
 
 class DepartmentsTableSeeder extends Seeder
@@ -11,6 +12,58 @@ class DepartmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /** PRODUCTION */
+
+        $production = Department::create([
+            'code' => 'production',
+            'name' => 'Production',
+            'parent_id' => null,
+        ]);
+
+        Department::create([
+            'code' => 'hm',
+            'name' => 'Heavy Maintenance',
+            'parent_id' => $production->id,
+        ]);
+
+        /** PRODUCTION > WORKSHOP */
+
+        $workshop = Department::create([
+            'code' => 'workshop',
+            'name' => 'Workshop',
+            'parent_id' => $production->id,
+        ]);
+
+        Department::create([
+            'code' => 'instrument',
+            'name' => 'Instrument Shop',
+            'parent_id' => $workshop->id,
+        ]);
+
+        // TODO: Continue input workshops. See: Landing page.
+
+        /** SUPPORTING */
+
+        $supporting = Department::create([
+            'code' => 'suporting',
+            'name' => 'Suporting',
+            'parent_id' => null,
+        ]);
+
+        /** FINANCE */
+
+        $finance = Department::create([
+            'code' => 'finance',
+            'name' => 'Finance',
+            'parent_id' => null,
+        ]);
+
+        /** ACCOUNTING */
+
+        $accounting = Department::create([
+            'code' => 'accounting',
+            'name' => 'Accounting',
+            'parent_id' => null,
+        ]);
     }
 }
