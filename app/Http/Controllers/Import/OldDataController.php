3e80240044495006097479136c9d4cca33d0749c
R. Kukuh <rkukuh@gmail.com>
2018-11-06 15:32:25 +0700

    Create import controller for work area

---
+++ app/Http/Controllers/Import/OldDataController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Import;
+
+use App\Imports\WorkAreasImport;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Http\Controllers\Controller;
+
+class OldDataController extends Controller
+{
+    protected $import_directory = 'import/';
+
+    public function workAreas()
+    {
+        Excel::import(new WorkAreasImport, $this->import_directory . 'work-areas.xlsx');
+
+        return "Imported.";
+    }
+}
