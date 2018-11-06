e3ba16a8848c241ba3357913cf5a79e19775bdd7
R. Kukuh <rkukuh@gmail.com>
2018-11-06 15:32:13 +0700

    Create import file for work area

---
+++ app/Imports/WorkAreasImport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Type;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class WorkAreasImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Type([
+            'code'  => str_slug($row[0]),
+            'name'  => strtoupper($row[0]),
+            'of'    => 'work-area',
+        ]);
+    }
+}
