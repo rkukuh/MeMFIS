967bdce1573e6398213a1c3be6b27bceaec84a02
audhy <audhy.vk01@gmail.com>
2018-11-06 14:33:18 +0700

    revert

---
+++ public/js/frontend/functions/action-buttons.js
@@ -0,0 +1,5 @@
+let save_changes_button = function () {
+    $('.btn-success').removeClass('add-category');
+    $('.btn-success').addClass('edit-category');
+    $('.btn-success').html("<span><i class='fa fa-save'></i><span>Save Changes</span></span>");
+}
