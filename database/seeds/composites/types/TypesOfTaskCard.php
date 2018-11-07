563e722f39a48e48b8400d6ad61b357010f92ead
R. Kukuh <rkukuh@gmail.com>
2018-11-07 10:47:48 +0700

    Create type seeder for Task Card

---
+++ database/seeds/composites/types/TypesOfTaskCard.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Models\Type;
+use Illuminate\Database\Seeder;
+
+class TypesOfTaskCard extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Type::create([
+            'code' => 'inspection',
+            'name' => 'Inspection',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'check',
+            'name' => 'Check',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'visual-check',
+            'name' => 'Visual Check',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'service',
+            'name' => 'Service',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'repair',
+            'name' => 'Repair',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'lubrication',
+            'name' => 'Lubrication',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'replacement',
+            'name' => 'Replacement',
+            'of'  => 'taskcard',
+        ]);
+
+        Type::create([
+            'code' => 'overhaul',
+            'name' => 'Overhaul',
+            'of'  => 'taskcard',
+        ]);
+    }
+}
