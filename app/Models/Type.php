a3c818853664c5d1a95040c18799d23cf44e8b10
R. Kukuh <rkukuh@gmail.com>
2018-11-07 11:07:12 +0700

    Define local scope for maintenance cycle type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -105,6 +105,17 @@ class Type extends MemfisModel
     }
 
     /**
+     * Scope a query to only include type of A/C maintenance cycle.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfMaintenanceCycle(Builder $query)
+    {
+        return $query->where('of', 'maintenance-cycle');
+    }
+
+    /**
      * Scope a query to only include type of phone.
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
