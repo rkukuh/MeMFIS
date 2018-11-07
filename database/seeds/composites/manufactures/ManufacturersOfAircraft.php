5e286aa3895da6c69809a7aa48eda3e329ae4350
R. Kukuh <rkukuh@gmail.com>
2018-11-07 15:15:54 +0700

    Create initial data seeder

---
+++ database/seeds/composites/manufactures/ManufacturersOfAircraft.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Models\Manufacturer;
+use Illuminate\Database\Seeder;
+
+class ManufacturersOfAircraft extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Manufacturer::create([
+            'code' => 'boeing',
+            'name' => 'Boeing'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'airbus',
+            'name' => 'Airbus'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'fokker',
+            'name' => 'Fokker'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'sukhoi',
+            'name' => 'Sukhoi'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'cessna',
+            'name' => 'Cessna'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'xian',
+            'name' => 'Xian'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'caravan',
+            'name' => 'Caravan'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'atr',
+            'name' => 'ATR (Aerei da Trasporto Regionale)'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'ptdi',
+            'name' => 'PT. Dirgantara Indonesia'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'dhc',
+            'name' => 'DHC (de Havilland Aircraft of Canada Ltd.)'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'casa',
+            'name' => 'CASA (Construcciones Aeronáuticas SA)'
+        ]);
+
+        Manufacturer::create([
+            'code' => 'bombardier',
+            'name' => 'Bombardier Aerospace'
+        ]);
+    }
+}
