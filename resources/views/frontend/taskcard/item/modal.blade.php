5e5971e69bf05eead877d5133367cb4d6813a284
audhy <audhy.vk01@gmail.com>
2018-11-08 14:39:39 +0700

    modal item on taskcard

---
+++ resources/views/frontend/taskcard/item/modal.blade.php
@@ -0,0 +1,135 @@
+{{-- <div class="modal fade" id="modal_item"  role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                @include('frontend.common.label.create-new')
+
+                <h5 class="modal-title" id="TitleModalItem">
+                    Item
+
+                    <small id="item-storage" class="m--font-focus"></small>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="ItemForm">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                    <div class="m-portlet__body">
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Item @include('frontend.common.label.required')
+                                </label>
+
+                                @component('frontend.common.input.select')
+                                    @slot('id', 'item')
+                                    @slot('text', 'Item')
+                                    @slot('name', 'item')
+                                    @slot('style', 'width:100%')
+                                    @slot('id_error', 'item')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Quantity @include('frontend.common.label.required')
+                                </label>
+
+                                @component('frontend.common.input.numeric')
+                                    @slot('text', 'Quantity')
+                                    @slot('name', 'quantity')
+                                    @slot('id', 'quantity')
+                                    @slot('id_error', 'quantity')
+                                @endcomponent
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="flex">
+                            <div class="action-buttons">
+                                    @component('frontend.common.buttons.submit')
+                                        @slot('class', 'add-item')
+                                        @slot('type', 'button')
+                                    @endcomponent
+                                @include('frontend.common.buttons.reset')
+                                @include('frontend.common.buttons.close')
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+<div class="modal fade" id="modal_item"  role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                @include('frontend.common.label.create-new')
+
+                <h5 class="modal-title" id="TitleModalItem">
+                    Item
+
+                    <small id="item-storage" class="m--font-focus"></small>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                    <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="ItemForm">
+                        <input type="hidden" class="form-control form-control-danger m-input" name="id" id="id">
+                        <div class="m-portlet__body">
+                            <div class="form-group m-form__group row ">
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <label class="form-control-label">
+                                        Item @include('frontend.common.label.required')
+                                    </label>
+    
+                                    @component('frontend.common.input.select')
+                                        @slot('id', 'item')
+                                        @slot('text', 'Item')
+                                        @slot('name', 'item')
+                                        @slot('style', 'width:100%')
+                                        @slot('id_error', 'item')
+                                    @endcomponent
+                                </div>
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <label class="form-control-label">
+                                        Quantity @include('frontend.common.label.required')
+                                    </label>
+    
+                                    @component('frontend.common.input.numeric')
+                                        @slot('text', 'Quantity')
+                                        @slot('name', 'quantity')
+                                        @slot('id', 'quantity')
+                                        @slot('id_error', 'quantity')
+                                    @endcomponent
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="flex">
+                                <div class="action-buttons">
+                                        @component('frontend.common.buttons.submit')
+                                            @slot('class', 'add-item')
+                                            @slot('type', 'button')
+                                        @endcomponent
+                                    @include('frontend.common.buttons.reset')
+                                    @include('frontend.common.buttons.close')
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+    
+                </div>
+        </div>
+    </div>
+</div>
