2a62d3fa3c7d74667162eb38cc9ceea13184f17b
audhy <audhy.vk01@gmail.com>
2018-11-08 13:29:07 +0700

    taskcard combobox js

---
+++ public/js/frontend/functions/fill-combobox/taskcard.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    Taskcard = function () {
+        $.ajax({
+            url: '/get-categories-item/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let index = 1;
+
+                $('select[name="taskcard"]').empty();
+
+                $.each(data, function (key, value) {
+                    if (index == 1) {
+                        $('select[name="taskcard"]').append(
+                            '<option> Select a Repeat Type</option>'
+                        );
+
+                        index = 0;
+                    }
+
+                    $('select[name="taskcard"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    Taskcard();
+});
