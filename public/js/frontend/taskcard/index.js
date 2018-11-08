bf2731581a865a668ac979845c4feaa8445e6edd
audhy <audhy.vk01@gmail.com>
2018-11-08 13:31:23 +0700

    taskcard index js

--- public/js/frontend/taskcard/index.js
+++ public/js/frontend/taskcard/index.js
@@ -42,22 +42,38 @@ let TaskCard = {
                     }
                 }
             },
-            columns: [{
-                    field: 'id',
-                    title: '#',
-                    sortable: !1,
-                    width: 40
-                },
+            columns: [
                 {
-                    field: 'name',
-                    title: 'Name',
+                    field: 'title',
+                    title: 'Tittle',
                     sortable: 'asc',
                     filterable: !1,
                     width: 150
                 },
                 {
-                    field: 'description',
-                    title: 'Description',
+                    field: 'type_id',
+                    title: 'Type',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'work_area',
+                    title: 'Area',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'zone',
+                    title: 'Zone',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'rii',
+                    title: 'Rii',
                     sortable: 'asc',
                     filterable: !1,
                     width: 150,
@@ -79,146 +95,6 @@ let TaskCard = {
             ]
         });
 
-        let simpan = $('.modal-footer').on('click', '.add', function () {
-            $('#name-error').html('');
-            $('#simpan').text('Simpan');
-
-            let registerForm = $('#CustomerForm');
-            let name = $('input[name=name]').val();
-            let formData = registerForm.serialize();
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type: 'post',
-                url: '/category',
-                data: {
-                    _token: $('input[name=_token]').val(),
-                    name: name
-                },
-                success: function (data) {
-                    if (data.errors) {
-                        if (data.errors.name) {
-                            $('#name-error').html(data.errors.name[0]);
-
-                            document.getElementById('name').value = name;
-                        }
-                    } else {
-                        $('#modal_customer').modal('hide');
-
-                        toastr.success('Data berhasil disimpan.', 'Sukses', {
-                            timeOut: 5000
-                        });
-
-                        let table = $('.m_datatable').mDatatable();
-
-                        table.originalDataSet = [];
-                        table.reload();
-                    }
-                }
-            });
-        });
-
-        let edit = $('.m_datatable').on('click', '.edit', function () {
-            $('#button').show();
-            $('#simpan').text('Perbarui');
-
-            let triggerid = $(this).data('id');
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type: 'get',
-                url: '/category/' + triggerid + '/edit',
-                success: function (data) {
-                    document.getElementById('id').value = data.id;
-                    document.getElementById('name').value = data.name;
-
-                    $('.btn-success').addClass('update');
-                    $('.btn-success').removeClass('add');
-                },
-                error: function (jqXhr, json, errorThrown) {
-                    let errorsHtml = '';
-                    let errors = jqXhr.responseJSON;
-
-                    $.each(errors.errors, function (index, value) {
-                        $('#kategori-error').html(value);
-                    });
-                }
-            });
-        });
-
-        let update = $('.modal-footer').on('click', '.update', function () {
-            $('#button').show();
-            $('#name-error').html('');
-            $('#simpan').text('Perbarui');
-
-            let name = $('input[name=name]').val();
-            let triggerid = $('input[name=id]').val();
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type: 'put',
-                url: '/category/' + triggerid,
-                data: {
-                    _token: $('input[name=_token]').val(),
-                    name: name
-                },
-                success: function (data) {
-                    if (data.errors) {
-                        if (data.errors.name) {
-                            $('#name-error').html(data.errors.name[0]);
-
-                            document.getElementById('name').value = name;
-                        }
-                    } else {
-                        $('#modal_customer').modal('hide');
-
-                        toastr.success('Data berhasil disimpan.', 'Sukses', {
-                            timeOut: 5000
-                        });
-
-                        let table = $('.m_datatable').mDatatable();
-
-                        table.originalDataSet = [];
-                        table.reload();
-                    }
-                }
-            });
-        });
-
-        let show = $('.m_datatable').on('click', '.show', function () {
-            $('#button').hide();
-            $('#simpan').text('Perbarui');
-
-            let triggerid = $(this).data('id');
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type: 'get',
-                url: '/category/' + triggerid,
-                success: function (data) {
-                    document.getElementById('TitleModalCustomer').innerHTML = 'Detail Customer #ID-' + triggerid;
-                    document.getElementById('name').value = data.name;
-                    document.getElementById('name').readOnly = true;
-                },
-                error: function (jqXhr, json, errorThrown) {
-                    let errorsHtml = '';
-                    let errors = jqXhr.responseJSON;
-
-                    $.each(errors.errors, function (index, value) {
-                        $('#kategori-error').html(value);
-                    });
-                }
-            });
-        });
-
         let remove = $('.m_datatable').on('click', '.delete', function () {
             let triggerid = $(this).data('id');
 
@@ -238,7 +114,7 @@ let TaskCard = {
                             )
                         },
                         type: 'DELETE',
-                        url: '/category/' + triggerid + '',
+                        url: '/taskcard/' + triggerid + '',
                         success: function (data) {
                             toastr.success(
                                 'Data berhasil dihapus.',
@@ -276,11 +152,6 @@ let TaskCard = {
             });
         });
 
-        $('#modal_customer').on('hidden.bs.modal', function (e) {
-            $(this).find('#CustomerForm')[0].reset();
-
-            $('#name-error').html('');
-        });
     }
 };
 
