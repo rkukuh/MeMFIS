f9467d5707175262e09bcbafd7e854db2d9a0bb7
audhy <audhy.vk01@gmail.com>
2018-11-08 20:40:22 +0700

    FIX typo

--- app/Http/Requests/Frontend/ItemStorageUpdate.php
+++ app/Http/Requests/Frontend/ItemStorageUpdate.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class ItemStoragekUpdate extends FormRequest
+class ItemStorageUpdate extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
