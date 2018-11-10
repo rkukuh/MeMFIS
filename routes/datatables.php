518fe28c1eb56dd338b64b523fe4ac0c5a3e3667
R. Kukuh <rkukuh@gmail.com>
2018-11-10 17:11:29 +0700

    Define dedicated route for datatables

--- routes/datatables.php
+++ routes/datatables.php
@@ -14,7 +14,17 @@ Route::name('datatables.')->group(function () {
         Route::get('/item-unit/{item}','ItemUnitDatatables@index')->name('item-unit.index');
         Route::get('/item-storage/{item}','ItemStorageDatatables@index')->name('item-storage.index');
 
-        Route::get('/travel-request/{employee}','TravelRequestDatatables@index')->name('employee-travel-request.index');
+
+        /** EMPLOYEE  */
+
+        Route::name('employee.')->group(function () {
+            Route::group(['prefix' => 'employee'], function () {
+
+                Route::get('/history/{employee}','EmployeeHistoryDatatables@index')->name('history.index');
+                Route::get('/travel-request/{employee}','TravelRequestDatatables@index')->name('travel-request.index');
+
+            });
+        });
 
     });
 
