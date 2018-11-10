435f6fe7bd031cd5ebba17b4f06ab820c6a899c9
R. Kukuh <rkukuh@gmail.com>
2018-11-10 19:03:02 +0700

    Implement new routes name

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -90,7 +90,7 @@
                             </a>
                         </li>
                         <li class="m-menu__item" aria-haspopup="true">
-                            <a href="{{ route('frontend.employee-history.index') }}" class="m-menu__link">
+                            <a href="{{ route('frontend.employee.history.index') }}" class="m-menu__link">
                                 <i class="m-menu__link-bullet m-menu__link-bullet--dot">
                                     <span></span>
                                 </i>
@@ -110,7 +110,7 @@
                             </a>
                         </li>
                         <li class="m-menu__item" aria-haspopup="true">
-                            <a href="{{ route('frontend.travel-request.index') }}" class="m-menu__link">
+                            <a href="{{ route('frontend.employee.travel-request.index') }}" class="m-menu__link">
                                 <i class="m-menu__link-bullet m-menu__link-bullet--dot">
                                     <span></span>
                                 </i>
