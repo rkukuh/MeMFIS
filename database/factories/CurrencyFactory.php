410270a1cb976dfa40f42eb611f67f7f90488386
R. Kukuh <rkukuh@gmail.com>
2018-11-13 13:27:45 +0700

    Define factory for Currency

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -5,8 +5,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Currency::class, function (Faker $faker) {
 
+    $name = 'Currency Dummy #' . $faker->unixTime();
+
     return [
-        //
+        'code'   => str_slug($name),
+        'name'   => $name,
+        'symbol' => 'CUD',
     ];
 
 });
