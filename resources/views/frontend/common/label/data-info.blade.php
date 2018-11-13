3a5c13c591309c6df657dc14508118b0666254cd
audhy <audhy.vk01@gmail.com>
2018-11-13 14:08:06 +0700

    refactor component

--- resources/views/frontend/common/label/data-info.blade.php
+++ resources/views/frontend/common/label/data-info.blade.php
@@ -1,4 +1,4 @@
-<{{ $element or 'div'}}
+<{{ $element or 'div'}} id="{{ $id or '' }}" name="{{ $name or '' }}"
     style="background-color: {{ $background_color or 'beige' }};
     padding: {{ $padding or '15' }}px;"
     class="{{ $class or '' }}">
