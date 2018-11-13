5683c372ce1664e4492fd96f90fe62e6e0bb209f
audhy <audhy.vk01@gmail.com>
2018-11-13 14:08:29 +0700

    refactor component datapicker

--- resources/views/frontend/common/input/datepicker.blade.php
+++ resources/views/frontend/common/input/datepicker.blade.php
@@ -1,13 +1,20 @@
-<input
-    type="text"
-    id="{{ $id or 'm_datepicker_1' }}"
-    name="{{ $name or '' }}"
-    class="form-control
-           {{$class or ''}}"
-    style="{{$style or ''}}"
-    placeholder="{{ $placeholder or '' }}"
-    readonly
->
+<div class="input-group date">
+    <input
+        type="text"
+        id="{{ $id or 'm_datepicker_1' }}"
+        name="{{ $name or '' }}"
+        class="form-control
+            {{$class or ''}}"
+        style="{{$style or ''}}"
+        placeholder="{{ $placeholder or '' }}"
+        readonly
+    >
+    <div class="input-group-append">
+        <span class="input-group-text">
+        <i class="la la-calendar glyphicon-th"></i>
+        </span>
+    </div>
+</div>
 
 <span class="m-form__help">
     {{ $help_text or '' }}
