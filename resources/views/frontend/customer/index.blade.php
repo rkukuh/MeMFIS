51a651523a907ec30c33f9b6ae3e22029e5cc40c
audhy <audhy.vk01@gmail.com>
2018-11-14 20:37:27 +0700

    FIX :: typo

--- resources/views/frontend/customer/index.blade.php
+++ resources/views/frontend/customer/index.blade.php
@@ -63,7 +63,7 @@
                                     </div>
                                     <div class="col-xl-4 order-1 order-xl-2 m--align-right">
                                             @component('frontend.common.buttons.create')
-                                                @slot('text', 'Material')
+                                                @slot('text', 'Customer')
                                                 @slot('href', route('frontend.customer.create') )
                                             @endcomponent
 
