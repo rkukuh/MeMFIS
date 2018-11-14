d3ade5fc0af12c6f91d634410760631b7b837086
R. Kukuh <rkukuh@gmail.com>
2018-11-14 12:32:15 +0700

    Define factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,12 +1,30 @@
 <?php
 
+use Carbon\Carbon;
+use App\Models\Type;
+use App\Models\Journal;
 use App\Models\Customer;
 use Faker\Generator as Faker;
 
 $factory->define(Customer::class, function (Faker $faker) {
 
+    $name = 'Customer Dummy #' . $faker->unixTime();
+
     return [
-        //
+        'code' => str_slug($name),
+        'name' => $name,
+        'payment_term' => $faker->randomElement([
+            null,
+            Type::ofPaymentTerm()->get()->random()->id
+        ]),
+        'banned_at' => $faker->randomElement([
+            null,
+            Carbon::now()
+        ]),
+        'account_code' => $faker->randomElement([
+            null,
+            Journal::get()->random()->id
+        ])
     ];
 
 });
