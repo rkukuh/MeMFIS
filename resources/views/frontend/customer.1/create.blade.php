0ae3856f7357aebabe9fe0411fc2c91148e8e7e6
audhy <audhy.vk01@gmail.com>
2018-11-14 20:36:55 +0700

    test

---
+++ resources/views/frontend/customer.1/create.blade.php
@@ -0,0 +1,107 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader hidden">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    HR Form
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="{{ route('frontend.hr-form.index') }}" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                HR Form
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+<div class="m-content">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="m-portlet m-portlet--tabs">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-tools">
+                            <ul class="nav nav-tabs  m-tabs-line m-tabs-line--primary" role="tablist">
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link active" data-toggle="tab" href="#general" role="tab">
+                                            <i class="la la-book"></i>
+                                            General
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#address" role="tab">
+                                            <i class="la la-map"></i>
+                                            Address
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#contact" role="tab">
+                                            <i class="la la-phone"></i>
+                                            Contact
+                                        </a>
+                                    </li>
+                                </ul>
+                    </div>
+                </div>
+                </div>
+                <div class="m-portlet__body">
+                    <div class="tab-content">
+                            <div class="tab-pane active" id="general" role="tabpanel">
+                                    @include('frontend.customer.tabs-create.general')
+                                </div>
+                                <div class="tab-pane" id="address" role="tabpanel">
+                                    @include('frontend.customer.tabs-create.address')
+                                </div>
+                                <div class="tab-pane" id="contact" role="tabpanel">
+                                    @include('frontend.customer.tabs-create.contact')
+                                </div>
+                    </div>
+                    <div class="form-group m-form__group row">
+                            <div class="col-sm-12 col-md-12 col-lg-12 footer">
+                                <div class="flex">
+                                    <div class="action-buttons">
+                                        @component('frontend.common.buttons.submit')
+                                            @slot('type','button')
+                                            @slot('id', 'add-customer')
+                                            @slot('class', 'add-customer')
+                                        @endcomponent
+
+                                        @include('frontend.common.buttons.reset')
+
+                                        @component('frontend.common.buttons.back')
+                                            @slot('href', route('frontend.item.index'))
+                                        @endcomponent
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
+    <script src="{{ asset('js/frontend/employee/hr-form-management.js')}}"></script>
+
+    <script src="{{ asset('js/frontend/functions/fill-combobox.js')}}"></script>
+    <script src="{{ asset('js/frontend/functions/select2.js')}}"></script>
+@endpush
