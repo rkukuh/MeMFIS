d4b8000b04465b338119bac7ab883cfd6bcb0de4
R. Kukuh <rkukuh@gmail.com>
2018-11-14 12:40:56 +0700

    Define fillable columns

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,5 +6,12 @@ use App\MemfisModel;
 
 class Address extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'type_id',
+        'addresss',
+        'latitude',
+        'longitude',
+        'addressable_id',
+        'addressable_type',
+    ];
 }
