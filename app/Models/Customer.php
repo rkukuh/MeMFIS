c3eb36f588e2698f452f95311442362047129ef0
R. Kukuh <rkukuh@gmail.com>
2018-11-14 12:20:58 +0700

    Define fillable columns

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,25 +7,12 @@ use App\MemfisModel;
 class Customer extends MemfisModel
 {
     protected $fillable = [
-        'branch_id',
         'code',
         'name',
-        'email',
-        'phone1',
-        'phone2',
-        'fax',
-        'contactpersoon',
-        'IDType',
-        'IDNumber',
-        'NPWE',
-        'NPWPAdress',
-        'Leveling',
-        'xType',
-        'xType1',
-        'Type2',
-        'xType3',
-        'active',
-        'ToP',
-        'AccountCode',
+        'payment_term',
+        'banned_at',
+        'account_code',
     ];
+
+    protected $dates = ['banned_at'];
 }
