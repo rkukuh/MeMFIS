8cde74fe29eff80914d1edcd32fcf35d81947aeb
R. Kukuh <rkukuh@gmail.com>
2018-11-14 13:15:23 +0700

    Define factory for Address

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Models\Type;
 use App\Models\Address;
 use Faker\Generator as Faker;
 
 $factory->define(Address::class, function (Faker $faker) {
 
     return [
-        //
+        'type_id' => Type::ofAddress()->get()->random()->id,
+        'address' => $faker->address,
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude,
     ];
 
 });
