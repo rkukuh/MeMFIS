fe41b5ed7448e55f03666a085f6d59d9cbc1e8bf
R. Kukuh <rkukuh@gmail.com>
2018-11-14 13:59:23 +0700

    Define factory

--- database/factories/EmailFactory.php
+++ database/factories/EmailFactory.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Models\Type;
 use App\Models\Email;
 use Faker\Generator as Faker;
 
 $factory->define(Email::class, function (Faker $faker) {
 
     return [
-        //
+        'address' => $faker->companyEmail,
+        'type_id' => Type::ofEmail()->get()->random()->id,
+        'is_primary' => $faker->boolean,
     ];
 
 });
