e9ceaed8fb86b5fc5708c2785c78e00dc99971d5
audhy <audhy.vk01@gmail.com>
2018-11-15 09:59:00 +0700

    data binding phone datatable for customer

--- app/Http/Controllers/Datatables/Customer/PhoneDatatables.php
+++ app/Http/Controllers/Datatables/Customer/PhoneDatatables.php
@@ -12,13 +12,12 @@ class PhoneDatatables extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  \App\Models\Customer  $customer
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Customer $customer)
     {
-        $customers = Customer::All();
-
-        $data = $alldata = json_decode($customers);
+        $data = $alldata = json_decode($customer->phones);
 
         $datatable = array_merge(['pagination' => [], 'sort' => [], 'query' => []], $_REQUEST);
 
