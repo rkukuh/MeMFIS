b3e606aad00c89260e2cb1c08402489af36457ce
audhy <audhy.vk01@gmail.com>
2018-11-15 12:57:39 +0700

    try

--- resources/views/frontend/customer/edit.blade.php
+++ resources/views/frontend/customer/edit.blade.php
@@ -54,6 +54,43 @@
                                         <legend class="w-auto">Identifier</legend>
 
                                         <div class="form-group m-form__group row">
+                                                <div class="col-sm-12 col-md-12 col-lg-12">    
+
+                                        Repeater 1 
+                                        <div class='repeater'>
+                                          <div data-repeater-list="group-a">
+                                            <div data-repeater-item>
+                                                    <div class="col-sm-6 col-md-6 col-lg-6">    
+
+                                              {{-- <input type="text" name="text-input" /> --}}
+                                              @component('frontend.common.input.text')
+                                                @slot('name', 'text-input')
+                                                @slot('text', 'Phone')
+                                              @endcomponent
+                                                    </div>
+                                              <input data-repeater-delete class="btn-sm btn btn-danger" type="button" value="Delete"/>
+                                            </div>
+                                          </div>
+                                          <input data-repeater-create type="button" class="btn btn btn-sm btn-brand m-btn m-btn--icon m-btn--pill m-btn--wide" value="Add"/>
+                                                                                           </div>
+                                        
+                                        {{-- Repeater 2
+                                          <div class='repeater'>
+                                      <!-- Make sure the repeater list value is different from the first repeater  -->
+                                          <div data-repeater-list="group-b">
+                                            <div data-repeater-item>
+                                              <input type="text" name="text-input" value="G"/>
+                                              <input data-repeater-delete type="button" value="Delete"/>
+                                            </div>
+                                      
+                                          </div>
+                                          <input data-repeater-create type="button" value="Add"/>
+                                                                                           </div> --}}
+
+                                                </div>
+                                        </div>
+
+                                        <div class="form-group m-form__group row">
                                             <div class="col-sm-12 col-md-12 col-lg-12">
                                                 <label class="form-control-label">
                                                     Code @include('frontend.common.label.required')
@@ -178,7 +215,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="m-portlet">
+                {{-- <div class="m-portlet">
                     <div class="m-portlet__head">
                         <div class="m-portlet__head-caption">
                             <div class="m-portlet__head-title">
@@ -214,8 +251,8 @@
                             <div class="customer_phone_datatable" id="customer_phone_datatable"></div>
                         </div>
                     </div>
-                </div>
-                <div class="m-portlet">
+                </div> --}}
+                {{-- <div class="m-portlet">
                     <div class="m-portlet__head">
                         <div class="m-portlet__head-caption">
                             <div class="m-portlet__head-title">
@@ -251,8 +288,8 @@
                             <div class="customer_email_datatable" id="customer_email_datatable"></div>
                         </div>
                     </div>
-                </div>
-                <div class="m-portlet">
+                </div> --}}
+                {{-- <div class="m-portlet">
                     <div class="m-portlet__head">
                         <div class="m-portlet__head-caption">
                             <div class="m-portlet__head-title">
@@ -288,7 +325,7 @@
                             <div class="customer_fax_datatable" id="customer_fax_datatable"></div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
@@ -311,7 +348,59 @@
 @endpush
 
 @push('footer-scripts')
-    <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
+<script>
+        $(document).ready(function () {
+            $('.repeater').repeater({
+                // (Optional)
+                // start with an empty list of repeaters. Set your first (and only)
+                // "data-repeater-item" with style="display:none;" and pass the
+                // following configuration flag
+                initEmpty: false,
+                // (Optional)
+                // "defaultValues" sets the values of added items.  The keys of
+                // defaultValues refer to the value of the input's name attribute.
+                // If a default value is not specified for an input, then it will
+                // have its value cleared.
+                defaultValues: {
+                    'text-input': 'foo'
+                },
+                // (Optional)
+                // "show" is called just after an item is added.  The item is hidden
+                // at this point.  If a show callback is not given the item will
+                // have $(this).show() called on it.
+                show: function () {
+                    $(this).slideDown();
+                },
+                // (Optional)
+                // "hide" is called when a user clicks on a data-repeater-delete
+                // element.  The item is still visible.  "hide" is passed a function
+                // as its first argument which will properly remove the item.
+                // "hide" allows for a confirmation step, to send a delete request
+                // to the server, etc.  If a hide callback is not given the item
+                // will be deleted.
+                hide: function (deleteElement) {
+                //     if(confirm('Are you sure you want to delete this element?')) {
+                        $(this).slideUp(deleteElement);
+                //     }
+                },
+                // (Optional)
+                // You can use this if you need to manually re-index the list
+                // for example if you are using a drag and drop library to reorder
+                // list items.
+                ready: function (setIndexes) {
+                },
+                // (Optional)
+                // Removes the delete button from the first list item,
+                // defaults to false.
+                isFirstItemUndeletable: true
+            })
+        });
+        </script>
+    {{-- <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script> --}}
+    {{-- <script src="{{ asset('js/frontend/functions/form-repeater/email.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/form-repeater/phone.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/form-repeater/fax.js') }}"></script> --}}
     <script src="{{ asset('js/frontend/customer/edit.js') }}"></script>
     <script src="{{ asset('js/frontend/customer/form-reset.js') }}"></script>
+
 @endpush
