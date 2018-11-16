687cff474c86d3645e7505888a2d083839492bd1
audhy <audhy.vk01@gmail.com>
2018-11-16 08:11:16 +0700

    remove unused

--- resources/views/frontend/customer/edit.blade.php
+++ resources/views/frontend/customer/edit.blade.php
@@ -238,117 +238,6 @@
                         </div>
                     </div>
                 </div>
-                {{-- <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                                <span class="m-portlet__head-icon m--hide">
-                                    <i class="la la-gear"></i>
-                                </span>
-
-                                @include('frontend.common.label.datalist')
-
-                                <h3 class="m-portlet__head-text">
-                                    Phone
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="m-portlet m-portlet--mobile">
-                        <div class="m-portlet__body">
-                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                <div class="row align-items-center">
-                                    <div class="col-xl-12 order-12 order-xl-12 m--align-right">
-                                        @component('frontend.common.buttons.create-new')
-                                            @slot('text', 'Phone')
-                                            @slot('id', 'customer-phone')
-                                            @slot('data_target', '#modal_phone')
-                                        @endcomponent
-
-                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            @include('frontend.customer.phone.modal')
-
-                            <div class="customer_phone_datatable" id="customer_phone_datatable"></div>
-                        </div>
-                    </div>
-                </div> --}}
-                {{-- <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                                <span class="m-portlet__head-icon m--hide">
-                                    <i class="la la-gear"></i>
-                                </span>
-
-                                @include('frontend.common.label.datalist')
-
-                                <h3 class="m-portlet__head-text">
-                                    Email
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="m-portlet m-portlet--mobile">
-                        <div class="m-portlet__body">
-                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                <div class="row align-items-center">
-                                    <div class="col-xl-12 order-12 order-xl-12 m--align-right">
-                                        @component('frontend.common.buttons.create-new')
-                                            @slot('text', 'Email')
-                                            @slot('id', 'customer-email')
-                                            @slot('data_target', '#modal_email')
-                                        @endcomponent
-
-                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            @include('frontend.customer.email.modal')
-
-                            <div class="customer_email_datatable" id="customer_email_datatable"></div>
-                        </div>
-                    </div>
-                </div> --}}
-                {{-- <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                                <span class="m-portlet__head-icon m--hide">
-                                    <i class="la la-gear"></i>
-                                </span>
-
-                                @include('frontend.common.label.datalist')
-
-                                <h3 class="m-portlet__head-text">
-                                    Fax
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="m-portlet m-portlet--mobile">
-                        <div class="m-portlet__body">
-                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                <div class="row align-items-center">
-                                    <div class="col-xl-12 order-12 order-xl-12 m--align-right">
-                                        @component('frontend.common.buttons.create-new')
-                                            @slot('text', 'Fax')
-                                            @slot('id', 'customer-fax')
-                                            @slot('data_target', '#modal_fax')
-                                        @endcomponent
-
-                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            @include('frontend.customer.fax.modal')
-
-                            <div class="customer_fax_datatable" id="customer_fax_datatable"></div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </div>
@@ -372,11 +261,6 @@
 
 @push('footer-scripts')
     <script src="{{ asset('js/frontend/functions/select2/core.js') }}"></script>
-
-    {{-- <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script> --}}
-    {{-- <script src="{{ asset('js/frontend/functions/form-repeater/email.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/form-repeater/phone.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/form-repeater/fax.js') }}"></script> --}}
     <script src="{{ asset('js/frontend/customer/edit.js') }}"></script>
     <script src="{{ asset('js/frontend/customer/form-reset.js') }}"></script>
 
