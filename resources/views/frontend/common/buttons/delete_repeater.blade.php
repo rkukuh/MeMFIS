6a9aaae21a9556b6225dff4501732ca2432430ab
audhy <audhy.vk01@gmail.com>
2018-11-16 08:09:47 +0700

    delete buttom repeater

---
+++ resources/views/frontend/common/buttons/delete_repeater.blade.php
@@ -0,0 +1,9 @@
+<div data-repeater-delete="" 
+    class="btn
+           btn-{{ $color or 'danger' }}
+           btn-{{ $size or 'sm' }}
+           {{ $class or '' }}">
+        <span>
+            <i class="la la-trash-o"></i>
+        </span>
+</div>
