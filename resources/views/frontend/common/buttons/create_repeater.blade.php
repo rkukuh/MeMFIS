995ed628917224931591bfea7825e7dbbfed5382
audhy <audhy.vk01@gmail.com>
2018-11-16 08:09:39 +0700

    create buttom repeater

---
+++ resources/views/frontend/common/buttons/create_repeater.blade.php
@@ -0,0 +1,13 @@
+<div data-repeater-create="" 
+    class= "btn 
+            btn-{{ $color or 'brand' }}
+            btn-{{ $size or 'sm' }}
+            m-btn 
+            m-btn--icon 
+            m-btn--pill 
+            m-btn--wide">
+    <span>
+        <i class="la la-plus"></i>
+        <span>Add</span>
+    </span>
+</div>
