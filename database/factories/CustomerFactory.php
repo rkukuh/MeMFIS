a1c53e76acef5ead1c2163b6e93e5d946b919ad6
R. Kukuh <rkukuh@gmail.com>
2018-11-18 16:58:44 +0700

    Create and use faker's callback instead

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,8 +1,14 @@
 <?php
 
 use Carbon\Carbon;
+use App\Models\Fax;
 use App\Models\Type;
+use App\Models\Phone;
+use App\Models\Email;
+use App\Models\Address;
+use App\Models\Website;
 use App\Models\Journal;
+use App\Models\Document;
 use App\Models\Customer;
 use Faker\Generator as Faker;
 
@@ -21,10 +27,38 @@ $factory->define(Customer::class, function (Faker $faker) {
             null,
             Carbon::now()
         ]),
-        'account_code' => $faker->randomElement([
-            null,
-            Journal::get()->random()->id
-        ])
     ];
 
+});
+
+/** Callbacks */
+
+$factory->afterCreating(Customer::class, function ($employee, $faker) {
+    if ($faker->boolean) {
+        $employee->addresses()->saveMany(factory(Address::class, rand(2, 4))->make());
+    }
+
+    if ($faker->boolean) {
+        $employee->documents()->saveMany(factory(Document::class, rand(1, 3))->make());
+    }
+
+    if ($faker->boolean) {
+        $employee->emails()->saveMany(factory(Email::class, rand(1, 2))->make());
+    }
+
+    if ($faker->boolean) {
+        $employee->faxes()->saveMany(factory(Fax::class, rand(1, 2))->make());
+    }
+
+    if ($faker->boolean) {
+        $employee->journal()->associate(Journal::get()->random())->save();
+    }
+
+    if ($faker->boolean) {
+        $employee->phones()->saveMany(factory(Phone::class, rand(1, 2))->make());
+    }
+
+    if ($faker->boolean) {
+        $employee->websites()->saveMany(factory(Website::class, rand(2, 4))->make());
+    }
 });
--- database/seeds/dummies/Customers.php
+++ database/seeds/dummies/Customers.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Models\Fax;
-use App\Models\Phone;
-use App\Models\Email;
-use App\Models\Address;
-use App\Models\Website;
 use App\Models\Customer;
-use App\Models\Document;
 use Illuminate\Database\Seeder;
 
 class Customers extends Seeder
@@ -18,39 +12,6 @@ class Customers extends Seeder
      */
     public function run()
     {
-        factory(Customer::class, config('memfis.examples.customers'))
-            ->create()
-            ->each(function ($customer) {
-
-                /** Address */
-
-                $customer->addresses()
-                         ->saveMany(factory(Address::class, rand(2, 4))->make());
-
-                /** Document */
-
-                $customer->documents()
-                         ->saveMany(factory(Document::class, rand(1, 3))->make());
-
-                /** Email */
-
-                $customer->emails()
-                         ->saveMany(factory(Email::class, rand(1, 2))->make());
-
-                /** Fax */
-
-                $customer->faxes()
-                         ->saveMany(factory(Fax::class, rand(1, 2))->make());
-
-                /** Phone */
-
-                $customer->phones()
-                         ->saveMany(factory(Phone::class, rand(1, 2))->make());
-
-                /** Website */
-
-                $customer->websites()
-                         ->saveMany(factory(Website::class, rand(2, 4))->make());
-            });
+        factory(Customer::class, config('memfis.examples.customers'))->create();
     }
 }
