6fa557041092d72bc1a01769d766caf6518575b5
audhy <audhy.vk01@gmail.com>
2018-11-19 14:00:29 +0700

    Edit view: email,fax,phone is now use Active or not (use slider form field) as status

--- public/js/frontend/customer/edit.js
+++ public/js/frontend/customer/edit.js
@@ -1,6 +1,6 @@
 let Customer = {
     init: function () {
-        $('.customer_address_datatable').mDatatable({
+        $('.customer_document_datatable').mDatatable({
             data: {
                 type: 'remote',
                 source: {
@@ -56,22 +56,456 @@ let Customer = {
                     sortable: 'asc',
                     filterable: !1,
                 },
-                {
-                    field: 'Actions',
-                    width: 110,
-                    title: 'Actions',
-                    sortable: !1,
-                    overflow: 'visible',
-                    template: function (t, e, i) {
-                        return (
-                            '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" href="#" data-id=' +
-                            t.id +
-                            ' title="Delete"><i class="la la-trash"></i></a>\t\t\t\t\t\t\t'
-                        );
-                    }
-                }
             ]
         });
+        $('.customer_address_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/customer/'+ customer_uuid +'/address',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 3,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'address',
+                    title: 'Address',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'type.name',
+                    title: 'Type Address',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+            ]
+        });
+
+        $('.customer_phone_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/customer/'+ customer_uuid +'/phone',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 3,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'number',
+                    title: 'Number',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t){
+                        return '<span>'+t.number+'</span>'
+                    }
+                },          
+                {
+                    field: 'type.name',
+                    title: 'Type Phone',
+                    sortable: 'asc',
+                    filterable: !1,
+                },     
+                {
+                    field: 'is_active',
+                    title: 'Active',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        if(t.is_active == 1){
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" checked name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }
+                        else{
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }    
+                    }
+                },  
+            ]
+        });
+
+        $('.customer_email_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/customer/'+ customer_uuid +'/email',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 3,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'address',
+                    title: 'Email',
+                    sortable: 'asc',
+                    filterable: !1,
+
+                },
+                {
+                    field: 'type.name',
+                    title: 'Type Email',
+                    sortable: 'asc',
+                    filterable: !1,
+                },                    
+                {
+                    field: 'is_active',
+                    title: 'Active',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        if(t.is_active == 1){
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" checked name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }
+                        else{
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }    
+                    }
+                },               
+            ]
+        });
+
+        $('.customer_fax_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/customer/'+ customer_uuid +'/fax',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 3,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'number',
+                    title: 'Number',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'type.name',
+                    title: 'Type Fax',
+                    sortable: 'asc',
+                    filterable: !1,
+                },                                   
+                {
+                    field: 'is_active',
+                    title: 'Active',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        if(t.is_active == 1){
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" checked name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }
+                        else{
+                            return ' <span class="m-switch  m-switch--outline m-switch--icon m-switch--md">'+
+                            '<label>'+
+                            '<input type="checkbox" name="" id="">'+
+                            '<span></span>'+
+                            '</label>'+
+                            '</span>'
+
+                        }    
+                    }
+                },  
+            ]
+        });
+
+        $('.customer_website_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/customer/'+ customer_uuid +'/website',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 3,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'url',
+                    title: 'URL',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'type.name',
+                    title: 'Type Fax',
+                    sortable: 'asc',
+                    filterable: !1,
+                },                                   
+            ]
+        });
+        // $('.customer_address_datatable').mDatatable({
+        //     data: {
+        //         type: 'remote',
+        //         source: {
+        //             read: {
+        //                 method: 'GET',
+        //                 url: '/datatables/customer/'+ customer_uuid +'/document',
+        //                 map: function (raw) {
+        //                     let dataSet = raw;
+
+        //                     if (typeof raw.data !== 'undefined') {
+        //                         dataSet = raw.data;
+        //                     }
+
+        //                     return dataSet;
+        //                 }
+        //             }
+        //         },
+        //         pageSize: 10,
+        //         serverPaging: !0,
+        //         serverFiltering: !0,
+        //         serverSorting: !0
+        //     },
+        //     layout: {
+        //         theme: 'default',
+        //         class: '',
+        //         scroll: false,
+        //         footer: !1
+        //     },
+        //     sortable: !0,
+        //     perpage: 3,
+        //     filterable: !1,
+        //     pagination: !0,
+        //     search: {
+        //         input: $('#generalSearch')
+        //     },
+        //     toolbar: {
+        //         items: {
+        //             pagination: {
+        //                 pageSizeSelect: [3, 5, 10, 20, 30, 50, 100]
+        //             }
+        //         }
+        //     },
+        //     columns: [
+        //         {
+        //             field: 'number',
+        //             title: 'Document Number',
+        //             sortable: 'asc',
+        //             filterable: !1,
+        //         },
+        //         {
+        //             field: 'type.name',
+        //             title: 'Type Document',
+        //             sortable: 'asc',
+        //             filterable: !1,
+        //         },
+        //         {
+        //             field: 'Actions',
+        //             width: 110,
+        //             title: 'Actions',
+        //             sortable: !1,
+        //             overflow: 'visible',
+        //             template: function (t, e, i) {
+        //                 return (
+        //                     '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" href="#" data-id=' +
+        //                     t.id +
+        //                     ' title="Delete"><i class="la la-trash"></i></a>\t\t\t\t\t\t\t'
+        //                 );
+        //             }
+        //         }
+        //     ]
+        // });
 
         $('.customer_phone_datatable').mDatatable({
             data: {
