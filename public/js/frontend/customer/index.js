76b61cc8b22bb3386e74299585a7793dd92eb140
audhy <audhy.vk01@gmail.com>
2018-11-19 12:12:34 +0700

    Fix issue #67

--- public/js/frontend/customer/index.js
+++ public/js/frontend/customer/index.js
@@ -20,7 +20,6 @@ let Customer = {
                 },
                 pageSize: 10,
                 serverPaging: !0,
-                serverFiltering: !0,
                 serverSorting: !0
             },
             layout: {
