8c082988eca86c51d1eacc87a062516af6adfde9
audhy <audhy.vk01@gmail.com>
2018-11-19 09:29:08 +0700

    All views: Sebutan 'code' dalam bisnis itu apa?

--- resources/views/frontend/customer/edit.blade.php
+++ resources/views/frontend/customer/edit.blade.php
@@ -56,7 +56,7 @@
                                         <div class="form-group m-form__group row">
                                             <div class="col-sm-12 col-md-12 col-lg-12">
                                                 <label class="form-control-label">
-                                                    Code @include('frontend.common.label.required')
+                                                    Customer Code @include('frontend.common.label.required')
                                                 </label>
                                         
                                                 @component('frontend.common.input.text')
