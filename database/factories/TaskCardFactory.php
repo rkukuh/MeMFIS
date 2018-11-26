99ae40a71973484ea3724507dc79778d440b0dda
R. Kukuh <rkukuh@gmail.com>
2018-11-26 15:33:10 +0700

    WIP: Factory

--- database/factories/TaskCardFactory.php
+++ database/factories/TaskCardFactory.php
@@ -6,7 +6,26 @@ use Faker\Generator as Faker;
 $factory->define(TaskCard::class, function (Faker $faker) {
 
     return [
-        //
+        'title' => '',
+        'type_id' => '',
+        'otr_certification_id' => '',
+        'work_area' => '',
+        'zone' => '',
+        'access' => '',
+        'is_rii' => '',
+        'applicability_airplane' => '',
+        'applicability_engine' => '',
+        'is_applicability_engine_all' => '',
+        'source' => '',
+        'effectivity' => '',
     ];
 
+});
+
+/** Callbacks */
+
+$factory->afterCreating(TaskCard::class, function ($category, $faker) {
+    if ($faker->boolean) {
+        $category->descriptions()->save(factory(Description::class)->make());
+    }
 });
