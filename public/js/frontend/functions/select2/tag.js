a07b2583f791bc2ca61e19e9bc72838a1f625b66
audhy <audhy.vk01@gmail.com>
2018-11-27 14:28:49 +0700

    try

--- public/js/frontend/functions/select2/tag.js
+++ public/js/frontend/functions/select2/tag.js
@@ -1,11 +1,20 @@
-let TagSelect2 = {
-    init: function () {
-        $('#tag, #tag_validate').select2({
-            placeholder: 'Select a Tag'
-        });
-    }
-};
+// let TagSelect2 = {
+//     init: function () {
+//         $('#tag, #tag_validate').select2({
+//             tags: true
+//         // tags: ["value1", "value2", "value3", "value4", "value5"]
+//         });
+//         // $("#tag").select2({
+//         //     tags: true
+//         //   });
+//     }
+// };
 
 jQuery(document).ready(function () {
-    TagSelect2.init();
+    // TagSelect2.init();
+    $('#tag, #tag_validate').select2({
+        // tags: true
+    tags: ["value1", "value2", "value3", "value4", "value5"]
+    });
+
 });
