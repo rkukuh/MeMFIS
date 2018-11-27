be5f24b197706ed009db119238a5b337871cf866
audhy <audhy.vk01@gmail.com>
2018-11-27 15:29:36 +0700

    add text when not found

--- public/js/frontend/functions/select2/tag.js
+++ public/js/frontend/functions/select2/tag.js
@@ -1,20 +1,12 @@
-// let TagSelect2 = {
-//     init: function () {
-//         $('#tag, #tag_validate').select2({
-//             tags: true
-//         // tags: ["value1", "value2", "value3", "value4", "value5"]
-//         });
-//         // $("#tag").select2({
-//         //     tags: true
-//         //   });
-//     }
-// };
+let TagSelect2 = {
+    init: function () {
+        $('#tag, #tag_validate').select2({
+            placeholder: 'Select a Tag',
+            tags: true
+        });
+    }
+};
 
 jQuery(document).ready(function () {
-    // TagSelect2.init();
-    $('#tag, #tag_validate').select2({
-        // tags: true
-    tags: ["value1", "value2", "value3", "value4", "value5"]
-    });
-
+    TagSelect2.init();
 });
