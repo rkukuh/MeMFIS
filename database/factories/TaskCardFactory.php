aaec4be69f71e89dd3589ff901ef055e0c1d8f8f
R. Kukuh <rkukuh@gmail.com>
2018-11-29 12:48:33 +0700

    Update factory faker

--- database/factories/TaskCardFactory.php
+++ database/factories/TaskCardFactory.php
@@ -2,6 +2,7 @@
 
 use App\Models\Type;
 use App\Models\TaskCard;
+use App\Models\Description;
 use Faker\Generator as Faker;
 
 $factory->define(TaskCard::class, function (Faker $faker) {
@@ -9,16 +10,17 @@ $factory->define(TaskCard::class, function (Faker $faker) {
     return [
         'title' => 'TaskCard Dummy #' . $faker->unixTime(),
         'type_id' => Type::ofTaskCard()->get()->random()->id,
-        'otr_certification_id' => null, // TODO: Refactor its entity name
         'work_area' => Type::ofWorkArea()->get()->random()->id,
-        'zone' => null,
-        'access' => null,
+        'zone' => 'Zone Dummy #' . $faker->unixTime(),
+        'access' => 'Access Dummy #' . $faker->unixTime(),
         'is_rii' => $faker->boolean,
-        'applicability_airplane' => '', // TODO: Refactor to M-M polymorph
-        'applicability_engine' => '', // TODO: Refactor to M-M polymorph
         'is_applicability_engine_all' => $faker->boolean,
         'source' => null,
         'effectivity' => null,
+
+        // 'otr_certification_id' => null, // TODO: Refactor its entity name
+        // 'applicability_aircraft' => '', // TODO: Refactor to M-M polymorph
+        // 'applicability_engine' => '', // TODO: Refactor to M-M polymorph
     ];
 
 });
