fe4f6fde2cc20c88f4adafb77345286a84355303
R. Kukuh <rkukuh@gmail.com>
2018-11-29 12:48:10 +0700

    Update columns

--- database/migrations/2018_11_07_102547_create_taskcards_table.php
+++ database/migrations/2018_11_07_102547_create_taskcards_table.php
@@ -18,16 +18,14 @@ class CreateTaskcardsTable extends Migration
             $table->char('uuid', 36)->unique();
             $table->string('title');
             $table->unsignedInteger('type_id');
-            $table->unsignedInteger('otr_certification_id');
             $table->unsignedInteger('work_area');
             $table->string('zone');
             $table->string('access');
-            $table->boolean('is_rii');
-            $table->string('applicability_airplane');
-            $table->unsignedInteger('applicability_engine')->nullable();
-            $table->boolean('is_applicability_engine_all')->nullable();
-            $table->string('source')->nullabel();
-            $table->string('effectifity')->nullabel();
+            $table->boolean('is_rii')->default(false);
+            $table->boolean('is_applicability_airplane_all')->default(false);
+            $table->boolean('is_applicability_engine_all')->default(false);
+            $table->string('source')->nullable();
+            $table->string('effectivity')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
@@ -36,18 +34,8 @@ class CreateTaskcardsTable extends Migration
                     ->onUpdate('cascade')
                     ->onDelete('restrict');
 
-            $table->foreign('otr_certification_id')
-                    ->references('id')->on('otr_certifications')
-                    ->onUpdate('cascade')
-                    ->onDelete('restrict');
-
             $table->foreign('work_area')
                     ->references('id')->on('types')
-                    ->onUpdate('cascade')
-                    ->onDelete('restrict');
-
-            $table->foreign('applicability_engine')
-                    ->references('id')->on('items')
                     ->onUpdate('cascade')
                     ->onDelete('restrict');
 
