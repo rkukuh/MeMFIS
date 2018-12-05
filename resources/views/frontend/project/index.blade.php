5e77899b0107f851015ad900019f5c8401bad5d9
audhy <audhy.vk01@gmail.com>
2018-12-05 15:14:38 +0700

    refactor

--- resources/views/frontend/project/index.blade.php
+++ resources/views/frontend/project/index.blade.php
@@ -29,99 +29,54 @@
 
 <div class="m-content">
     <div class="row">
-        <div class="col-sm-6 col-md-6 col-lg-6" id="fadein">
+        <div class="col-sm-6 col-md-6 col-lg-6" id="shrink" onclick="location.href='/project/hm';">
             <div class="m-portlet m--bg-accent m-portlet--bordered-semi m-portlet--skin-dark m-portlet--full-height ">
                 <div class="m-portlet__body">
 
                     <div class="m-widget7 m-widget7--skin-dark">
-
-                        <div class="m-widget7__desc">
-                                <h1>Heavy Maintenance</h1>
-
-                        </div>
-                        {{-- <div class="m-widget7__user"> --}}
-                            {{-- <div class="m-widget7__user-img">
-                                <img src="{{asset('assets/metronic/app/media/img/users/user4.jpg')}}"
-                                class="m--img-rounded m--marginless" alt="" />
-
-                            </div> --}}
-                            {{-- <div class="m-widget7__info">
-                                <span class="m-widget7__username">
-                                        Dan Bold   
-                                        </span><br>
-                                <span class="m-widget7__time">
-                                        3 days ago
-                                        </span>
-                            </div> --}}
-                        {{-- </div> --}}
-                        {{-- <div class="m-widget7__button">
-                            <a class="m-btn m-btn--pill btn btn-danger" href="#" role="button">All Feeds</a>
-                        </div> --}}
+                        <div class="m-widget7__desc"></div>
                     </div>
-                    {{-- <img src="{{asset('assets/metronic/app/media/img/users/user4.jpg')}}"
-                    class="m--img-rounded m--marginless" alt="" /> --}}
 
                     <div align="center">
-                            <img src="{{asset('img/project/hm_1.jpg')}}"
-                            class="m--img-rounded m--marginless" alt="" />
-                            <img src="{{asset('img/project/hm_2.jpg')}}"
-                            class="m--img-rounded m--marginless" alt="" />
-                            <img src="{{asset('img/project/hm_3.jpg')}}"
-                            class="m--img-rounded m--marginless" alt="" />
-        
+                        <img src="{{asset('img/project/hm_1.jpg')}}" class=" m--marginless" alt="" />
+                        <img src="{{asset('img/project/hm_2.jpg')}}" class=" m--marginless" alt="" />
+                        <img src="{{asset('img/project/hm_3.jpg')}}" class=" m--marginless" alt="" />
                     </div>
-                    
+
                 </div>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-6" id="fadein" onclick="location.href='/tes';">
+        <div class="col-sm-6 col-md-6 col-lg-6" id="shrink" onclick="location.href='/project/workshop';">
             <div class="m-portlet m--bg-danger m-portlet--bordered-semi m-portlet--skin-dark m-portlet--full-height ">
                 <div class="m-portlet__body">
                     <div class="m-widget7 m-widget7--skin-dark">
-                        <div class="m-widget7__desc">
-                            <h1>Workshop</h1>
-                        </div>
-                        <div class="m-widget7__user">
-                            {{-- <div class="m-widget7__user-img shrink">
-                                <img src="{{asset('assets/metronic/app/media/img/users/user4.jpg')}}"
-                                class="m--img-rounded m--marginless" alt="" />
-
-                            </div> --}}
-                            {{-- <div class="m-widget7__info">
-                                <span class="m-widget7__username">
-                                            Nick Mana   
-                                            </span><br>
-                                <span class="m-widget7__time">
-                                            6 hours ago
-                                            </span>
-                            </div> --}}
-                        </div>
-                        {{-- <div class="m-widget7__button">
-                            <a class="m-btn m-btn--pill btn btn-accent" href="#" role="button">All Feeds</a>
-                        </div> --}}
+                        <div class="m-widget7__desc"></div>
                     </div>
+                    <div align="center">
+                        <img src="{{asset('img/project/workshop_1.jpg')}}" class=" m--marginless" alt="" />
+                        <img src="{{asset('img/project/workshop_2.jpg')}}" class=" m--marginless" alt="" />
+                        <img src="{{asset('img/project/workshop_3.jpg')}}" class=" m--marginless" alt="" />
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-6">
+            <h1 align="center" style="margin-top:-260px;color:white" onclick="location.href='/project/hm';">Heavy Maintenance</h1>
+
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
+            <h1 align="center" style="margin-top:-260px;color:white" onclick="location.href='/project/workshop';">Workshop</h1>
+
+        </div>
+    </div>
+
 </div>
 @endsection
  @push('footer-scripts')
 <script src="{{ asset('js/frontend/project/index.js')}}"></script>
-<script>
-//     jQuery(document).ready(function () {
-//         $('.shrink').on('click', function () {
-//                 alert('tes');
-// });
 
-//     });
-// $div
-// .on('mouseenter', function(){
-//     $(this).animate({ margin: -10, width: "+=20", height: "+=20" });
-// })
-// .on('mouseleave', function(){
-//     $(this).animate({ margin: 0, width: "-=20", height: "-=20" });
-// })​
-</script>
 
 @endpush
