a1dfb856d18dc1bfe928a7bedb4d52ba465f29ab
audhy <audhy.vk01@gmail.com>
2018-12-05 16:03:10 +0700

    add project to sidebar

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -26,6 +26,14 @@
                     </span>
                 </a>
             </li>
+            <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
+                    <a href="{{ route('frontend.project.index') }}" class="m-menu__link m-menu__toggle">
+                        <i class="m-menu__link-icon flaticon-list-3"></i>
+                        <span class="m-menu__link-text">
+                            Project
+                        </span>
+                    </a>
+            </li>
             <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" style="display: none;">
                 <a href="{{ route('frontend.supplier.index') }}" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
