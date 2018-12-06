d4eeec100881d97da4399e601c3e99cc1a7ccc14
audhy <audhy.vk01@gmail.com>
2018-12-06 13:44:18 +0700

    redesign

--- resources/views/frontend/project/index.blade.php
+++ resources/views/frontend/project/index.blade.php
@@ -29,54 +29,29 @@
 
 <div class="m-content">
     <div class="row">
-        <div class="col-sm-6 col-md-6 col-lg-6" id="shrink" onclick="location.href='/project/hm';">
-            <div class="m-portlet m--bg-accent m-portlet--bordered-semi m-portlet--skin-dark m-portlet--full-height ">
-                <div class="m-portlet__body">
-
-                    <div class="m-widget7 m-widget7--skin-dark">
-                        <div class="m-widget7__desc"></div>
-                    </div>
-
-                    <div align="center">
-                        <img src="{{asset('img/project/hm_1.jpg')}}" class=" m--marginless" alt="" />
-                        <img src="{{asset('img/project/hm_2.jpg')}}" class=" m--marginless" alt="" />
-                        <img src="{{asset('img/project/hm_3.jpg')}}" class=" m--marginless" alt="" />
-                    </div>
-
+        <div class="col-sm-6 col-md-6 col-lg-6" onclick="location.href='/project/hm';">
+            <div class="view third-effect" >
+                <img class="responsive" src="{{ asset('img/project/hm_icon.jpg') }}"  />
+                <div class="mask"  >
+                    <h1 style="color:white">Heavy Maintenance</h1>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-6" id="shrink" onclick="location.href='/project/workshop';">
-            <div class="m-portlet m--bg-danger m-portlet--bordered-semi m-portlet--skin-dark m-portlet--full-height ">
-                <div class="m-portlet__body">
-                    <div class="m-widget7 m-widget7--skin-dark">
-                        <div class="m-widget7__desc"></div>
-                    </div>
-                    <div align="center">
-                        <img src="{{asset('img/project/workshop_1.jpg')}}" class=" m--marginless" alt="" />
-                        <img src="{{asset('img/project/workshop_2.jpg')}}" class=" m--marginless" alt="" />
-                        <img src="{{asset('img/project/workshop_3.jpg')}}" class=" m--marginless" alt="" />
-                    </div>
-
+        <div class="col-sm-6 col-md-6 col-lg-6" onclick="location.href='/project/workshop';">
+            <div class="view third-effect" style="margin-left:0px">
+                <img class="responsive" src="{{ asset('img/project/workshop_icon.jpg') }}" />
+                <div class="mask">
+                    <h1 style="color:white">Workshop</h1>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-6 col-md-6 col-lg-6">
-            <h1 align="center" style="margin-top:-260px;color:white" onclick="location.href='/project/hm';">Heavy Maintenance</h1>
-
-        </div>
-        <div class="col-sm-6 col-md-6 col-lg-6">
-            <h1 align="center" style="margin-top:-260px;color:white" onclick="location.href='/project/workshop';">Workshop</h1>
-
-        </div>
-    </div>
-
 </div>
 @endsection
- @push('footer-scripts')
+ @push('header-scripts')
+<link rel="stylesheet" href="{{ asset('css3/style.css')}} " type="text/css" media="screen" />
+<link rel="stylesheet" href="{{ asset('css3/thirdeffect.css')}} " type="text/css" media="screen" /> 
+@endpush @push('footer-scripts')
 <script src="{{ asset('js/frontend/project/index.js')}}"></script>
-
 
 @endpush
