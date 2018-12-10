3ae02bb839b71907f9eb8247f1439a8826fd6d20
audhy <audhy.vk01@gmail.com>
2018-12-10 11:09:45 +0700

    function crud currency

--- app/Http/Controllers/Frontend/CurrencyController.php
+++ app/Http/Controllers/Frontend/CurrencyController.php
@@ -37,7 +37,9 @@ class CurrencyController extends Controller
      */
     public function store(CurrencyStore $request)
     {
-        //
+        $currencies = Currency::create($request->all());
+
+        return response()->json($currencies);
     }
 
     /**
@@ -59,7 +61,7 @@ class CurrencyController extends Controller
      */
     public function edit(Currency $currency)
     {
-        //
+        return response()->json($bank);
     }
 
     /**
@@ -71,7 +73,9 @@ class CurrencyController extends Controller
      */
     public function update(CurrencyUpdate $request, Currency $currency)
     {
-        //
+        $currency->update($request->all());
+
+        return response()->json($currency);
     }
 
     /**
@@ -82,6 +86,8 @@ class CurrencyController extends Controller
      */
     public function destroy(Currency $currency)
     {
-        //
+        $currency->delete();
+
+        return response()->json($currency);
     }
 }
