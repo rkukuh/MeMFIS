3b2be7fb0320ef292434df0d38ac449c690eb74c
RKukuh <rkukuh@gmail.com>
2018-12-10 14:52:22 +0700

    Define factory faker

--- database/factories/LanguageFactory.php
+++ database/factories/LanguageFactory.php
@@ -6,7 +6,9 @@ use Faker\Generator as Faker;
 $factory->define(Language::class, function (Faker $faker) {
 
     return [
-        //
+        'name' => $faker->country,
+        'iso_639_1' => $faker->languageCode,
+        'iso_639_2' => strtolower($faker->currencyCode),
     ];
 
 });
