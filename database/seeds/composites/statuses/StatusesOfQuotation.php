5b35f986c3bf81d05ae871533113be18bac55aa7
TomreinPH <husni.sosnet@gmail.com>
2018-12-27 15:05:04 +0700

    Create initial seeder for Quotation's statuses

---
+++ database/seeds/composites/statuses/StatusesOfQuotation.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Status;
+use Illuminate\Database\Seeder;
+
+class StatusesOfQuotation extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Status::create([
+            'code' => 'open',
+            'name' => 'Open',
+            'of'   => 'quotation',
+        ]);
+
+        Status::create([
+            'code' => 'close',
+            'name' => 'Close',
+            'of'   => 'quotation',
+        ]);
+
+        Status::create([
+            'code' => 'approve',
+            'name' => 'Approve',
+            'of'   => 'quotation',
+        ]);
+
+        Status::create([
+            'code' => 'cancel',
+            'name' => 'Cancel',
+            'of'   => 'quotation',
+        ]);
+    }
+}
