3831fe54c96444fafa8d2fdadd0349f432456667
audhy <audhy.vk01@gmail.com>
2019-01-02 09:25:17 +0700

    remove unused

--- public/js/frontend/functions/fill-combobox/scheduled-payment-type.js
+++ public/js/frontend/functions/fill-combobox/scheduled-payment-type.js
@@ -1,15 +1,3 @@
-let ScheduledPaymentTypeSelect2 = {
-    init: function () {
-        $('#scheduled_payment_type, #scheduled_payment_type_validate').select2({
-            placeholder: 'Select a Type'
-        });
-    }
-};
-
-jQuery(document).ready(function () {
-    ScheduledPaymentTypeSelect2.init();
-});
-
 $(document).ready(function () {
     units = function () {
         $.ajax({
