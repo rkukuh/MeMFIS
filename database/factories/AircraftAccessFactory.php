a7db482ba5dae8b3cb153987e9e3ea71a507433a
R. Kukuh <rkukuh@gmail.com>
2019-01-02 16:46:04 +0700

    Define factories' attributes

--- database/factories/AircraftAccessFactory.php
+++ database/factories/AircraftAccessFactory.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Models\Aircraft;
 use App\Models\AircraftAccess;
 use Faker\Generator as Faker;
 
 $factory->define(AircraftAccess::class, function (Faker $faker) {
 
     return [
-        //
+        'name' => $faker->numerify('###'),
+        'aircraft_id' => Aircraft::get()->random()->id,
     ];
 
 });
--- database/factories/AircraftZoneFactory.php
+++ database/factories/AircraftZoneFactory.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Models\Aircraft;
 use App\Models\AircraftZone;
 use Faker\Generator as Faker;
 
 $factory->define(AircraftZone::class, function (Faker $faker) {
 
     return [
-        //
+        'name' => $faker->numerify('###'),
+        'aircraft_id' => Aircraft::get()->random()->id,
     ];
 
 });
