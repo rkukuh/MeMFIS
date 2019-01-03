5204f64649268a39b1543f2210b1f878c082b3d9
audhy <audhy.vk01@gmail.com>
2019-01-03 13:13:48 +0700

    taskcard routine and non routine select2

---
+++ public/js/frontend/functions/fill-combobox/taskcard-routine-type.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    TaskcardRoutineType = function () {
+        $.ajax({
+            url: '/get-takcard-routine-types/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $('select[name="taskcard_routine_type"]').empty();
+
+                $('select[name="taskcard_routine_type"]').append(
+                    '<option> Select a Taskcard Routine Type</option>'
+                );
+
+                $.each(data, function (key, value) {
+                    $('select[name="taskcard_routine_type"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    TaskcardRoutineType();
+});
---
+++ public/js/frontend/functions/select2/taskcard-routine-type.js
@@ -0,0 +1,11 @@
+    let TaskcardRoutineTypeSelect2 = {
+    init: function () {
+        $('#taskcard_routine_type, #taskcard_routine_type_validate').select2({
+            placeholder: 'Select a Taskcard Routine Type '
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    TaskcardRoutineTypeSelect2.init();
+});
