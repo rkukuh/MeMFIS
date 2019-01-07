0353a5fa6bc5ed28ca42354db0b6e71b8e21e15b
R. Kukuh <rkukuh@gmail.com>
2019-01-07 12:15:45 +0700

    Move to Admin namespace

---
+++ tests/Unit/Admin/ManufacturerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Admin;
+
+use App\User;
+use Tests\TestCase;
+use App\Models\Manufacturer;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManufacturerTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /** ATTRIBUTES */
+
+    /** @test */
+    public function requires_a_uuid()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $manufacturer = factory(Manufacturer::class)->create();
+
+        $this->assertDatabaseHas('manufacturers', ['uuid' => $manufacturer->uuid]);
+    }
+}
