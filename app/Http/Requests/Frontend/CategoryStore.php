c0ad28266dc7dbe2ca6c59376d51437d2f02379f
R. Kukuh <rkukuh@gmail.com>
2019-01-08 04:38:08 +0700

    Define test cases

--- app/Http/Requests/Frontend/CategoryStore.php
+++ app/Http/Requests/Frontend/CategoryStore.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Frontend;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CategoryStore extends FormRequest
 {
@@ -28,5 +30,9 @@ class CategoryStore extends FormRequest
             'name' => 'required',
             'of' => 'required',
         ];
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()]));
     }
 }
