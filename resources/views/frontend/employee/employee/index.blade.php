7eb086fc1c720401635cd9e2177cbb4b92b6ee25
R. Kukuh <rkukuh@gmail.com>
2019-01-07 10:24:33 +0700

    Remove unused employee's relation

--- resources/views/frontend/employee/employee/index.blade.php
+++ resources/views/frontend/employee/employee/index.blade.php
@@ -28,106 +28,102 @@
         </div>
     </div>
 
-<div class="m-content">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12">
-        <div class="m-portlet m-portlet--tabs">
+    <div class="m-content">
+        <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12">
-                <div class="m-portlet__head">
-                    <div class="m-portlet__head-tools">
-                        <ul class="nav nav-tabs m-tabs-line m-tabs-line--primary m-tabs-line--2x" role="tablist">
-                            <li class="nav-item m-tabs__item">
-                                <a class="nav-link m-tabs__link active" data-toggle="tab" href="#m_tabs_6_1" role="tab">
-                                    <i class="la la-cog"></i> Personnels
-                                </a>
-                            </li>
-                            <li class="nav-item m-tabs__item">
-                                <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_2" role="tab">
-                                    <i class="la la-briefcase"></i> Education
-                                </a>
-                            </li>
-                            <li class="nav-item m-tabs__item">
-                                <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_3" role="tab">
-                                    <i class="la la-bell-o"></i> Certification
-                                </a>
-                            </li>
-                            <li class="nav-item m-tabs__item">
-                                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_4" role="tab">
-                                        <i class="la la-bell-o"></i> General License
-                                    </a>
-                            </li>
-                            <li class="nav-item m-tabs__item">
-                                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_5" role="tab">
-                                        <i class="la la-bell-o"></i> AMEL
-                                    </a>
-                                </li>
-                            <li class="nav-item m-tabs__item">
+                <div class="m-portlet m-portlet--tabs">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="m-portlet__head">
+                            <div class="m-portlet__head-tools">
+                                <ul class="nav nav-tabs m-tabs-line m-tabs-line--primary m-tabs-line--2x" role="tablist">
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link active" data-toggle="tab" href="#m_tabs_6_1" role="tab">
+                                            <i class="la la-cog"></i> Personnels
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_2" role="tab">
+                                            <i class="la la-briefcase"></i> Education
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_3" role="tab">
+                                            <i class="la la-bell-o"></i> Certification
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_4" role="tab">
+                                            <i class="la la-bell-o"></i> General License
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_5" role="tab">
+                                            <i class="la la-bell-o"></i> AMEL
+                                        </a>
+                                    </li>
+                                    <li class="nav-item m-tabs__item">
                                         <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_6" role="tab">
                                             <i class="la la-bell-o"></i> OTR
                                         </a>
-                            </li>
-    
-                            <li class="nav-item m-tabs__item">
-                                <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_7" role="tab">
-                                    <i class="la la-bell-o"></i> Languages
-                                </a>
-                            </li>    
-                            <li class="nav-item dropdown m-tabs__item">
-                                <a class="nav-link m-tabs__link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true"><i class="la la-map-marker"></i>More</a>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_9">Emergency Contacts</a>
-                                        <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_10">Documents</a>
-                                        <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_11">Tamporarily Deactivated Personnels</a>
-                                        <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_12">Terminated Personnel Data</a>
-                                </div>
-                            </li>
-                        </ul>
+                                    </li>
+
+                                    <li class="nav-item m-tabs__item">
+                                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_tabs_6_7" role="tab">
+                                            <i class="la la-bell-o"></i> Languages
+                                        </a>
+                                    </li>
+                                    <li class="nav-item dropdown m-tabs__item">
+                                        <a class="nav-link m-tabs__link dropdown-toggle" data-toggle="dropdown" href="#"
+                                        role="button" aria-haspopup="true" aria-expanded="true"><i class="la la-map-marker"></i>More</a>
+                                        <div class="dropdown-menu dropdown-menu-right">
+                                            <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_9">Emergency
+                                                Contacts</a>
+                                            <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_10">Documents</a>
+                                            <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_11">Tamporarily
+                                                Deactivated Personnels</a>
+                                            <a class="dropdown-item" data-toggle="tab" href="#m_tabs_6_12">Terminated
+                                                Personnel Data</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet__body">
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="m_tabs_6_1" role="tabpanel">
+                                @include('frontend.employee.employee.include.employee')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_2" role="tabpanel">
+                                @include('frontend.employee.employee.include.education')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_3" role="tabpanel">
+                                @include('frontend.employee.employee.include.certification')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_4" role="tabpanel">
+                                @include('frontend.employee.employee.include.general-license')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_5" role="tabpanel">
+                                @include('frontend.employee.employee.include.amel')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_6" role="tabpanel">
+                                @include('frontend.employee.employee.include.otr')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_7" role="tabpanel">
+                                @include('frontend.employee.employee.include.languages')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_9" role="tabpanel">
+                                @include('frontend.employee.employee.include.emergency-contact')
+                            </div>
+                            <div class="tab-pane" id="m_tabs_6_10" role="tabpanel">
+                                @include('frontend.employee.employee.include.document')
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-                <div class="m-portlet__body">                   
-                    <div class="tab-content">
-                        <div class="tab-pane active" id="m_tabs_6_1" role="tabpanel">
-                                @include('frontend.employee.employee.include.employee')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_2" role="tabpanel">
-                                @include('frontend.employee.employee.include.education')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_3" role="tabpanel">
-                                @include('frontend.employee.employee.include.certification')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_4" role="tabpanel">
-                                @include('frontend.employee.employee.include.general-license')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_5" role="tabpanel">
-                                @include('frontend.employee.employee.include.amel')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_6" role="tabpanel">
-                                @include('frontend.employee.employee.include.otr')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_7" role="tabpanel">
-                                @include('frontend.employee.employee.include.languages')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_9" role="tabpanel">
-                                @include('frontend.employee.employee.include.emergency-contact')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_10" role="tabpanel">
-                                @include('frontend.employee.employee.include.document')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_11" role="tabpanel">
-                                @include('frontend.employee.employee.include.temporarily-deactivated-employee')
-                        </div>
-                        <div class="tab-pane" id="m_tabs_6_12" role="tabpanel">
-                                @include('frontend.employee.employee.include.terminated-employee-data')
-                        </div>
-                    </div>      
-                </div>
             </div>
-        </div>      
+        </div>
     </div>
-</div>
-
-   
 @endsection
 
 @push('footer-scripts')
