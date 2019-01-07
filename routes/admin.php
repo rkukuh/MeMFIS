28efa4aafb393a95bb3c41d5d474b1b6b8d76615
R. Kukuh <rkukuh@gmail.com>
2019-01-07 10:39:04 +0700

    Remove unused route

--- routes/admin.php
+++ routes/admin.php
@@ -62,7 +62,6 @@ Route::name('admin.')->group(function () {
         /** TRANSACTION */
 
         Route::resource('project', 'ProjectController');
-        Route::resource('taskcard', 'TaskCardController');
         Route::resource('quotation', 'QuotationController');
         Route::resource('workpackage', 'WorkPackageController');
 
