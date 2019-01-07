70e68eddaa3de2794797fba79e737ca0854c6ef2
R. Kukuh <rkukuh@gmail.com>
2019-01-07 10:43:31 +0700

    Create dedicated controller and form request for task card SI

---
+++ app/Http/Controllers/Frontend/TaskCard/TaskCardSIController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\TaskCard;
+
+use App\Models\TaskCard;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\TaskCardSIStore;
+use App\Http\Requests\Frontend\TaskCardSIUpdate;
+
+class TaskCardSIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Frontend\TaskCardSIStore  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskCardSIStore $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskCard $taskCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Taskcard $taskCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Frontend\TaskCardSIUpdate  $request
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TaskCardSIUpdate $request, Taskcard $taskCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Taskcard $taskCard)
+    {
+        //
+    }
+}
