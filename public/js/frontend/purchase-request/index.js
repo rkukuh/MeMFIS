6be153b0b11a5ce37197ef426f530430299ed685
audhy <audhy.vk01@gmail.com>
2019-01-09 14:40:34 +0700

    purchase-request WIP

---
+++ public/js/frontend/purchase-request/index.js
@@ -0,0 +1,165 @@
+let Item = {
+    init: function () {
+        $('.item_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/item',
+
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'Date.',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        return '<a href="/item/'+t.uuid+'">' + t.code + "</a>"
+                    }
+                },
+                {
+                    field: 'name',
+                    title: 'PR Number',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Type',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        return t.unit.name + ' (' + t.unit.symbol + ')'
+                    }
+                },
+                {
+                    field: "is_ppn",
+                    title: "Description",
+                    template: function (t) {
+                        if (t.is_ppn === 1) {
+                            return '<span class="m-badge m-badge--brand m-badge--wide">PPN: ' + t.ppn_amount + '%</span>'
+                        }
+                        else {
+                            return '<span class="m-badge m-badge--warning m-badge--wide">No</span>'
+                        }
+                    }
+                },
+                {
+                    field: "is_stock",
+                    title: "Status",
+                    template: function (t) {
+                        var e = {
+                            1: {
+                                title: "Yes",
+                                class: "m-badge--brand"
+                            },
+                            0: {
+                                title: "No",
+                                class: " m-badge--warning"
+                            }
+                        };
+
+                        return '<span class="m-badge ' + e[t.is_stock].class + ' m-badge--wide">' + e[t.is_stock].title + "</span>"
+                    }
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="/item/' + t.uuid + '/edit" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-id="' + t.uuid +'">' +
+                                '<i class="la la-pencil"></i>' +
+                            '</a>' +
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" data-id="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+        $('.item_datatable').on('click', '.delete', function () {
+            let item_uuid = $(this).data('id');
+
+            swal({
+                title: 'Sure want to remove?',
+                type: 'question',
+                confirmButtonText: 'Yes, REMOVE',
+                confirmButtonColor: '#d33',
+                cancelButtonText: 'Cancel',
+                showCancelButton: true,
+            })
+            .then(result => {
+                if (result.value) {
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
+                                'content'
+                            )
+                        },
+                        type: 'DELETE',
+                        url: '/item/' + item_uuid + '',
+                        success: function (data) {
+                            toastr.success('Material has been deleted.', 'Deleted', {
+                                    timeOut: 5000
+                                }
+                            );
+
+                            let table = $('.item_datatable').mDatatable();
+
+                            table.originalDataSet = [];
+                            table.reload();
+                        },
+                        error: function (jqXhr, json, errorThrown) {
+                            let errors = jqXhr.responseJSON;
+
+                            $.each(errors.errors, function (index, value) {
+                                $('#delete-error').html(value);
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    Item.init();
+});
