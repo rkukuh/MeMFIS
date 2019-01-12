d3009a56aa6910c40ef7f128e92450794cc1e601
audhy <audhy.vk01@gmail.com>
2019-01-12 17:10:09 +0700

    Create summary button component

---
+++ resources/views/frontend/common/buttons/summary.blade.php
@@ -0,0 +1,15 @@
+<a
+    href="{{ $href or '' }}"
+    id={{ $id or '' }}
+    name={{ $name or '' }}
+    class="btn m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air
+            btn-{{ $color or 'primary' }}
+            btn-{{ $size or 'md' }}
+            {{ $class or '' }}"
+    style="{{ $style or '' }}">
+
+    <span>
+        <i class="la la-{{ $icon or 'file'}}"></i>
+        <span>{{ $text or 'Add' }}</span>
+    </span>
+</a>
