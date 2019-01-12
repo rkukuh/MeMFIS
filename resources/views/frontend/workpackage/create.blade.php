2cc01f593bb8155523faa89624465cc6f309055d
audhy <audhy.vk01@gmail.com>
2019-01-12 17:05:55 +0700

    path modal file

--- resources/views/frontend/workpackage/create.blade.php
+++ resources/views/frontend/workpackage/create.blade.php
@@ -111,9 +111,15 @@
                                                         <div class="tab-content">
                                                             <div class="tab-pane active show" id="m_tabs_1_1" role="tabpanel">
                                                                 @include('frontend.workpackage.routine.index')
+                                                                @include('frontend.workpackage.routine.basic.modal')
+                                                                @include('frontend.workpackage.routine.sip.modal')
+                                                                @include('frontend.workpackage.routine.cpcp.modal')
                                                             </div>
                                                             <div class="tab-pane" id="m_tabs_1_2" role="tabpanel">
                                                                 @include('frontend.workpackage.nonroutine.index')
+                                                                @include('frontend.workpackage.nonroutine.adsb.modal')
+                                                                @include('frontend.workpackage.nonroutine.cmrawl.modal')
+                                                                @include('frontend.workpackage.nonroutine.si.modal')
                                                             </div>
                                                             <div class="tab-pane" id="m_tabs_1_3" role="tabpanel">
                                                                 @include('frontend.workpackage.item.index')
