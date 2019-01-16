b50f8434fdc5fbfb555c55e35aa1e5c9d88dc36e
audhy <audhy.vk01@gmail.com>
2019-01-17 05:24:25 +0700

    Project modal

---
+++ public/js/frontend/project/hm/datatables.js
@@ -0,0 +1,251 @@
+let Datatables = {
+    init: function () {
+        $("#basic_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-routine/basic/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+        $("#sip_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-routine/sip/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+        $("#cpcp_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-routine/cpcp/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+        $("#adsb_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-eo/adsb/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+        $("#cmrawl_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-eo/cmrawl/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+        $("#si_datatable").DataTable({
+            "dom": '<"top"f>rt<"bottom">pl',
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            serverSide: !0,
+            lengthMenu: [5, 10, 25, 50],
+            pageLength: 5,
+            ajax: "/datatables/taskcard-si/si/modal",
+            columns: [
+                {
+                    data: "number"
+                },
+                {
+                    data: "title"
+                },
+                {
+                    data: "work_area"
+                },
+                {
+                    data: "manhour"
+                },
+                {
+                    data: "Actions"
+                }
+            ],
+            columnDefs: [{
+                    targets: -1,
+                    orderable: !1,
+                    render: function (a, e, t, n) {
+                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-code="' + t.code + '" data-name="' + t.name + '">\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+                    }
+                },
+
+            ]
+        })
+
+        $('<a class="btn m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air btn-primary btn-sm refresh" style="margin-left: 60%; color: white;"><span><i class="la la-refresh"></i><span>Reload</span></span> </button>').appendTo('div.dataTables_filter');
+        $('.paging_simple_numbers').addClass('pull-left');
+        $('.dataTables_length').addClass('pull-right');
+        $('.dataTables_info').addClass('pull-left');
+        $('.dataTables_info').addClass('margin-info');
+        $('.paging_simple_numbers').addClass('padding-datatable');
+
+        $('.dataTables_filter').on('click', '.refresh', function () {
+            $('#basic_datatable').DataTable().ajax.reload();
+            $('#sip_datatable').DataTable().ajax.reload();
+            $('#cpcp_datatable').DataTable().ajax.reload();
+            $('#adsb_datatable').DataTable().ajax.reload();
+            $('#cmrawl_datatable').DataTable().ajax.reload();
+            $('#si_datatable').DataTable().ajax.reload();
+        });
+
+        $('.dataTable').on('click', '.select-account_code', function () {
+            let uuid = $(this).data('uuid');
+            let code = $(this).data('code');
+            let name = $(this).data('name');
+
+            document.getElementById('account_code').value = uuid;
+
+            $('.search-journal').html(code + " - " + name);
+            $('#modal_account_code').modal('hide');
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    Datatables.init();
+});
