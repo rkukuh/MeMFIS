37eb7092c5a64f69c108a097203ee4520a57395a
audhy <audhy.vk01@gmail.com>
2019-01-17 22:13:28 +0700

    spacing

--- public/js/frontend/functions/datepicker/date.js
+++ public/js/frontend/functions/datepicker/date.js
@@ -1,17 +1,17 @@
 let GetDate = {
-    init: function() {
+    init: function () {
 
-            $("#date").datetimepicker({
-                format: "yyyy/mm/dd",
-                todayHighlight: !0,
-                autoclose: !0,
-                startView: 2,
-                minView: 2,
-                forceParse: 0,
-                pickerPosition: "bottom-left"
-            })
+        $("#date").datetimepicker({
+            format: "yyyy/mm/dd",
+            todayHighlight: !0,
+            autoclose: !0,
+            startView: 2,
+            minView: 2,
+            forceParse: 0,
+            pickerPosition: "bottom-left"
+        })
     }
 };
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     GetDate.init();
 });
