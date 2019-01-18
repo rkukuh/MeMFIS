0935537c7fa4b7e666c5a3e51d30db51fa75303c
ibnuprtma <pibnu87@gmail.com>
2019-01-18 10:09:34 +0700

    rename title

--- resources/views/frontend/price-list/modal-item.blade.php
+++ resources/views/frontend/price-list/modal-item.blade.php
@@ -2,7 +2,7 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="TitleModalItem">Item</h5>
+                    <h5 class="modal-title" id="TitleModalItem">Item test</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
