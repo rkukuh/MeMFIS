9e0e77001c0eedde2f7ad28ee79aad66924e4fa9
audhy <audhy.vk01@gmail.com>
2019-01-19 18:21:27 +0700

    create new label multiple

---
+++ resources/views/frontend/common/label/required-multiple.blade.php
@@ -0,0 +1,8 @@
+<{{ $html_tag or 'span' }}
+    class="{{$class or ''}}"
+    style="font-weight: {{ $font_weight or 'bold' }};
+    color:{{ $color or 'red' }};">
+
+    {{ $text or '* (multiple)' }}
+
+</{{ $html_tag or 'span' }}>
