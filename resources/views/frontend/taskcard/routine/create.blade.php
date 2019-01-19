61d8bb6a24f6cd5a1a02b929b4713181c3aa8c8e
audhy <audhy.vk01@gmail.com>
2019-01-19 16:57:09 +0700

    remove unused

--- resources/views/frontend/taskcard/routine/create.blade.php
+++ resources/views/frontend/taskcard/routine/create.blade.php
@@ -480,31 +480,6 @@
 @endpush
 
 @push('footer-scripts')
-    <script>
-        var autoExpand = function (field) {
-
-        // Reset field height
-        field.style.height = 'inherit';
-
-        // Get the computed styles for the element
-        var computed = window.getComputedStyle(field);
-
-        // Calculate the height
-        var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-                    + parseInt(computed.getPropertyValue('padding-top'), 10)
-                    + field.scrollHeight
-                    + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-                    + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-
-        field.style.height = height + 'px';
-
-        };
-
-        document.addEventListener('input', function (event) {
-        if (event.target.tagName.toLowerCase() !== 'textarea') return;
-        autoExpand(event.target);
-        }, false);
-    </script>
     <script src="{{ asset('js/frontend/functions/select2/ac-type.js') }}"></script>
     <script src="{{ asset('js/frontend/functions/select2/zone.js') }}"></script>
     <script src="{{ asset('js/frontend/functions/fill-combobox/zone.js') }}"></script>
@@ -545,5 +520,5 @@
 
     <script src="{{ asset('js/frontend/taskcard/routine/create.js') }}"></script>
 
-    <script src="{{ asset('js/frontend/taskcard/form-reset.js') }}"></script>
+    <script src="{{ asset('js/frontend/taskcard/routine/form-reset.js') }}"></script>
 @endpush
