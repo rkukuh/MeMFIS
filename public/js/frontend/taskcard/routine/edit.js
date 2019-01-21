b290a5e2d61423177c51445887f7c80ab74b6fae
audhy <audhy.vk01@gmail.com>
2019-01-21 12:13:01 +0700

    initial datatable

--- public/js/frontend/taskcard/routine/edit.js
+++ public/js/frontend/taskcard/routine/edit.js
@@ -1,5 +1,336 @@
 let TaskCard = {
     init: function () {
+        $('.tool_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard/item/',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 5,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'name',
+                    title: 'Item',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'quantity',
+                    title: 'Quantity',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                    template: function (t) {
+                        return t.name + ' (' + t.symbol + ')'
+                    }
+                },
+                {
+                    field: 'unit',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                    template: function (t) {
+                        return t.name + ' (' + t.symbol + ')'
+                    }
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    width: 50,
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" ' +
+                            'data-item_id="' + t.uuid + '" ' +
+                            'data-unit_id="' + t.uuid + '">' +
+                            '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+        $('.item_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard/item/',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            perpage: 5,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'name',
+                    title: 'Item',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'quantity',
+                    title: 'Quantity',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                    template: function (t) {
+                        return t.name + ' (' + t.symbol + ')'
+                    }
+                },
+                {
+                    field: 'unit',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                    template: function (t) {
+                        return t.name + ' (' + t.symbol + ')'
+                    }
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    width: 50,
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" ' +
+                            'data-item_id="' + t.uuid + '" ' +
+                            'data-unit_id="' + t.uuid + '">' +
+                            '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+        $('.threshold_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard/threshold/',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                perpage: 5,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'name',
+                    title: 'Item',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    width: 50,
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" ' +
+                            'data-item_id="' + t.uuid + '" ' +
+                            'data-unit_id="' + t.uuid + '">' +
+                            '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+        $('.repeat_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard/repeat',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                perpage: 5,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'name',
+                    title: 'Item',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    width: 50,
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" ' +
+                            'data-item_id="' + t.uuid + '" ' +
+                            'data-unit_id="' + t.uuid + '">' +
+                            '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+        $(document).ready(function () {
+            $('.reset-item').removeClass('reset');
+        });
+
+        $('.footer').on('click', '.reset', function () {
+            window.location.href = '/taskcard/1/edit';
+        });
+
+        $('.reset-item').on('click', function () {
+            document.getElementById('quantity').value = '';
+
+            $('#item').select2('val', 'All');
+            $('#item_unit_id').select2('val', 'All');
+
+            $('#item-error').html('');
+            $('#quantity-error').html('');
+            $('#unit-error').html('');
+
+        });
+
 
         $(document).ready(function () {
 
