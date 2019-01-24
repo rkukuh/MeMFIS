8e0697b405499a16d3f6e8e7d0881b53f503e22f
R. Kukuh <rkukuh@gmail.com>
2019-01-24 19:03:21 +0700

    Define 1-M relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,8 +9,24 @@ class Project extends MemfisModel
     protected $fillable = [
         'code',
         'title',
+        'customer_id',
         'no_wo',
         'aircraft_register',
         'aircraft_sn',
     ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-to-Many: A project must have a customer
+     *
+     * This function will retrieve the customer of a project.
+     * See: Customer's projects() method for the inverse
+     *
+     * @return mixed
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
