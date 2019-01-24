7483cddf47010302212f6f0e781fe647401af80d
R. Kukuh <rkukuh@gmail.com>
2019-01-24 19:03:36 +0700

    Define FK attribute value

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Project;
+use App\Models\Customer;
 use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
@@ -10,6 +11,13 @@ $factory->define(Project::class, function (Faker $faker) {
     return [
         'code' => 'PRJ-' . $code,
         'title' => 'Project Dummy #' . $code,
+        'customer_id' => function () {
+            if (Customer::count()) {
+                return Customer::get()->random()->id;
+            }
+
+            return factory(Customer::class)->create()->id;
+        },
         'no_wo' => 'WO-' . $faker->randomNumber(),
         'aircraft_register' => 'AC-REG-' . $faker->randomNumber(),
         'aircraft_sn' => 'AC-SN-' . $faker->randomNumber(),
