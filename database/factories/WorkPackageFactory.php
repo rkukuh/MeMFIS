48f1925a47d19c39d861a92709976fecb68eff1b
R. Kukuh <rkukuh@gmail.com>
2019-01-24 19:42:39 +0700

    Define attributes value

--- database/factories/WorkPackageFactory.php
+++ database/factories/WorkPackageFactory.php
@@ -5,8 +5,13 @@ use Faker\Generator as Faker;
 
 $factory->define(WorkPackage::class, function (Faker $faker) {
 
+    $code  = $faker->unixTime();
+
     return [
-        //
+        'code' => $faker->randomElement([null, 'WP-' . $code]),
+        'title' => 'WorkPackage Dummy #' . $code,
+        'is_template' => $faker->boolean(),
+        'description' => $faker->randomElement([null, $faker->text(rand(100, 500) * 10)]),
     ];
 
 });
