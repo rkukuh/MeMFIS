56070cdeea5037dd4d435fd7945601f225702fe7
R. Kukuh <rkukuh@gmail.com>
2019-01-24 19:59:35 +0700

    Define attribute value

--- database/factories/WorkPackageFactory.php
+++ database/factories/WorkPackageFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Aircraft;
 use App\Models\WorkPackage;
 use Faker\Generator as Faker;
 
@@ -11,6 +12,13 @@ $factory->define(WorkPackage::class, function (Faker $faker) {
         'code' => $faker->randomElement([null, 'WP-' . $code]),
         'title' => 'WorkPackage Dummy #' . $code,
         'is_template' => $faker->boolean(),
+        'aircraft_id' => function () {
+            if (Aircraft::count()) {
+                return Aircraft::get()->random()->id;
+            }
+
+            return factory(Aircraft::class)->create()->id;
+        },
         'description' => $faker->randomElement([null, $faker->text(rand(100, 500) * 10)]),
     ];
 
