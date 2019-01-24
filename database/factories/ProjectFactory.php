f9c7511b84928139e1de2d67e5c9192e0152f20b
R. Kukuh <rkukuh@gmail.com>
2019-01-24 18:52:10 +0700

    Define attributes

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -5,8 +5,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
 
+    $code  = $faker->unixTime();
+
     return [
-        //
+        'code' => 'PRJ-' . $code,
+        'title' => 'Project Dummy #' . $code,
+        'no_wo' => 'WO-' . $faker->randomNumber(),
+        'aircraft_register' => 'AC-REG-' . $faker->randomNumber(),
+        'aircraft_sn' => 'AC-SN-' . $faker->randomNumber(),
     ];
 
 });
