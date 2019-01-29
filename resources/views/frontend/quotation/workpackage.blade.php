f2fac263c701f707154cd6f442df2866b2b430f6
audhy <audhy.vk01@gmail.com>
2019-01-29 18:56:43 +0700

    workpackage in quotation

---
+++ resources/views/frontend/quotation/workpackage.blade.php
@@ -0,0 +1,132 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader hidden">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Work Package
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="{{ route('frontend.workpackage.index') }}" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Work Package
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+
+                                @include('frontend.common.label.show')
+
+                                <h3 class="m-portlet__head-text">
+                                    Work Package
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <form id="itemform" name="itemform">
+                                <div class="m-portlet__body">
+                                        <div class="form-group m-form__group row">
+                                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                                @component('frontend.common.label.header')
+                                                    @slot('text','Routine')
+                                                @endcomponent
+                                                @include('frontend.quotation.routine.index')
+                                                @component('frontend.common.label.header')
+                                                    @slot('text','Non-Routine')
+                                                @endcomponent
+                                                @include('frontend.quotation.nonroutine.index')
+                                                @component('frontend.common.label.header')
+                                                    @slot('text','Material & Tool List')
+                                                @endcomponent
+                                                @include('frontend.quotation.item.index')
+
+                                            </div>
+                                        </div>
+                                        <div class="form-group m-form__group row">
+                                            <div class="col-sm-12 col-md-12 col-lg-12 footer">
+                                                <div class="flex">
+                                                    <div class="action-buttons">
+                                                        @component('frontend.common.buttons.submit')
+                                                            @slot('type','button')
+                                                            @slot('id', 'add-taskcard')
+                                                            @slot('class', 'add-taskcard')
+                                                        @endcomponent
+
+                                                        @include('frontend.common.buttons.reset')
+
+                                                        @component('frontend.common.buttons.back')
+                                                            @slot('href', route('frontend.workpackage.index'))
+                                                        @endcomponent
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('header-scripts')
+    <style>
+        fieldset {
+            margin-bottom: 30px;
+        }
+
+        .padding-datatable {
+            padding: 0px
+        }
+
+        .margin-info {
+            margin-left: 5px
+        }
+    </style>
+@endpush
+
+@push('footer-scripts')
+    <script src="{{ asset('js/frontend/functions/select2/applicability-airplane.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/applicability-airplane.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/quotation.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/quotation.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/customer.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/customer.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/aircraft.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/select2/series.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/workpackage/create.js') }}"></script>
+    <script src="{{ asset('js/frontend/workpackage/form-reset.js') }}"></script>
+    <script src="{{ asset('js/frontend/workpackage/datatables.js')}}"></script>
+    <script src="{{ asset('assets/metronic/vendors/custom/datatables/datatables.bundle.js') }}"></script>
+@endpush
