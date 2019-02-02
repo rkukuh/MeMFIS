7e8826753068bd1799f0fe44f69e98d00bff89be
audhy <audhy.vk01@gmail.com>
2019-02-02 16:33:33 +0700

    WIP grn

---
+++ resources/views/frontend/good-recieved-note/index.blade.php
@@ -0,0 +1,104 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader hidden">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Good Recieved Note
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="{{ route('frontend.quotation.index') }}" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Good Recieved Note
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+
+                                @include('frontend.common.label.datalist')
+
+                                <h3 class="m-portlet__head-text">
+                                    Good Recieved Note
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                <div class="row align-items-center">
+                                    <div class="col-xl-8 order-2 order-xl-1">
+                                        <div class="form-group m-form__group row align-items-center">
+                                            <div class="col-md-4">
+                                                <div class="m-input-icon m-input-icon--left">
+                                                    <input type="text" class="form-control m-input" placeholder="Search..."
+                                                        id="generalSearch">
+                                                    <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                        <span><i class="la la-search"></i></span>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="m-form__group m-form__group--inline">
+                                                    <div class="m-form__label">
+                                                        <label>Date:</label>
+                                                    </div>
+                                                    <div class="m-form__control">
+                                                        @component('frontend.common.input.daterangepicker')
+                                                            @slot('id','daterange_purchase_order')
+                                                        @endcomponent
+                                                    </div>
+                                                </div>
+                                                <div class="d-md-none m--margin-bottom-10"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 order-1 order-xl-2 m--align-right">
+                                        @component('frontend.common.buttons.create')
+                                            @slot('text', 'Purchase Order')
+                                            @slot('href', route('frontend.purchase-order.create') )
+                                        @endcomponent
+
+                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="purchase_order_datatable" id="purchase_order_datatable"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('js/frontend/purchase-order/index.js')}}"></script>
+    <script src="{{ asset('js/frontend/functions/daterange/purchase-order.js') }}" ></script>
+
+@endpush
