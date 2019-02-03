27358da3f819485c5a0f6c43c9ff95d7961a1679
audhy <audhy.vk01@gmail.com>
2019-02-03 21:16:20 +0700

    search po component

--- resources/views/frontend/common/good-recieved-note/index.blade.php
+++ resources/views/frontend/common/good-recieved-note/index.blade.php
@@ -4,19 +4,19 @@
   <div class="search-journal" id="search-journal"
        style="line-height:{{ $line_height or '50px' }};
               margin-left:{{ $margin_left or '9px' }};">
-      {{ $text or 'Search Purchase Request' }}
-      @component('frontend.common.purchase-request.button-create')
+      {{ $text or 'Search Purchase Order' }}
+      @component('frontend.common.good-recieved-note.button-create')
           @slot('text', '')
           @slot('size', 'sm')
           @slot('icon', 'search')
-          @slot('data_target', '#modal_purchase_request')
+          @slot('data_target', '#modal_purchase_order')
           @slot('style','float:right;margin-top:10px;margin-right:6px;')
       @endcomponent
   </div>
 </div>
-@include('frontend.common.purchase-request.modal')
+@include('frontend.common.good-recieved-note.modal')
 
 @push('footer-scripts')
-    <script src="{{ asset('js/frontend/common/purchase-request.js') }}"></script>
+    <script src="{{ asset('js/frontend/common/good-recieved-note.js') }}"></script>
     <script src="{{ asset('assets/metronic/vendors/custom/datatables/datatables.bundle.js') }}"></script>
 @endpush
