70bdffba90e32c7278d2b10af201965beb0005d1
audhy <audhy.vk01@gmail.com>
2019-02-04 07:44:00 +0700

    grn create ui

--- resources/views/frontend/good-recieved-note/create.blade.php
+++ resources/views/frontend/good-recieved-note/create.blade.php
@@ -78,7 +78,7 @@
                                                         <label class="form-control-label">
                                                             Ref PO @include('frontend.common.label.required')
                                                         </label>
-                                                        @include('frontend.common.good-recieved-note.index')
+                                                        @include('frontend.common.purchase-order.index')
 
                                                         @component('frontend.common.input.hidden')
                                                             @slot('id', 'ref-po')
@@ -157,13 +157,17 @@
                                                             Warehouse @include('frontend.common.label.required')
                                                         </label>
 
-                                                        @component('frontend.common.input.textarea')
-                                                            @slot('id', 'shipping_address')
-                                                            @slot('text', 'Shipping Adddress')
-                                                            @slot('name', 'shipping_address')
-                                                            @slot('rows', '3')
-                                                            @slot('id_error', 'shipping_address')
+                                                        @include('frontend.common.warehouse.index')
+
+                                                        @component('frontend.common.input.hidden')
+                                                            @slot('id', 'warehouse')
+                                                            @slot('name', 'warehouse')
                                                         @endcomponent
+                                                    </div>
+                                                </div>
+                                                <div class="form-group m-form__group row">
+                                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                                        <div class="purchase_order_datatable" id="purchase_order_datatable"></div>
                                                     </div>
                                                 </div>
                                                 <div class="form-group m-form__group row">
@@ -216,6 +220,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
+
                                             </div>
                                         </div>
                                         <div class="form-group m-form__group row">
@@ -247,95 +252,11 @@
     </div>
 @endsection
 
-@push('header-scripts')
-    <style>
-        #map { height: 200px; }
-    </style>
-    <style>
-        fieldset {
-            margin-bottom: 30px;
-        }
-
-        .padding-datatable {
-            padding: 0px
-        }
-
-        .margin-info {
-            margin-left: 5px
-        }
-    </style>
-@endpush
 @push('footer-scripts')
-    <script>
-    function myFunction(object) {
-        // var numItems = $('.project').length
-
-        // var x = this.getElementById("type_website");
-        var x = object;
-        var y = x.name;
-
-        var numb = y.match(/\d/g);
-        var z = x.value;
-        var projectUuid = z;
-    }
-    </script>
-
-    <script type="text/javascript">
-        let simpan = $('.tes').on('click', '.save', function () {
-        var usertype=[];
-        $("select[name=project]").each(function(){
-            usertype.push($(this).val());
-            // alert($(this).val());
-        });
-        var ajaxdata={"UserType":usertype};
-
-        console.log(JSON.stringify(ajaxdata));
-        });
-    </script>
-    <script>
-        function initMap() {
-            var myLatLng = {lat: -7.265757, lng: 112.734146};
-
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom    : 10,
-                center  : myLatLng
-            });
-
-            var marker = new google.maps.Marker({
-                position    : myLatLng,
-                map         : map,
-                title       : 'Hello World!'
-            });
-        }
-    </script>
-    <script src="{{ asset('js/frontend/functions/repeater-core.js') }}"></script>
-
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
-
-    <script src="{{ asset('js/frontend/functions/select2/supplier.js') }}"></script>
-
-    <script src="{{ asset('js/frontend/functions/select2/customer.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/fill-combobox/customer.js') }}"></script>
-
-    <script src="{{ asset('js/frontend/functions/select2/currency.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/fill-combobox/currency.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/project.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/fill-combobox/scheduled-payment-type.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/fill-combobox/project.js') }}"></script>
 
 
-    <script src="{{ asset('js/frontend/functions/select2/ref.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/phone.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/email.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/fax.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/address.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/attn.js') }}"></script>
-    <script src="{{ asset('js/frontend/functions/select2/scheduled-payment-type.js') }}"></script>
 
-    <script src="{{ asset('js/frontend/quotation/create.js') }}"></script>
-    <script src="{{ asset('js/frontend/quotation/form-reset.js') }}"></script>
+    <script src="{{ asset('js/frontend/good-recieved-note/create.js') }}"></script>
     <script src="{{ asset('js/frontend/functions/datepicker/date.js')}}"></script>
-    <script src="{{ asset('js/frontend/functions/datepicker/valid-until.js')}}"></script>
-    <script src="{{ asset('js/frontend/functions/datepicker/date-shipping.js')}}"></script>
 
 @endpush
