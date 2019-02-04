6e43dc98359ec5eff0dc83122ca8d0399ecbaa1b
R. Kukuh <rkukuh@gmail.com>
2019-02-04 19:30:56 +0700

    Remove unused testing routes

--- routes/testing.php
+++ routes/testing.php
@@ -4,10 +4,6 @@ Route::name('testing.')->group(function () {
 
     Route::group(['prefix' => '_test'], function () {
 
-        Route::get('/greeting/{name?}', function ($name = 'World') {
-            return "Hello $name !";
-        });
-
         Route::get('/tools', function () {
             // $tool = App\Models\Category::ofItem()->where('code', 'tool')->first()->code;
             // return $tool;
@@ -31,16 +27,7 @@ Route::name('testing.')->group(function () {
 
             return $items;
         });
-
-        // Route::resource('/testing', 'Frontend\TestingController');
-        // Route::view('/maps', 'Frontend\testing\maps')->name('maps');
-        // Route::get('/view', 'Frontend\TestingController@view')->name('view');
-        // Route::get('/blank', 'Frontend\TestingController@blank')->name('blank');
-        // Route::post('/text', 'Frontend\TestingController@text')->name('text');
-        // Route::post('/photo', 'Frontend\TestingController@photo')->name('photo');
-        // Route::get('/metronic', 'Frontend\TestingController@metronic')->name('metronic');
-        // Route::post('/testing-photos','Frontend\TestingController@postPhotos')->name('testing-photos');
-
+        
         Route::view('/select2', 'frontend/testing/select2')->name('select2');
         Route::get('test', 'Frontend\FillComboxController@test')->name('test');
 
