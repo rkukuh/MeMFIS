f4fd4a21e197935cf3422c0b521232e4698508e7
R. Kukuh <rkukuh@gmail.com>
2019-02-08 19:36:42 +0700

    Define factory attributes values

--- database/factories/PurchaseRequestFactory.php
+++ database/factories/PurchaseRequestFactory.php
@@ -1,12 +1,28 @@
 <?php
 
-use App\Models\PurchaseRequest;
+use Carbon\Carbon;
+use App\Models\Type;
 use Faker\Generator as Faker;
+use App\Models\PurchaseRequest;
 
 $factory->define(PurchaseRequest::class, function (Faker $faker) {
 
+    $number  = $faker->unixTime();
+
     return [
-        //
+        'number' => $number,
+        'type_id' => function () use ($faker) {
+            if (Type::ofPurchaseRequest()->count()) {
+                Type::ofPurchaseRequest()->get()->random()->id;
+            }
+
+            return $faker->randomElement([
+                factory(Type::class)->states('purchase-request')->create()->id,
+            ]);
+        },
+        'requested_at' => $faker->randomElement([null, Carbon::now()]),
+        'required_at' => $faker->randomElement([null, Carbon::now()]),
+        'description' => $faker->randomElement([null, $faker->paragraph(rand(10, 20))]),
     ];
 
 });
