336e944da555b23e8d9b45cc20c529cebeac7cff
audhy <audhy.vk01@gmail.com>
2019-02-09 15:17:12 +0700

    WIP

--- public/js/frontend/common/purchase-request.js
+++ public/js/frontend/common/purchase-request.js
@@ -1,59 +1,171 @@
-let PurchaseRequest = {
+let PurchaseOrder = {
     init: function () {
-        $("#purhcase_request_datatable").DataTable({
-            "dom": '<"top"f>rt<"bottom">pl',
-            responsive: !0,
-            searchDelay: 500,
-            processing: !0,
-            serverSide: !0,
-            lengthMenu: [5, 10, 25, 50 ],
-            pageLength:5,
-            ajax: "/datatables/purchase-request/modal",
-            columns: [
-                {
-                    data: "number"
-                },
-                {
-                    data: "type.name"
-                },
-                {
-                    data: "Actions"
-                }
-            ],
-            columnDefs: [
-                {
-                    targets: -1,
-                    orderable: !1,
-                    render: function (a, e, t, n) {
-                        return '<a class="btn btn-primary btn-sm m-btn--hover-brand select-account_code" title="View" data-uuid="' + t.uuid + '" data-number="' + t.number + '" >\n<span><i class="la la-edit"></i><span>Use</span></span></a>'
+        $('.purchase_order_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/quotation',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
                     }
                 },
-
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'id',
+                    title: '#',
+                    sortable: !1,
+                    width: 40
+                },
+                {
+                    field: 'quotation_number',
+                    title: 'Date',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'customer',
+                    title: 'PO Number',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'valid_until',
+                    title: 'Pr Number',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'status',
+                    title: 'Vendor',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'status',
+                    title: 'Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'status',
+                    title: 'Approved By',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: 'Actions',
+                    width: 110,
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_customer" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill show" title="Details" data-id=' +
+                            t.id +
+                            '>\t\t\t\t\t\t\t<i class="la la-search"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t'
+                        );
+                    }
+                }
             ]
-        })
-
-        $('<a class="btn m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air btn-primary btn-sm refresh" style="margin-left: 60%; color: white;"><span><i class="la la-refresh"></i><span>Reload</span></span> </button>').appendTo('div.dataTables_filter');
-        $('.paging_simple_numbers').addClass('pull-left');
-        $('.dataTables_length').addClass('pull-right');
-        $('.dataTables_info').addClass('pull-left');
-        $('.dataTables_info').addClass('margin-info');
-        $('.paging_simple_numbers').addClass('padding-datatable');
-
-        $('.dataTables_filter').on('click', '.refresh', function () {
-            $('#m_datatable_journal').DataTable().ajax.reload();
         });
 
-        $('.dataTable').on('click', '.select-account_code', function () {
-            let uuid = $(this).data('uuid');
-            let number = $(this).data('number');
+        let remove = $('.m_datatable').on('click', '.delete', function () {
+            let triggerid = $(this).data('id');
 
-            document.getElementById('ref-pr').value = uuid;
-            $('.search-purchase_request').html(number);
-            $('#modal_purchase_request').modal('hide');
+            swal({
+                title: 'Are you sure?',
+                text: 'You will not be able to recover this imaginary file!',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, keep it'
+            }).then(result => {
+                if (result.value) {
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
+                                'content'
+                            )
+                        },
+                        type: 'DELETE',
+                        url: '/category/' + triggerid + '',
+                        success: function (data) {
+                            toastr.success(
+                                'Data Berhasil Dihapus.',
+                                'Sukses!', {
+                                    timeOut: 5000
+                                }
+                            );
+
+                            let table = $('.m_datatable').mDatatable();
+
+                            table.originalDataSet = [];
+                            table.reload();
+                        },
+                        error: function (jqXhr, json, errorThrown) {
+                            let errorsHtml = '';
+                            let errors = jqXhr.responseJSON;
+
+                            $.each(errors.errors, function (index, value) {
+                                $('#delete-error').html(value);
+                            });
+                        }
+                    });
+                    swal(
+                        'Deleted!',
+                        'Your imaginary file has been deleted.',
+                        'success'
+                    );
+                } else {
+                    swal(
+                        'Cancelled',
+                        'Your imaginary file is safe :)',
+                        'error'
+                    );
+                }
+            });
         });
+
     }
 };
 
 jQuery(document).ready(function () {
-    PurchaseRequest.init();
+    PurchaseOrder.init();
 });
