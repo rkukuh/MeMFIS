1f2b43f452c93fea55695b83add6d601a9e4d361
R. Kukuh <rkukuh@gmail.com>
2019-02-09 15:57:21 +0700

    Define attributes faker value

--- database/factories/GoodsReceivedFactory.php
+++ database/factories/GoodsReceivedFactory.php
@@ -1,12 +1,20 @@
 <?php
 
+use Carbon\Carbon;
 use App\Models\GoodsReceived;
 use Faker\Generator as Faker;
 
 $factory->define(GoodsReceived::class, function (Faker $faker) {
 
+    $number  = $faker->unixTime();
+    $plate_no = strtoupper($faker->randomLetter) . ' ' . $faker->numberBetween($min = 1000, $max = 9999) . ' ' . strtoupper($faker->randomLetter) . strtoupper($faker->randomLetter);
+
     return [
-        //
+        'number' => 'GRN-' . $number,
+        'received_at' => $faker->randomElement([null, Carbon::now()]),
+        'vehicle_no' => $faker->randomElement([null, $plate_no]),
+        'container_no' => $faker->randomElement([null, 'CON-' . $faker->numberBetween($min = 1000, $max = 9999)]),
+        'description' => $faker->randomElement([null, $faker->paragraph(rand(10, 20))]),
     ];
 
 });
