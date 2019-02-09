7abdb43011ede0d15acd7fa3e4045aa2c4e20aeb
R. Kukuh <rkukuh@gmail.com>
2019-02-09 12:05:31 +0700

    Add columns

--- database/migrations/2018_09_24_143842_create_suppliers_table.php
+++ database/migrations/2018_09_24_143842_create_suppliers_table.php
@@ -17,21 +17,10 @@ class CreateSuppliersTable extends Migration
             $table->increments('id');
             $table->char('uuid', 36)->unique();
             $table->string('code');
-            $table->string('npwp');
-            $table->string('nppkp');
-            $table->integer('top');
-            $table->integer('active');
-            $table->unsignedInteger('account_code')->nullable();
-            $table->string('contact_person');
-            $table->string('contact_person_position');
-            $table->string('barcode');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('account_code')
-                  ->references('id')->on('journals')
-                  ->onUpdate('cascade')
-                  ->onDelete('restrict');
+            $table->index('code');
         });
     }
 
