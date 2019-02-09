180658592062d1b653fa0a3983012250acb91e2b
R. Kukuh <rkukuh@gmail.com>
2019-02-09 15:56:58 +0700

    Define fillable and dates attributes

--- app/Models/GoodsReceived.php
+++ app/Models/GoodsReceived.php
@@ -7,4 +7,14 @@ use App\MemfisModel;
 class GoodsReceived extends MemfisModel
 {
     protected $table = 'goods_received';
+
+    protected $fillable = [
+        'number',
+        'received_at',
+        'vehicle_no',
+        'container_no',
+        'description',
+    ];
+
+    protected $dates = ['received_at'];
 }
