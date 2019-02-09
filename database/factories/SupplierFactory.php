65b0c868f353e0e530b86326e99647351bec828a
R. Kukuh <rkukuh@gmail.com>
2019-02-09 12:05:52 +0700

    Define attribute's faker

--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -5,8 +5,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Supplier::class, function (Faker $faker) {
 
+    $number  = $faker->unixTime();
+
     return [
-        //
+        'code' => 'SUP-' . $number,
     ];
 
 });
