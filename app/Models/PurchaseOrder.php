634278d7b8dfda3270d7286f1168b991986143cc
R. Kukuh <rkukuh@gmail.com>
2019-02-09 11:06:22 +0700

    Define fillable attributes

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -6,5 +6,17 @@ use App\MemfisModel;
 
 class PurchaseOrder extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'number',
+        'ordered_at',
+        'valid_until',
+        'ship_at',
+        'exchange_rate',
+        'total_before_tax',
+        'tax_amount',
+        'total_after_tax',
+        'description',
+    ];
+
+    protected $dates = ['ordered_at', 'valid_until', 'ship_at'];
 }
