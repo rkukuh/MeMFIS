2c535b29f0b2677ffb45a175e5151f4e92547b11
audhy <audhy.vk01@gmail.com>
2019-02-12 13:14:07 +0700

    change authorize true form request Grn

--- app/Http/Requests/Frontend/GoodsReceivedStore.php
+++ app/Http/Requests/Frontend/GoodsReceivedStore.php
@@ -13,7 +13,7 @@ class GoodsReceivedStore extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Frontend/GoodsReceivedUpdate.php
+++ app/Http/Requests/Frontend/GoodsReceivedUpdate.php
@@ -13,7 +13,7 @@ class GoodsReceivedUpdate extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
