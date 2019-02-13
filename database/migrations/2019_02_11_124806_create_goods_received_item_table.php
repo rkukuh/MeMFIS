61330223569b334eff879c1c8685a5f097ea1ebe
R. Kukuh <rkukuh@gmail.com>
2019-02-13 11:31:02 +0700

    Rename migration

--- database/migrations/2019_02_11_124806_create_goods_received_item_table.php
+++ database/migrations/2019_02_11_124806_create_goods_received_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGoodsReceivedItem extends Migration
+class CreateGoodsReceivedItemTable extends Migration
 {
     /**
      * Run the migrations.
