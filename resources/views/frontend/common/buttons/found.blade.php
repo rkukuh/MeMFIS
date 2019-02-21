9e713170af38903fdc979197a91354136c1e5f7a
audhy <audhy.vk01@gmail.com>
2019-02-21 08:42:06 +0700

    button found component

---
+++ resources/views/frontend/common/buttons/found.blade.php
@@ -0,0 +1,19 @@
+<button
+    id="{{ $id or '' }}"
+    type="{{ $type or 'submit' }}"
+    name="{{ $name or 'submit' }}"
+    value="{{ $value or '' }}"
+    class="btn
+           btn-{{ $color or 'primary' }}
+           btn-{{ $size or 'md' }}
+               {{ $class or '' }} add"
+    style="{{ $style or '' }}"
+    target="{{ $target or '' }}"
+>
+
+    <span>
+        <i class="fab {{ $icon or 'fa-get-pocket' }}"></i>
+
+        <span>{{ $text or 'Found Discrepancy' }}</span>
+    </span>
+</button>
