bbda14356b613917c97476aca88a8f0af65bdc37
audhy <audhy.vk01@gmail.com>
2019-02-21 10:39:43 +0700

    job-card js

---
+++ public/js/frontend/job-card/index.js
@@ -0,0 +1,177 @@
+let Item = {
+    init: function () {
+        $('.job_card_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/item',
+
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'JO No.',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        return '<a href="/job-card/ppc/show">' + t.code + "</a>"
+                    }
+                },
+                {
+                    field: 'name',
+                    title: 'TC No',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Task',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Skill',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Material(s)',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Tool(s)',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Status',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Est. Mhrs',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit',
+                    title: 'Actual. Mhrs',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-id="' + t.uuid +'">' +
+                                '<i class="la la-print"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+        $('.job_card_datatable').on('click', '.delete', function () {
+            let item_uuid = $(this).data('id');
+
+            swal({
+                title: 'Sure want to remove?',
+                type: 'question',
+                confirmButtonText: 'Yes, REMOVE',
+                confirmButtonColor: '#d33',
+                cancelButtonText: 'Cancel',
+                showCancelButton: true,
+            })
+            .then(result => {
+                if (result.value) {
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
+                                'content'
+                            )
+                        },
+                        type: 'DELETE',
+                        url: '/item/' + item_uuid + '',
+                        success: function (data) {
+                            toastr.success('Material has been deleted.', 'Deleted', {
+                                    timeOut: 5000
+                                }
+                            );
+
+                            let table = $('.job_card_datatable').mDatatable();
+
+                            table.originalDataSet = [];
+                            table.reload();
+                        },
+                        error: function (jqXhr, json, errorThrown) {
+                            let errors = jqXhr.responseJSON;
+
+                            $.each(errors.errors, function (index, value) {
+                                $('#delete-error').html(value);
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    Item.init();
+});
