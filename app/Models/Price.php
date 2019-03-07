4c672f3d03d0be5d9391fb6f9472b16064dbb14b
R. Kukuh <rkukuh@gmail.com>
2019-03-07 14:42:04 +0700

    Define fillable attributes and relationship

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -6,5 +6,23 @@ use App\MemfisModel;
 
 class Price extends MemfisModel
 {
-    //
+    protected $fillable = [
+        'priceable_type',
+        'priceable_id',
+        'amount',
+    ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * Polymorphic: An entity can have zero or many prices.
+     *
+     * This function will get all of the owning priceable models.
+     * See:
+     * - Item's prices() method for the inverse
+     */
+    public function priceable()
+    {
+        return $this->morphTo();
+    }
 }
