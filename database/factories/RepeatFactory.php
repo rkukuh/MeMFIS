f57ff571386c845f861a4f1e7c34050c85f8c838
R. Kukuh <rkukuh@gmail.com>
2019-03-07 13:50:48 +0700

    Define attributes' faker

--- database/factories/RepeatFactory.php
+++ database/factories/RepeatFactory.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Models\Type;
 use App\Models\Repeat;
 use Faker\Generator as Faker;
 
 $factory->define(Repeat::class, function (Faker $faker) {
 
     return [
-        //
+        'type_id' => function () {
+            if (Type::ofMaintenanceCycle()->count()) {
+                Type::ofMaintenanceCycle()->get()->random()->id;
+            }
+
+            return factory(Type::class)->states('maintenance-cycle')->create()->id;
+        },
+        'amount' => rand(2, 10) * 100,
     ];
 
 });
