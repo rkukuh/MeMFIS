5cc8020936f1437792c52ce874d8ec68974d76be
ibnuprtma <pibnu87@gmail.com>
2019-03-08 12:51:17 +0700

    fix issue #135

--- resources/views/frontend/job-card/engineer/index.blade.php
+++ resources/views/frontend/job-card/engineer/index.blade.php
@@ -48,9 +48,9 @@
                     </div>
                     <div class="m-portlet m-portlet--mobile">
                         <div class="m-portlet__body">
-                            <div class="row align-items-center">
-                                <div class="col-xl-8 order-2 order-xl-1">
-                                    <div class="form-group m-form__group row align-items-center">
+                            <div class="row align-items-center" style="margin-bottom:150px; margin-top:150px;">
+                                <div class="col-xl-12 order-2 order-xl-1">
+                                    <div class="form-group m-form__group row align-items-center d-flex justify-content-center">
                                         <div class="col-md-4">
                                             <div class="m-input-icon m-input-icon--left">
                                                 <input type="text" class="form-control m-input" placeholder="Search..."
@@ -59,12 +59,14 @@
                                                     <span><i class="la la-search"></i></span>
                                                 </span>
                                             </div>
+                                            <div class="d-flex justify-content-end mt-4">
+                                                @component('frontend.common.buttons.create')
+                                                    @slot('text', 'Search')
+                                                    @slot('icon','search')
+                                                @endcomponent
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-
-                                    <div class="m-separator m-separator--dashed d-xl-none"></div>
                                 </div>
                             </div>
                         </div>
