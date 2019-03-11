ef65431d7b0f2774d1aab50748754ef867a86839
ibnuprtma <pibnu87@gmail.com>
2019-03-11 14:09:43 +0700

    redesign show project

--- resources/views/frontend/project/hm/show.blade.php
+++ resources/views/frontend/project/hm/show.blade.php
@@ -22,24 +22,25 @@
             </div>
             <div class="form-group m-form__group row">
               <div class="col-sm-12 col-md-12 col-lg-12">
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active show" data-toggle="tab" href="#" data-target="#m_tabs_taskcard">Taskcard List(s)</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#m_tabs_tool_material">General Tool and Material</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#m_tabs_enginner">Engineer Team</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#m_tabs_manhour">Manhours Propotion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#m_tabs_facility">Facility Used</a>
-                    </li>
-                </ul>
-
+                <div class="px-2 py-2">
+                  <ul class="nav nav-tabs" role="tablist">
+                      <li class="nav-item">
+                          <a class="nav-link active show" data-toggle="tab" href="#" data-target="#m_tabs_taskcard">Taskcard List(s)</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#m_tabs_tool_material">General Tool and Material</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#m_tabs_enginner">Engineer Team</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#m_tabs_manhour">Manhours Propotion</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#m_tabs_facility">Facility Used</a>
+                      </li>
+                  </ul>
+                </div>
                 <div class="tab-content">
                     <div class="tab-pane active show" id="m_tabs_taskcard" role="tabpanel">
                         @include('frontend.project.hm.taskcard.index')
