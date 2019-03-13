81c59e0a979def82451f0f233683ad40a60c6346
audhy <audhy.vk01@gmail.com>
2019-03-13 10:39:09 +0700

    load datatable threshold and repeat

--- public/js/frontend/taskcard/non-routine/si/show.js
+++ public/js/frontend/taskcard/non-routine/si/show.js
@@ -130,6 +130,127 @@ let TaskCard = {
             ]
         });
 
+
+        $('.threshold_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard-routine/'+taskcard_uuid+'/thresholds',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                perpage: 5,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'amount',
+                    title: 'Amount',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'type_id',
+                    title: 'Type',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+
+            ]
+        });
+
+        $('.repeat_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/taskcard-routine/'+taskcard_uuid+'/repeats',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                perpage: 5,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: 'amount',
+                    title: 'Amount',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'type_id',
+                    title: 'Type',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+
+            ]
+        });
+
     }
 };
 
