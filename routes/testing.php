e84f932ff0a5e0ee3c3aec872b0827d6797603a4
audhy <audhy.vk01@gmail.com>
2019-03-13 10:09:38 +0700

    testing where type of taskcard

--- routes/testing.php
+++ routes/testing.php
@@ -7,6 +7,28 @@ Route::name('testing.')->group(function () {
         Route::view('/select2', 'frontend/testing/select2')->name('select2');
         Route::get('test', 'Frontend\FillComboxController@test')->name('test');
 
+        Route::get('/basic', function () {
+            // $tool = App\Models\Category::ofItem()->where('code', 'tool')->first()->code;
+            // return $tool;
+        // $taskcards = App\Models\TaskCard::with('type', function($query){
+        //     $query->where('name', 'Basic');
+        // })->get();
+
+            $taskcards  = App\Models\TaskCard::with('type')
+                                    ->whereHas('type', function ($query) {
+                                        $query->where('name', 'Basic');
+                                    })->get();
+
+        dd($taskcards);
+            // $items = App\Models\Item::with('categories')
+            //                         ->whereHas('categories', function ($query) {
+            //                             $query->where('code', 'tool');
+            //                         })->get();
+
+            // return $items;
+        });
+
+
         Route::view('/select2-repeater', 'frontend/testing/select2Repeater')->name('select2-repeater');
         Route::view('/select2-repeater2', 'frontend/testing/repeaterBlank')->name('select2-repeater2');
         Route::view('/datatable', 'frontend/testing/datatable')->name('datatable');
