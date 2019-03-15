d548d90e3a6300a5e631f66a7c7f43cb3b04ac8a
audhy <audhy.vk01@gmail.com>
2019-03-15 13:59:13 +0700

    add new fill combobox

---
+++ public/js/frontend/functions/fill-combobox/category-material.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    category = function () {
+        $.ajax({
+            url: '/get-categories-material/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+
+                $('select[name="category"]').empty();
+
+                $('select[name="category"]').append(
+                    '<option value=""> Select a Category</option>'
+                );
+
+                $.each(data, function (key, value) {
+
+                    $('select[name="category"]').append(
+                        '<option value="' + key + '">' + value + '</option>'
+                    );
+                });
+            }
+        });
+    };
+
+    category();
+});
