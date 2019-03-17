2d18014a5a0a16da40c9ba1b75eaef3b3eacc01b
audhy <audhy.vk01@gmail.com>
2019-03-17 19:38:09 +0700

    remove duplicate portlet

--- resources/views/frontend/taskcard/nonroutine/si/create.blade.php
+++ resources/views/frontend/taskcard/nonroutine/si/create.blade.php
@@ -336,68 +336,6 @@
                                 </span>
 
                                 <h3 class="m-portlet__head-text">
-                                    Threshold
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="m-portlet m-portlet--mobile">
-                        <div class="m-portlet__body">
-                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                <div class="row align-items-center">
-                                    <div class="col-xl-12 order-12 order-xl-12 m--align-right">
-
-                                        @component('frontend.common.buttons.create-new')
-                                            @slot('attribute', 'disabled')
-                                        @endcomponent
-
-                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                                <span class="m-portlet__head-icon m--hide">
-                                    <i class="la la-gear"></i>
-                                    </span>
-
-                                <h3 class="m-portlet__head-text">
-                                    Repeat
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="m-portlet m-portlet--mobile">
-                        <div class="m-portlet__body">
-                            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                <div class="row align-items-center">
-                                    <div class="col-xl-12 order-12 order-xl-12 m--align-right">
-
-                                        @component('frontend.common.buttons.create-new')
-                                            @slot('attribute', 'disabled')
-                                        @endcomponent
-
-                                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                                <span class="m-portlet__head-icon m--hide">
-                                    <i class="la la-gear"></i>
-                                </span>
-
-                                <h3 class="m-portlet__head-text">
                                     Thresholds
                                 </h3>
                             </div>
