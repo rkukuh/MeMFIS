2a1b995a98948ae65b0b12db978f2c1a7c58ded4
audhy <audhy.vk01@gmail.com>
2019-03-19 09:37:14 +0700

    fix issue #144 and #145

--- public/js/frontend/functions/fill-combobox/tag.js
+++ public/js/frontend/functions/fill-combobox/tag.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
                 $.each(data, function (key, value) {
                     $('select[name="tag"]').append(
-                        '<option value="' + key + '">' + value + '</option>'
+                        '<option value="' + value + '">' + value + '</option>'
                     );
                 });
             }
