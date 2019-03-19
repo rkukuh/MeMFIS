0a834bef0820b25af3ba0da8f1c01c27ff445952
audhy <audhy.vk01@gmail.com>
2019-03-19 13:33:25 +0700

    rename description to Instruction

--- resources/views/frontend/taskcard/routine/create.blade.php
+++ resources/views/frontend/taskcard/routine/create.blade.php
@@ -369,7 +369,7 @@
                                     <div class="form-group m-form__group row">
                                         <div class="col-sm-12 col-md-12 col-lg-12">
                                             <label class="form-control-label">
-                                                Description @include('frontend.common.label.optional')
+                                                Instruction @include('frontend.common.label.optional')
                                             </label>
 
                                             @component('frontend.common.input.textarea')
