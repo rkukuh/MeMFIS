a5aaf2761db8de6f9ad232f8a9c41da9afeb62c3
audhy <audhy.vk01@gmail.com>
2019-04-04 09:16:34 +0700

    grouping project by type

--- routes/frontend.php
+++ routes/frontend.php
@@ -69,8 +69,20 @@ Route::name('frontend.')->group(function () {
         Route::resource('certification-employee', 'CertificationEmployeeController');
 
         /** TRANSACTION */
+        Route::namespace('Project')->group(function () {
 
-        Route::resource('project', 'ProjectController');
+            Route::view('/project', 'frontend.project.index')->name('project.index');
+
+            Route::resource('project-hm', 'ProjectHMController', [
+                'parameters' => ['project-hm' => 'project']
+            ]);
+            Route::resource('project-workshop', 'ProjectWorkshopController', [
+                'parameters' => ['project-workshop' => 'project']
+            ]);
+
+        });
+
+
         Route::resource('quotation', 'QuotationController');
         Route::get('quotation/{project}/project', 'QuotationController@project')->name('quotation.project');
 
@@ -329,7 +341,7 @@ Route::name('frontend.')->group(function () {
         Route::view('/summary/workpackage-summary', 'frontend.workpackage.summary')->name('summary.workpackage-summary');
         Route::view('/summary/routine-summary', 'frontend.workpackage.routine.summary')->name('summary.routine-summary');
         Route::view('/summary/nonroutine-summary', 'frontend.workpackage.nonroutine.summary')->name('summary.nonroutine-summary');
-        
+
         /** PRICE LIST */
 
         Route::view('/price-list', 'frontend.price-list.index')->name('price-list.index');
