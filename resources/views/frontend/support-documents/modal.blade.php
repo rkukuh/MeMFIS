5c10e8d1077daca7a3d33b84d55b2379f2d34390
ThamrinPH <husni.sosnet@gmail.com>
2019-04-04 08:48:12 +0700

    Refactor Support documents

--- resources/views/frontend/support-documents/modal.blade.php
+++ resources/views/frontend/support-documents/modal.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="modal_support_documents" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal_add_document" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="TitleModalAircraft">Aircraft</h5>
+                <h5 class="modal-title" id="TitleModalAircraft">Support Documents</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
