6b9960a54dcb18d5736b74d296ca2dc745747cb9
ThamrinPH <husni.sosnet@gmail.com>
2019-04-05 15:33:56 +0700

    Project-HM update validation

--- app/Http/Requests/Frontend/ProjectHMUpdate.php
+++ app/Http/Requests/Frontend/ProjectHMUpdate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Frontend;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProjectHMUpdate extends FormRequest
 {
@@ -24,7 +26,40 @@ class ProjectHMUpdate extends FormRequest
     public function rules()
     {
         return [
-            //
+            'customer_id' => 'required',
+            'no_wo' => 'required',
+            'aircraft_id' => 'required',
+            'aircraft_register' => 'required',
+            'aircraft_sn' => 'required',
         ];
+    }
+
+    /**
+     * Set custom validation error message
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'aircraft_sn.required' => 'The aircraft serial number is required',
+            'customer_id.required' => 'The customer is required',
+            'aircraft_id.required' => 'The aircraft is required',
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        //
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()]));
     }
 }
