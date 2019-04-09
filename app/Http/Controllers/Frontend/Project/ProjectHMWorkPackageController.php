89d3de1c7f14cdb5497a45116a189ad759cb1fd6
audhy <audhy.vk01@gmail.com>
2019-04-09 14:19:49 +0700

    refactor

--- app/Http/Controllers/Frontend/Project/ProjectHMWorkPackageController.php
+++ app/Http/Controllers/Frontend/Project/ProjectHMWorkPackageController.php
@@ -107,9 +107,8 @@ class ProjectHMWorkPackageController extends Controller
      * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Project $project,$workPackage)
+    public function destroy(Project $project,WorkPackage $workPackage)
     {
-        $workPackage = WorkPackage::where('uuid',$workPackage)->first();
         $project->workpackages()->detach($workPackage);
 
         return response()->json($project);
