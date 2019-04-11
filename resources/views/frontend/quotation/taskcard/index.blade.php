9aa4f6700a868c87d046c3da66f2a59a29509bd8
audhy <audhy.vk01@gmail.com>
2019-04-11 13:55:43 +0700

    refactor path file

--- resources/views/frontend/quotation/taskcard/index.blade.php
+++ resources/views/frontend/quotation/taskcard/index.blade.php
@@ -1,4 +1,43 @@
 <div class="px-4">
+    <div class="m-portlet__body">
+        <div class="form-group m-form__group row ">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <label class="form-control-label">
+                    Job Request Description @include('frontend.common.label.required')
+                </label>
+                @component('frontend.common.input.textarea')
+                    @slot('text', 'Description')
+                    @slot('name', 'description')
+                    @slot('id', 'description')
+                    @slot('rows', '5')
+                    @slot('id_error', 'description')
+                @endcomponent
+            </div>
+        </div>
+        <div class="form-group m-form__group row ">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    Total Manhours @include('frontend.common.label.required')
+                </label>
+                @component('frontend.common.label.data-info')
+                    @slot('text', $total_mhrs)
+                @endcomponent
+
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <label class="form-control-label">
+                    Manhours Rate @include('frontend.common.label.required')
+                </label>
+                @component('frontend.common.input.number')
+                    @slot('text', 'rate')
+                    @slot('name', 'rate')
+                    @slot('id', 'rate')
+                    @slot('id_error', 'rate')
+                @endcomponent
+            </div>
+        </div>
+    </div>
+
     <ul class="nav nav-tabs" role="tablist">
         <li class="nav-item">
             <a class="nav-link active show" data-toggle="tab" href="#" data-target="#m_tabs_1_1">Routine</a>
@@ -10,17 +49,17 @@
 </div>
 <div class="tab-content">
     <div class="tab-pane active show" id="m_tabs_1_1" role="tabpanel">
-        @include('frontend.project.hm.taskcard.routine.index')
-        @include('frontend.project.hm.taskcard.routine.basic.modal')
-        @include('frontend.project.hm.taskcard.routine.sip.modal')
-        @include('frontend.project.hm.taskcard.routine.cpcp.modal')
+        @include('frontend.quotation.taskcard.routine.index')
+        @include('frontend.quotation.taskcard.routine.basic.modal')
+        @include('frontend.quotation.taskcard.routine.sip.modal')
+        @include('frontend.quotation.taskcard.routine.cpcp.modal')
     </div>
     <div class="tab-pane" id="m_tabs_1_2" role="tabpanel">
-        @include('frontend.project.hm.taskcard.nonroutine.index')
-        @include('frontend.project.hm.taskcard.nonroutine.adsb.modal')
-        @include('frontend.project.hm.taskcard.nonroutine.cmrawl.modal')
-        @include('frontend.project.hm.taskcard.nonroutine.si.modal')
-        @include('frontend.project.hm.taskcard.nonroutine.htcrr.modal')
-        @include('frontend.project.hm.taskcard.nonroutine.htcrr.workshop-task')
+        @include('frontend.quotation.taskcard.nonroutine.index')
+        @include('frontend.quotation.taskcard.nonroutine.adsb.modal')
+        @include('frontend.quotation.taskcard.nonroutine.cmrawl.modal')
+        @include('frontend.quotation.taskcard.nonroutine.si.modal')
+        @include('frontend.quotation.taskcard.nonroutine.htcrr.modal')
+        @include('frontend.quotation.taskcard.nonroutine.htcrr.workshop-task')
     </div>
 </div>
