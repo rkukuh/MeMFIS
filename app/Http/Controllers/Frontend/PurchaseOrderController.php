c5d92c88650120dc9564490ff7537ed1652cd1cd
audhy <audhy.vk01@gmail.com>
2019-04-18 14:51:56 +0700

    create purchase order

--- app/Http/Controllers/Frontend/PurchaseOrderController.php
+++ app/Http/Controllers/Frontend/PurchaseOrderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Carbon\Carbon;
+use App\Models\Type;
 use App\Models\PurchaseOrder;
+use App\Models\PurchaseRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Frontend\PurchaseOrderStore;
 use App\Http\Requests\Frontend\PurchaseOrderUpdate;
@@ -37,7 +40,32 @@ class PurchaseOrderController extends Controller
      */
     public function store(PurchaseOrderStore $request)
     {
-        //
+        $request->purchase_request_id = PurchaseRequest::where('uuid',$request->purchase_request_id)->first()->id;
+        $request->ordered_at = Carbon::parse($request->ordered_at);
+        $request->valid_until = Carbon::parse($request->valid_until);
+        $request->ship_at = Carbon::parse($request->ship_at);
+        $request->top_start_at = Carbon::parse($request->top_start_at);
+        $request->top_type = Type::where('code',$request->top_type)->first()->id;
+
+
+        $purchaseOrder = PurchaseOrder::create([
+            'number' => $request->purchase_request_id,
+            'purchase_request_id' => $request->purchase_request_id,
+            'currency_id' => $request->currency_id,
+            'exchange_rate' => $request->exchange_rate,
+            'vendor_id' => $request->vendor_id,
+            'top_type' =>$request->top_type,
+            'shipping_address' => $request->shipping_address,
+            'description' => $request->description,
+            'top_day_amount'=> $request->top_day_amount,
+            'top_start_at'=> $request->top_start_at,
+
+            'ordered_at' => $request->ordered_at,
+            'valid_until' => $request->valid_until,
+            'ship_at' => $request->ship_at,
+            ]);
+
+        return response()->json($purchaseOrder);
     }
 
     /**
