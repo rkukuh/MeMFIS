b2a21d113f64b52d3d2b0a6c9b46ecfee8c51dea
ThamrinPH <husni.sosnet@gmail.com>
2019-04-25 15:02:44 +0700

    Spacing

--- resources/views/frontend/quotation/taskcard/nonroutine/index.blade.php
+++ resources/views/frontend/quotation/taskcard/nonroutine/index.blade.php
@@ -1,38 +1,39 @@
 <div class="m-portlet m-portlet--mobile">
-        <div class="m-portlet__body">
-            <h1>AD/SB</h1>
-            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="row align-items-center">
-                    <div class="col-xl-6 order-2 order-xl-1">
-                        <div class="form-group m-form__group row align-items-center">
-                            <div class="col-md-6">
-                                <div class="m-input-icon m-input-icon--left">
-                                    <input type="text" class="form-control m-input" placeholder="Search..."
-                                        id="generalSearch">
-                                    <span class="m-input-icon__icon m-input-icon__icon--left">
-                                        <span><i class="la la-search"></i></span>
-                                    </span>
-                                </div>
+    
+    <div class="m-portlet__body">
+        <h1>AD/SB</h1>
+        <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+            <div class="row align-items-center">
+                <div class="col-xl-6 order-2 order-xl-1">
+                    <div class="form-group m-form__group row align-items-center">
+                        <div class="col-md-6">
+                            <div class="m-input-icon m-input-icon--left">
+                                <input type="text" class="form-control m-input" placeholder="Search..."
+                                    id="generalSearch">
+                                <span class="m-input-icon__icon m-input-icon__icon--left">
+                                    <span><i class="la la-search"></i></span>
+                                </span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-6 order-1 order-xl-2 m--align-right">
-                        @component('frontend.common.buttons.summary')
-                            @slot('text', 'AD/SB Summary')
-                            @slot('href', route('frontend.summary.ad-sb', $workPackage->uuid) )
-                        @endcomponent
+                </div>
+                <div class="col-xl-6 order-1 order-xl-2 m--align-right">
+                    @component('frontend.common.buttons.summary')
+                        @slot('text', 'AD/SB Summary')
+                        @slot('href', route('frontend.summary.ad-sb', $workPackage->uuid) )
+                    @endcomponent
 
-                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                    </div>
+                    <div class="m-separator m-separator--dashed d-xl-none"></div>
                 </div>
             </div>
-
-            <div class="ad-sb_datatable wp-datatable" id="scrolling_both"></div>
         </div>
+
+        <div class="ad-sb_datatable wp-datatable" id="scrolling_both"></div>
     </div>
+
     <div class="m-portlet m-portlet--mobile">
         <div class="m-portlet__body">
-            <h1>CMR/AWL</h1>
+        <h1>CMR/AWL</h1>
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                 <div class="row align-items-center">
                     <div class="col-xl-6 order-2 order-xl-1">
@@ -63,9 +64,10 @@
             <div class="cmr-awl_datatable wp-datatable" id="scrolling_both"></div>
         </div>
     </div>
+
     <div class="m-portlet m-portlet--mobile">
         <div class="m-portlet__body">
-            <h1>Special Instruction</h1>
+        <h1>Special Instruction</h1>
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                 <div class="row align-items-center">
                     <div class="col-xl-6 order-2 order-xl-1">
@@ -96,9 +98,10 @@
             <div class="si_datatable wp-datatable" id="scrolling_both"></div>
         </div>
     </div>
+
     <div class="m-portlet m-portlet--mobile">
         <div class="m-portlet__body">
-            <h1>HT/CRR</h1>
+        <h1>HT/CRR</h1>
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                 <div class="row align-items-center">
                     <div class="col-xl-6 order-2 order-xl-1">
@@ -124,6 +127,8 @@
             <div class="ht_crr_datatable wp-datatable" id="scrolling_both"></div>
         </div>
     </div>
+
+</div>
 
     @push('footer-scripts')
         <script src="{{ asset('js/frontend/workpackage/non-routine/index.js')}}"></script>
