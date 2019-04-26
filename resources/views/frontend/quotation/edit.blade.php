76b1fb723ac37056c1c4885f08e15e118c96378e
audhy <audhy.vk01@gmail.com>
2019-04-26 11:07:23 +0700

    WIP

--- resources/views/frontend/quotation/edit.blade.php
+++ resources/views/frontend/quotation/edit.blade.php
@@ -406,6 +406,89 @@
 
                                                     <div class="summary_datatable" id="scrolling_both"></div>
 
+                                                    {{-- <table class="summary-datatable" id="html_table" width="100%">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td width=60%></td>
+                                                                <td width=20%>Sub Total</td>
+                                                                <td width=20%>
+                                                                    <select name="" id="">
+                                                                        <option value="">5%</option>
+                                                                        <option value="">10%</option>
+                                                                        <option value="">15%</option>
+                                                                    </select>
+                                                                </td>
+                                                            </tr> --}}
+                                                            {{-- <tr>
+                                                                <td width=60%></td>
+                                                                <td width=20%>Tax</td>
+                                                                <td width=20%>
+                                                                    <select name="" id="">
+                                                                        <option value="">5%</option>
+                                                                        <option value="">10%</option>
+                                                                        <option value="">15%</option>
+                                                                    </select>
+                                                                </td>
+                                                            </tr> --}}
+                                                            <br><hr>
+                                                            <div class="form-group m-form__group row">
+                                                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                                                </div>
+                                                                <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                    Sub Total
+                                                                </div>
+                                                                <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                    Rp. 100000000
+                                                                </div>
+                                                                <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                </div>
+                                                                <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                </div>
+                                                            </div>
+                                                            <div class='repeater'>
+                                                                    <div data-repeater-list="group-document">
+                                                                        <div data-repeater-item>
+                                                                            <div class="form-group m-form__group row">
+                                                                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                                                                </div>
+                                                                                <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                                    <select id="type_website" name="type_website" class="form-control">
+                                                                                        <option value="">
+                                                                                            Select a Type
+                                                                                        </option>
+                                                                                    </select>
+                                                                                </div>
+                                                                                <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                                        @component('frontend.common.input.text')
+                                                                                        @slot('id', 'document')
+                                                                                        @slot('name', 'document')
+                                                                                    @endcomponent
+                                                                                </div>
+                                                                                <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                                    @include('frontend.common.buttons.create_repeater')
+                                                                                </div>
+                                                                                <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                                    @include('frontend.common.buttons.delete_repeater')
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group m-form__group row">
+                                                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                                                        </div>
+                                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                            Total in Rupiah
+                                                                        </div>
+                                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                            Rp. 100000000
+                                                                        </div>
+                                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                        </div>
+                                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                        </div>
+                                                                    </div>
+
                                                 </div>
                                             </div>
                                         </div>
