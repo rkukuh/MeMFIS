3e0c4cdadf12a23ce59518b8233fe66f051d595f
ibnuprtma <pibnu86@gmail.com>
2019-05-01 14:19:19 +0700

    WIP quotation show summary

--- resources/views/frontend/quotation/show.blade.php
+++ resources/views/frontend/quotation/show.blade.php
@@ -361,54 +361,66 @@
 
                                                 </div>
                                                 <div class="tab-pane" id="m_tabs_summary" role="tabpanel">
-                                                    <table class="summary-datatable" id="html_table" width="100%">
-                                                        <thead>
-                                                            <tr>
-                                                                <th title="Field #1" data-field="No">No</th>
-                                                                <th title="Field #2" data-field="Job_Request">Job Request Description</th>
-                                                                <th title="Field #2" data-field="Cost">Cost</th>
-                                                                <th title="Field #2" data-field="Disc">Disc %</th>
-                                                            </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                            <tr>
-                                                                <td><a href="/quotation-view/workpackage">57520-0405</a> </td>
-                                                                <td>Sunny Garton</td>
-                                                                <td>$100000</td>
-                                                                <td>0%</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td><a href="/quotation-view/workpackage">43269-858</a></td>
-                                                                <td>Sandor Engley</td>
-                                                                <td>$50</td>
-                                                                <td>10%</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td></td>
-                                                                <td></td>
-                                                                <td>Sub Total</td>
-                                                                <td>$100050</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td></td>
-                                                                <td></td>
-                                                                <td>Tax</td>
-                                                                <td>
-                                                                    <select name="" id="">
-                                                                        <option value="">5%</option>
-                                                                        <option value="">10%</option>
-                                                                        <option value="">15%</option>
-                                                                    </select>
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td></td>
-                                                                <td></td>
-                                                                <td>Total in Rupiah</td>
-                                                                <td>Rp. 100000000</td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
+
+                                                    <div class="summary_datatable" id="scrolling_both"></div>
+                                                    <br><hr>
+                                                    <div class="form-group m-form__group row">
+                                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                                        </div>
+                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                            Sub Total
+                                                        </div>
+                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                            Rp. 100000000
+                                                        </div>
+                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                        </div>
+                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                        </div>
+                                                    </div>
+                                                    <div class='repeater'>
+                                                        <div data-repeater-list="group-document">
+                                                            <div data-repeater-item>
+                                                                <div class="form-group m-form__group row">
+                                                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                                                    </div>
+                                                                    <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                        <select id="type_website" name="type_website" class="form-control">
+                                                                            <option value="">
+                                                                                Select a Type
+                                                                            </option>
+                                                                        </select>
+                                                                    </div>
+                                                                    <div class="col-sm-2 col-md-2 col-lg-2">
+                                                                            @component('frontend.common.input.text')
+                                                                            @slot('id', 'document')
+                                                                            @slot('name', 'document')
+                                                                        @endcomponent
+                                                                    </div>
+                                                                    <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                        @include('frontend.common.buttons.create_repeater')
+                                                                    </div>
+                                                                    <div class="col-sm-1 col-md-1 col-lg-1">
+                                                                        @include('frontend.common.buttons.delete_repeater')
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group m-form__group row">
+                                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                                        </div>
+                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                            Total in Rupiah
+                                                        </div>
+                                                        <div class="col-sm-2 col-md-2 col-lg-2">
+                                                            Rp. 100000000
+                                                        </div>
+                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                        </div>
+                                                        <div class="col-sm-1 col-md-1 col-lg-1">
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -466,5 +478,6 @@
     <script src="{{ asset('js/frontend/functions/select2/currency.js') }}"></script>
     <script src="{{ asset('js/frontend/quotation/workpackage/show.js') }}"></script>
     <script src="{{ asset('js/frontend/quotation/show.js') }}"></script>
-
+    <script src="{{ asset('js/frontend/quotation/summary.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/repeater-core.js') }}"></script>
 @endpush
