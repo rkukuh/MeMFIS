91447190090ee2a12b7026b62ef79f59df73bbea
ThamrinPH <husni.sosnet@gmail.com>
2019-05-06 10:30:56 +0700

    refactor

--- public/js/frontend/workpackage/item/form-reset.js
+++ public/js/frontend/workpackage/item/form-reset.js
@@ -5,7 +5,7 @@ let material_reset = function () {
 }
 
 let tool_reset = function () {
-    document.getElementById('quantity_item').value = '';
+    document.getElementById('quantity').value = '';
     $('#tool').select2('val', 'All');
     $('#unit_tool').select2('val', 'All');
 }
