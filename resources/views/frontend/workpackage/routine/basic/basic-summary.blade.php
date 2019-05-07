fcab880b4097101c85c9d1edcb70b595f5f14d2b
ThamrinPH <husni.sosnet@gmail.com>
2019-05-07 13:11:44 +0700

    Basic Summary

--- resources/views/frontend/workpackage/routine/basic/basic-summary.blade.php
+++ resources/views/frontend/workpackage/routine/basic/basic-summary.blade.php
@@ -27,6 +27,7 @@
 
     <!--Begin::Section-->
     <div class="row">
+        
         <div class="col-xl-8">
             <div class="m-portlet m-portlet--mobile ">
                 <div class="m-portlet__head">
@@ -68,6 +69,7 @@
                 </div>
             </div>
         </div>
+
         <div class="col-xl-4">
             <div class="m-widget29">
                 <div class="m-portlet m-portlet--mobile">
@@ -110,6 +112,7 @@
                 </div>
             </div>
         </div>
+
     </div>
     <!--End::Section-->
 
@@ -209,17 +212,17 @@
 
             <div class="m-portlet m-portlet--mobile">
                 <div class="m-portlet__body">
-                    <div id="m_accordion_3" class="m-accordion m-accordion--default m-accordion--solid m-accordion--section m-accordion--padding-lg m-accordion--toggle-arrow" role="tablist">
+                    <div id="m_accordion_4" class="m-accordion m-accordion--default m-accordion--solid m-accordion--section m-accordion--padding-lg m-accordion--toggle-arrow" role="tablist">
 
                         <div class="m-accordion__item ">
-                            <div class="m-accordion__item-head collapsed" srole="tab" id="m_accordion_3_item_1_head" data-toggle="collapse" href="#m_accordion_3_item_1_body" aria-expanded="false">
+                            <div class="m-accordion__item-head collapsed" srole="tab" id="m_accordion_4_item_1_head" data-toggle="collapse" href="#m_accordion_4_item_1_body" aria-expanded="false">
                                 <span class="m-accordion__item-icon"></span>
-                                <span class="m-accordion__item-title"> <h1>Tool(S) List (from taskcard)</h1></span>
+                                <span class="m-accordion__item-title"> <h1>General Tools(S) List</h1></span>
 
                                 <span class="m-accordion__item-mode"></span>
                             </div>
 
-                            <div class="m-accordion__item-body collapse show" id="m_accordion_3_item_1_body" class=" " role="tabpanel" aria-labelledby="m_accordion_3_item_1_head" data-parent="#m_accordion_1">
+                            <div class="m-accordion__item-body collapse show" id="m_accordion_4_item_1_body" class=" " role="tabpanel" aria-labelledby="m_accordion_4_item_1_head" data-parent="#m_accordion_1">
 
                                 <div class="m-portlet m-portlet--mobile">
                                     <div class="m-portlet__body">
@@ -260,7 +263,7 @@
                         <div class="m-accordion__item ">
                             <div class="m-accordion__item-head collapsed" srole="tab" id="m_accordion_3_item_1_head" data-toggle="collapse" href="#m_accordion_3_item_1_body" aria-expanded="false">
                                 <span class="m-accordion__item-icon"></span>
-                                <span class="m-accordion__item-title"> <h1>Tool(S) List (from taskcard)</h1></span>
+                                <span class="m-accordion__item-title"> <h1>General Material(S) List</h1></span>
 
                                 <span class="m-accordion__item-mode"></span>
                             </div>
@@ -287,7 +290,7 @@
                                         </div>
 
                                         <div class="m-accordion__item-content">
-                                            <div class="general_tools_datatable" id="scrolling_both"></div>
+                                            <div class="general_materials_datatable" id="scrolling_both"></div>
 
                                         </div>
                                     </div>
@@ -298,76 +301,6 @@
                     </div>
                 </div>
             </div>
-
-            <div class="m-portlet m-portlet--mobile">
-                <div class="m-portlet__body">
-                    <h1>Item(S) List (from taskcard)</h1>
-                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                        <div class="row align-items-center">
-                            <div class="col-xl-6 order-2 order-xl-1">
-                                <div class="form-group m-form__group row align-items-center">
-                                    <div class="col-md-6">
-                                        <div class="m-input-icon m-input-icon--left">
-                                            <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                            <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                <span><i class="la la-search"></i></span>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="general_items_datatable" id="scrolling_both"></div>
-                </div>
-            </div>
-
-            <div class="m-portlet m-portlet--mobile">
-                <div class="m-portlet__body">
-                    <h1>General Material(S) List</h1>
-                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                        <div class="row align-items-center">
-                            <div class="col-xl-6 order-2 order-xl-1">
-                                <div class="form-group m-form__group row align-items-center">
-                                    <div class="col-md-6">
-                                        <div class="m-input-icon m-input-icon--left">
-                                            <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                            <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                <span><i class="la la-search"></i></span>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="general_materials_datatable" id="scrolling_both"></div>
-                </div>
-            </div>
-
-            <div class="m-portlet m-portlet--mobile">
-                <div class="m-portlet__body">
-                    <h1>General Tools(S) List</h1>
-                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                        <div class="row align-items-center">
-                            <div class="col-xl-6 order-2 order-xl-1">
-                                <div class="form-group m-form__group row align-items-center">
-                                    <div class="col-md-6">
-                                        <div class="m-input-icon m-input-icon--left">
-                                            <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                            <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                <span><i class="la la-search"></i></span>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="general_tools_datatable" id="scrolling_both"></div>
-                </div>
-            </div>
-
 
             <div class="m-portlet m-portlet--mobile">
                 <div class="m-portlet__body">
