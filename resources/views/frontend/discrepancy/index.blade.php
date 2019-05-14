53cc0179e578c879a5a4456fd431d218c26524b7
ThamrinPH <husni.sosnet@gmail.com>
2019-05-14 16:48:32 +0700

    WIP: DISCREPANCY

--- resources/views/frontend/discrepancy/index.blade.php
+++ resources/views/frontend/discrepancy/index.blade.php
@@ -5,7 +5,7 @@
         <div class="d-flex align-items-center">
             <div class="mr-auto">
                 <h3 class="m-subheader__title m-subheader__title--separator">
-                    TaskCard
+                    Discrepancy
                 </h3>
                 <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
                     <li class="m-nav__item m-nav__item--home">
@@ -17,9 +17,9 @@
                         -
                     </li>
                     <li class="m-nav__item">
-                        <a href="{{ route('frontend.taskcard.index') }}" class="m-nav__link">
+                        <a href="{{ route('frontend.discrepancy.index') }}" class="m-nav__link">
                             <span class="m-nav__link-text">
-                                TaskCard
+                                Discrepancy
                             </span>
                         </a>
                     </li>
@@ -42,7 +42,7 @@
                                 @include('frontend.common.label.datalist')
 
                                 <h3 class="m-portlet__head-text">
-                                    TaskCard
+                                    Discrepancy
                                 </h3>
                             </div>
                         </div>
@@ -65,43 +65,17 @@
                                         </div>
                                     </div>
                                     <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-                                            <div class="m-btn-group m-btn-group--pill btn-group" role="group" aria-label="Button group with nested dropdown">
-                                            <a href="{{route('frontend.taskcard-routine.create')}}" class="m-btn btn btn-primary">
-                                                    <span>
-                                                        <i class="la la-plus-circle"></i>
-                                                    <span>Routine</span>
-                                                    </span>
-                                                </a>
+                                        @component('frontend.common.buttons.create')
+                                            @slot('text', 'Discrepancy')
+                                            @slot('href', route('frontend.discrepancy.create') )
+                                        @endcomponent
 
-                                                <div class="m-btn-group btn-group" role="group">
-                                                <button id="btnGroupDrop1" type="button" class="btn btn-primary m-btn m-btn--pill-last dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <span>
-                                                        <i class="la la-plus-circle"></i>
-                                                    <span>Non - Routine</span>
-                                                    </span>
-                                                </button>
-                                                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                                    <a class="dropdown-item" href="{{route('frontend.taskcard-eo.create')}}">
-                                                        <span>
-                                                            <i class="la la-plus-circle"></i>
-                                                        <span>Engineering Order</span>
-                                                        </span>
-                                                    </a>
-                                                    <a class="dropdown-item" href="{{route('frontend.taskcard-si.create')}}">
-                                                        <span>
-                                                            <i class="la la-plus-circle"></i>
-                                                        <span>Special Instruction</span>
-                                                        </span>
-                                                    </a>
-                                                </div>
-                                                </div>
-                                            </div>
                                         <div class="m-separator m-separator--dashed d-xl-none"></div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="taskcard_datatable" id="scrolling_both"></div>
+                            <div class="Discrepancy_datatable" id="scrolling_both"></div>
                         </div>
                     </div>
                 </div>
@@ -112,5 +86,5 @@
 
 @push('footer-scripts')
     <script src="{{ asset('assets/metronic/demo/default/custom/crud/forms/widgets/form-repeater.js')}}"></script>
-    <script src="{{ asset('js/frontend/taskcard/index.js')}}"></script>
+    <script src="{{ asset('js/frontend/Discrepancy/index.js')}}"></script>
 @endpush
