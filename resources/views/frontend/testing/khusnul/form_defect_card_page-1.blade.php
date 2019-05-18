82b259472c415209e1321938fa8e9e797fe02233
ibnuprtma <pibnu86@gmail.com>
2019-05-18 11:35:38 +0700

    add a progress khusnul

---
+++ resources/views/frontend/testing/khusnul/form_defect_card_page-1.blade.php
@@ -0,0 +1,203 @@
+<html>
+<head>
+     <style>
+        input{
+            vertical-align: middle;
+        }
+    </style>
+
+</head>
+<body background="images/header1.jpg"
+ style="background-repeat: no-repeat;
+        background-position: top;
+        background-size: 595px;
+        height: 842px;
+        width: 595px;
+        margin-left: auto;
+        margin-right: auto;
+        font-family: Arial;
+        font-size: 12px;
+">
+<div style="padding-top: 100px">
+    <table>
+        <tr>
+            <td width="495px">
+<div>
+
+    <fieldset>
+        <legend style="color:#14ACBF; font-size: 16px;"><b>DC No : DC-022877</b></legend>
+        <table style="font-size: 12px;">
+            <tr>
+                <td style="width: 100px;">Issued Date</td>
+                <td>:</td>
+                <td style="width: 110px;">&nbsp;</td>
+                <td style="width: 100px;">Project No.</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Ref. JC No.</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+                <td>A/C Type</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Taskcard No.</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+                <td>A/C Reg</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Company Task</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+                <td>A/C S/N</td>
+                <td>:</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </fieldset>
+
+            </td>
+</div>
+    <td>
+        <img src="images/qr.png"width="100px">
+    </td>
+    </tr>
+    </table>
+<div style="padding-top: 5px">
+    <table width="100%" border="1px" style="border-collapse:collapse;">
+        <thead style="background-color: lightgray; font-size: 12px; height: 35px;">
+        <tr>
+            <th>Skill</th>
+            <th>ATA</th>
+            <th>Area/Zone</th>
+            <th>Seq. No</th>
+            <th>Eng. Qty</th>
+            <th>Mech. Qty</th>
+            <th>Est. Mhrs</th>
+        </tr>
+        </thead>
+        <tbody style="height: 35px">
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+<div style="padding-top: 10px;">
+    <table border="0" style="border-collapse:collapse; font-size: 12px;">
+        <tr>
+            <th rowspan="2" valign="top"  align="left" width="95px">Complaint</th>
+            <th rowspan="2" valign="top">:</th>
+            <td width="500px" style=" height: 17px;"><input style="border: hidden; border-bottom: 1px dashed; width: 100%;"></td>
+        </tr>
+        <tr>
+            <td style=" height: 17px;"><input style="border: hidden; border-bottom: 1px dashed; width: 100%;"></td>
+        </tr>
+        <tr>
+            <th rowspan="2" valign="top" align="left">Remark</th>
+            <th rowspan="2" valign="top">:</th>
+            <td style=" height: 17px;"><input style="border: hidden; border-bottom: 1px dashed; width: 100%;"></td>
+        </tr>
+        <tr>
+            <td style=" height: 17px;"><input style="border: hidden; border-bottom: 1px dashed; width: 100%;"></td>
+        </tr>
+    </table>
+
+</div>
+
+<div style="padding-top: 10px">
+    <table style="font-size: 12px;">
+        <lable>Propose Corection</lable>
+        <tr>
+            <td colspan="3" width="130px">
+                <input type="checkbox">1. REMOVE</br>
+                <input type="checkbox">2. INSTALL</br>
+                <input type="checkbox">3. RECTIFICATION</br>
+            </td>
+            <td colspan="3" width="118px">
+                <input type="checkbox">4. REPAIR</br>
+                <input type="checkbox">5. REPLACE</br>
+                <input type="checkbox">6. NDT</br>
+            </td>
+            <td colspan="3" width="118px">
+                <input type="checkbox">7. TEST</br>
+                <input type="checkbox">8. SHOP VISIT</br>
+                <input type="checkbox">9.  <input style="width: 70px; border: hidden; border-bottom: 1px dashed;"></br>
+            </td>
+            <td colspan="3" width="119px">
+                <input type="checkbox">10. <input style="width: 70px; border: hidden; border-bottom: 1px dashed;"></br>
+                <input type="checkbox">11.  <input style="width: 70px; border: hidden; border-bottom: 1px dashed;"></br>
+                <input type="checkbox">12.  <input style="width: 70px; border: hidden; border-bottom: 1px dashed;"></br>
+            </td>
+            <td colspan="3" width="110px" align="center">
+                RII Req.</br>
+                <input type="checkbox"><b>YES</b>
+                <input type="checkbox"><b>NO</b>
+            </td>
+        </tr>
+    </table>
+</div>
+
+<div style="padding-top: 5px">
+    <table width="100%" border="1px" style="border-collapse:collapse; font-size: 12px;">
+        <tbody style="height: 40px;">
+        <tr valign="top" >
+            <td style="width: 150px;">Name/Sign : </td>
+            <td style="width: 150px; border-left: hidden;">Stamp :</td>
+            <td >Customer Sign :</td>
+            <td style="border-left: hidden; font-size: 10px;"><input type="checkbox"> AGREE</br><input type="checkbox"> DISAGREE</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+<div style="padding-top: 10px;">
+    <table width="100%" border="1px" style="border-collapse:collapse; font-size: 12px;">
+        <thead style="background-color: orange; height: 35px;">
+        <tr>
+            <th style="width: 40px" rowspan="2">No</th>
+            <th rowspan="2" style="width: 210px;">Reference</th>
+            <th rowspan="2">Date</th>
+            <th rowspan="2">Actual</br>Mhrs.</th>
+            <td>ENG</td>
+            <td style="width: 90px;">ENG</td>
+        </tr>
+        <tr>
+            <td>(Name)</td>
+            <td>(Stamp + Sign)</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr style="height: 170px;">
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr valign="top"  style="height: 40px;">
+            <td style="font-size: 8px;" colspan="3">Remark :&emsp;*   FOR RII</br>&emsp;&emsp;&emsp;&emsp;&emsp;** CHECK AS APLICABLE</br>&emsp;&emsp;&emsp;&emsp;&emsp;***  OTHER THAN ABOVE, MENTIONED, DESCRIBE, SAY</td>
+            <td style="font-size: 8px;" align="center">Total</td>
+            <td style="font-size: 8px;">RII ITEM: </td>
+            <td style="font-size: 8px;" align="bottom" align="right" style="border-left: hidden;">(Stamp + Sign)</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+</body>
+</html>
---
+++ resources/views/frontend/testing/khusnul/form_defect_card_page-2.blade.php
@@ -0,0 +1,148 @@
+<html>
+<head>
+
+</head>
+<body background="images/header2.jpg"
+      style="background-repeat: no-repeat;
+        background-position: top;
+        background-size:  842px;
+        width: 842px;
+        height: 595px;
+        margin-left: auto;
+        margin-right: auto;
+        font-family: Arial;
+        font-size: 12px;
+">
+<div style="padding-top: 70px">
+    <h2 align="right" style="padding-right: 135px; color: #14ACBF; font-size: 16px;">DC No : DC-022877</h2>
+</div>
+<div style="padding-top: 5px;">
+
+    <div>
+<table>
+    <td width="842px" height="30px" align="center" style="background-color: orange; border-radius: 7px;"><b style="font-size: 14px;">COMPONENT / PART USED</b></td>
+</table>
+    </div>
+    <div style="padding-top: 5px;">
+        <table width="100%" border="1px" style="border-collapse:collapse; font-size: 11px;">
+                <thead align="center">
+                <tr style="background-color: lightgray">
+                    <th width="46" rowspan="2">NO</th>
+                    <th width="125" rowspan="2">ITEM DESCRIPTION</th>
+                    <th width="125" rowspan="2">PART NUMBER</th>
+                    <th colspan="2">SERIAL NO.</th>
+                    <th width="126" rowspan="2">QTY</th>
+                    <th width="126" rowspan="2">IPC REF</th>
+                    <th width="126" rowspan="2">REMARK</th>
+                </tr>
+                <tr style="background-color: lightgray">
+                    <th width="126" height="23">OFF</th>
+                    <th width="126">ON</th>
+                </tr>
+                </thead>
+            <tbody align="center">
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="31px">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                </tbody>
+    </div>
+</div>
+</body>
+</html>
