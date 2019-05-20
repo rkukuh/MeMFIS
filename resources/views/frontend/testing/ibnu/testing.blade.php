86d8539947dea590bb283e408ea4b6bc3b5e43b0
ibnuprtma <pibnu86@gmail.com>
2019-05-20 14:03:16 +0700

    testing view embeb

--- resources/views/frontend/testing/ibnu/testing.blade.php
+++ resources/views/frontend/testing/ibnu/testing.blade.php
@@ -1,6 +1,35 @@
 @extends('frontend.master')
 
 @section('content')
+<div class="m-content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="m-portlet">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <span class="m-portlet__head-icon m--hide">
+                                <i class="la la-gear"></i>
+                            </span>
 
+                            @include('frontend.common.label.datalist')
+
+                            <h3 class="m-portlet__head-text">
+                                Testing
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet m-portlet--mobile">
+                    <div class="m-portlet__body">
+                        <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                            <embed src= "/storage/s.pdf" width= "100%" height= "375">
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+    </div>
+</div>
 
 @endsection
