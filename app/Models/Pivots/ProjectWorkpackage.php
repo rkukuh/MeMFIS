5cb7f4b953af4a3d588367e5492e5285996ac72c
R. Kukuh <rkukuh@gmail.com>
2019-05-24 00:58:56 +0700

    Define relationship

--- app/Models/Pivots/ProjectWorkpackage.php
+++ app/Models/Pivots/ProjectWorkpackage.php
@@ -4,6 +4,7 @@ namespace App\Models\Pivots;
 
 use App\Models\Project;
 use App\Models\WorkPackage;
+use App\Models\ProjectWorkPackageManhour;
 use App\Models\ProjectWorkPackageEngineer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\Pivot;
@@ -27,6 +28,19 @@ class ProjectWorkPackage extends Pivot
     public function engineers()
     {
         return $this->hasMany(ProjectWorkPackageEngineer::class, 'project_workpackage_id');
+    }
+
+    /**
+     * One-to-Many: A project's workpackages may have one or many manhour.
+     *
+     * This function will retrieve all the manhour of a project's workpackages.
+     * See: Project WorkPackage Manhour's header() method for the inverse
+     *
+     * @return mixed
+     */
+    public function manhours()
+    {
+        return $this->hasMany(ProjectWorkPackageManhour::class, 'project_workpackage_id');
     }
 
     /**
