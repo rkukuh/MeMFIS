c74fee06938f2da1ef94d58f89ee4ef850841bb3
R. Kukuh <rkukuh@gmail.com>
2019-05-24 00:58:47 +0700

    Add columns

---
+++ database/migrations/2019_05_23_232006_create_project_workpackage_manhours_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectworkpackagemanhoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_workpackage_manhours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->unsignedInteger('project_workpackage_id');
+            $table->unsignedInteger('engineer_type_id');
+            $table->integer('proportion_amount');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('project_workpackage_id')
+                    ->references('id')->on('project_workpackage')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('engineer_type_id')
+                    ->references('id')->on('types')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projectworkpackagemanhours');
+    }
+}
