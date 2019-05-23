0cb70723b9623fcfda927dfea11eda3fc4486196
R. Kukuh <rkukuh@gmail.com>
2019-05-24 00:59:15 +0700

    Define dummy data

--- database/seeds/dummies/ProjectWorkPackageManhours.php
+++ database/seeds/dummies/ProjectWorkPackageManhours.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Models\ProjectWorkPackageManhour;
+use App\Models\Type;
 use Illuminate\Database\Seeder;
+use App\Models\Pivots\ProjectWorkPackage;
+use App\Models\ProjectWorkPackageManhour;
 
 class ProjectWorkPackageManhours extends Seeder
 {
@@ -12,6 +14,17 @@ class ProjectWorkPackageManhours extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        for ($i = 1; $i <= ProjectWorkPackage::count(); $i++) {
+            $project_workpackage = ProjectWorkPackage::find($i);
+
+            for ($j = 1; $j <= rand(3, 5); $j++) {
+                $project_workpackage->manhours()->create([
+                    'engineer_type_id' => Type::ofProjectWorkPackageManhour()->get()->random()->id,
+                    'proportion_amount' => $faker->numberBetween(10, 20),
+                ]);
+            }
+        }
     }
 }
