51bc14a272b8b628e2ee3af6a929f44c07657678
R. Kukuh <rkukuh@gmail.com>
2019-05-24 15:05:59 +0700

    Define attributes and relationships

--- app/Models/ProjectWorkPackageFacility.php
+++ app/Models/ProjectWorkPackageFacility.php
@@ -6,5 +6,32 @@ use App\MemfisModel;
 
 class ProjectWorkPackageFacility extends MemfisModel
 {
-    //
+    protected $table = 'project_workpackage_facilities';
+    
+    protected $fillable = ['facility_id'];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-Way: A Project WorkPackage Facitlity must have a facility assigned to.
+     *
+     * @return mixed
+     */
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
+
+    /**
+     * One-to-Many: A project's workpackage may have one or many facility.
+     *
+     * This function will retrieve the header of a project's workpackage.
+     * See: Project WorkPackage's facilities() method for the inverse
+     *
+     * @return mixed
+     */
+    public function header()
+    {
+        return $this->belongsTo(ProjectWorkPackage::class, 'project_workpackage_id');
+    }
 }
