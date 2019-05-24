aedbc8b48bb63e1e6a37d9691391cb2837ee10ba
audhy <audhy.vk01@gmail.com>
2019-05-24 14:04:17 +0700

    refactor

--- public/js/frontend/workpackage/item/create.js
+++ public/js/frontend/workpackage/item/create.js
@@ -1,6 +1,6 @@
 let Workpackage3 = {
     init: function () {
-        $('.tools_datatable').mDatatable({
+        $('.general_tools_datatable').mDatatable({
             data: {
                 type: 'remote',
                 source: {
@@ -78,6 +78,30 @@ let Workpackage3 = {
                     filterable: !1,
                 },
                 {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
                     field: 'Actions',
                     sortable: !1,
                     overflow: 'visible',
@@ -93,7 +117,7 @@ let Workpackage3 = {
         });
 
 
-        $('.materials_datatable').mDatatable({
+        $('.general_materials_datatable').mDatatable({
             data: {
                 type: 'remote',
                 source: {
@@ -168,6 +192,496 @@ let Workpackage3 = {
                 {
                     field: 'description',
                     title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-material" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+        $('.routine_tools_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/workpackage/'+workPackage_uuid+'/general-tools',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-tool" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+
+        $('.routine_materials_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/workpackage/'+workPackage_uuid+'/general-materials',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-material" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+        $('.non_routine_tools_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/workpackage/'+workPackage_uuid+'/general-tools',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-tool" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+            ]
+        });
+
+
+        $('.non_routine_materials_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/workpackage/'+workPackage_uuid+'/general-materials',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !0,
+                serverFiltering: !0,
+                serverSorting: !0
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'unit_price',
+                    title: 'Unit Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'selling_price',
+                    title: 'Selling Price',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'total',
+                    title: 'Sub Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'note',
+                    title: 'Marketing Note',
                     sortable: 'asc',
                     filterable: !1,
                 },
