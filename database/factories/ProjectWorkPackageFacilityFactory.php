37d2db50a99ded340c9a597411cbd21ddf876b4e
R. Kukuh <rkukuh@gmail.com>
2019-05-24 15:06:15 +0700

    Define dummy data

--- database/factories/ProjectWorkPackageFacilityFactory.php
+++ database/factories/ProjectWorkPackageFacilityFactory.php
@@ -1,12 +1,19 @@
 <?php
 
-use App\Models\ProjectWorkPackageFacility;
+use App\Models\Facility;
 use Faker\Generator as Faker;
+use App\Models\Pivots\ProjectWorkPackage;
+use App\Models\ProjectWorkPackageFacility;
 
 $factory->define(ProjectWorkPackageFacility::class, function (Faker $faker) {
 
     return [
-        //
+        'project_workpackage_id' => function () {
+            return ProjectWorkPackage::get()->random()->id;
+        },
+        'facility_id' => function () {
+            return Facility::get()->random()->id;
+        },
     ];
 
 });
--- database/seeds/dummies/ProjectWorkPackageFacilities.php
+++ database/seeds/dummies/ProjectWorkPackageFacilities.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Models\ProjectWorkPackageFacility;
+use App\Models\Facility;
 use Illuminate\Database\Seeder;
+use App\Models\Pivots\ProjectWorkPackage;
+use App\Models\ProjectWorkPackageFacility;
 
 class ProjectWorkPackageFacilities extends Seeder
 {
@@ -12,6 +14,14 @@ class ProjectWorkPackageFacilities extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 1; $i <= ProjectWorkPackage::count(); $i++) {
+            $project_workpackage = ProjectWorkPackage::find($i);
+
+            for ($j = 1; $j <= rand(2, Facility::count()); $j++) {
+                $project_workpackage->facilities()->create([
+                    'facility_id' => Facility::find($j)->id,
+                ]);
+            }
+        }
     }
 }
