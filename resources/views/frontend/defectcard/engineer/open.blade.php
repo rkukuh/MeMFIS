e42293522192c8168ff9f60ea9dbe89f8b7728c6
ibnuprtma <pibnu86@gmail.com>
2019-05-26 00:28:07 +0700

    refactory

--- resources/views/frontend/defectcard/engineer/open.blade.php
+++ resources/views/frontend/defectcard/engineer/open.blade.php
@@ -365,104 +365,6 @@
                         </div>
                     </div>
                     <div class="form-group m-form__group row">
-                      <div class="col-sm-12 col-md-12 col-lg-12">
-                          <div class="m-portlet">
-                              <div class="m-portlet__head">
-                                  <div class="m-portlet__head-caption">
-                                      <div class="m-portlet__head-title">
-                                          <span class="m-portlet__head-icon m--hide">
-                                              <i class="la la-gear"></i>
-                                          </span>
-
-                                          @include('frontend.common.label.datalist')
-
-                                          <h3 class="m-portlet__head-text">
-                                              Tool(s) List
-                                          </h3>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="m-portlet m-portlet--mobile">
-                                  <div class="m-portlet__body">
-                                      <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                          <div class="row align-items-center">
-                                              <div class="col-xl-8 order-2 order-xl-1">
-                                                  <div class="form-group m-form__group row align-items-center">
-                                                      <div class="col-md-4">
-                                                          <div class="m-input-icon m-input-icon--left">
-                                                              <input type="text" class="form-control m-input" placeholder="Search..."
-                                                                  id="generalSearch">
-                                                              <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                                  <span><i class="la la-search"></i></span>
-                                                              </span>
-                                                          </div>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                              <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-                                                  <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                              </div>
-                                          </div>
-                                      </div>
-                                      <div class="defectcard_tool_datatable" id="scrolling_both"></div>
-
-                                      @include('frontend.purchase-order.modal-check')
-
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                    </div>
-                    <div class="form-group m-form__group row">
-                      <div class="col-sm-12 col-md-12 col-lg-12">
-                        <div class="m-portlet">
-                            <div class="m-portlet__head">
-                                <div class="m-portlet__head-caption">
-                                    <div class="m-portlet__head-title">
-                                        <span class="m-portlet__head-icon m--hide">
-                                            <i class="la la-gear"></i>
-                                        </span>
-
-                                        @include('frontend.common.label.datalist')
-
-                                        <h3 class="m-portlet__head-text">
-                                            Material(s) List
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="m-portlet m-portlet--mobile">
-                                <div class="m-portlet__body">
-                                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                        <div class="row align-items-center">
-                                            <div class="col-xl-8 order-2 order-xl-1">
-                                                <div class="form-group m-form__group row align-items-center">
-                                                    <div class="col-md-4">
-                                                        <div class="m-input-icon m-input-icon--left">
-                                                            <input type="text" class="form-control m-input" placeholder="Search..."
-                                                                id="generalSearch">
-                                                            <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                                <span><i class="la la-search"></i></span>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-4 order-1 order-xl-2 m--align-right">
-                                                <div class="m-separator m-separator--dashed d-xl-none"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="defectcard_material_datatable" id="scrolling_both"></div>
-
-                                    @include('frontend.purchase-order.modal-check')
-
-                                </div>
-                            </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group m-form__group row">
                       <div class="col-sm-12 col-md-12 col-lg-12 footer">
                           <div class="flex">
                               <div class="action-buttons">
@@ -489,6 +391,66 @@
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-lg-5">
+        <div class="m-portlet">
+            <div class="m-portlet__head">
+                <div class="m-portlet__head-caption">
+                    <div class="m-portlet__head-title">
+                        <span class="m-portlet__head-icon m--hide">
+                            <i class="la la-gear"></i>
+                        </span>
+
+                        @include('frontend.common.label.datalist')
+
+                        <h3 class="m-portlet__head-text">
+                            Tool(s) List
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="m-portlet m-portlet--mobile">
+                <div class="m-portlet__body">
+                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                        <div class="row align-items-center">
+                            <div class="col-xl-12 order-12 order-xl-12 m--align-right">
+                                <div class="defectcard_tool_datatable" id="scrolling_both"></div>
+                                <div class="m-separator m-separator--dashed d-xl-none"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="m-portlet">
+            <div class="m-portlet__head">
+                <div class="m-portlet__head-caption">
+                    <div class="m-portlet__head-title">
+                        <span class="m-portlet__head-icon m--hide">
+                            <i class="la la-gear"></i>
+                        </span>
+
+                        @include('frontend.common.label.datalist')
+
+                        <h3 class="m-portlet__head-text">
+                            Material(s) List
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="m-portlet m-portlet--mobile">
+                <div class="m-portlet__body">
+                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                        <div class="row align-items-center">
+                            <div class="col-xl-12 order-12 order-xl-12 m--align-right">
+                                    <div class="defectcard_material_datatable" id="scrolling_both"></div>
+                                <div class="m-separator m-separator--dashed d-xl-none"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </div>  
