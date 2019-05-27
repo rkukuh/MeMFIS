ebdb871d8e3e55d1fb0d81cb299d3cebb244e506
audhy <audhy.vk01@gmail.com>
2019-05-27 23:42:23 +0700

    fix bug search jobcard engineer

--- resources/views/frontend/job-card/engineer/index.blade.php
+++ resources/views/frontend/job-card/engineer/index.blade.php
@@ -52,19 +52,25 @@
                             <div class="col-xl-12 order-2 order-xl-1">
                                 <div class="form-group m-form__group row align-items-center d-flex justify-content-center">
                                     <div class="col-md-4">
-                                        <div class="m-input-icon m-input-icon--left">
-                                            <input type="text" class="form-control m-input" id="search" name="search" placeholder="Search...">
+                                        <form method="POST" action="{{route('frontend.engineer.jobcard.seacrh')}}">
+                                            {!! csrf_field() !!}
 
-                                            {{-- <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch"> --}}
-                                            <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                <span><i class="la la-search"></i></span>
-                                            </span>
-                                        </div>
-                                        <div class="d-flex justify-content-end mt-4 search">
-                                            @include('frontend.common.buttons.filter')
+                                            <div class="m-input-icon m-input-icon--left">
+                                                <input type="text" class="form-control m-input" id="search" name="search" placeholder="Search...">
 
-                                            @include('frontend.common.buttons.search')
-                                        </div>
+                                                {{-- <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch"> --}}
+                                                <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                    <span><i class="la la-search"></i></span>
+                                                </span>
+                                            </div>
+                                            <div class="d-flex justify-content-end mt-4 search">
+                                                @include('frontend.common.buttons.filter')
+
+                                                @component('frontend.common.buttons.search')
+                                                    @slot('id','btn-search')
+                                                @endcomponent()
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
@@ -102,10 +108,12 @@
 <script src="{{ asset('js/frontend/job-card/item/index.js') }}"></script>
 <script src="{{ asset('js/frontend/job-card/tool/index.js') }}"></script>
 <script>
-    $('.search').on('click', '.search', function() {
-        let search = $('input[name=search]').val();
-
-        window.location.href = '/jobcard-engineer/' + search;
+    let input = document.getElementById("search");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+        event.preventDefault();
+            document.getElementById("btn-search").click();
+        }
     });
 </script>
 
