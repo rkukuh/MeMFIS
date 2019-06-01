ca1c1c007a92d61b3df9fe5e88e8901bc5a27219
R. Kukuh <rkukuh@gmail.com>
2019-06-01 15:58:08 +0700

    Add columns

--- database/migrations/2019_06_01_153515_create_defectcards_table.php
+++ database/migrations/2019_06_01_153515_create_defectcards_table.php
@@ -15,7 +15,17 @@ class CreateDefectcardsTable extends Migration
     {
         Schema::create('defectcards', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('code');
+            $table->unsignedInteger('jobcard_id');
+            $table->integer('engineer_quantity');
+            $table->integer('helper_quantity');
+            $table->unsignedDecimal('estimation_manhour', 8, 2)->nullable();
+            $table->boolean('is_rii');
+            $table->text('complain');
+            $table->text('description');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
