687058da18b07b92e7c5f39701ca5ec6f968323f
R. Kukuh <rkukuh@gmail.com>
2019-06-01 15:58:05 +0700

    Add columns

--- database/migrations/2019_06_01_152602_create_htcrr_table.php
+++ database/migrations/2019_06_01_152602_create_htcrr_table.php
@@ -15,7 +15,19 @@ class CreateHtcrrTable extends Migration
     {
         Schema::create('htcrr', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('code');
+            $table->string('part_number');
+            $table->unsignedInteger('skill_id');
+            $table->boolean('is_rii');
+            $table->unsignedDecimal('estimation_manhour', 8, 2)->nullable();
+            $table->timestamp('removed_at')->nullable();
+            $table->unsignedInteger('removed_by');
+            $table->timestamp('installed_at')->nullable();
+            $table->unsignedInteger('installed_by');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
