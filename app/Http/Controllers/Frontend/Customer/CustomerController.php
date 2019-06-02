18920dc94e81531baae64091cd2a7e341849c121
audhy <audhy.vk01@gmail.com>
2019-06-02 23:43:52 +0700

    create customer WIP (kurang document)

--- app/Http/Controllers/Frontend/Customer/CustomerController.php
+++ app/Http/Controllers/Frontend/Customer/CustomerController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Frontend\Customer;
 
+use App\Models\Fax;
 use App\Models\Type;
 use App\Models\Email;
 use App\Models\Level;
+use App\Models\Phone;
+use App\Models\Website;
 use App\Models\Customer;
+use App\Models\Document;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Frontend\CustomerStore;
 use App\Http\Requests\Frontend\CustomerUpdate;
@@ -50,38 +54,42 @@ class CustomerController extends Controller
         $attentions = [];
 
         $level = Level::where('uuid',$request->level)->first();
-        for ($person = 0; $person < sizeof($request->attn_name_array); $person++) {
+        // for ($person = 0; $person < sizeof($request->attn_name_array); $person++) {
 
-            $contact['name']     = $request->attn_name_array[$person];
-            $contact['position'] = $request->attn_position_array[$person];
-            $contact['phones'] = $request->attn_phone_array[$person];
-            $contact['ext'] = $request->attn_ext_array[$person];
-            $contact['fax'] = $request->attn_fax_array[$person];
-            $contact['emails'] = $request->attn_email_array[$person];
+        //     $contact['name']     = $request->attn_name_array[$person];
+        //     $contact['position'] = $request->attn_position_array[$person];
+        //     $contact['phones'] = $request->attn_phone_array[$person];
+        //     $contact['ext'] = $request->attn_ext_array[$person];
+        //     $contact['fax'] = $request->attn_fax_array[$person];
+        //     $contact['emails'] = $request->attn_email_array[$person];
 
-            array_push($attentions, $contact);
-        }
+        //     array_push($attentions, $contact);
+        // }
 
-        $request->merge(['attention' => json_encode($attentions)]);
+        // $request->merge(['attention' => json_encode($attentions)]);
         // $request->merge(['code' => "auto-generate");
         if ($customer = Customer::create($request->all())) {
             $customer->levels()->attach($level);
 
             if(is_array($request->phone_array)){
                 for ($i=0; $i < sizeof($request->phone_array) ; $i++) {
+                    $phone_type = Type::ofPhone()->where('code',$request->type_phone_array[$i])->first();
+
                     $customer->phones()->save(new Phone([
-                        'number' => $request->phone[$i],
-                        'ext' => $request->ext[$i],
-                        'type_id' => $request->phone_type[$i],
+                        'number' => $request->phone_array[$i],
+                        'ext' => $request->ext_phone_array[$i],
+                        'type_id' => $phone_type->id,
                     ]));
                 }
             }
 
             if(is_array($request->fax_array)){
                 for ($i=0; $i < sizeof($request->fax_array) ; $i++) {
+                    $fax_type = Type::ofFax()->where('code',$request->type_fax_array[$i])->first();
+
                     $customer->faxes()->save(new Fax([
-                        'number' => $request->fax[$i],
-                        'type_id' => $request->fax_type[$i],
+                        'number' => $request->fax_array[$i],
+                        'type_id' => $fax_type->id,
                     ]));
                 }
             }
@@ -89,9 +97,10 @@ class CustomerController extends Controller
             if(is_array($request->website_array)){
                 for ($i=0; $i < sizeof($request->website_array) ; $i++) {
                     if($request->website_type[$i] !== "Select a Website Type"){
+                        $website_type = Type::ofWebsite()->where('uuid',$request->type_website_array[$i])->first();
                         $customer->websites()->save(new Website([
-                            'url' => $request->web[$i],
-                            'type_id' => $request->website_type[$i],
+                            'url' => $request->website_array[$i],
+                            'type_id' => $website_type->id,
                         ]));
                     }
                 }
@@ -99,19 +108,26 @@ class CustomerController extends Controller
 
             if(is_array($request->email_array)){
                 for ($i=0; $i < sizeof($request->email_array) ; $i++) {
-                    $customer->emailes()->save(new Email([
-                        'address' => $request->email[$i],
-                        'type_id' => $request->email_type[$i],
+                    $email_type = Type::ofEmail()->where('code',$request->type_email_array[$i])->first();
+
+                    $customer->emails()->save(new Email([
+                        'address' => $request->email_array[$i],
+                        'type_id' => $email_type->id,
                     ]));
                 }
             }
 
-            if(is_array($request->document_array)){
-                for ($i=0; $i < sizeof($request->document_array) ; $i++) {
-                    $customer->documentes()->save(new Document([
-                        'number' => $request->document[$i],
-                        'type_id' => $request->document_type[$i],
-                    ]));
+            // if(is_array($request->document_array)){
+            if(is_array($request->type_document_array)){
+                for ($i=0; $i < sizeof($request->type_document_array) ; $i++) {
+                    if($request->website_type[$i] !== "Select a Document Type"){
+                        $document_type = Type::ofDocument()->where('uuid',$request->type_document_array[$i])->first();
+
+                        $customer->documents()->save(new Document([
+                            'number' =>' $request->document[$i]',
+                            'type_id' => $document_type->id,
+                        ]));
+                        }
                 }
             }
 
@@ -166,7 +182,7 @@ class CustomerController extends Controller
         $level = Level::where('uuid',$request->level)->first();
         // dd(sizeof($request->attn_name_array));
         for ($person = 0; $person < sizeof($request->attn_name_array) - 1; $person++) {
-            
+
             $contact['name']     = $request->attn_name_array[$person];
             $contact['position'] = $request->attn_position_array[$person];
             $contact['phones'] = $request->attn_phone_array[$person];
