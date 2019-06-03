f9645f87eecf022cdcad1bd9639bad68de141f75
R. Kukuh <rkukuh@gmail.com>
2019-06-03 15:30:56 +0700

    Define fillables

--- app/Models/DefectCard.php
+++ app/Models/DefectCard.php
@@ -7,4 +7,17 @@ use App\MemfisModel;
 class DefectCard extends MemfisModel
 {
     protected $table = 'defectcards';
+
+    protected $fillable = [
+        'code',
+        'jobcard_id',
+        'engineer_quantity',
+        'helper_quantity',
+        'estimation_manhour',
+        'is_rii',
+        'propose_correction_id',
+        'propose_correction',
+        'complaint',
+        'description',
+    ];
 }
