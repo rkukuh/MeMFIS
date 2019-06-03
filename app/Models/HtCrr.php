808dd5d526b2cd59e694cf34a67d0cba1152a7ed
R. Kukuh <rkukuh@gmail.com>
2019-06-03 13:54:26 +0700

    Define HT/CRR's attributes factory value

--- app/Models/HtCrr.php
+++ app/Models/HtCrr.php
@@ -7,4 +7,22 @@ use App\MemfisModel;
 class HtCrr extends MemfisModel
 {
     protected $table = 'htcrr';
+
+    protected $fillable = [
+        'code',
+        'part_number',
+        'skill_id',
+        'is_rii',
+        'estimation_manhour',
+        'removed_at',
+        'removed_by',
+        'installed_at',
+        'installed_by',
+        'description',
+    ];
+
+    protected $dates = [
+        'removed_at',
+        'installed_at',
+    ];
 }
--- database/factories/HtCrrFactory.php
+++ database/factories/HtCrrFactory.php
@@ -1,12 +1,26 @@
 <?php
 
+use Carbon\Carbon;
+use App\Models\Type;
 use App\Models\HtCrr;
+use App\Models\Employee;
 use Faker\Generator as Faker;
 
 $factory->define(HtCrr::class, function (Faker $faker) {
 
+    $number = $faker->unixTime();
+
     return [
-        //
+        'code' => 'HTCRR-DUM-' . $number,
+        'part_number' => 'P/N-' . $faker->randomNumber,
+        'skill_id' => Type::ofTaskCardSkill()->get()->random()->id,
+        'is_rii' => $faker->boolean,
+        'estimation_manhour' => $faker->randomFloat(2, 0, 9999),
+        'removed_at' => Carbon::now()->subWeeks(rand(1, 3)),
+        'removed_by' => Employee::get()->random()->id,
+        'installed_at' => Carbon::now()->addWeeks(rand(1, 3)),
+        'installed_by' => Employee::get()->random()->id,
+        'description' => $faker->randomElement([null, $faker->text]),
     ];
 
 });
