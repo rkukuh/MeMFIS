aa87765daac7094dd8cb30cef2db3a8b87debfe7
R. Kukuh <rkukuh@gmail.com>
2019-06-03 14:43:06 +0700

    Define 1-M relationship: HT/CRR and Employee

--- app/Models/HtCrr.php
+++ app/Models/HtCrr.php
@@ -25,4 +25,32 @@ class HtCrr extends MemfisModel
         'removed_at',
         'installed_at',
     ];
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-to-Many: An HT/CRR may have one installer.
+     *
+     * This function will retrieve the installer of an HT/CRR.
+     * See: Employee's htcrr_installed() method for the inverse
+     *
+     * @return mixed
+     */
+    public function installedBy()
+    {
+        return $this->belongsTo(Employee::class, 'installed_by');
+    }
+
+    /**
+     * One-to-Many: An HT/CRR may have one remover.
+     *
+     * This function will retrieve the remover of an HT/CRR.
+     * See: Employee's htcrr_removed() method for the inverse
+     *
+     * @return mixed
+     */
+    public function removedBy()
+    {
+        return $this->belongsTo(Employee::class, 'removed_by');
+    }
 }
