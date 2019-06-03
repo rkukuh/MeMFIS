8ecc89b33e93576986e7ad4ae628309f4f8fa209
R. Kukuh <rkukuh@gmail.com>
2019-06-03 15:30:47 +0700

    Add column and FK constraints

--- database/migrations/2019_06_01_153515_create_defectcards_table.php
+++ database/migrations/2019_06_01_153515_create_defectcards_table.php
@@ -24,10 +24,20 @@ class CreateDefectcardsTable extends Migration
             $table->boolean('is_rii');
             $table->unsignedInteger('propose_correction_id')->nullable();
             $table->string('propose_correction')->nullable();
-            $table->text('complaint');
-            $table->text('description');
+            $table->text('complaint')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('jobcard_id')
+                    ->references('id')->on('jobcards')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('propose_correction_id')
+                    ->references('id')->on('types')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
         });
     }
 
