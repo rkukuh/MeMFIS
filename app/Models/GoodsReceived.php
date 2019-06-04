e2fad0476f52623afb92e3594b65ded3d4d3c16c
R. Kukuh <rkukuh@gmail.com>
2019-06-04 15:10:58 +0700

    Change GRN approval system to Approval polymorph

--- app/Models/GoodsReceived.php
+++ app/Models/GoodsReceived.php
@@ -16,29 +16,22 @@ class GoodsReceived extends MemfisModel
         'container_no',
         'purchase_order_id',
         'storage_id',
-        'approved_by',
-        'approved_at',
         'description',
     ];
 
-    protected $dates = [
-        'received_at', 
-        'approved_at'
-    ];
+    protected $dates = ['received_at'];
 
     /*************************************** RELATIONSHIP ****************************************/
 
     /**
-     * One-to-Many: A GRN may have one approver.
+     * Polymorphic: An entity can have zero or many approvals.
      *
-     * This function will retrieve the approver of a GRN.
-     * See: Employee's grn_approved() method for the inverse
-     *
-     * @return mixed
+     * This function will get all Quotation's approvals.
+     * See: Approvals's approvable() method for the inverse
      */
-    public function approvedBy()
+    public function approvals()
     {
-        return $this->belongsTo(Employee::class, 'approved_by');
+        return $this->morphMany(Approval::class, 'approvable');
     }
 
     /**
