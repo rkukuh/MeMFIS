1e577966c816b7c7984cf86103cbdb1f743be502
R. Kukuh <rkukuh@gmail.com>
2019-06-07 15:23:24 +0700

    Define factory's values

--- database/factories/ProgressFactory.php
+++ database/factories/ProgressFactory.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Models\Status;
 use App\Models\Progress;
+use App\Models\Employee;
 use Faker\Generator as Faker;
 
 $factory->define(Progress::class, function (Faker $faker) {
 
     return [
-        //
+        'status_id' => Status::ofJobCard()->get()->random()->id,
+        'conducted_by' => Employee::get()->random()->id
     ];
 
 });
