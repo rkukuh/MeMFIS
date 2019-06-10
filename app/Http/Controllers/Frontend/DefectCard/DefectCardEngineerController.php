35480b5c7d3d5eac34aec43774cdb73add75db56
audhy <audhy.vk01@gmail.com>
2019-06-10 23:36:58 +0700

    reason pause and close defectcard engineer

--- app/Http/Controllers/Frontend/DefectCard/DefectCardEngineerController.php
+++ app/Http/Controllers/Frontend/DefectCard/DefectCardEngineerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend\DefectCard;
 
 use Auth;
 use Validator;
+use App\Models\Type;
 use App\Models\Status;
 use App\Models\Approval;
 use App\Models\Progress;
@@ -14,10 +15,18 @@ use App\Http\Controllers\Controller;
 class DefectCardEngineerController extends Controller
 {
     protected $statuses;
+    protected $break;
+    protected $waiting;
+    protected $other;
+    protected $accomplished;
 
     public function __construct()
     {
         $this->statuses = Status::ofDefectCard()->get();
+        $this->break = Type::ofJobCardPauseReason()->where('code','break-time')->first()->uuid;
+        $this->waiting = Type::ofJobCardPauseReason()->where('code','waiting-material')->first()->uuid;
+        $this->other = Type::ofJobCardPauseReason()->where('code','other')->first()->uuid;
+        $this->accomplished = Type::ofJobCardCloseReason()->where('code','accomplished')->first()->uuid;
     }
 
     /**
@@ -78,6 +87,10 @@ class DefectCardEngineerController extends Controller
         }
         else if($this->statuses->where('id',$defectcard->progresses->last()->status_id)->first()->code == "progress"){
             return view('frontend.defect-card.engineer.progress-resume', [
+                'break' => $this->break,
+                'waiting' => $this->waiting,
+                'other' => $this->other,
+                'accomplished' => $this->accomplished,
                 'defectcard' => $defectcard,
                 'pending' => $this->statuses->where('code','pending')->first(),
                 'closed' => $this->statuses->where('code','closed')->first(),
@@ -121,6 +134,8 @@ class DefectCardEngineerController extends Controller
         if($this->statuses->where('uuid',$request->progress)->first()->code == 'pending'){
             $defectcard->progresses()->save(new Progress([
                 'status_id' =>  $this->statuses->where('code','pending')->first()->id,
+                'reason_id' =>  Type::ofJobCardPauseReason()->where('uuid',$request->pause)->first()->id,
+                'reason_text' =>  $request->reason,
                 'progressed_by' => Auth::id()
             ]));
             return redirect()->route('frontend.defectcard-engineer.index');
@@ -128,6 +143,8 @@ class DefectCardEngineerController extends Controller
         if($this->statuses->where('uuid',$request->progress)->first()->code == 'closed'){
             $defectcard->progresses()->save(new Progress([
                 'status_id' =>  $this->statuses->where('code','closed')->first()->id,
+                'reason_id' =>  Type::ofJobCardCloseReason()->where('uuid',$request->accomplishment)->first()->id,
+                'reason_text' =>  $request->note,
                 'progressed_by' => Auth::id()
             ]));
 
