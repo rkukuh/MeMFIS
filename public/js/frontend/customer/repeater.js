b8addca9f5b75bd8694f82c6e9d7efe07ab47aa2
ThamrinPH <husni.sosnet@gmail.com>
2019-06-11 15:59:42 +0700

    WIP Customer

--- public/js/frontend/customer/repeater.js
+++ public/js/frontend/customer/repeater.js
@@ -1,15 +1,40 @@
+$("div.repeaterAttention").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyAttention").clone();
+    $(this).closest(".repeaterRow").after(newRow);
+    newRow.slideDown("slow", function () { newRow.removeClass("CopyAttention hidden"); });
+});
 
 $("div.repeaterAttention").on("click", ".DeleteRow", function (event) {
-    let counter = $("div.repeaterRow").length;
+    let counter = $("div.repeaterAttention > div.repeaterRow").length;
     if(counter > 2){
         $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
     }
 });
 
-$("div.repeaterAttention").on("click", ".AddRow", function (event) {
-    let newRow = $(".Copy").clone();
+$("div.repeaterPhone").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyPhone").clone();
     $(this).closest(".repeaterRow").after(newRow);
-    $('.select2multiple').select2();
-    newRow.slideDown("slow", function () { newRow.removeClass("Copy hidden"); });
+    newRow.slideDown("slow", function () { newRow.removeClass("CopyPhone hidden"); });
 });
+
+$("div.repeaterPhone").on("click", ".DeleteRow", function (event) {
+    let counter = $("div.repeaterPhone > div.repeaterRow").length;
+    if(counter > 2){
+        $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
+    }
+});
+
+$("div.repeaterFax").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyFax").clone();
+    $(this).closest(".repeaterRow").after(newRow);
+    newRow.slideDown("slow", function () { newRow.removeClass("CopyFax hidden"); });
+});
+
+$("div.repeaterFax").on("click", ".DeleteRow", function (event) {
+    let counter = $("div.repeaterFax > div.repeaterRow").length;
+    if(counter > 2){
+        $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
+    }
+});
+
 
