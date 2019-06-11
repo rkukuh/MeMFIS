4e9976d509de95de02090b06820b8e8048f181de
ThamrinPH <husni.sosnet@gmail.com>
2019-06-11 16:49:57 +0700

    WIP Customer create

--- public/js/frontend/customer/repeater.js
+++ public/js/frontend/customer/repeater.js
@@ -37,4 +37,50 @@ $("div.repeaterFax").on("click", ".DeleteRow", function (event) {
     }
 });
 
+$("div.repeaterWebsite").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyWebsite").clone();
+    $(this).closest(".repeaterRow").after(newRow);
+    newRow.slideDown("slow", function () { 
+        $('.select').select2();
+        newRow.removeClass("CopyWebsite hidden"); 
+        });
+});
+
+$("div.repeaterWebsite").on("click", ".DeleteRow", function (event) {
+    let counter = $("div.repeaterWebsite > div.repeaterRow").length;
+    if(counter > 2){
+        $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
+    }
+});
+
+
+$("div.repeaterEmail").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyEmail").clone();
+    $(this).closest(".repeaterRow").after(newRow);
+    newRow.slideDown("slow", function () { newRow.removeClass("CopyEmail hidden"); });
+});
+
+$("div.repeaterEmail").on("click", ".DeleteRow", function (event) {
+    let counter = $("div.repeaterEmail > div.repeaterRow").length;
+    if(counter > 2){
+        $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
+    }
+});
+
+$("div.repeaterDocument").on("click", ".AddRow", function (event) {
+    let newRow = $(".CopyDocument").clone();
+    $(this).closest(".repeaterRow").after(newRow);
+    newRow.slideDown("slow", function () {
+         $('.select').select2();
+         newRow.removeClass("CopyDocument hidden"); 
+         });
+});
+
+$("div.repeaterDocument").on("click", ".DeleteRow", function (event) {
+    let counter = $("div.repeaterDocument > div.repeaterRow").length;
+    if(counter > 2){
+        $(this).closest(".repeaterRow").slideUp("slow", function () { $(this).remove(); });
+    }
+});
+
 
