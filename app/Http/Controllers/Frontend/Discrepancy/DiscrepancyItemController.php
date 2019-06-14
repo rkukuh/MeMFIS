e0c8f819dc94d359e75dc48825c5f98f2e74939e
audhy <audhy.vk01@gmail.com>
2019-06-14 16:11:56 +0700

    store function item defectcard

---
+++ app/Http/Controllers/Frontend/Discrepancy/DiscrepancyItemController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Discrepancy;
+
+use App\Models\Item;
+use App\Models\Type;
+use App\Models\DefectCard;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DiscrepancyItemController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(DefectCard $discrepancy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(DefectCard $discrepancy)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,DefectCard $discrepancy)
+    {
+        $discrepancy->items()->attach($discrepancy->id, [
+            'item_id' => $request->item_id,
+            'unit_id' => $request->unit_id,
+            'quantity' => $request->quantity,
+            'ipc_ref'=> $request->ipc_ref,
+            'sn_on'=> $request->sn_on,
+            'sn_off'=> $request->sn_off,
+        ]);
+
+        return response()->json($discrepancy);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DefectCard $discrepancy,Item $item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DefectCard $discrepancy,Item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DefectCard $discrepancy,Item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DefectCard $discrepancy,Item $item)
+    {
+        //
+    }
+}
