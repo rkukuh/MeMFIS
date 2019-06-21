23131f4dd740637d23818e62258d46ac66e45d01
ibnuprtma <pibnu86@gmail.com>
2019-06-21 15:31:40 +0700

    wip

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -113,6 +113,21 @@
                 <div class="m-menu__submenu">
                     <span class="m-menu__arrow"></span>
                     <ul class="m-menu__subnav">
+                            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"><a href="javascript:;" class="m-menu__link m-menu__toggle"
+                                title="Non functional dummy link"><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
+                                    class="m-menu__link-text">Hard Time</span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
+                                    <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+                                        <ul class="m-menu__subnav">
+                                            <li class="m-menu__item " aria-haspopup="true"><a href="{{ route('frontend.taskcard-routine.create') }}"
+                                                class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
+                                                class="m-menu__link-text">Engineer</span></a>
+                                            </li>
+                                            <li class="m-menu__item " aria-haspopup="true"><a href="{{ route('frontend.taskcard-routine.create') }}"
+                                                class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
+                                                class="m-menu__link-text">Mechanic</span></a>
+                                            </li>
+                                        </ul>
+                                    </div>
                         <li class="m-menu__item" aria-haspopup="true">
                             <a href="{{ route('frontend.jobcard-mechanic.index') }}" class="m-menu__link">
                                 <i class="m-menu__link-bullet m-menu__link-bullet--dot">
