e4c0b42a98020db4c77a4166576622de0b3c5b04
R. Kukuh <rkukuh@gmail.com>
2019-06-29 13:26:50 +0700

    Define one way relationship in TaskCard-WorkPackage pivot

--- app/Models/Pivots/TaskCardWorkPackage.php
+++ app/Models/Pivots/TaskCardWorkPackage.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Pivots;
 
+use App\Models\TaskCard;
+use App\Models\WorkPackage;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class TaskCardWorkPackage extends Pivot
@@ -9,4 +11,26 @@ class TaskCardWorkPackage extends Pivot
     use SoftDeletes;
 
     protected $table = 'taskcard_workpackage';
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-Way: A WorkPackage's TaskCard must have a task card assigned to.
+     *
+     * @return mixed
+     */
+    public function taskcard()
+    {
+        return $this->belongsTo(TaskCard::class);
+    }
+
+    /**
+     * One-Way: A WorkPackage's TaskCard must have a workpackage owning.
+     *
+     * @return mixed
+     */
+    public function workpackage()
+    {
+        return $this->belongsTo(WorkPackage::class);
+    }
 }
