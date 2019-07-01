277aba4b845c8aadf3d3aabb5b6303784ca1621a
R. Kukuh <rkukuh@gmail.com>
2019-07-02 05:49:23 +0700

    Define one-way relationship for Quotation and WorkPackage

--- app/Models/Pivots/QuotationWorkPackage.php
+++ app/Models/Pivots/QuotationWorkPackage.php
@@ -2,9 +2,36 @@
 
 namespace App\Models\Pivots;
 
+use App\Models\Quotation;
+use App\Models\WorkPackage;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class QuotationWorkPackage extends Pivot
 {
-    // 
+    use SoftDeletes;
+
+    protected $table = 'quotation_workpackage';
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-Way: A Quotation's WorkPackages must have a quotation owning to.
+     *
+     * @return mixed
+     */
+    public function quotation()
+    {
+        return $this->belongsTo(Quotation::class);
+    }
+
+    /**
+     * One-Way: A Quotation's WorkPackages must have a workpackage assigned to.
+     *
+     * @return mixed
+     */
+    public function workpackage()
+    {
+        return $this->belongsTo(WorkPackage::class);
+    }
 }
--- database/migrations/2019_07_01_144332_create_quotation_workpackage_items_table.php
+++ database/migrations/2019_07_01_144332_create_quotation_workpackage_items_table.php
@@ -20,6 +20,10 @@ class CreateQuotationWorkpackageItemsTable extends Migration
             $table->unsignedInteger('item_id');
             $table->double('quantity');
             $table->unsignedInteger('unit_id');
+            $table->unsignedInteger('price_id')->nullable();
+            $table->double('price_amount')->nullable();
+            $table->double('subtotal')->nullable();
+            $table->string('note')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
@@ -35,6 +39,11 @@ class CreateQuotationWorkpackageItemsTable extends Migration
 
             $table->foreign('unit_id')
                     ->references('id')->on('units')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('price_id')
+                    ->references('id')->on('prices')
                     ->onUpdate('cascade')
                     ->onDelete('restrict');
         });
