5b4a0ae8bd2376b694515fdefb0a171a79e93cec
audhy <audhy.vk01@gmail.com>
2019-07-09 18:44:22 +0700

    remove unused

--- public/js/frontend/workpackage/edit.js
+++ public/js/frontend/workpackage/edit.js
@@ -1,97 +1,6 @@
 let Workpackage = {
     init: function () {
-        $('.taskcard_datatable').mDatatable({
-            data: {
-                type: 'remote',
-                source: {
-                    read: {
-                        method: 'GET',
-                        url: '/datatables/customer',
-                        map: function (raw) {
-                            let dataSet = raw;
 
-                            if (typeof raw.data !== 'undefined') {
-                                dataSet = raw.data;
-                            }
-
-                            return dataSet;
-                        }
-                    }
-                },
-                pageSize: 10,
-                serverPaging: !0,
-                serverFiltering: !0,
-                serverSorting: !0
-            },
-            layout: {
-                theme: 'default',
-                class: '',
-                scroll: false,
-                footer: !1
-            },
-            sortable: !0,
-            filterable: !1,
-            pagination: !0,
-            search: {
-                input: $('#generalSearch')
-            },
-            toolbar: {
-                items: {
-                    pagination: {
-                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
-                    }
-                }
-            },
-            columns: [
-                {
-                    field: 'title',
-                    title: 'Tittle',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150
-                },
-                {
-                    field: 'type_id',
-                    title: 'Type',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'work_area',
-                    title: 'Area',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'zone',
-                    title: 'Zone',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'rii',
-                    title: 'Rii',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'id',
-                    title: '#',
-                    width: 50,
-                    sortable: !1,
-                    textAlign: 'center',
-                    name: 'sas',
-                    selector: {
-                        class: 'm-checkbox--solid m-checkbox--brand'
-                    }
-                },
-
-            ]
-        });
         $('.nav-tabs').on('click', '.routine', function () {
 
             let basic = $('.basic_datatable').mDatatable();
@@ -130,50 +39,10 @@ let Workpackage = {
 
         });
 
-        // let update = $('.modal-footer').on('click', '.update', function () {
-        //     $('#button').show();
-        //     $('#name-error').html('');
-        //     $('#simpan').text('Perbarui');
-
-        //     let name = $('input[name=name]').val();
-        //     let triggerid = $('input[name=id]').val();
-
-        //     $.ajax({
-        //         headers: {
-        //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        //         },
-        //         type: 'put',
-        //         url: '/category/' + triggerid,
-        //         data: {
-        //             _token: $('input[name=_token]').val(),
-        //             name: name
-        //         },
-        //         success: function (data) {
-        //             if (data.errors) {
-        //                 if (data.errors.name) {
-        //                     $('#name-error').html(data.errors.name[0]);
-
-        //                     document.getElementById('name').value = name;
-        //                 }
-        //             } else {
-        //                 $('#modal_customer').modal('hide');
-
-        //                 toastr.success('Data berhasil disimpan.', 'Sukses', {
-        //                     timeOut: 5000
-        //                 });
-
-        //                 let table = $('.m_datatable').mDatatable();
-
-        //                 table.originalDataSet = [];
-        //                 table.reload();
-        //             }
-        //         }
-        //     });
-        // });
-
         let triggeruuid ="";
         let material_datatables_init = true;
         let tool_datatables_init = true;
+
         //Basic taskcard Datatable
         $('.basic_datatable').on('click', '.material', function () {
             if(material_datatables_init == true){
@@ -350,7 +219,7 @@ let Workpackage = {
             });
 
         });
-        
+
 
         //CPCP taskcard Datatable
         $('.cpcp_datatable').on('click', '.material', function () {
@@ -728,7 +597,7 @@ let Workpackage = {
             });
         });
     }
-    
+
 
 };
 
