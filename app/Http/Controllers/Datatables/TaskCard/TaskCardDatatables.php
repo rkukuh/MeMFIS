4b7a1b42b7c7852baf9c05ae9e7e771b3be931a8
audhy <audhy.vk01@gmail.com>
2019-07-10 10:55:58 +0700

    rename folder

---
+++ app/Http/Controllers/Datatables/TaskCard/TaskCardDatatables.php
@@ -0,0 +1,312 @@
+<?php
+
+namespace App\Http\Controllers\Datatables\TaskCard;
+
+use App\Models\TaskCard;
+use App\Models\ListUtil;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskCardDatatables extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = $alldata = TaskCard::with('type','aircrafts','task')->get();
+
+        foreach($alldata as $taskcard){
+            if(isset($taskcard->workarea) ){
+                $taskcard->workarea_name .= $taskcard->workarea->name;
+            }
+        }
+
+        foreach($alldata as $item){
+            if(isset($item->aircrafts) ){
+                for($index = 0; sizeof($item->aircrafts) > $index; $index++){
+                    if(sizeof($item->aircrafts)-1 == $index){
+                    $item->pesawat .= $item->aircrafts[$index]->name;
+                    }
+                    else{
+                    $item->pesawat .= $item->aircrafts[$index]->name.", ";
+                    }
+                }
+            }
+        }
+
+        foreach($alldata as $item){
+            if(isset($item->skills) ){
+                if(sizeof($item->skills) == 3){
+                    $item->skill .= "ERI";
+                }
+                else if(sizeof($item->skills) == 1){
+                    $item->skill .= $item->skills[0]->name;
+                }
+                else{
+                    $item->skill .= '';
+                }
+            }
+        }
+
+        $data = $alldata = json_decode($alldata);
+
+        $datatable = array_merge(['pagination' => [], 'sort' => [], 'query' => []], $_REQUEST);
+
+        $filter = isset($datatable['query']['generalSearch']) && is_string($datatable['query']['generalSearch'])
+                    ? $datatable['query']['generalSearch'] : '';
+
+        if (! empty($filter)) {
+            $data = array_filter($data, function ($a) use ($filter) {
+                return (boolean)preg_grep("/$filter/i", (array)$a);
+            });
+
+            unset($datatable['query']['generalSearch']);
+        }
+
+        $query = isset($datatable['query']) && is_array($datatable['query']) ? $datatable['query'] : null;
+
+        if (is_array($query)) {
+            $query = array_filter($query);
+
+            foreach ($query as $key => $val) {
+                $data = $this->list_filter($data, [$key => $val]);
+            }
+        }
+
+        $sort  = ! empty($datatable['sort']['sort']) ? $datatable['sort']['sort'] : 'asc';
+        $field = ! empty($datatable['sort']['field']) ? $datatable['sort']['field'] : 'RecordID';
+
+        $meta    = [];
+        $page    = ! empty($datatable['pagination']['page']) ? (int)$datatable['pagination']['page'] : 1;
+        $perpage = ! empty($datatable['pagination']['perpage']) ? (int)$datatable['pagination']['perpage'] : -1;
+
+        $pages = 1;
+        $total = count($data);
+
+        usort($data, function ($a, $b) use ($sort, $field) {
+            if (! isset($a->$field) || ! isset($b->$field)) {
+                return false;
+            }
+
+            if ($sort === 'asc') {
+                return $a->$field > $b->$field ? true : false;
+            }
+
+            return $a->$field < $b->$field ? true : false;
+        });
+
+        if ($perpage > 0) {
+            $pages  = ceil($total / $perpage);
+            $page   = max($page, 1);
+            $page   = min($page, $pages);
+            $offset = ($page - 1) * $perpage;
+
+            if ($offset < 0) {
+                $offset = 0;
+            }
+
+            $data = array_slice($data, $offset, $perpage, true);
+        }
+
+        $meta = [
+            'page'    => $page,
+            'pages'   => $pages,
+            'perpage' => $perpage,
+            'total'   => $total,
+        ];
+
+        if (isset($datatable['requestIds']) && filter_var($datatable['requestIds'], FILTER_VALIDATE_BOOLEAN)) {
+            $meta['rowIds'] = array_map(function ($row) {
+                return $row->RecordID;
+            }, $alldata);
+        }
+
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+        $result = [
+            'meta' => $meta + [
+                    'sort'  => $sort,
+                    'field' => $field,
+                ],
+            'data' => $data,
+        ];
+
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function filter(Request $request)
+    {
+        // dd($request->taskcard_routine_type);
+
+        $TaskCard = TaskCard::with('task','aircrafts','skills','type');
+
+        if (!empty($request->task_type_id)) {
+            $TaskCard->whereHas('task', function ($query) use ($request) {
+                $query->where('task_id', $request->task_type_id);
+            });
+        }
+        if (!empty($request->aircrafts)) {
+            $TaskCard->whereHas('aircrafts', function ($query) use ($request) {
+                $query->whereIn('aircraft_id', $request->aircrafts);
+            });
+        }
+        if (!empty($request->skills)) {
+            $TaskCard->whereHas('skills', function ($query) use ($request) {
+                $query->where('skill_id', $request->skills);
+            });
+        }
+        if (!empty($request->taskcard_routine_type)) {
+            $TaskCard->whereHas('type', function ($query) use ($request) {
+                $query->whereIn('type_id', $request->taskcard_routine_type);
+            });
+        }
+        // if (!empty($request->task_card_no)) {
+        //     $TaskCard->orderBy('number', $request->task_card_no);
+        // }
+        $TaskCard = $TaskCard->get();
+
+        foreach($TaskCard as $item){
+            if(isset($item->aircrafts) ){
+                for($index = 0; sizeof($item->aircrafts) > $index; $index++){
+                    if(sizeof($item->aircrafts)-1 == $index){
+                    $item->pesawat .= $item->aircrafts[$index]->name;
+                    }
+                    else{
+                    $item->pesawat .= $item->aircrafts[$index]->name.", ";
+                    }
+                }
+            }
+        }
+
+        foreach($TaskCard as $item){
+            if(isset($item->skills) ){
+                if(sizeof($item->skills) == 3){
+                    $item->skill .= "ERI";
+                }
+                else if(sizeof($item->skills) == 1){
+                    $item->skill .= $item->skills[0]->name;
+                }
+                else{
+                    $item->skill .= '';
+                }
+            }
+        }
+
+        // dd($TaskCard->get());
+
+        $data = $alldata = json_decode($TaskCard);
+        $datatable = array_merge(['pagination' => [], 'sort' => [], 'query' => []], $_REQUEST);
+
+        $filter = isset($datatable['query']['generalSearch']) && is_string($datatable['query']['generalSearch'])
+                    ? $datatable['query']['generalSearch'] : '';
+
+        if (! empty($filter)) {
+            $data = array_filter($data, function ($a) use ($filter) {
+                return (boolean)preg_grep("/$filter/i", (array)$a);
+            });
+
+            unset($datatable['query']['generalSearch']);
+        }
+
+        $query = isset($datatable['query']) && is_array($datatable['query']) ? $datatable['query'] : null;
+
+        if (is_array($query)) {
+            $query = array_filter($query);
+
+            foreach ($query as $key => $val) {
+                $data = $this->list_filter($data, [$key => $val]);
+            }
+        }
+
+        $sort  = ! empty($datatable['sort']['sort']) ? $datatable['sort']['sort'] : 'asc';
+        $field = ! empty($datatable['sort']['field']) ? $datatable['sort']['field'] : 'RecordID';
+
+        $meta    = [];
+        $page    = ! empty($datatable['pagination']['page']) ? (int)$datatable['pagination']['page'] : 1;
+        $perpage = ! empty($datatable['pagination']['perpage']) ? (int)$datatable['pagination']['perpage'] : -1;
+
+        $pages = 1;
+        $total = count($data);
+
+        usort($data, function ($a, $b) use ($sort, $field) {
+            if (! isset($a->$field) || ! isset($b->$field)) {
+                return false;
+            }
+
+            if ($sort === 'asc') {
+                return $a->$field > $b->$field ? true : false;
+            }
+
+            return $a->$field < $b->$field ? true : false;
+        });
+
+        if ($perpage > 0) {
+            $pages  = ceil($total / $perpage);
+            $page   = max($page, 1);
+            $page   = min($page, $pages);
+            $offset = ($page - 1) * $perpage;
+
+            if ($offset < 0) {
+                $offset = 0;
+            }
+
+            $data = array_slice($data, $offset, $perpage, true);
+        }
+
+        $meta = [
+            'page'    => $page,
+            'pages'   => $pages,
+            'perpage' => $perpage,
+            'total'   => $total,
+        ];
+
+        if (isset($datatable['requestIds']) && filter_var($datatable['requestIds'], FILTER_VALIDATE_BOOLEAN)) {
+            $meta['rowIds'] = array_map(function ($row) {
+                return $row->RecordID;
+            }, $alldata);
+        }
+
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+        $result = [
+            'meta' => $meta + [
+                    'sort'  => $sort,
+                    'field' => $field,
+                ],
+            'data' => $data,
+        ];
+
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Show data from model with flter on datatable.
+     *
+     * @param $list, $args, $operator
+     * @return \Illuminate\Http\Response
+     */
+    public function list_filter($list, $args = array(), $operator = 'AND')
+    {
+        if (! is_array($list)) {
+            return array();
+        }
+
+        $util = new ListUtil($list);
+
+        return $util->filter($args, $operator);
+    }
+}
