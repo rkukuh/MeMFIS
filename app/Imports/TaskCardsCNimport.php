0bb8ecc8ce1bce1adbc3087c1d6dbe3d6a7fbf92
audhy <audhy.vk01@gmail.com>
2019-07-10 07:18:35 +0700

    fix taskcard import if company task null

--- app/Imports/TaskCardsCNimport.php
+++ app/Imports/TaskCardsCNimport.php
@@ -189,12 +189,14 @@ class TaskCardsCNimport implements ToModel, WithHeadingRow
                     ->where('name', 'Basic')->first();
             }
         }
-        $additionals = [];
         if( $row['company_task']) {
+            $additionals = [];
             $additionals["company_task"] = $row['company_task'];
-        // }else{
-        //     $additionals["company_task"] = "";
+            $additionals =  json_encode($additionals);
+        }else{
+            $additionals = null;
         }
+
         $taskcard =  new TaskCard([
             'number' => $row['number'],
             'title' => $row['title'],
@@ -212,7 +214,7 @@ class TaskCardsCNimport implements ToModel, WithHeadingRow
             'reference' => $row['reference'],
             'version' => json_encode(explode(';',$row['version'])),
             'description' => $row['description'],
-            'additionals' => json_encode($additionals)
+            'additionals' => $additionals
         ]);
 
         $taskcard->save();
