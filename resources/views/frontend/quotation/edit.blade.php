d62c3206f708b0c9889d48624f7036999b1dd682
audhy <audhy.vk01@gmail.com>
2019-07-10 14:34:35 +0700

    fix quotation bug

--- resources/views/frontend/quotation/edit.blade.php
+++ resources/views/frontend/quotation/edit.blade.php
@@ -86,7 +86,7 @@
                                                             @endcomponent
                                                         </div>
                                                     </div>
-                                                    <input type="hidden" id="customer_id" name="customer_id" value="{{ $quotation->customer->uuid }}">
+                                                    <input type="hidden" id="customer_id" name="customer_id" value="{{ $quotation->project->customer->uuid }}">
 
                                                 </div>
                                                 <div class="col-sm-6 col-md-6 col-lg-6">
