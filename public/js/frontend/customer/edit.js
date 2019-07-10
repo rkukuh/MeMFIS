f045a90da25ac607866cdd3b16aa451861de76d3
ThamrinPH <husni.sosnet@gmail.com>
2019-07-10 09:38:07 +0700

    Customer edit

--- public/js/frontend/customer/edit.js
+++ public/js/frontend/customer/edit.js
@@ -748,89 +748,119 @@ let Customer = {
             let name = $('input[name=name]').val();
             let payment_term = $('input[name=term_of_payment]').val();
             let account_code = $('#account_code').val();
-            let level = $('#customer-level').val();
+            let level = $('select[name="customer-level"]').val();
 
             let phone_array = [];
-            $('#phone').each(function (i) {
-                phone_array[i] = document.getElementsByName('group-phone[' + i + '][phone]')[0].value;
+            $('input[name^=phone_array]').each(function (i) {
+                phone_array[i] = $(this).val();
             });
-
+            phone_array.pop();
 
             let ext_phone_array = [];
-            $('#ext ').each(function (i) {
-                ext_phone_array[i] = document.getElementsByName('group-phone[' + i + '][ext]')[0].value;
+            $('input[name^=ext]').each(function (i) {
+                ext_phone_array[i] = $(this).val();
             });
+            ext_phone_array.pop();
 
             let type_phone_array = [];
-            $('input[name^=phone_array]').each(function (i) {
-                type_phone_array[i] = $('input[name="group-phone[' + i + '][type_phone]"]:checked').val();
+            $('#type_phone ').each(function (i) {
+                type_phone_array[i] = $(this).val();
             });
-
+            type_phone_array.pop();
 
             let fax_array = [];
             $('#fax ').each(function (i) {
-                fax_array[i] = document.getElementsByName('group-fax[' + i + '][fax]')[0].value;
+                fax_array[i] = $(this).val();
             });
+            fax_array.pop();
 
             let type_fax_array = [];
-            $('#fax ').each(function (i) {
-                type_fax_array[i] = $('input[name="group-fax[' + i + '][type_fax]"]:checked').val();
+            $('#type_fax ').each(function (i) {
+                type_fax_array[i] = $(this).val();
             });
+            type_fax_array.pop();
 
             let website_array = [];
             $('#website ').each(function (i) {
-                website_array[i] = document.getElementsByName('group-website[' + i + '][website]')[0].value;
+                website_array[i] = $(this).val();
             });
+            website_array.pop();
+
+            let type_website_array = [];
+            $('select[name^=type_website]').each(function (i) {
+                type_website_array[i] = $(this).val();
+            });
+            type_website_array.pop();
 
             let email_array = [];
             $('input[name^=email_array]').each(function (i) {
-                email_array[i] = document.getElementsByName('group-email[' + i + '][email]')[0].value;
+                email_array[i] = $(this).val();
             });
+            email_array.pop();
 
             let type_email_array = [];
+            $('#type_email ').each(function (i) {
+                type_email_array[i] = $(this).val();
+            });
+            type_email_array.pop();
+
+            let document_array = [];
             $('#email ').each(function (i) {
-                type_email_array[i] = $('input[name="group-email[' + i + '][type_email]"]:checked').val();
+                document_array[i] = $(this).val();
             });
+            document_array.pop();
 
-
-            if (document.getElementById("active").checked) {
-                banned = 1;
-            } else {
-                banned = 0;
-            }
-
-            let attn_name_array = [];
-            $('input[name="attn-name"]').each(function (i) {
-                attn_name_array[i] = $('input[name="attn-name"]')[i].value;
+            let type_document_array = [];
+            $('input[name^=type_document]').each(function (i) {
+                type_document_array[i] = $(this).val();
             });
+            type_document_array.pop();
 
-            let attn_position_array = [];
-            $('input[name="attn-position"]').each(function (i) {
-                attn_position_array[i] = $('input[name="attn-position"]')[i].value;
-            });
-            attn_position_array.pop();
-            
             let attn_phone_array = [];
-            $('input[name="attn-phone"]').each(function (i) {
-                attn_phone_array[i] = $('input[name="attn-phone"]')[i].value;
+            $('select[name^=attn-phone]').each(function () {
+                let attn_phone_array_row = [];
+                ai = 0;
+                $(this).val().forEach(function(entry) {
+                    attn_phone_array_row[ai] = entry;
+                    ai++;
+                });;
+                attn_phone_array.push(attn_phone_array_row);
             });
             attn_phone_array.pop();
 
+            let attn_position_array = [];
+            $('#attn-position ').each(function (i) {
+                attn_position_array[i] = $(this).val();
+            });
+            attn_position_array.pop();
+
+            let attn_name_array = [];
+            $('#attn-name ').each(function (i) {
+                attn_name_array[i] = $(this).val();
+            });
+            attn_name_array.pop();
+
             let attn_ext_array = [];
-            $('input[name="attn-ext"]').each(function (i) {
-                attn_ext_array[i] = $('input[name="attn-ext"]')[i].value;
+            $('#attn-ext ').each(function (i) {
+                attn_ext_array[i] = $(this).val();
             });
             attn_ext_array.pop();
-            
+
             let attn_fax_array = [];
-            $('input[name="attn-fax"]').each(function (i) {
-                attn_fax_array[i] = $('input[name="attn-fax"]')[i].value;
+            $('#attn-fax ').each(function (i) {
+                attn_fax_array[i] = $(this).val();
             });
             attn_fax_array.pop();
 
             let attn_email_array = [];
-            $('input[name="attn-email"]').each(function (i) {
-                attn_email_array[i] = $('input[name="attn-email"]')[i].value;
+            $('select[name^=attn-email]').each(function (i) {
+                let attn_email_array_row = [];
+                ai = 0;
+                $(this).val().forEach(function(entry) {
+                    attn_email_array_row[ai] = entry;
+                   ai++;
+               });;
+               attn_email_array.push(attn_email_array_row);
             });
             attn_email_array.pop();
 
@@ -852,8 +882,11 @@ let Customer = {
                     fax_array: fax_array,
                     type_fax_array: type_fax_array,
                     website_array: website_array,
+                    type_website_array: type_website_array,
                     email_array: email_array,
                     type_email_array: type_email_array,
+                    document_array: document_array,
+                    type_document_array: type_document_array,
                     attn_phone_array:attn_phone_array,
                     attn_name_array:attn_name_array,
                     attn_position_array:attn_position_array,
