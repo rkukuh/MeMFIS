f8407f7ed206cfca873e9813f430b635d4b46b2e
audhy <audhy.vk01@gmail.com>
2019-07-18 16:31:42 +0700

    wip jobcard

--- resources/views/frontend/job-card/filter.blade.php
+++ resources/views/frontend/job-card/filter.blade.php
@@ -133,7 +133,7 @@
 <script src="{{ asset('js/frontend/functions/select2/taskcard-routine-type.js') }}"></script>
 <script src="{{ asset('js/frontend/functions/fill-combobox/taskcard-routine-type.js') }}"></script>
 
-<script src="{{ asset('js/frontend/job-card/filter.js') }}"></script>
+{{-- <script src="{{ asset('js/frontend/job-card/filter.js') }}"></script> --}}
 
 
 @endpush
