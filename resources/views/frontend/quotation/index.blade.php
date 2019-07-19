64592d9669d403ed070a7aabfe10bf7c7c809068
ibnuprtma <pibnu86@gmail.com>
2019-07-19 16:47:34 +0700

    add filter quotation index

--- resources/views/frontend/quotation/index.blade.php
+++ resources/views/frontend/quotation/index.blade.php
@@ -53,6 +53,9 @@
                                 <div class="row align-items-center">
                                     <div class="col-xl-8 order-2 order-xl-1">
                                         <div class="form-group m-form__group row align-items-center">
+                                            <div class="col-md-2">
+                                                @include('frontend.common.buttons.filter')
+                                            </div>
                                             <div class="col-md-4">
                                                 <div class="m-input-icon m-input-icon--left">
                                                     <input type="text" class="form-control m-input" placeholder="Search..."
@@ -86,6 +89,9 @@
                                 </div>
                             </div>
 
+                            <div class="col-lg-12">
+                                @include('frontend.quotation.filter')
+                            </div>
                             <div class="m_datatable" id="scrolling_both"></div>
                         </div>
                     </div>
