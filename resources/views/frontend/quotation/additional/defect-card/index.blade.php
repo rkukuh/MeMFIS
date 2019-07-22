ebf5d9dd0f32fb07f0b503930750e9348b3a45f2
ibnuprtma <pibnu86@gmail.com>
2019-07-22 15:32:31 +0700

    wip additional task quotation

---
+++ resources/views/frontend/quotation/additional/defect-card/index.blade.php
@@ -0,0 +1,30 @@
+
+<div class="form-group m-form__group row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="m-portlet m-portlet--mobile">
+                <div class="m-portlet__body">
+                    <div class="row align-items-center">
+    
+                        <div class="col-xl-8 order-2 order-xl-1">
+                            <div class="form-group m-form__group row align-items-center">
+                                <div class="col-md-4">
+                                    <div class="m-input-icon m-input-icon--left">
+                                        <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
+                                        <span class="m-input-icon__icon m-input-icon__icon--left">
+                                            <span><i class="la la-search"></i></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 order-1 order-xl-2 m--align-right">
+    
+                            <div class="m-separator m-separator--dashed d-xl-none"></div>
+                        </div>
+    
+                    </div>
+                    <div class="defect_card_datatable" id="scrolling_both"></div>
+                </div>
+            </div>
+        </div>
+    </div>
