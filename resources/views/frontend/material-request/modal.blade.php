e4f5ffd5c22b99b87a157e46a058b0f55add6b46
ibnuprtma <pibnu86@gmail.com>
2019-07-23 14:20:19 +0700

    UI form material request project

---
+++ resources/views/frontend/material-request/modal.blade.php
@@ -0,0 +1,96 @@
+<div class="modal fade" id="modal_material_request" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                @include('frontend.common.label.create-new')
+
+                <h5 class="modal-title" id="TitleModalInstruction">
+                    Material Request Project
+
+                    <small id="instruction" class="m--font-focus"></small>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed" id="InstructionForm">
+                    <input type="hidden" class="form-control form-control-danger m-input" name="uuid" id="uuid">
+                    <div class="m-portlet__body">
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                <label class="form-control-label">
+                                    Item
+                                </label>
+
+                                @component('frontend.common.input.select2')
+                                    @slot('text', 'Item')
+                                    @slot('id', 'item')
+                                    @slot('name', 'item')
+                                    @slot('id_error', 'item')
+                                    @slot('style', 'width:100%')
+                                @endcomponent
+                            </div>
+                        </div>
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Qty 
+                                </label>
+        
+                                @component('frontend.common.input.number')
+                                    @slot('id', 'qty')
+                                    @slot('text', 'Qty')
+                                    @slot('name', 'qty')
+                                    @slot('id_error', 'qty')
+                                @endcomponent
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <label class="form-control-label">
+                                    Unit
+                                </label>
+
+                                @component('frontend.common.input.select2')
+                                    @slot('text', 'Unit')
+                                    @slot('id', 'unit_id')
+                                    @slot('name', 'unit_id')
+                                    @slot('id_error', 'unit')
+                                @endcomponent
+                            </div>
+                        </div>
+                        <div class="form-group m-form__group row ">
+                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                <label class="form-control-label">
+                                    Remark
+                                </label>
+
+                                @component('frontend.common.input.textarea')
+                                    @slot('rows', '3')
+                                    @slot('id', 'note')
+                                    @slot('name', 'note')
+                                    @slot('text', 'Note')
+                                    @slot('required', 'required')
+                                @endcomponent
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer modal-footer-instruction">
+                        <div class="flex">
+                            <div class="action-buttons">
+                                    @component('frontend.common.buttons.submit')
+                                        @slot('class', 'add-instruction')
+                                        @slot('type', 'button')
+                                    @endcomponent
+                                    @component('frontend.common.buttons.reset')
+                                        @slot('class', 'reset-item')
+                                    @endcomponent
+                                @include('frontend.common.buttons.close')
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/material-request/project/create.blade.php
@@ -0,0 +1,250 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader hidden">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Material Request Project
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="{{ route('frontend.material-request.index') }}" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Material Request Project
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+
+                                @include('frontend.common.label.create-new')
+
+                                <h3 class="m-portlet__head-text">
+                                    Material Request Project
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <form id="itemform" name="itemform">
+                                <div class="m-portlet__body">
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Date @include('frontend.common.label.required')
+                                            </label>
+
+                                            @component('frontend.common.input.datepicker')
+                                                @slot('id', 'date')
+                                                @slot('text', 'Date')
+                                                @slot('name', 'date')
+                                                @slot('id_error','requested_at')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Storage
+                                            </label>
+
+                                            @component('frontend.common.input.select2')
+                                                @slot('text', 'Storage')
+                                                @slot('id', 'item_storage_id')
+                                                @slot('name', 'item_storage_id')
+                                                @slot('id_error', 'item_storage_id')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Ref Job Card No. @include('frontend.common.label.required')
+                                            </label>
+
+                                            @include('frontend.common.warehouse.index')
+
+                                            @component('frontend.common.input.hidden')
+                                                @slot('id', 'warehouse')
+                                                @slot('name', 'warehouse')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Location 
+                                            </label>
+
+                                            @component('frontend.common.label.data-info')
+                                                @slot('text', 'Generated')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Project No.
+                                            </label>
+
+                                            @component('frontend.common.label.data-info')
+                                                @slot('id', 'project_number')
+                                                @slot('text', 'generate')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Section Code
+                                            </label>
+
+                                            @component('frontend.common.input.text')
+                                                @slot('text', 'Section Code')
+                                                @slot('id', 'section_code')
+                                                @slot('name', 'section_code')
+                                                @slot('id_error', 'section_code')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <div class="form-group m-form__group row">
+                                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                                    <label class="form-control-label">
+                                                        A/C Type
+                                                    </label>
+
+                                                    @component('frontend.common.label.data-info')
+                                                        @slot('id', 'actype')
+                                                        @slot('text', 'Generate')
+                                                    @endcomponent
+                                                </div>
+                                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                                    <label class="form-control-label">
+                                                        A/C Reg
+                                                    </label>
+
+                                                    @component('frontend.common.label.data-info')
+                                                        @slot('id', 'acreg')
+                                                        @slot('text', 'Generate')
+                                                    @endcomponent
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Recieved By 
+                                            </label>
+
+                                            @component('frontend.common.input.select2')
+                                                @slot('text', 'Recieved By')
+                                                @slot('id', 'received-by')
+                                                @slot('name', 'received-by')
+                                                @slot('id_error', 'received-by')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-12 col-md-12 col-lg-12">
+                                            <label class="form-control-label">
+                                                Remark
+                                            </label>
+
+                                            @component('frontend.common.input.textarea')
+                                                @slot('rows', '5')
+                                                @slot('id', 'description')
+                                                @slot('name', 'description')
+                                                @slot('text', 'Description')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-12 col-md-12 col-lg-12">
+
+                                            @include('frontend.material-request.modal')
+                                            <div class="purchase_order_datatable" id="purchase_order_datatable"></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-12 col-md-12 col-lg-12 footer">
+                                            <div class="flex">
+                                                <div class="action-buttons">
+                                                    @component('frontend.common.buttons.submit')
+                                                        @slot('type','button')
+                                                        @slot('id', 'add-workpackage')
+                                                        @slot('class', 'add-workpackage')
+                                                    @endcomponent
+
+                                                    @include('frontend.common.buttons.reset')
+
+                                                    @include('frontend.common.buttons.back')
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('header-scripts')
+    <style>
+        fieldset {
+            margin-bottom: 30px;
+        }
+
+        .padding-datatable {
+            padding: 0px
+        }
+
+        .margin-info {
+            margin-left: 5px
+        }
+    </style>
+@endpush
+
+@push('footer-scripts')
+
+    <script src="{{ asset('js/frontend/material-request/create.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/datepicker/date.js')}}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/received-by.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/item.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/item.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/unit.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/unit.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/storage.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/storage.js') }}"></script>
+
+    <script src="{{ asset('js/frontend/functions/select2/item.js') }}"></script>
+    <script src="{{ asset('js/frontend/functions/fill-combobox/item.js') }}"></script>
+@endpush
