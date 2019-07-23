d1496198d910a9984ea529adec00892d5e87976d
audhy <audhy.vk01@gmail.com>
2019-07-23 13:24:16 +0700

    fix bug

--- resources/views/frontend/quotation/additional/modal-project.blade.php
+++ resources/views/frontend/quotation/additional/modal-project.blade.php
@@ -1,36 +1,25 @@
 <div class="modal fade" id="modal_additional_quotation" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Project</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" align="left">
-                <div class="form-group m-form__group row">
-                    <div class="col-sm-12 col-md-12 col-lg-12">
-                        <label class="form-control-label">
-                            Project
-                        </label>
-
-                        @component('frontend.common.input.select2')
-                            @slot('id', 'project-additional')
-                            @slot('text', 'Project')
-                            @slot('name', 'project-additional')
-                            @slot('id_error', 'project-additional')
-                        @endcomponent
-                    </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Project</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
+                <div class="modal-body" align="left">
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-12 col-md-12 col-lg-12">
                             <label class="form-control-label">
                                 Project
                             </label>
-    
-                            <select name="type_website" class="form-control selectWebsite">
-                                    <option value="">
-                                        Select a Project
-                                    </option>
-                            </select>
+
+                            @component('frontend.common.input.select2')
+                                @slot('id', 'project-additional')
+                                @slot('text', 'Project')
+                                @slot('name', 'project-additional')
+                                @slot('id_error', 'project-additional')
+                            @endcomponent
                         </div>
                     </div>
                 </div>
@@ -49,12 +38,11 @@
             </div>
         </div>
     </div>
-</div>
-@push('footer-scripts')
-    <script>
-        $('.modal-footer').on('click', '.create', function () {
-            let project_uuid =$('#project-additional').val();
-            window.location.href = 'quotation-additional/create/'+project_uuid+'/';
-        });
-    </script>
-@endpush
+    @push('footer-scripts')
+        <script>
+            $('.modal-footer').on('click', '.create', function () {
+                let project_uuid =$('#project-additional').val();
+                window.location.href = 'quotation-additional/create/'+project_uuid+'/';
+            });
+        </script>
+    @endpush
