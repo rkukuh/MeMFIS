7bbff6c3505232b9e8fafa10522c69c1e14b3f57
audhy <audhy.vk01@gmail.com>
2019-07-24 19:51:34 +0700

    fix duplicated document number

--- app/Http/Controllers/Frontend/Quotation/QuotationController.php
+++ app/Http/Controllers/Frontend/Quotation/QuotationController.php
@@ -76,7 +76,7 @@ class QuotationController extends Controller
         $contact['fax'] = $request->attention_fax;
         $contact['email'] = $request->attention_email;
 
-        $request->merge(['number' => DocumentNumber::generate('QPRO-', Quotation::count()+1)]);
+        $request->merge(['number' => DocumentNumber::generate('QPRO-', Quotation::withTrashed()->count()+1)]);
         $request->merge(['attention' => json_encode($contact)]);
         $request->merge(['project_id' => Project::where('uuid',$request->project_id)->first()->id]);
 
@@ -332,7 +332,7 @@ class QuotationController extends Controller
                 }
 
                 $jobcard = JobCard::create([
-                    'number' => DocumentNumber::generate('J'.$tc_code.'-', JobCard::count()+1),
+                    'number' => DocumentNumber::generate('J'.$tc_code.'-', JobCard::withTrashed()->count()+1),
                     'taskcard_id' => $tc->id,
                     'quotation_id' => $quotation->id,
                     'data_taskcard' => $tc->toJson(),
