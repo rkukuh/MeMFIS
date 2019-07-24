2b9a4c8c3eb590bcc00f1bc140729fcb65287f3f
ibnuprtma <pibnu86@gmail.com>
2019-07-24 12:17:23 +0700

    fix broken Ui

--- resources/views/frontend/quotation/index.blade.php
+++ resources/views/frontend/quotation/index.blade.php
@@ -51,7 +51,7 @@
                         <div class="m-portlet__body">
                             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                                 <div class="row align-items-center">
-                                    <div class="col-xl-8 order-2 order-xl-1">
+                                    <div class="col-xl-7 order-2 order-xl-1">
                                         <div class="form-group m-form__group row align-items-center">
                                             <div class="col-md-2">
                                                 @include('frontend.common.buttons.filter')
@@ -67,7 +67,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-xl-4 order-1 order-xl-2 m--align-right">
+                                    <div class="col-xl-5 order-1 order-xl-2 m--align-right">
                                         <div class="m-btn-group m-btn-group--pill btn-group" role="group" aria-label="Button group with nested dropdown">
                                             <a href="{{route('frontend.quotation.create')}}" class="m-btn btn btn-primary">
                                                 <span>
@@ -88,9 +88,10 @@
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="col-lg-12">
-                                @include('frontend.quotation.filter')
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    @include('frontend.quotation.filter')
+                                </div>
                             </div>
                             <div class="m_datatable" id="scrolling_both"></div>
                         </div>
