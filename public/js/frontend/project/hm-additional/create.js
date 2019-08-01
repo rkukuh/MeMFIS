0d98bc4400c74ee49d95970855a2b798da113fb2
audhy <audhy.vk01@gmail.com>
2019-08-01 12:35:14 +0700

    init datatable material on additional project

--- public/js/frontend/project/hm-additional/create.js
+++ public/js/frontend/project/hm-additional/create.js
@@ -117,9 +117,193 @@ let AdditionalTaskCreate = (function() {
     return {
 
         init: function() {
+            $('.materials_datatable').mDatatable({
+                data: {
+                    type: 'remote',
+                    source: {
+                        read: {
+                            method: 'GET',
+                            url: '/datatables/project/additional/initial',
+                            map: function (raw) {
+                                let dataSet = raw;
+
+                                if (typeof raw.data !== 'undefined') {
+                                    dataSet = raw.data;
+                                }
+
+                                return dataSet;
+                            }
+                        }
+                    },
+                    pageSize: 10,
+                    serverPaging: !0,
+                    serverFiltering: !0,
+                    serverSorting: !0
+                },
+                layout: {
+                    theme: 'default',
+                    class: '',
+                    scroll: false,
+                    footer: !1
+                },
+                sortable: !0,
+                filterable: !1,
+                pagination: !0,
+                search: {
+                    input: $('#generalSearch')
+                },
+                toolbar: {
+                    items: {
+                        pagination: {
+                            pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                        }
+                    }
+                },
+                columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-tool" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+                ]
+            });
+            $('.tools_datatable').mDatatable({
+                data: {
+                    type: 'remote',
+                    source: {
+                        read: {
+                            method: 'GET',
+                            url: '/datatables/project/additional/initial',
+                            map: function (raw) {
+                                let dataSet = raw;
+
+                                if (typeof raw.data !== 'undefined') {
+                                    dataSet = raw.data;
+                                }
+
+                                return dataSet;
+                            }
+                        }
+                    },
+                    pageSize: 10,
+                    serverPaging: !0,
+                    serverFiltering: !0,
+                    serverSorting: !0
+                },
+                layout: {
+                    theme: 'default',
+                    class: '',
+                    scroll: false,
+                    footer: !1
+                },
+                sortable: !0,
+                filterable: !1,
+                pagination: !0,
+                search: {
+                    input: $('#generalSearch')
+                },
+                toolbar: {
+                    items: {
+                        pagination: {
+                            pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                        }
+                    }
+                },
+                columns: [{
+                    field: 'code',
+                    title: 'P/N',
+                    sortable: !1,
+                },
+                {
+                    field: 'name',
+                    title: 'Title',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Tool Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.quantity',
+                    title: 'Qty',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'pivot.unit_id',
+                    title: 'Unit',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+                        return (
+                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete-tool" title="Delete" data-uuid="' + t.uuid + '">' +
+                                '<i class="la la-trash"></i>' +
+                            '</a>'
+                        );
+                    }
+                }
+                ]
+            });
+
             !(function() {
               (t.extensions = { checkbox: {} }),
                 (t.search = { input: $("#generalSearch1") });
+              let additional_material_datatables_init = true;
               let e = $("#defect_card_datatable").mDatatable(t);
               $("#m_form_status1").on("change", function() {
                 e.search(
@@ -151,8 +335,9 @@ let AdditionalTaskCreate = (function() {
                 $(".nav-tabs")
                   .on("click", ".mat-tool", function() {
                     let uuids = e.checkbox().getSelectedId();
-                    additional_materials_get_datatable(uuids);
                     additional_tools_get_datatable(uuids);
+
+                    additional_materials_get_datatable(uuids);
                   });
             })();
 
