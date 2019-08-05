3f28f6a1af4e696d7a6eb16c1faddf44a8fc052b
ibnuprtma <pibnu86@gmail.com>
2019-08-05 20:53:58 +0700

    add sidebar menu leave period and leave types

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -430,6 +430,22 @@
                     </span>
                 </a>
             </li>
+            <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
+                <a href="{{ route('frontend.hr.leave-period.index') }}" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-list-3"></i>
+                    <span class="m-menu__link-text">
+                        Leave Period
+                    </span>
+                </a>
+            </li>
+            <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
+                <a href="{{ route('frontend.hr.leave-types.index') }}" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-list-3"></i>
+                    <span class="m-menu__link-text">
+                        Leave Types
+                    </span>
+                </a>
+            </li>
             <li class="m-menu__section">
                 <h4 class="m-menu__section-text">
                     WAREHOUSE
