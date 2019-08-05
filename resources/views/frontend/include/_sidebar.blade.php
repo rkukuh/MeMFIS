5bcedfbe45fd1af40aaac6f4b27a14e9169e03ad
ibnuprtma <pibnu86@gmail.com>
2019-08-05 13:48:41 +0700

    initial route sidebar

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -341,7 +341,7 @@
                 </a>
             </li>
             <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
-                <a href="#" class="m-menu__link m-menu__toggle">
+                <a href="{{ route('frontend.work-progress-report.index')}}" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
                         Work Progress Report
