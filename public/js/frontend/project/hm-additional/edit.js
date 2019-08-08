8dfa3734ab0449f7048ae6712757f43a436a2491
TomreinPH <husni.sosnet@gmail.com>
2019-08-08 11:38:39 +0700

    erase extra '/'

--- public/js/frontend/project/hm-additional/edit.js
+++ public/js/frontend/project/hm-additional/edit.js
@@ -511,7 +511,7 @@ let AdditionalTaskCreate = (function() {
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
                     type: 'POST',
-                    url: '/project-hm-additional/'+project_uuid+'/',
+                    url: '/project-hm-additional/'+project_uuid,
                     processData: false,
                     contentType: false,
                     cache: false,
