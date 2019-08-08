015ab4e15d931df7143c3c27082b05d6c9985a0c
R. Kukuh <rkukuh@gmail.com>
2019-08-08 07:19:19 +0700

    Create and finish entity for Quotation's Defect Card's Item

---
+++ app/Http/Controllers/Admin/QuotationDefectCardItemController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\QuotationDefectCardItem;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\QuotationDefectCardItemStore;
+use App\Http\Requests\Admin\QuotationDefectCardItemUpdate;
+
+class QuotationDefectCardItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\QuotationDefectCardItemStore  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(QuotationDefectCardItemStore $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\QuotationDefectCardItem  $quotationDefectCardItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QuotationDefectCardItem $quotationDefectCardItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\QuotationDefectCardItem  $quotationDefectCardItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QuotationDefectCardItem $quotationDefectCardItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Admin\QuotationDefectCardItemUpdate  $request
+     * @param  \App\Models\QuotationDefectCardItem  $quotationDefectCardItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(QuotationDefectCardItemUpdate $request, QuotationDefectCardItem $quotationDefectCardItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\QuotationDefectCardItem  $quotationDefectCardItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QuotationDefectCardItem $quotationDefectCardItem)
+    {
+        //
+    }
+}
---
+++ database/seeds/QuotationDefectCardItemsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuotationDefectCardItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
