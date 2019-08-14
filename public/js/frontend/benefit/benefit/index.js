0346a5024d704d72c53f31e7f3503d90aefdba9c
ibnuprtma <pibnu86@gmail.com>
2019-08-14 18:46:54 +0700

    refactory ui and add view bpjs

---
+++ public/js/frontend/benefit/benefit/index.js
@@ -0,0 +1,74 @@
+
+let Benefit = {
+    init: function () {
+        $('.benefit_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '/datatables/benefit',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !1,
+                serverSorting: !1
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [{
+                    field: 'code',
+                    title: 'Code',
+                    sortable: 'asc',
+                    filterable: !1,
+                    template: function (t) {
+                        return '<a href="/benefit/'+t.uuid+'">' + t.code + "</a>"
+                    }
+                },
+                {
+                    field: 'name',
+                    title: 'Benefits Name',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: 'description',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1
+                }
+            ]
+        });
+
+    }
+};
+
+jQuery(document).ready(function () {
+    Benefit.init();
+});
