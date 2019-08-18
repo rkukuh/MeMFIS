5ea89538970b4c5ae9e54c2a3ae04c55c57c8bed
ibnuprtma <pibnu86@gmail.com>
2019-08-18 11:27:54 +0700

    wip benefit

---
+++ resources/views/frontend/employee/employee/include/benefit/create.blade.php
@@ -0,0 +1,39 @@
+<div class="jumbotron p-3" style="background:#294294;">
+    <div class="row">
+        <div class="col">
+            <h1 class="display-5 text-white">Salary Addition</h1>
+        </div>
+    </div>
+</div>
+
+<div class="form-group m-form__group row">
+    <div class="col-sm-12 col-md-12 col-lg-12">
+        <fieldset class="border">
+            <legend class="w-auto">Allowance</legend>
+            <div class="form-group m-form__group row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <div style="border-bottom:3px solid black">
+                        <table width="100%" cellpadding="7" border="1">
+                            <tr>
+                                <td align="left" width="30%"><b>Benefits Name</b></td>
+                                <td align="center" width="25%"><b>Amount</b></td>
+                                <td align="center" width="30%"><b>Description</b></td>
+                                <td align="center" width="15%"><b>Action</b></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div>
+                        <table width="100%" cellpadding="7" border="1">
+                                <tr>
+                                    <td align="left" width="30%"></td>
+                                    <td align="center" width="25%"></td>
+                                    <td align="center" width="30%"></td>
+                                    <td align="center" width="15%"></td>
+                                </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+    </div>
+</div>
