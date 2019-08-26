710e54ada2dc1f84fb350e4fc63f488626dd5d7c
TomreinPH <husni.sosnet@gmail.com>
2019-08-26 11:13:18 +0700

    refactor

--- resources/views/frontend/project/htcrr/engineer/index.blade.php
+++ resources/views/frontend/project/htcrr/engineer/index.blade.php
@@ -1,4 +1,4 @@
-@if(isset($htcrr_engineers) && $htcrr_engineers[0] !== 0)
+@if(isset($htcrr_engineers))
 <div class="form-group m-form__group row px-4 pb-4">
     <div class="col-sm-12 col-md-12 col-lg-12">
         @if(in_array('Airframe',$skills))
