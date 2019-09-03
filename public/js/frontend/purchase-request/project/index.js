9798ec677dc1b18e11dcdbaf12f813c917e45bb6
audhy <audhy.vk01@gmail.com>
2019-09-03 16:30:05 +0700

    fix bug

--- public/js/frontend/purchase-request/project/index.js
+++ public/js/frontend/purchase-request/project/index.js
@@ -110,7 +110,7 @@ let PurchaseRequest = {
                             '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill approve" title="Approve" data-uuid="' + t.uuid +'">' +
                                 '<i class="la la-check"></i>' +
                             '</a>' +
-                            '<a href="/purchase-request-poject/' + t.uuid + '/edit" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-uuid="' + t.uuid +'">' +
+                            '<a href="/purchase-request-project/' + t.uuid + '/edit" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-uuid="' + t.uuid +'">' +
                                 '<i class="la la-pencil"></i>' +
                             '</a>' +
                             '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" data-uuid="' + t.uuid + '">' +
