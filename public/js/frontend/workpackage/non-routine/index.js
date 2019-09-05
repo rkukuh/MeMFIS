aea040c1d55d77a43233ffe1714438fc6788de34
Iqbalaulia <16410100012@stikom.edu>
2019-09-05 22:45:50 +0700

    add tools and material on wp non routine summary

--- public/js/frontend/workpackage/non-routine/index.js
+++ public/js/frontend/workpackage/non-routine/index.js
@@ -146,6 +146,32 @@ let NonRoutineWorkpackage = {
                     }
                 },
                 {
+                    field: "material",
+                    title: "Material",
+                    sortable: "asc",
+                    filterable: !1,
+                    template: function(t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_material_taskcard_wp" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill material" title="Material" data-uuid=' +
+                            t.uuid +
+                            '>\t\t\t\t\t\t\t<i class="la la-wrench"></i></button>\t\t\t\t\t\t'
+                        );
+                    }
+                },
+                {
+                    field: "tool",
+                    title: "Tool",
+                    sortable: "asc",
+                    filterable: !1,
+                    template: function(t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_tool_taskcard_wp" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill tool" title="Tool" data-uuid=' +
+                            t.uuid +
+                            '>\t\t\t\t\t\t\t<i class="la la-wrench"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t'
+                        );
+                    }
+                },
+                {
                     field: "sequence",
                     title: "Sequence",
                     sortable: "asc",
@@ -372,6 +398,32 @@ let NonRoutineWorkpackage = {
                         }
 
                         return "";
+                    }
+                },
+                {
+                    field: "material",
+                    title: "Material",
+                    sortable: "asc",
+                    filterable: !1,
+                    template: function(t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_material_taskcard_wp" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill material" title="Material" data-uuid=' +
+                            t.uuid +
+                            '>\t\t\t\t\t\t\t<i class="la la-wrench"></i></button>\t\t\t\t\t\t'
+                        );
+                    }
+                },
+                {
+                    field: "tool",
+                    title: "Tool",
+                    sortable: "asc",
+                    filterable: !1,
+                    template: function(t, e, i) {
+                        return (
+                            '<button data-toggle="modal" data-target="#modal_tool_taskcard_wp" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill tool" title="Tool" data-uuid=' +
+                            t.uuid +
+                            '>\t\t\t\t\t\t\t<i class="la la-wrench"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t'
+                        );
                     }
                 },
                 {
