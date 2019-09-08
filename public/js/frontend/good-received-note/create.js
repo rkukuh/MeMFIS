5bc6a52ac25bdedfcb4bfcf9e6c1f28cc174d899
audhy <audhy.vk01@gmail.com>
2019-09-08 21:27:44 +0700

    grn created

--- public/js/frontend/good-received-note/create.js
+++ public/js/frontend/good-received-note/create.js
@@ -1,129 +1,5 @@
 let goods_received_note = {
     init: function () {
-        $('.purchase_order_datatable').mDatatable({
-            data: {
-                type: 'remote',
-                source: {
-                    read: {
-                        method: 'GET',
-                        url: '/datatables/quotation',
-                        map: function (raw) {
-                            let dataSet = raw;
-
-                            if (typeof raw.data !== 'undefined') {
-                                dataSet = raw.data;
-                            }
-
-                            return dataSet;
-                        }
-                    }
-                },
-                pageSize: 10,
-                serverPaging: !0,
-                serverFiltering: !0,
-                serverSorting: !0
-            },
-            layout: {
-                theme: 'default',
-                class: '',
-                scroll: false,
-                footer: !1
-            },
-            sortable: !0,
-            filterable: !1,
-            pagination: !0,
-            search: {
-                input: $('#generalSearch')
-            },
-            toolbar: {
-                items: {
-                    pagination: {
-                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
-                    }
-                }
-            },
-            columns: [{
-                    field: 'id',
-                    title: '#',
-                    sortable: !1,
-                    width: 40
-                },
-                {
-                    field: 'quotation_number',
-                    title: 'P/N',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150
-                },
-                {
-                    field: 'customer',
-                    title: 'Item Description',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150
-                },
-                {
-                    field: 'valid_until',
-                    title: 'Qty PR',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150
-                },
-                {
-                    field: 'status',
-                    title: 'Qty PO',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'status',
-                    title: 'Qty',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'status',
-                    title: 'Unit',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'status',
-                    title: 'Remark',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'status',
-                    title: 'Expired Date',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
-                },
-                {
-                    field: 'Actions',
-                    width: 110,
-                    sortable: !1,
-                    overflow: 'visible',
-                    template: function (t, e, i) {
-                        return (
-                            '<button data-toggle="modal" data-target="#modal_grn" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-instruction_uuid=' +
-                            t.uuid +
-                            '>\t\t\t\t\t\t\t<i class="la la-pencil"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t' +
-                            '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" href="#" data-uuid=' +
-                            t.uuid +
-                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" data-uuid="' + t.uuid + '">' +
-                                '<i class="la la-trash"></i>' +
-                            '</a>'
-                        );
-                    }
-                }
-            ]
-        });
 
         let remove = $('.m_datatable').on('click', '.delete', function () {
             let triggerid = $(this).data('id');
@@ -210,7 +86,7 @@ let goods_received_note = {
                 },
                 success: function (response) {
                     if (response.errors) {
-                        console.log(errors);
+                        // console.log(errors);
                         // if (response.errors.title) {
                         //     $('#title-error').html(response.errors.title[0]);
                         // }
@@ -222,11 +98,11 @@ let goods_received_note = {
                         //    taskcard_reset();
 
 
-                        toastr.success('Taskcard has been created.', 'Success', {
+                        toastr.success('GRN has been created.', 'Success', {
                             timeOut: 5000
                         });
 
-                        // window.location.href = '/goods-received/'+response.uuid+'/edit';
+                        window.location.href = '/goods-received/'+response.uuid+'/edit';
                     }
                 }
             });
