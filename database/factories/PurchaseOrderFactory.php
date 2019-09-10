95b86572607430eadd3a0786a7df6206f8bd5e3c
R. Kukuh <rkukuh@gmail.com>
2019-09-11 06:08:51 +0700

    Update attributes

--- database/factories/PurchaseOrderFactory.php
+++ database/factories/PurchaseOrderFactory.php
@@ -101,12 +101,13 @@ $factory->afterCreating(PurchaseOrder::class, function ($purchase_order, $faker)
 
             $purchase_order->items()->save($item, [
                 'quantity' => rand(1, 10),
+                'quantity_unit' => rand(1, 10),
                 'unit_id' => $unit->id,
                 'price' => rand(10, 100) * 1000000,
-                'subtotal_before_discount' => rand(100, 200) * 1000000,
-                'discount_type' => 'amount',
-                'discount_value' => rand(10, 20) * 100000,
-                'subtotal_after_discount' => rand(100, 200) * 1000000,
+                'tax_percent' => $faker->randomElement([5, 10]),
+                'tax_amount' => rand(10, 15) * 1000,
+                'subtotal_before_discount' => rand(150, 200) * 1000000,
+                'subtotal_after_discount' => rand(100, 150) * 1000000,
                 'note' => $faker->randomElement([null, $faker->sentence]),
             ]);
         }
