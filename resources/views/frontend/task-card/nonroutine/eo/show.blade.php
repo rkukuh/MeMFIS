a4042a3f6d653e9299d5cf26f4960c9ff7807a3d
ibnuprtma <pibnu86@gmail.com>
2019-09-11 15:39:08 +0700

    fix tool and material instruction show

--- resources/views/frontend/task-card/nonroutine/eo/show.blade.php
+++ resources/views/frontend/task-card/nonroutine/eo/show.blade.php
@@ -427,8 +427,8 @@
                     <div class="m-portlet m-portlet--mobile">
                         <div class="m-portlet__body">
                             @include('frontend.task-card.nonroutine.eo.instruction.modal')
-                            @include('frontend.task-card.nonroutine.eo.tool.index')
-                            @include('frontend.task-card.nonroutine.eo.item.index')
+                            @include('frontend.task-card.nonroutine.eo.tool.show')
+                            @include('frontend.task-card.nonroutine.eo.item.show')
 
                             <div class="instruction_datatable" id="instruction_datatable"></div>
                         </div>
@@ -525,7 +525,7 @@
 
     <script src="{{ asset('js/frontend/functions/datepicker/date.js')}}"></script>
     <script src="{{ asset('js/frontend/taskcard/non-routine/eo/show.js') }}"></script>
-    <script src="{{ asset('js/frontend/taskcard/non-routine/eo/item/index.js') }}"></script>
-    <script src="{{ asset('js/frontend/taskcard/non-routine/eo/tool/index.js') }}"></script>
+    <script src="{{ asset('js/frontend/taskcard/non-routine/eo/item/show.js') }}"></script>
+    <script src="{{ asset('js/frontend/taskcard/non-routine/eo/tool/show.js') }}"></script>
     <script src="{{ asset('assets/metronic/vendors/custom/datatables/datatables.bundle.js') }}"></script>
 @endpush
