83f22623fd8faf2fd8b10aa9c7e3346b3b75ecc4
R. Kukuh <rkukuh@gmail.com>
2019-09-11 07:44:51 +0700

    Convert to english

--- database/seeds/PromosTableSeeder.php
+++ database/seeds/PromosTableSeeder.php
@@ -14,72 +14,72 @@ class PromosTableSeeder extends Seeder
     {
         Promo::create([
             'code' => '1-free-1',
-            'name' => 'Beli 1 Gratis 1',
+            'name' => 'Buy 1 Free 1',
         ]);
 
         Promo::create([
             'code' => '2-1',
-            'name' => 'Beli 2 Gratis 2',
+            'name' => 'Buy 2 Free 2',
         ]);
 
         Promo::create([
             'code' => 'amount-100k',
-            'name' => 'Diskon 100 Ribu',
+            'name' => 'Discount 100.000',
         ]);
 
         Promo::create([
             'code' => 'amount-250k',
-            'name' => 'Diskon 250 Ribu',
+            'name' => 'Discount 250.000',
         ]);
 
         Promo::create([
             'code' => 'amount-1m',
-            'name' => 'Diskon 1 Juta',
+            'name' => 'Discount 1.000.000',
         ]);
 
         Promo::create([
             'code' => 'amount-1.5m',
-            'name' => 'Diskon 1,5 Juta',
+            'name' => 'Discount 1.500.000',
         ]);
 
         Promo::create([
             'code' => '1-amount-500k',
-            'name' => 'Beli 1 Diskon 500 Ribu',
+            'name' => 'Buy 1 Discount 500.000',
         ]);
 
         Promo::create([
             'code' => '3-amount-1m',
-            'name' => 'Beli 3 Diskon 1 Juta',
+            'name' => 'Buy 3 Discount 1.000.000',
         ]);
 
         Promo::create([
             'code' => 'percent-10',
-            'name' => 'Diskon 10%',
+            'name' => 'Discount 10%',
         ]);
 
         Promo::create([
             'code' => 'percent-20',
-            'name' => 'Diskon 20%',
+            'name' => 'Discount 20%',
         ]);
 
         Promo::create([
             'code' => 'percent-25',
-            'name' => 'Diskon 25%',
+            'name' => 'Discount 25%',
         ]);
 
         Promo::create([
             'code' => '1-percent-20',
-            'name' => 'Beli 1 Diskon 20%',
+            'name' => 'Buy 1 Discount 20%',
         ]);
 
         Promo::create([
             'code' => '1-percent-20',
-            'name' => 'Beli 1 Diskon 20%',
+            'name' => 'Buy 1 Discount 20%',
         ]);
 
         Promo::create([
             'code' => '2-percent-15',
-            'name' => 'Beli 2 Diskon 15%',
+            'name' => 'Buy 2 Discount 15%',
         ]);
     }
 }
