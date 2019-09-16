32d16bc59005f98c90c4fedec217f30c78d466f3
audhy <audhy.vk01@gmail.com>
2019-09-16 19:57:02 +0700

    validasi uom taskcard si

--- public/js/frontend/taskcard/non-routine/si/edit.js
+++ public/js/frontend/taskcard/non-routine/si/edit.js
@@ -429,6 +429,11 @@ let TaskCard = {
                         if (data.errors.quantity) {
                             $('#quantity_item-error').html(data.errors.quantity[0]);
                         }
+
+                        if (data.errors.uom) {
+                            $('#unit_material-error').html(data.errors.uom[0]);
+                        }
+
                         document.getElementById('material').value = material;
                         document.getElementById('quantity').value = quantity;
 
@@ -491,6 +496,11 @@ let TaskCard = {
                         if (data.errors.quantity) {
                             $('#quantity-error').html(data.errors.quantity[0]);
                         }
+
+                        if (data.errors.uom) {
+                            $('#unit_tool-error').html(data.errors.uom[0]);
+                        }
+
                         document.getElementById('tool').value = tool;
                         document.getElementById('quantity').value = quantity;
                     } else {
