b6090cc32fc76071b300e2dd1a136a01d3db50df
R. Kukuh <rkukuh@gmail.com>
2019-09-18 09:40:13 +0700

    Use shorthand code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ $factory->define(User::class, function (Faker $faker)
         'name' => $first_name . ' ' . $last_name,
         'email' => strtolower($first_name . '.' . $last_name) . '@memfis.dev',
         'password' => Hash::make('rahasia'),
-        'is_active' => $faker->boolean(),
+        'is_active' => $faker->boolean,
         'remember_token' => str_random(10),
     ];
 
@@ -30,19 +30,19 @@ $factory->afterCreating(User::class, function ($user, $faker) {
         Role::where('name', 'dummy')->first()
     );
 
-    $user->employee()->save(factory(Employee::class)->make([
-        'first_name' => $user->name,
-        'last_name' => $user->name,
-        'dob' => Carbon::now()->subYear(rand(20, 50)),
-        'dob_place' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
-        'gender' => $faker->randomElement(['m', 'f']),
-        'religion' => $faker->randomElement(['islam','khonghucu','budha','kristen','hindu']),
-        'marital_status' => $faker->randomElement(['s','m']),
-        'nationality' => $faker->randomElement(['Indonesia','Japan','Zimbabwe','South Africa']),
-        'country' => 'indonesia',
-        'city' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
-        'joined_date' => Carbon::now()->toDateString(),
-        'updated_at' => null,
-    ]));
+    // $user->employee()->save(factory(Employee::class)->make([
+    //     'first_name' => $user->name,
+    //     'last_name' => $user->name,
+    //     'dob' => Carbon::now()->subYear(rand(20, 50)),
+    //     'dob_place' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
+    //     'gender' => $faker->randomElement(['m', 'f']),
+    //     'religion' => $faker->randomElement(['islam','khonghucu','budha','kristen','hindu']),
+    //     'marital_status' => $faker->randomElement(['s','m']),
+    //     'nationality' => $faker->randomElement(['Indonesia','Japan','Zimbabwe','South Africa']),
+    //     'country' => 'indonesia',
+    //     'city' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
+    //     'joined_date' => Carbon::now()->toDateString(),
+    //     'updated_at' => null,
+    // ]));
 
 });
