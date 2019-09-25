60eb182a330e59ee9fd6c6aaa2a9816e4f9566a6
audhy <audhy.vk01@gmail.com>
2019-09-25 15:50:26 +0700

    wip

--- routes/testing.php
+++ routes/testing.php
@@ -45,7 +45,8 @@ Route::name('testing.')->group(function () {
             foreach($defectcard->progresses->groupby('progressed_by')->sortBy('created_at') as $key => $values){
                 $date1 = null;
                 foreach($values as $value){
-                    if($statuses->where('id',$value->status_id)->first()->code <> "open" or $statuses->where('id',$value->status_id)->first()->code <> "released" or $statuses->where('id',$value->status_id)->first()->code <> "rii-released"){
+                    if($statuses->where('id',$value->status_id)->first()->code <> "removal-open" and $statuses->where('id',$value->status_id)->first()->code <> "installation-open" and $statuses->where('id',$value->status_id)->first()->code <> "released" and $statuses->where('id',$value->status_id)->first()->code <> "rii-released"){
+                        // dump($statuses->where('id',$value->status_id)->first()->code);
                         if($htcrr->helpers->where('userID',$key)->first() == null){
                             if($date1 <> null){
                                 $t1 = Carbon\Carbon::parse($date1);
@@ -63,7 +64,8 @@ Route::name('testing.')->group(function () {
             $manhours_break = 0;
             foreach($defectcard->progresses->groupby('progressed_by')->sortBy('created_at') as $key => $values){
                 for($i=0; $i<sizeOf($values->toArray()); $i++){
-                    if($statuses->where('id',$values[$i]->status_id)->first()->code == "pending"){
+                    if($statuses->where('id',$values[$i]->status_id)->first()->code == "removal-pending" or $statuses->where('id',$values[$i]->status_id)->first()->code == "installation-pending"){
+                        // dump('s');
                         if($defectcard->helpers->where('userID',$key)->first() == null){
                             if($date1 <> null){
                                 if($i+1 < sizeOf($values->toArray())){
@@ -79,11 +81,132 @@ Route::name('testing.')->group(function () {
             }
 
             $manhours_break = $manhours_break/3600;
+            // dd($manhours_break);
             $actual_manhours =number_format($manhours-$manhours_break, 2);
             //$jobcard->actual .= $actual_manhours;
 
 
             dd($actual_manhours);
+        });
+
+        Route::get('/manhour-htcrr-2', function () {
+            $htcrr = App\Models\HtCrr::find(15);
+
+            $defectcard = $htcrr;
+
+            $statuses = App\Models\Status::ofHtCrr()->get();
+            // $defectcard = App\Models\DefectCard::where('uuid',$jobcard->uuid)->first();
+            foreach($defectcard->helpers as $helper){
+                $helper->userID .= $helper->user->id;
+            }
+
+            //calculating defect card's actual manhours
+            $manhours_removal = 0;
+            $manhours_installation = 0;
+            foreach($defectcard->progresses->groupby('progressed_by')->sortBy('created_at') as $key => $values){
+                $date1_removal = null;
+                $date1_installation = null;
+                foreach($values as $value){
+                    if($statuses->where('id',$value->status_id)->first()->code <> "removal-open" and $statuses->where('id',$value->status_id)->first()->code <> "installation-open" and $statuses->where('id',$value->status_id)->first()->code <> "released" and $statuses->where('id',$value->status_id)->first()->code <> "rii-released"){
+                        if (strpos($statuses->where('id',$value->status_id)->first()->code , "removal") !== false) {
+                            if($htcrr->helpers->where('userID',$key)->first() == null){
+                                if($date1_removal <> null){
+                                    // dump($statuses->where('id',$value->status_id)->first()->code);
+                                    $t1 = Carbon\Carbon::parse($date1_removal);
+                                    $t2 = Carbon\Carbon::parse($value->created_at);
+                                    $diff = $t1->diffInSeconds($t2);
+                                    $manhours_removal = $manhours_removal + $diff;
+                                }
+                                $date1_removal = $value->created_at;
+                                // dump($date1_removal);
+                            }
+                        }
+                        else if(strpos($statuses->where('id',$value->status_id)->first()->code , "installation") !== false){
+                            if($htcrr->helpers->where('userID',$key)->first() == null){
+                                if($date1_installation <> null){
+                                    $t3 = Carbon\Carbon::parse($date1_installation);
+                                    $t4 = Carbon\Carbon::parse($value->created_at);
+                                    $diff2 = $t3->diffInSeconds($t4);
+                                    $manhours_installation = $manhours_installation + $diff2;
+                                }
+                                $date1_installation = $value->created_at;
+                            }
+                        }
+
+
+
+
+
+                    }
+
+                }
+            }
+
+            $manhours_removal = $manhours_removal/3600;
+            $manhours_installation = $manhours_installation/3600;
+            $manhours_break_removal = 0;
+            $manhours_break_installation = 0;
+            foreach($defectcard->progresses->groupby('progressed_by')->sortBy('created_at') as $key => $values){
+                for($i=0; $i<sizeOf($values->toArray()); $i++){
+                    // if($statuses->where('id',$values[$i]->status_id)->first()->code == "removal-pending" or $statuses->where('id',$values[$i]->status_id)->first()->code == "installation-pending"){
+                        // dump('s');
+                        // dump(strpos("removal-pending", "removal-pending"));
+                        // dump($statuses->where('id',$values[$i]->status_id)->first()->code );
+                        // if (strpos("removal-pending", "removal") !== false) {
+                        if ($statuses->where('id',$values[$i]->status_id)->first()->code == "removal-pending") {
+                            // dump('1');
+                            if($defectcard->helpers->where('userID',$key)->first() == null){
+                                if($date1_removal <> null){
+                                    if($i+1 < sizeOf($values->toArray())){
+                                        $t5 = Carbon\Carbon::parse($values[$i]->created_at);
+                                        $t6 = Carbon\Carbon::parse($values[$i+1]->created_at);
+                                        $diff3 = $t5->diffInSeconds($t6);
+                                        $manhours_break_removal = $manhours_break_removal + $diff3;
+                                        // dump($t5."----".$t6);
+                                    }
+                                }
+                            }
+
+                        }
+                        // else if(strpos("installation-pending" , "installation") !== false){
+                        else if($statuses->where('id',$values[$i]->status_id)->first()->code  == "installation-pending"){
+                            // dump('2');
+                            // dump($statuses->where('id',$values[$i]->status_id)->first()->code );
+
+                            if($defectcard->helpers->where('userID',$key)->first() == null){
+                                if($date1_installation <> null){
+                                    if($i+1 < sizeOf($values->toArray())){
+                                        // dump($statuses->where('id',$values[$i]->status_id)->first()->code );
+                                        $t7 = Carbon\Carbon::parse($values[$i]->created_at);
+                                        $t8 = Carbon\Carbon::parse($values[$i+1]->created_at);
+                                        $diff4 = $t7->diffInSeconds($t8);
+                                        $manhours_break_installation = $manhours_break_installation + $diff4;
+                                    }
+                                }
+                            }
+                        }
+
+
+
+
+                    // }
+                }
+            }
+            // dump($manhours_removal);
+            // dump($manhours_installation);
+            // dump($manhours_break_removal);
+            // dump($manhours_break_installation);
+            $manhours_break_removal = $manhours_break_removal/3600;
+            $manhours_break_installation = $manhours_break_installation/3600;
+            // dd($manhours_break);
+            $actual_manhours_removal =number_format($manhours_removal-$manhours_break_removal, 2);
+            $actual_manhours_installation =number_format($manhours_installation-$manhours_break_installation, 2);
+            //$jobcard->actual .= $actual_manhours;
+
+            dump($actual_manhours_removal);
+            dump($actual_manhours_installation);
+
+
         });
 
         Route::get('/wp', function () {
