d9e774dab99fadb39e6aa6068b2c7d934d40c04a
audhy <audhy.vk01@gmail.com>
2019-09-26 17:15:30 +0700

    refactor

--- database/seeds/CustomersTableSeeder.php
+++ database/seeds/CustomersTableSeeder.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Models\Fax;
-use App\Models\Type;
-use App\Models\Email;
-use App\Models\Level;
-use App\Models\Phone;
-use App\Models\Website;
-use App\Models\Customer;
-use App\Models\Address;
 use Illuminate\Database\Seeder;
 
 class CustomersTableSeeder extends Seeder
--- database/seeds/dummies/Customers.php
+++ database/seeds/dummies/Customers.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Models\Fax;
+use App\Models\Type;
+use App\Models\Email;
+use App\Models\Level;
+use App\Models\Phone;
+use App\Models\Website;
 use App\Models\Customer;
+use App\Models\Address;
 use Illuminate\Database\Seeder;
 
 class Customers extends Seeder
