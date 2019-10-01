766afcc79a4271cf13d56be0b882b6c97bb18f39
Iqbalaulia <16410100012@stikom.edu>
2019-10-01 15:18:37 +0700

    add required on discrepancy engineer create

--- resources/views/frontend/discrepancy/engineer/create.blade.php
+++ resources/views/frontend/discrepancy/engineer/create.blade.php
@@ -128,7 +128,7 @@
                                         </div>
                                         <div class="col-sm-6 col-md-6 col-lg-6">
                                             <label class="form-control-label">
-                                                Zone
+                                                Zone  @include('frontend.common.label.required')
                                             </label>
 
                                             @component('frontend.common.input.select2')
