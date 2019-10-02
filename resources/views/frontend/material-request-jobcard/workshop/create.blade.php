078a28986e71e87d03563f5ef9d15ef88adfe71e
ibnuprtma <pibnu86@gmail.com>
2019-10-02 22:06:40 +0700

    fix typo name

--- resources/views/frontend/material-request-jobcard/workshop/create.blade.php
+++ resources/views/frontend/material-request-jobcard/workshop/create.blade.php
@@ -5,7 +5,7 @@
         <div class="d-flex align-items-center">
             <div class="mr-auto">
                 <h3 class="m-subheader__title m-subheader__title--separator">
-                    Material Request Project
+                    Material Request Jobcard
                 </h3>
                 <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
                     <li class="m-nav__item m-nav__item--home">
@@ -19,7 +19,7 @@
                     <li class="m-nav__item">
                         <a href="{{ route('frontend.material-request-jobcard.index') }}" class="m-nav__link">
                             <span class="m-nav__link-text">
-                                Material Request Project
+                                Material Request Jobcard
                             </span>
                         </a>
                     </li>
@@ -41,7 +41,7 @@
                                 @include('frontend.common.label.create-new')
 
                                 <h3 class="m-portlet__head-text">
-                                    Material Request Project
+                                    Material Request Jobcard
                                 </h3>
                             </div>
                         </div>
