0bcd1b1c07d42737aed7e8ef8137a6a4ea6fc7c7
ibnuprtma <pibnu86@gmail.com>
2019-10-03 09:11:55 +0700

    add button create item

--- resources/views/frontend/material-request-jobcard/workshop/edit.blade.php
+++ resources/views/frontend/material-request-jobcard/workshop/edit.blade.php
@@ -200,6 +200,15 @@
                                                                         </div>
                                                                     </div>
                                                                 </div>
+                                                                <div class="col-xl-4 order-1 order-xl-2 m--align-right">
+                                                                    @component('frontend.common.buttons.create-new')
+                                                                        @slot('text', 'Item')
+                                                                        @slot('data_target', '#modal_material_request')
+                                                                    @endcomponent
+                    
+                            
+                                                                    <div class="m-separator m-separator--dashed d-xl-none"></div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                         @include('frontend.material-request-jobcard.modal')
