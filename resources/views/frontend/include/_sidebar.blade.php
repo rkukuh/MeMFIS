2363153f8c1da3edddab05826b25f9b4b0962760
ibnuprtma <pibnu86@gmail.com>
2019-10-04 10:38:28 +0700

    initial side material request

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -787,6 +787,14 @@
                 </div>
             </li>
             <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
+                <a href="{{ route('frontend.material-request.index') }}" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-list-3"></i>
+                    <span class="m-menu__link-text">
+                        Material Request
+                    </span>
+                </a>
+            </li>
+            <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
                 <a href="{{ route('frontend.gse-tool-returned.index') }}" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
