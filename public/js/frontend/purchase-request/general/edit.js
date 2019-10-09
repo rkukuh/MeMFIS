e8653f628da594018fd42f29fcfb0fc0986975f3
audhy <audhy.vk01@gmail.com>
2019-10-09 15:51:13 +0700

    refactor

--- public/js/frontend/purchase-request/general/edit.js
+++ public/js/frontend/purchase-request/general/edit.js
@@ -158,16 +158,22 @@ let PurchaseRequest = {
                     remark: remark,
                 },
                 success: function(response) {
-                    $('#modal_general').modal('hide');
+                    if (response.title == "Danger") {
+                        toastr.error("Item already exists!", "Error", {
+                            timeOut: 5000
+                        });
+                    } else {
+                        $('#modal_general').modal('hide');
 
-                    toastr.success("Item has been added.", "Success", {
-                        timeOut: 5000
-                    });
+                        toastr.success("Item has been added.", "Success", {
+                            timeOut: 5000
+                        });
 
-                    let table = $(".item_datatable").mDatatable();
+                        let table = $(".item_datatable").mDatatable();
 
-                    table.originalDataSet = [];
-                    table.reload();
+                        table.originalDataSet = [];
+                        table.reload();
+                    }
                 }
             });
         });
@@ -231,13 +237,16 @@ let PurchaseRequest = {
             document.getElementById('qty').value = $(this).data('quantity');
             document.getElementById('uuid').value = $(this).data('uuid');
             document.getElementById('remark').value = $(this).data('remark');
+
+            $('.btn-success').addClass('update-item');
+            $('.btn-success').removeClass('add-item');
         });
 
         $(".modal-footer").on("click", ".update-item", function() {
             let uuid = $("input[name=uuid]").val();
-            let quantity = $("#qty-material").val();
-            let unit_id = $("#unit_material").val();
-            let note = $("#remark-material").val();
+            let quantity = $("input[name=qty]").val();
+            let unit = $("#unit_id").val();
+            let remark = $("#remark").val();
 
             $.ajax({
                 headers: {
@@ -247,21 +256,18 @@ let PurchaseRequest = {
                 type: "PUT",
                 data: {
                     quantity: quantity,
-                    unit_id: unit_id,
-                    note: note,
+                    unit_id: unit,
+                    note: remark,
                 },
-                success: function(response) {
-                    if (response.errors) {
-                        console.log(errors);
+                success: function(data) {
+                    if (data.errors) {
                         // if (response.errors.title) {
                         //     $('#title-error').html(response.errors.title[0]);
                         // }
 
                         // document.getElementById('manual_affected_id').value = manual_affected_id;
                     } else {
-                        //    taskcard_reset();
-                        $('#modal_general_update').modal('hide');
-
+                        $('#modal_general').modal('hide');
                         toastr.success(
                             "Item has been updated.",
                             "Success",
@@ -274,6 +280,9 @@ let PurchaseRequest = {
 
                         table.originalDataSet = [];
                         table.reload();
+                        $('.btn-success').removeClass('update-item');
+                        $('.btn-success').addClass('add-item');
+
 
                     }
                 }
