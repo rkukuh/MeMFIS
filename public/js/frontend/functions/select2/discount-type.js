8535d106c0f11d0efa00af29f0bd19c61342175a
ThamrinPH <husni.sosnet@gmail.com>
2019-10-10 09:33:09 +0700

    discount-type component refactor

--- public/js/frontend/functions/select2/discount-type.js
+++ public/js/frontend/functions/select2/discount-type.js
@@ -1,10 +1,5 @@
 let DiscountTypeSelect2 = {
-    init: function () {
-        $('select[name^="discount-type"]').append(
-            '<option value="amount">Amount</option>',
-            '<option value="percentage">Percentage</option>'
-        );
-        
+    init: function () {        
         $('select[name^="discount-type"], #discount-type_validate').select2({
             placeholder: 'Select a Discount Type'
         });
