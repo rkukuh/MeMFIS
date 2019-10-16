ccb609271edf6a44824d9c30c18e51bf19e003b7
ThamrinPH <husni.sosnet@gmail.com>
2019-10-16 12:37:31 +0700

    Revert "numbering jobcard"
    This reverts commit 7519cfdb7b2dc545ef427e0947b086544119a6a9.

--- app/Helpers/DocumentNumber.php
+++ app/Helpers/DocumentNumber.php
@@ -9,19 +9,7 @@ class DocumentNumber
     static function setNumber($count) {
         $date = Carbon::now()->format('Y/m');
 
-        if($count < 10){
-            $number = '00000' . $count; // 000005
-        }elseif($count > 10 && $count < 100){
-            $number = '0000' . $count;  // 000057
-        }elseif($count > 100 && $count < 1000){
-            $number = '000' . $count;   // 000512
-        }elseif($count > 1000 && $count < 10000){
-            $number = '00' . $count;    // 004574
-        }elseif($count > 10000 && $count < 100000){
-            $number = '0' . $count;     // 054789
-        }else{
-            $number = $count;
-        }
+        $number = '00000' . $count;
         
         $doc_number = $date . '/' . substr($number, strlen($number - 1));
 
