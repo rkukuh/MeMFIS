35e11feda4001237cb3c3990319ab1a09d6dfbe1
ibnuprtma <pibnu86@gmail.com>
2019-10-25 11:02:37 +0700

    init sidebar

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -765,6 +765,14 @@
                 </a>
             </li>
             <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
+                <a href="{{ route('frontend.stock-monitoring.index') }}" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-list-3"></i>
+                    <span class="m-menu__link-text">
+                        Stock Monitoring
+                    </span>
+                </a>
+            </li>
+            <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover" >
                 <a href="{{ route('frontend.interchange.index') }}" class="m-menu__link m-menu__toggle">
                     <i class="m-menu__link-icon flaticon-list-3"></i>
                     <span class="m-menu__link-text">
