9113e1418fd258bfb0dc0335699172d8ddde9517
audhy <audhy.vk01@gmail.com>
2019-10-25 21:33:16 +0700

    crud rir wip

--- public/js/frontend/receiving-inspection-report/create.js
+++ public/js/frontend/receiving-inspection-report/create.js
@@ -1,53 +1,212 @@
-let goods_received_note = {
-    init: function () {
+let receiving_inspection_report = {
+    init: function() {
+        function item(uuid) {
+            $(".rir_datatable").mDatatable({
+                data: {
+                    type: "remote",
+                    source: {
+                        read: {
+                            method: "GET",
+                            url: "/datatables/purchase-order/item/" + uuid,
+                            map: function(raw) {
+                                let dataSet = raw;
 
-        $('.footer').on('click', '.add-goods-received', function () {
-            let received_at = $('input[name=date]').val();
-            let received_by = $('#employee').val();
-            let ref_po = $('input[name=ref-po]').val();
-            let do_no = $('input[name=deliv-number]').val();
-            let do_date = $('input[name=do-date]').val();
-            let warehouse = $('input[name=warehouse]').val();
-            let description = $('#description').val();
-            let vehicle_no = $('input[name=vehicle-no]').val();
-            let container_no = $('input[name=container-no]').val();
+                                if (typeof raw.data !== "undefined") {
+                                    dataSet = raw.data;
+                                }
+
+                                return dataSet;
+                            }
+                        }
+                    },
+                    pageSize: 10,
+                    serverPaging: !0,
+                    serverFiltering: !1,
+                    serverSorting: !0
+                },
+                layout: {
+                    theme: "default",
+                    class: "",
+                    scroll: false,
+                    footer: !1
+                },
+                sortable: !0,
+                filterable: !1,
+                pagination: !0,
+                search: {
+                    input: $("#generalSearch")
+                },
+                toolbar: {
+                    items: {
+                        pagination: {
+                            pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                        }
+                    }
+                },
+                columns: [
+                    {
+                        field: "code",
+                        title: "P/N",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "name",
+                        title: "Item Description",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "",
+                        title: "Qty PR",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "pivot.quantity",
+                        title: "Qty PO",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "",
+                        title: "Qty",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "",
+                        title: "Unit",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "",
+                        title: "Remark",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    },
+                    {
+                        field: "",
+                        title: "Expired Date",
+                        sortable: "asc",
+                        filterable: !1,
+                        width: 150
+                    }
+                ]
+            });
+        }
+
+        let item_datatables_init = true;
+
+        $('select[name="purchase_order"]').on("change", function() {
+            let uuid = $("#purchase_order").val();
+            if (item_datatables_init == true) {
+                item_datatables_init = false;
+                item(uuid);
+                table = $(".rir_datatable").mDatatable();
+                table.originalDataSet = [];
+                table.reload();
+            } else {
+                let table = $(".rir_datatable").mDatatable();
+                table.destroy();
+                item(uuid);
+                table = $(".rir_datatable").mDatatable();
+                table.originalDataSet = [];
+                table.reload();
+            }
+        });
+
+        $('.action-buttons').on('click', '.add-rir', function () {
+            $('#name-error').html('');
+            $('#simpan').text('Simpan');
+
+            let general_document = [];
+            $.each($("input[name='general_document[]']:checked"), function() {
+                general_document.push($(this).val());
+            });
+
+            let technical_document = [];
+            $.each($("input[name='technical_document[]']:checked"), function() {
+                technical_document.push($(this).val());
+            });
+
+            let purchase_order = $('#purchase_order').val();
+            let vendor = $('#vendor').val();
+            let document = $('input[name=document]').val();
+            let date = $('#date').val();
+            let status = $('input[name="status"]:checked').val();
+            let type = $('input[name="type"]:checked').val();
+            let condition = $('input[name="condition"]:checked').val();
+            let preservation_check = $('input[name="preservation_check"]:checked').val();
+            let condition_material = $('input[name="condition_material"]:checked').val();
+            let quality = $('input[name="quality"]:checked').val();
+            let identification = $('input[name="identification"]:checked').val();
+            let packing_handling_check = $('#packing_handling_check').val();
+            let preservation_check_explain = $('#preservation_check_explain').val();
+            let document_check = $('#document_check').val();
+            let material_check = $('#material_check').val();
+            let decision = $('#decision').val();
+
 
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
+                type: 'post',
                 url: '/receiving-inspection-report',
-                type: 'POST',
                 data: {
-                    received_at:received_at,
-                    received_by:received_by,
-                    vehicle_no:vehicle_no,
-                    container_no:container_no,
-                    purchase_order_id:ref_po,
-                    do_no:do_no,
-                    do_date:do_date,
-                    storage_id:warehouse,
-                    description:description,
+                    _token: $('input[name=_token]').val(),
+                    general_document:general_document,
+                    technical_document: technical_document,
+                    purchase_order: purchase_order,
+                    date: date,
+                    vendor: vendor,
+                    document: document,
+                    status:status,
+                    type:type,
+                    condition:condition,
+                    preservation_check:preservation_check,
+                    condition_material:condition_material,
+                    quality:quality,
+                    identification:identification,
+                    packing_handling_check:packing_handling_check,
+                    preservation_check_explain:preservation_check_explain,
+                    document_check:document_check,
+                    material_check:material_check,
+                    decision:decision,
                 },
-                success: function (response) {
-                    if (response.errors) {
-                        // console.log(errors);
-                        // if (response.errors.title) {
-                        //     $('#title-error').html(response.errors.title[0]);
+                success: function (data) {
+                    if (data.errors) {
+                        // if (data.errors.aircraft_id) {
+                        //     $('#applicability-airplane-error').html(data.errors.aircraft_id[0]);
+                        // }
+                        // if (data.errors.title) {
+                        //     $('#title-error').html(data.errors.title[0]);
                         // }
 
-                        // document.getElementById('manual_affected_id').value = manual_affected_id;
-
+                        // document.getElementById('applicability-airplane').value = applicability-airplane;
+                        // document.getElementById('title').value = title;
 
                     } else {
-                        //    taskcard_reset();
+                        // $('#modal_customer').modal('hide');
 
-
-                        toastr.success('GRN has been created.', 'Success', {
+                        toastr.success('RIR has been created.', 'Success', {
                             timeOut: 5000
                         });
 
-                        window.location.href = '/goods-received/'+response.uuid+'/edit';
+                        // window.location.href = '/receiving-inspection-report/'+data.uuid+'/edit';
+                        // let table = $('.m_datatable').mDatatable();
+
+                        // table.originalDataSet = [];
+                        // table.reload();
                     }
                 }
             });
@@ -55,6 +214,6 @@ let goods_received_note = {
     }
 };
 
-jQuery(document).ready(function () {
-    goods_received_note.init();
+jQuery(document).ready(function() {
+    receiving_inspection_report.init();
 });
