63ecf05ab78756ffe987be4440e3c636f78348b0
audhy <audhy.vk01@gmail.com>
2019-10-31 14:06:21 +0700

    refactor

--- resources/views/frontend/gse/hm/create.blade.php
+++ resources/views/frontend/gse/hm/create.blade.php
@@ -86,7 +86,7 @@
 
                                             @component('frontend.common.label.data-info')
                                                 @slot('id', 'project_number')
-                                                @slot('text', 'generate')
+                                                @slot('text', '-')
                                             @endcomponent
                                         </div>
                                         <div class="col-sm-6 col-md-6 col-lg-6">
@@ -98,7 +98,7 @@
 
                                                     @component('frontend.common.label.data-info')
                                                         @slot('id', 'ac_type')
-                                                        @slot('text', 'Generate')
+                                                        @slot('text', '-')
                                                     @endcomponent
                                                 </div>
                                                 <div class="col-sm-6 col-md-6 col-lg-6">
@@ -108,7 +108,7 @@
 
                                                     @component('frontend.common.label.data-info')
                                                         @slot('id', 'ac_reg')
-                                                        @slot('text', 'Generate')
+                                                        @slot('text', '-')
                                                     @endcomponent
                                                 </div>
                                             </div>
@@ -171,50 +171,6 @@
                                         </div>
                                     </div>
                                     <hr>
-                                    <div class="form-group m-form__group row">
-                                        <div class="col-sm-12 col-md-12 col-lg-12">
-                                            <div class="m-portlet">
-                                                <div class="m-portlet__head">
-                                                    <div class="m-portlet__head-caption">
-                                                        <div class="m-portlet__head-title">
-                                                            <span class="m-portlet__head-icon m--hide">
-                                                                <i class="la la-gear"></i>
-                                                            </span>
-
-                                                            @include('frontend.common.label.datalist')
-
-                                                            <h3 class="m-portlet__head-text">
-                                                                Tool
-                                                            </h3>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="m-portlet m-portlet--mobile">
-                                                    <div class="m-portlet__body">
-                                                        <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                                                            <div class="row align-items-center">
-                                                                <div class="col-xl-8 order-2 order-xl-1">
-                                                                    <div class="form-group m-form__group row align-items-center">
-                                                                        <div class="col-md-4">
-                                                                            <div class="m-input-icon m-input-icon--left">
-                                                                                <input type="text" class="form-control m-input" placeholder="Search..."
-                                                                                    id="generalSearch">
-                                                                                <span class="m-input-icon__icon m-input-icon__icon--left">
-                                                                                    <span><i class="la la-search"></i></span>
-                                                                                </span>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        @include('frontend.gse.modal')
-                                                        <div class="gse_tool_returned_datatable" id="gse_tool_returned_datatable"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                     <div class="form-group m-form__group row">
                                         <div class="col-sm-12 col-md-12 col-lg-12 footer">
                                             <div class="flex">
