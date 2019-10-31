6dacf486f805b7d71cf15f1f2c8ed233b5cd9f68
audhy <audhy.vk01@gmail.com>
2019-11-01 06:58:23 +0700

    refactor

--- resources/views/frontend/gse/hm/create.blade.php
+++ resources/views/frontend/gse/hm/create.blade.php
@@ -172,6 +172,50 @@
                                     </div>
                                     <hr>
                                     <div class="form-group m-form__group row">
+                                        <div class="col-sm-12 col-md-12 col-lg-12">
+                                            <div class="m-portlet">
+                                                <div class="m-portlet__head">
+                                                    <div class="m-portlet__head-caption">
+                                                        <div class="m-portlet__head-title">
+                                                            <span class="m-portlet__head-icon m--hide">
+                                                                <i class="la la-gear"></i>
+                                                            </span>
+
+                                                            @include('frontend.common.label.datalist')
+
+                                                            <h3 class="m-portlet__head-text">
+                                                                Tool
+                                                            </h3>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="m-portlet m-portlet--mobile">
+                                                    <div class="m-portlet__body">
+                                                        <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                                            <div class="row align-items-center">
+                                                                <div class="col-xl-8 order-2 order-xl-1">
+                                                                    <div class="form-group m-form__group row align-items-center">
+                                                                        <div class="col-md-4">
+                                                                            <div class="m-input-icon m-input-icon--left">
+                                                                                <input type="text" class="form-control m-input" placeholder="Search..."
+                                                                                    id="generalSearch">
+                                                                                <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                                                    <span><i class="la la-search"></i></span>
+                                                                                </span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        @include('frontend.gse.modal')
+                                                        <div class="gse_tool_returned_datatable" id="gse_tool_returned_datatable"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
                                         <div class="col-sm-12 col-md-12 col-lg-12 footer">
                                             <div class="flex">
                                                 <div class="action-buttons">
