a2f2f0d9e844de24528798ea1bc160376b14a67a
audhy <audhy.vk01@gmail.com>
2019-11-05 14:41:12 +0700

    wip

--- app/Http/Controllers/Frontend/PurchaseRequest/ProjectPurchaseRequestController.php
+++ app/Http/Controllers/Frontend/PurchaseRequest/ProjectPurchaseRequestController.php
@@ -10,6 +10,7 @@ use App\Models\Project;
 use App\Models\Approval;
 use App\Helpers\DocumentNumber;
 use App\Models\PurchaseRequest;
+use App\Models\QuotationHtcrrItem;
 use App\Http\Controllers\Controller;
 use App\Models\QuotationWorkPackageTaskCardItem;
 use App\Http\Requests\Frontend\PurchaseRequestStore;
@@ -46,21 +47,53 @@ class ProjectPurchaseRequestController extends Controller
     public function store(PurchaseRequestStore $request)
     {
         $request->merge(['number' => DocumentNumber::generate('PR-', PurchaseRequest::withTrashed()->count()+1)]);
-        $request->merge(['project_id' =>Project::where('uuid',$request->project_id)->first()->id ]);
+        $request->merge(['purchase_requestable_type' => 'App\Models\Project']);
+        $request->merge(['purchase_requestable_id' =>Project::where('uuid',$request->project_id)->first()->id ]);
         $request->merge(['type_id' => Type::where('of','purchase-request')->where('name','Project')->first()->id ]);
         $request->merge(['requested_at' => Carbon::parse($request->requested_at)]);
         $request->merge(['required_at' => Carbon::parse($request->required_at)]);
         $purchaseRequest = PurchaseRequest::create($request->all());
 
-        $items = QuotationWorkPackageTaskCardItem::with('item','item.unit')->where('quotation_id',Project::find($request->project_id)->quotations->first()->id)->get();
+        $items = QuotationWorkPackageTaskCardItem::with('item','item.unit')->where('quotation_id',Project::where('uuid',$request->project_id)->first()->quotations->first()->id)->get();
 
         foreach($items as $item){
             $i = Item::find($item->item_id)->categories->first()->code;
             if($i == "raw" or $i == "cons" or $i == "comp"){
+                if($item->item->unit_id <> $item->unit_id){
+                    $quantity = $item->quantity;
+                    $qty_uom = $item->item->units->where('uom.unit_id',$item->unit_id)->first()->uom->quantity;
+                    $quantity_unit = $qty_uom*$quantity;
+                }
+                else{
+                    $quantity_unit = $item->quantity;
+                }
+
                 $purchaseRequest->items()->attach([$item->item_id => [
                     'quantity'=> $item->quantity,
                     'unit_id' => $item->unit_id,
-                    'quantity_unit' => $item->quantity]
+                    'quantity_unit' => $quantity_unit]
+                ]);
+            }
+        }
+
+        $items_htcrr = QuotationHtcrrItem::with('item','item.unit')->where('quotation_id',Project::where('uuid',$request->project_id)->first()->quotations->first()->id)->get();
+
+        foreach($items_htcrr as $item){
+            $i = Item::find($item->item_id)->categories->first()->code;
+            if($i == "raw" or $i == "cons" or $i == "comp"){
+                if($item->item->unit_id <> $item->unit_id){
+                    $quantity = $item->quantity;
+                    $qty_uom = $item->item->units->where('uom.unit_id',$item->unit_id)->first()->uom->quantity;
+                    $quantity_unit = $qty_uom*$quantity;
+                }
+                else{
+                    $quantity_unit = $item->quantity;
+                }
+
+                $purchaseRequest->items()->attach([$item->item_id => [
+                    'quantity'=> $item->quantity,
+                    'unit_id' => $item->unit_id,
+                    'quantity_unit' => $quantity_unit]
                 ]);
             }
         }
