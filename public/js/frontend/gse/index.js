98f3b90cbe5a4c03443b265e059abf2db31d57ff
ThamrinPH <husni.sosnet@gmail.com>
2019-11-06 14:18:32 +0700

    errors handler

--- public/js/frontend/gse/index.js
+++ public/js/frontend/gse/index.js
@@ -217,13 +217,14 @@ let GseToolReturned = {
                             table.originalDataSet = [];
                             table.reload();
                         },
-                        error: function (jqXhr, json, errorThrown) {
+                        error: function(jqXhr, json, errorThrown) {
                             let errors = jqXhr.responseJSON;
-                            toastr.error(errors.message, errors.title, {
-                                "closeButton": true,
-                                "timeOut": "0",
-                            }
-                            );
+                            $.each(errors, function(index, value) {
+                                toastr.error(value.message, value.title, {
+                                    closeButton: true,
+                                    timeOut: "0"
+                                });
+                            });
                         }
                     });
                 }
--- public/js/frontend/rts/progress.js
+++ public/js/frontend/rts/progress.js
@@ -1,23 +1,26 @@
 let TaskCard = {
-    init: function () {
+    init: function() {
         function strtrunc(str, max, add) {
-            add = add || '...';
-            return (typeof str === 'string' && str.length > max ? str.substring(0, max) + add : str);
-        };
+            add = add || "...";
+            return typeof str === "string" && str.length > max
+                ? str.substring(0, max) + add
+                : str;
+        }
 
-        $('.rts_datatable').mDatatable({
+        $(".rts_datatable").mDatatable({
             data: {
-                type: 'remote',
+                type: "remote",
                 source: {
                     read: {
-                        method: 'GET',
-                        url: '/datatables/release-to-service/progress',
-                        map: function (raw) {
+                        method: "GET",
+                        url: "/datatables/release-to-service/progress",
+                        map: function(raw) {
                             let dataSet = raw;
 
-                            if (typeof raw.data !== 'undefined') {
+                            if (typeof raw.data !== "undefined") {
                                 dataSet = raw.data;
                             }
+                            console.log(dataSet);
                             return dataSet;
                         }
                     }
@@ -28,8 +31,8 @@ let TaskCard = {
                 serverSorting: !1
             },
             layout: {
-                theme: 'default',
-                class: '',
+                theme: "default",
+                class: "",
                 scroll: false,
                 footer: !1
             },
@@ -37,7 +40,7 @@ let TaskCard = {
             filterable: !1,
             pagination: !0,
             search: {
-                input: $('#generalSearch')
+                input: $("#generalSearch")
             },
             toolbar: {
                 items: {
@@ -47,111 +50,121 @@ let TaskCard = {
                 }
             },
             columns: [
-
                 {
-                    field: 'created_at',
-                    title: 'Date',
-                    sortable: 'asc',
-                    filterable: !1,
-
+                    field: "created_at",
+                    title: "Date",
+                    sortable: "asc",
+                    filterable: !1
                 },
                 {
-                    field: 'code',
-                    title: 'Project No.',
-                    sortable: 'asc',
-                    filterable: !1,
+                    field: "code",
+                    title: "Project No.",
+                    sortable: "asc",
+                    filterable: !1
                 },
                 {
-                    field: 'title',
-                    title: 'Project Title',
-                    sortable: 'asc',
-                    filterable: !1,
+                    field: "title",
+                    title: "Project Title",
+                    sortable: "asc",
+                    filterable: !1
                 },
                 {
-                    field: 'customer_name',
-                    title: 'Customer',
-                    sortable: 'asc',
-                    filterable: !1,
-
+                    field: "customer_name",
+                    title: "Customer",
+                    sortable: "asc",
+                    filterable: !1
                 },
                 {
-                    field: 'status',
-                    title: 'Status',
-                    sortable: 'asc',
-                    filterable: !1,
+                    field: "status",
+                    title: "Status",
+                    sortable: "asc",
+                    filterable: !1
                 },
                 {
-                    field: 'Actions',
+                    field: "Actions",
                     sortable: !1,
-                    overflow: 'visible',
-                    template: function (t, e, i) {
-                        if(t.status == 'RTS'){
+                    overflow: "visible",
+                    template: function(t, e, i) {
+                        if (t.status == "RTS") {
                             return (
-                            '<a href="rts/'+t.rts.uuid+'/print" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill print" title="Print" data-id="' + t.uuid +'">' +
+                                '<a href="rts/' +
+                                t.uuid +
+                                '/print" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill print" title="Print" data-id="' +
+                                t.uuid +
+                                '">' +
                                 '<i class="la la-print"></i>' +
-                            '</a>'
+                                "</a>"
                             );
-                        }else{
+                        } else {
                             return (
-                                '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill approve" title="Approve" data-id="' + t.uuid + '">' +
-                                    '<i class="la la-check"></i>' +
-                                '</a>'+
-                                '<a href="/rts/'+t.uuid+'/project" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-id="' + t.uuid +'">' +
-                                    '<i class="la la-pencil"></i>' +
-                                '</a>'
+                                '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill approve" title="Approve" data-id="' +
+                                t.uuid +
+                                '">' +
+                                '<i class="la la-check"></i>' +
+                                "</a>" +
+                                '<a href="/rts/' +
+                                t.uuid +
+                                '/project" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-id="' +
+                                t.uuid +
+                                '">' +
+                                '<i class="la la-pencil"></i>' +
+                                "</a>"
                             );
                         }
                     }
                 }
             ]
         });
-        $('.rts_datatable').on('click', '.approve', function () {
-            let project_uuid = $(this).data('id');
+        $(".rts_datatable").on("click", ".approve", function() {
+            let project_uuid = $(this).data("id");
             swal({
-                title: 'Are you sure do you want to approve this release to service?',
-                type: 'warning',
-                confirmButtonText: 'Yes, Approve',
-                confirmButtonColor: '#34bfa3',
-                cancelButtonText: 'Cancel',
-                showCancelButton: true,
-            })
-            .then(result => {
+                title:
+                    "Are you sure do you want to approve this release to service?",
+                type: "warning",
+                confirmButtonText: "Yes, Approve",
+                confirmButtonColor: "#34bfa3",
+                cancelButtonText: "Cancel",
+                showCancelButton: true
+            }).then(result => {
                 if (result.value) {
                     $.ajax({
                         headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
-                                'content'
+                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                "content"
                             )
                         },
-                        type: 'POST',
-                        url: '/rts/' + project_uuid + '/project/approve',
-                        success: function (data) {
-                            toastr.success('Release to service has been approved.', 'Approved', {
+                        type: "POST",
+                        url: "/rts/" + project_uuid + "/project/approve",
+                        success: function(data) {
+                            toastr.success(
+                                "Release to service has been approved.",
+                                "Approved",
+                                {
                                     timeOut: 5000
                                 }
                             );
 
-                            let table = $('.rts_datatable').mDatatable();
+                            let table = $(".rts_datatable").mDatatable();
 
                             table.originalDataSet = [];
                             table.reload();
                         },
-                        error: function (jqXhr, json, errorThrown) {
+                        error: function(jqXhr, json, errorThrown) {
                             let errors = jqXhr.responseJSON;
-                            toastr.error(errors.message, errors.title, {
-                                "closeButton": true,
-                                "timeOut": "0",
-                            }
-                            );
+                            $.each(errors, function(index, value) {
+                                toastr.error(value.message, value.title, {
+                                    closeButton: true,
+                                    timeOut: "0"
+                                });
+                            });
                         }
                     });
                 }
             });
         });
-
     }
 };
 
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
     TaskCard.init();
 });
