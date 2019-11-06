cb297bc9e9a364c41fe1bffc7ff8f137d3c5dda4
andydptyo <andy.dptyo@gmail.com>
2019-11-06 21:42:12 +0700

    mutation : wip

--- public/js/frontend/material-transfer/edit.js
+++ public/js/frontend/material-transfer/edit.js
@@ -1,6 +1,5 @@
 let MutationEdit = {
     init: function () {
-
         $('.material_transfer_datatable').mDatatable({
             data: {
                 type: 'remote',
@@ -21,7 +20,6 @@ let MutationEdit = {
                 },
                 pageSize: 10,
                 serverPaging: !0,
-                serverFiltering: !1,
                 serverSorting: !0
             },
             layout: {
@@ -60,49 +58,43 @@ let MutationEdit = {
                     title: 'Part Number',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150
+                    template: function (t) {
+                        return '<a href="/item/' + t.uuid + '">' + t.code + "</a>"
+                    }
                 },
                 {
-                    field: 'pivot.serial_number',
+                    field: '',
                     title: 'Serial Number',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150
                 },
                 {
-                    field: 'description',
+                    field: '',
                     title: 'Item Description',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150
                 },
                 {
                     field: '',
                     title: 'Expired Date',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150
                 },
                 {
-                    field: 'pivot.quantity',
+                    field: '',
                     title: 'Qty',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150
                 },
                 {
-                    field: 'unit_name',
+                    field: '',
                     title: 'Unit',
                     sortable: 'asc',
                     filterable: !1,
-                    width: 150,
                 },
                 {
-                    field: 'note',
-                    title: 'Remark',
-                    sortable: 'asc',
-                    filterable: !1,
-                    width: 150,
+                    field: "description",
+                    title: "Remark",
                 },
                 {
                     field: 'Actions',
@@ -111,19 +103,18 @@ let MutationEdit = {
                     overflow: 'visible',
                     template: function (t, e, i) {
                         return (
-                            '<button data-toggle="modal" data-target="#modal_mutation" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit-item" title="Edit" data-item=' +
-                            t.uuid + ' data-date=' + t.pivot.expired_at + ' data-quantity=' + t.pivot.quantity + ' data-unit=' + t.unit_id + ' data-serial=' + t.pivot.serial_number + ' data-remark=' + t.pivot.description + '>\t\t\t\t\t\t\t<i class="la la-pencil"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t' +
+                            '<button data-toggle="modal" data-target="#modal_item" type="button" href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-instruction_uuid=' +
+                            t.uuid +
+                            '>\t\t\t\t\t\t\t<i class="la la-pencil"></i>\t\t\t\t\t\t</button>\t\t\t\t\t\t' +
                             '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" href="#" data-uuid=' +
                             t.uuid +
-                            '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" data-uuid="' + t.uuid + '">' +
-                            '<i class="la la-exchange"></i>' +
-                            '</a>' +
                             '<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill delete" title="Delete" data-uuid="' + t.uuid + '">' +
                             '<i class="la la-trash"></i>' +
                             '</a>'
                         );
                     }
                 }
+
             ]
         });
 
@@ -200,7 +191,7 @@ let MutationEdit = {
                     });
                 }
             });
-            $("#material").attr('disabled', true);
+            $("#item").attr('disabled', true);
 
             $.ajax({
                 url: '/get-item-unit-uuid/' + $(this).data('item'),
@@ -227,22 +218,16 @@ let MutationEdit = {
 
             document.getElementById('qty_request').value = $(this).data('quantity');
             document.getElementById('uuid').value = $(this).data('uuid');
-            let remark = $(this).data('remark');
-
-            if (remark === null) {
-                document.getElementById('item_remark').value = '';
-            } else {
-                document.getElementById('item_remark').value = $(this).data('remark');
-            }
-
+            document.getElementById('item_remark').value = $(this).data('remark');
             document.getElementById('serial_no').value = $(this).data('serial');
+            document.getElementById('exp_date').value = $(this).data('date');
 
             $('.btn-success').addClass('update-item');
             $('.btn-success').removeClass('add-item');
         });
 
         $(".modal-footer").on("click", ".update-item", function () {
-            let material = $("#material").val();
+            let item = $("#item").val();
             let quantity = $("input[name=qty_request]").val();
             let exp_date = $("#exp_date").val();
             let unit = $("#unit_id").val();
@@ -253,7 +238,7 @@ let MutationEdit = {
                 headers: {
                     "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
                 },
-                url: "/material-transfer/" + mutation_uuid + "/item/" + material,
+                url: "/material-transfer/" + mutation_uuid + "/item/" + item,
                 type: "PUT",
                 data: {
                     item_id: material,
@@ -276,11 +261,11 @@ let MutationEdit = {
                             .end();
                     });
 
-                    toastr.success("Item has been updated.", "Success", {
+                    toastr.success("Item has been added.", "Success", {
                         timeOut: 5000
                     });
 
-                    let table = $('.material_transfer_datatable').mDatatable();
+                    let table = $(".material_transfer_datatable").mDatatable();
 
                     table.originalDataSet = [];
                     table.reload();
@@ -335,26 +320,24 @@ let MutationEdit = {
         });
 
         $('.footer').on('click', '.update-mutation', function () {
-            let note = $('#description').val();
+            let ref_no = $('input[name=ref-no]').val();
+            let description = $('#remark').val();
             let section_code = $('input[name=section_code]').val();
             let storage_id = $('#item_storage_id').val();
             let date = $('input[name=date]').val();
-            let received_by = $('#received-by').val();
-            let jc_no = $("#ref_jobcard").val();
 
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
-                url: '/material-transfer/' + mutation_uuid,
+                url: '/material-transfer/' + inventoryout_uuid,
                 type: 'PUT',
                 data: {
-                    jc_no: jc_no,
+                    ref_no: ref_no,
                     storage_id: storage_id,
-                    requested_at: date,
-                    note: note,
+                    inventoried_at: date,
+                    description: description,
                     section: section_code,
-                    received_by: received_by
                 },
                 success: function (response) {
                     if (response.errors) {
@@ -367,6 +350,7 @@ let MutationEdit = {
                 }
             });
         });
+
     }
 };
 
@@ -374,8 +358,28 @@ jQuery(document).ready(function () {
     MutationEdit.init();
 });
 
-$("#item").on("change", function () {
+$("#item").change(function () {
+
     let item_uuid = $("#item").val();
+
+    $.ajax({
+        url: '/get-serial-number/' + item_uuid,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $('select[name="serial_no"]').empty();
+
+            $('select[name="serial_no"]').append(
+                '<option value=""> Select a Serial Number</option>'
+            );
+
+            $.each(data, function (key, value) {
+                $('select[name="serial_no"]').append(
+                    '<option value="' + value + '">' + value + '</option>'
+                );
+            });
+        }
+    });
 
     $.ajax({
         url: '/get-item-unit-uuid/' + item_uuid,
@@ -395,4 +399,15 @@ $("#item").on("change", function () {
             });
         }
     });
+});
+
+$("#serial_no").change(function () {
+    if ($("#serial_no").val() !== '') {
+        $("input[name=qty_request]").val(1);
+        $("input[name=qty_request]").prop('disabled', true);
+        $('#unit_id').prop('disabled', true);
+    } else {
+        $("input[name=qty_request]").prop('disabled', false);
+        $('#unit_id').prop('disabled', false);
+    }
 });
