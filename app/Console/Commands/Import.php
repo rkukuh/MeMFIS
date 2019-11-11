6cf32506231110244e631cbb8c32c22b34b4feec
audhy <audhy.vk01@gmail.com>
2019-11-11 13:18:03 +0700

    import wip

--- app/Console/Commands/Import.php
+++ app/Console/Commands/Import.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Http\Controllers\Import\OldDataController;
 
 class Import extends Command
 {
@@ -11,7 +12,7 @@ class Import extends Command
      *
      * @var string
      */
-    protected $signature = 'memfis:import {name} {--file}';
+    protected $signature = 'memfis:import-data {--name=}';
 
     /**
      * The console command description.
@@ -39,7 +40,46 @@ class Import extends Command
     {
         ini_set('memory_limit', '-1');
 
-        $this->line('Importing: Customers');
-        // dump($this->argument('file'));
+        switch ($this->option('name')) {
+            case 'customer':
+                $this->line('Importing: Customers');
+                app()->make(OldDataController::class)->customers();
+                break;
+            case 'employee':
+                $this->line('Importing: Employees, and create their user account');
+                app()->make(OldDataController::class)->users();
+                break;
+            case 'enginer':
+                $this->line('Importing: Engines');
+                app()->make(OldDataController::class)->engines();
+                break;
+            case 'workarea':
+                $this->line('Importing: Work Areas');
+                app()->make(OldDataController::class)->workAreas();
+                break;
+            case 'item':
+                $this->line('Importing: Materials and Tools');
+                app()->make(OldDataController::class)->materialsAndTools();
+                break;
+            case 'taskcard-boeing':
+                $this->line('Importing: Task Cards for Boeing');
+                app()->make(OldDataController::class)->taskCardsBoeing();
+                break;
+            case 'taskcard-trigana':
+                $this->line('Importing: Task Cards CPCP for Trigana');
+                app()->make(OldDataController::class)->taskCardsCPCPTrigana();
+                break;
+            case 'taskcard-cn':
+                $this->line('Importing: Task Cards for CN');
+                app()->make(OldDataController::class)->taskCardsCN();
+                break;
+            case 'item-cn':
+                $this->line('Importing: Materials and Tools for CN');
+                app()->make(OldDataController::class)->materialsAndToolsCN();
+                break;
+            default:
+                $this->line('Importing: File not Found');
+                break;
+        }
     }
 }
