d6ec4e2b8bfa2d285c5397b019efecbaabbd5028
ThamrinPH <husni.sosnet@gmail.com>
2019-11-12 15:14:04 +0700

    add fields

--- database/migrations/2019_11_12_142745_create_attendances_table.php
+++ database/migrations/2019_11_12_142745_create_attendances_table.php
@@ -14,8 +14,28 @@ class CreateAttendancesTable extends Migration
     public function up()
     {
         Schema::create('attendances', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
+            $table->char('uuid', 36)->unique();
+            $table->unsignedBigInteger('employee_id');
+            $table->date('date');
+            $table->time('in');
+            $table->time('out');
+            $table->bigInteger('late_in')->nullable();
+            $table->bigInteger('earlier_out')->nullable();
+            $table->bigInteger('overtime')->nullable();
+            $table->unsignedBigInteger('statuses_id')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('employee_id')
+            ->references('id')->on('employees')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->foreign('statuses_id')
+            ->references('id')->on('statuses')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
         });
     }
 
