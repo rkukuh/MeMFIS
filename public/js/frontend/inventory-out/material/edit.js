745c2db860479264945c55e6c084cc04197b43b6
andydptyo <andy.dptyo@gmail.com>
2019-11-14 05:05:55 +0700

    inventory-out : fix bug

--- public/js/frontend/inventory-out/material/edit.js
+++ public/js/frontend/inventory-out/material/edit.js
@@ -404,6 +404,25 @@ $("#material").change(function() {
             $("#exp_date").val(data);
         }
     });
+
+    $.ajax({
+        url: '/get-item-unit-uuid/' + item_uuid,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $('select[name="unit_id"]').empty();
+
+            $('select[name="unit_id"]').append(
+                '<option value=""> Select a Unit</option>'
+            );
+
+            $.each(data, function (key, value) {
+                $('select[name="unit_id"]').append(
+                    '<option value="' + key + '">' + value + '</option>'
+                );
+            });
+        }
+    });
 });
 
 $("#serial_no").change(function() {
