0e956afefb76ec47bf06158572577c784bc87cbd
ThamrinPH <husni.sosnet@gmail.com>
2019-11-13 10:05:30 +0700

    add fields

--- database/migrations/2019_11_13_092741_create_leaves_table.php
+++ database/migrations/2019_11_13_092741_create_leaves_table.php
@@ -15,7 +15,28 @@ class CreateLeavesTable extends Migration
     {
         Schema::create('leaves', function (Blueprint $table) {
             $table->increments('id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedBigInteger('employee_id')->index();
+            $table->unsignedBigInteger('status_id')->index();
+            $table->unsignedBigInteger('type_id')->nullable(); //correctin time type
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('employee_id')
+            ->references('id')->on('employees')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->foreign('status_id')
+            ->references('id')->on('statuses')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->foreign('type_id')
+            ->references('id')->on('types')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
         });
     }
 
