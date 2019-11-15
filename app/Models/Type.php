2e7c4f97dfb7e6ec164e3f82b7708b85a171a8de
audhy <audhy.vk01@gmail.com>
2019-11-15 14:13:22 +0700

    refactor

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -583,6 +583,17 @@ class Type extends MemfisModel
     }
 
     /**
+     * Scope a query to only include type of Tax payment method.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOfTaxPaymentMethod(Builder $query)
+    {
+        return $query->where('of', 'tax-payment-method');
+    }
+
+    /**
      * Scope a query to only include type of Term of Payment.
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
@@ -919,8 +930,8 @@ class Type extends MemfisModel
     public function websites()
     {
         return $this->hasMany(Website::class);
-    
-    
+
+
     /**
      * One-to-Many: A website may have zero or many type.
      *
