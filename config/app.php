468a0b84d454145a70e9356b87bc09cc3c976cb2
audhy <audhy.vk01@gmail.com>
2019-11-18 17:27:02 +0700

    install yajra

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Barryvdh\DomPDF\ServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
         // Directoryxx\Finac\FAServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
 
     ],
@@ -218,7 +219,7 @@ return [
         'PDF'       => Barryvdh\DomPDF\Facade::class,
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
-
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
