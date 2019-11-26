ab604ef2d3b3bbf9b8b3ee6b482a405dd45fee49
ThamrinPH <husni.sosnet@gmail.com>
2019-11-26 11:07:47 +0700

    workshift table seeder

--- database/seeds/WorkshiftsTableSeeder.php
+++ database/seeds/WorkshiftsTableSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use App\Models\Workshift;
+use App\Models\WorkshiftSchedule;
 
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
 class WorkshiftsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +14,68 @@ class WorkshiftsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $workshift = Workshift::create([
+            'code' => 'RGLR-001',
+            'name' => 'REGULAR SHIFT',
+            'description' => 'monday to friday',
+        ]);
+
+        $days = [];
+
+        $day = [
+            'day' => 'monday',
+            'in' => Carbon::createFromTimeString('06:30:00', 'Asia/Jakarta'),
+            'break_in' => Carbon::createFromTimeString('12:00:00', 'Asia/Jakarta'),
+            'break_out' => Carbon::createFromTimeString('13:00:00', 'Asia/Jakarta'),
+            'out' => Carbon::createFromTimeString('16:30:00', 'Asia/Jakarta')
+        ];
+        array_push($days,$day);
+
+        $day = [
+            'day' => 'tueday',
+            'in' => Carbon::createFromTimeString('06:30:00', 'Asia/Jakarta'),
+            'break_in' => Carbon::createFromTimeString('12:00:00', 'Asia/Jakarta'),
+            'break_out' => Carbon::createFromTimeString('13:00:00', 'Asia/Jakarta'),
+            'out' => Carbon::createFromTimeString('16:30:00', 'Asia/Jakarta')
+        ];
+        array_push($days,$day);
+
+        $day = [
+            'day' => 'wednesday',
+            'in' => Carbon::createFromTimeString('06:30:00', 'Asia/Jakarta'),
+            'break_in' => Carbon::createFromTimeString('12:00:00', 'Asia/Jakarta'),
+            'break_out' => Carbon::createFromTimeString('13:00:00', 'Asia/Jakarta'),
+            'out' => Carbon::createFromTimeString('16:30:00', 'Asia/Jakarta')
+        ];
+        array_push($days,$day);
+
+        $day = [
+            'day' => 'thurday',
+            'in' => Carbon::createFromTimeString('06:30:00', 'Asia/Jakarta'),
+            'break_in' => Carbon::createFromTimeString('12:00:00', 'Asia/Jakarta'),
+            'break_out' => Carbon::createFromTimeString('13:00:00', 'Asia/Jakarta'),
+            'out' => Carbon::createFromTimeString('16:30:00', 'Asia/Jakarta')
+        ];
+        array_push($days,$day);
+
+        $day = [
+            'day' => 'friday',
+            'in' => Carbon::createFromTimeString('06:30:00', 'Asia/Jakarta'),
+            'break_in' => Carbon::createFromTimeString('12:00:00', 'Asia/Jakarta'),
+            'break_out' => Carbon::createFromTimeString('13:00:00', 'Asia/Jakarta'),
+            'out' => Carbon::createFromTimeString('16:30:00', 'Asia/Jakarta')
+        ];
+        array_push($days,$day);
+
+        foreach($days as $day){
+            WorkshiftSchedule::create([
+                'in' => $day['in'],
+                'out' => $day['out'],
+                'days' => $day['day'],
+                'break_in' => $day['break_in'],
+                'break_out' => $day['break_out'],
+                'workshift_id' => $workshift->id,
+            ]);
+        }
     }
 }
