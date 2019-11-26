886c2b415479b1b719782116f6887a6382e320c5
ThamrinPH <husni.sosnet@gmail.com>
2019-11-26 11:08:13 +0700

    add new status of attendances

--- database/seeds/composites/statuses/StatusesOfAttendance.php
+++ database/seeds/composites/statuses/StatusesOfAttendance.php
@@ -19,14 +19,32 @@ class StatusesOfAttendance extends Seeder
         ]);
 
         Status::create([
-            'code' => 'undiscipline',
-            'name' => 'UNDISCIPLINE',
+            'code' => 'late',
+            'name' => 'LATE',
+            'of'   => 'attendance',
+        ]);
+
+        Status::create([
+            'code' => 'very-late',
+            'name' => 'VERY LATE',
+            'of'   => 'attendance',
+        ]);
+
+        Status::create([
+            'code' => 'earlier-leave',
+            'name' => 'EARLIER LEAVE',
             'of'   => 'attendance',
         ]);
 
         Status::create([
             'code' => 'normal',
             'name' => 'NORMAL',
+            'of'   => 'attendance',
+        ]);
+
+        Status::create([
+            'code' => 'undiscipline',
+            'name' => 'UNDISCIPLINE',
             'of'   => 'attendance',
         ]);
 
