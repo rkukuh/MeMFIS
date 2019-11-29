50e742c7bbd7dd3fd507e98f4553a3f5a958a03c
ibnuprtma <pibnu86@gmail.com>
2019-11-29 11:15:23 +0700

    job scope create edit show

---
+++ resources/views/frontend/job-scope/edit.blade.php
@@ -0,0 +1,108 @@
+@extends('frontend.master')
+
+@section('content')
+    <div class="m-subheader hidden">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">
+                    Job Scope Level
+                </h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">
+                        -
+                    </li>
+                    <li class="m-nav__item">
+                        <a href="{{ route('frontend.job-scope.index') }}" class="m-nav__link">
+                            <span class="m-nav__link-text">
+                                Job Scope Level
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="m-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="m-portlet">
+                    <div class="m-portlet__head">
+                        <div class="m-portlet__head-caption">
+                            <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon m--hide">
+                                    <i class="la la-gear"></i>
+                                </span>
+
+                                @include('frontend.common.label.edit')
+
+                                <h3 class="m-portlet__head-text">
+                                    Job Scope Level
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-portlet m-portlet--mobile">
+                        <div class="m-portlet__body">
+                            <form id="itemform" name="itemform">
+                                <div class="m-portlet__body">
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Level
+                                            </label>
+
+                                            @component('frontend.common.input.text')
+                                                @slot('id', 'level')
+                                                @slot('name', 'level')
+                                                @slot('id_error', 'level')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-6 col-md-6 col-lg-6">
+                                            <label class="form-control-label">
+                                                Job Scope Description
+                                            </label>
+
+                                            @component('frontend.common.input.text')
+                                                @slot('id', 'job_scope_description')
+                                                @slot('name', 'job_scope_description')
+                                                @slot('id_error', 'job_scope_description')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-12 col-md-12 col-lg-12 footer">
+                                            <div class="flex">
+                                                <div class="action-buttons">
+                                                    @component('frontend.common.buttons.submit')
+                                                        @slot('type','button')
+                                                        @slot('id', 'add-job-scope-level')
+                                                        @slot('class', 'add-job-scope-level')
+                                                    @endcomponent
+
+                                                    @include('frontend.common.buttons.reset')
+
+                                                    @include('frontend.common.buttons.back')
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('js/frontend/job-scope/create.js')}}"></script>
+@endpush
+
