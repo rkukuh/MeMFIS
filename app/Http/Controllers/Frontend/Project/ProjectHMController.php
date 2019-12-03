a94710d8853cf39dbf6c911ad8fb98687daebe68
audhy <audhy.vk01@gmail.com>
2019-12-03 14:32:43 +0700

    test

--- app/Http/Controllers/Frontend/Project/ProjectHMController.php
+++ app/Http/Controllers/Frontend/Project/ProjectHMController.php
@@ -86,13 +86,13 @@ class ProjectHMController extends Controller
      */
     public function edit(Project $project)
     {
-        if($project->approvals()->latest()->first()){
-            return redirect()->route('frontend.project-hm.show', [
-                'project' => $project->uuid
-            ]);
-        }
+        // if($project->approvals()->latest()->first()){
+        //     return redirect()->route('frontend.project-hm.show', [
+        //         'project' => $project->uuid
+        //     ]);
+        // }
         $attention = $project->quotations;
-        dd($project);
+        // dd($project);
         return view('frontend.project.hm.edit',[
             'project' => $project,
             'aircrafts' => $this->aircrafts,
