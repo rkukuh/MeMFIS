0777f8915add0fc75371024a8a03c180fea91480
ThamrinPH <husni.sosnet@gmail.com>
2019-12-04 09:37:02 +0700

    refactor

--- app/Imports/SimUserImport.php
+++ app/Imports/SimUserImport.php
@@ -25,52 +25,56 @@ class SimUserImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
+        $user = User::where('email', $row['email'])->first();
 
-        $faker = Faker\Factory::create();
+        if(empty($user)){
+                
+            $faker = Faker\Factory::create();
 
-        $name = $row['first_name'].' '.$row['last_name'];
-        $user = new User([
-            'name' => ucwords(strtolower($name)),
-            'email' => !empty($row['email']) ? strtolower($row['email']) : str_slug(strtolower($row['nama'])) . '@example.org',
-            'password' => Hash::make($row['passwords']),
-            'is_active' => 1
-        ]);
-
-        $user->save();
-
-        $role = Role::where('name', $row['roles'])->first();
-
-        $user->assignRole($role);
-
-        $employee = [
-            'code' => $faker->randomNumber($nbDigits = NULL, $strict = false),
-            'first_name' => ucwords(strtolower($row['first_name'])),
-            'last_name' => ucwords(strtolower($row['last_name'])),
-            'dob' => Carbon::now()->subYear(rand(20, 50)),
-            'dob_place' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
-            'gender_id' => Type::ofGender()->where('code', $faker->randomElement(['male','female']))->first()->id,
-            'religion_id' => Religion::where('code', $faker->randomElement(['christian-protestant','islam','kong-hu-cu','buddha','catholic','hindu']))->first()->id,
-            'marital_id' => Status::ofMarital()->where('code', $faker->randomElement(['married','single','cerai-hidup','cerai-mati']))->first()->id,
-            'country_id' => Country::first()->id,
-            'city' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
-            'joined_date' => Carbon::now()->toDateString(),
-            'updated_at' => null
-        ]; 
-
-        $user->employee()->create($employee);
-
-        $employee = $user->employee;
-
-        $workshift = Workshift::find($faker->randomElement([1,2])); 
-
-        $employee->workshifts()->attach($workshift->id, [
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
+            $name = $row['first_name'].' '.$row['last_name'];
+            $user = new User([
+                'name' => ucwords(strtolower($name)),
+                'email' => !empty($row['email']) ? strtolower($row['email']) : str_slug(strtolower($row['nama'])) . '@example.org',
+                'password' => Hash::make($row['passwords']),
+                'is_active' => 1
             ]);
-        
-        $employee->nationalities()->attach(Nationality::first()->id, [
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            ]);
+
+            $user->save();
+
+            $role = Role::where('name', $row['roles'])->first();
+
+            $user->assignRole($role);
+
+            $employee = [
+                'code' => $row['nrp'],//$faker->randomNumber($nbDigits = NULL, $strict = false),
+                'first_name' => ucwords(strtolower($row['first_name'])),
+                'last_name' => ucwords(strtolower($row['last_name'])),
+                'dob' => Carbon::now()->subYear(rand(20, 50)),
+                'dob_place' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
+                'gender_id' => Type::ofGender()->where('code', $faker->randomElement(['male','female']))->first()->id,
+                'religion_id' => Religion::where('code', $faker->randomElement(['christian-protestant','islam','kong-hu-cu','buddha','catholic','hindu']))->first()->id,
+                'marital_id' => Status::ofMarital()->where('code', $faker->randomElement(['married','single','cerai-hidup','cerai-mati']))->first()->id,
+                'country_id' => Country::first()->id,
+                'city' => $faker->randomElement(['Surabaya','Jakarta','Sidoarjo','Gresik']),
+                'joined_date' => Carbon::now()->toDateString(),
+                'updated_at' => null
+            ]; 
+
+            $user->employee()->create($employee);
+
+            $employee = $user->employee;
+
+            $workshift = Workshift::find($faker->randomElement([1,2])); 
+
+            $employee->workshifts()->attach($workshift->id, [
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                ]);
+            
+            $employee->nationalities()->attach(Nationality::first()->id, [
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                ]);
+        }
     }
 }
