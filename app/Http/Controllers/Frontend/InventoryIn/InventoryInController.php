254f7297fb1c40c92eef467e930472311b62058b
audhy <audhy.vk01@gmail.com>
2019-12-05 13:36:18 +0700

    try

--- app/Http/Controllers/Frontend/InventoryIn/InventoryInController.php
+++ app/Http/Controllers/Frontend/InventoryIn/InventoryInController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend\InventoryIn;
 
 use Auth;
+use Carbon\Carbon;
 use App\Models\Storage;
 use App\Models\Approval;
 use App\Models\InventoryIn;
@@ -42,6 +43,7 @@ class InventoryInController extends Controller
     public function store(InventoryInStore $request)
     {
         $request->merge(['number' => DocumentNumber::generate('ININ-', InventoryIn::withTrashed()->count() + 1)]);
+        $request->merge(['inventoried_at' => Carbon::parse($request->inventoried_at.'00:00:00')]);
         $request->merge(['inventoryinable_type' => 'App\Models\InventoryIn']);
         $request->merge(['inventoryinable_id' => InventoryIn::withTrashed()->count()+1]);
 
