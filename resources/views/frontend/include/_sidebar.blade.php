177ee02e5c0b1f72358e0cb47a15cda2b6b0e1dd
ThamrinPH <husni.sosnet@gmail.com>
2019-12-16 13:41:52 +0700

    hide unused navlink according to TW 12/16/2019 13:40:50

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -1041,7 +1041,7 @@
                         </span>
                     </a>
                 </li>
-                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
+                <!-- <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
                     <a href="" class="m-menu__link m-menu__toggle">
                         <i class="m-menu__link-icon flaticon-list-3"></i>
                         <span class="m-menu__link-text">
@@ -1064,7 +1064,7 @@
                             Leave Datalist
                         </span>
                     </a>
-                </li>
+                </li> -->
             @endrole
         </ul>
     </div>
