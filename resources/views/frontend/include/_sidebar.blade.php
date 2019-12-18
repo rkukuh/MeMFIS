b9cd6c8dd18d30849d662bbbdcad3a7528f5c7cb
ibnuprtma <pibnu86@gmail.com>
2019-12-18 16:32:37 +0700

    fix typo

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -551,6 +551,12 @@
                         <span class="m-menu__link-text">Master Asset</span>
                     </a>
                 </li>
+                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
+                    <a href="/bond" class="m-menu__link m-menu__toggle">
+                        <i class="m-menu__link-icon flaticon-list-3"></i>
+                        <span class="m-menu__link-text">Bond</span>
+                    </a>
+                </li>
             @endrole
 
             @hasanyrole('admin|scm')
