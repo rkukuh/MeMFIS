e2b95917bbc1c01248b410bec79502c0d1b9c6d9
ibnuprtma <pibnu86@gmail.com>
2019-12-18 14:05:24 +0700

    change payroll sidebar

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -1055,6 +1055,12 @@
                         </span>
                     </a>
                 </li>
+                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
+                    <a href="{{ route('frontend.payroll.index') }}" class="m-menu__link m-menu__toggle">
+                        <i class="m-menu__link-icon flaticon-list-3"></i>
+                        <span class="m-menu__link-text">Payroll</span>
+                    </a>
+                </li>
             @endrole
         </ul>
     </div>
