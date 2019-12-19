f66d787297cf57b3746959733d5966f644db1198
audhy <audhy.vk01@gmail.com>
2019-12-19 13:47:54 +0700

    fix bug

--- database/migrations/2019_12_13_124522_create_purchase_request_service_table.php
+++ database/migrations/2019_12_13_124522_create_purchase_request_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePurchaserequestservicesTable extends Migration
+class CreatePurchaseRequestServiceTable extends Migration
 {
     /**
      * Run the migrations.
