6d0fdd024371440a41035755ab3b997d5b5349a5
ThamrinPH <husni.sosnet@gmail.com>
2019-12-20 09:25:27 +0700

    only create when empty

--- app/Http/Controllers/Frontend/Employee/EmployeeAttendanceController.php
+++ app/Http/Controllers/Frontend/Employee/EmployeeAttendanceController.php
@@ -413,52 +413,61 @@ class EmployeeAttendanceController extends Controller
     public function createAttendances(){
         $days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
         // code yang seharusnya dijalankan sehari-hari
-        // $employees = Employee::get(); //todo where active and approved, tapi fitur belom ada
-        // // create attendance with null;
-        // $in = '00:00:00';
-        // $out = '00:00:00';
-        // foreach($employees as $employee){
-        //         EmployeeAttendance::create([
-        //         'employee_id' => $employee->id,
-        //         'date' => Carbon::today(),
-        //         'in' => $in,
-        //         'out' => $out
-        //     ]);
-        // }
-
-        // code yang dijalankan untuk trial
         $employees = Employee::get(); //todo where active and approved, tapi fitur belom ada
         // create attendance with null;
         $in = '00:00:00';
         $out = '00:00:00';
-        
-        foreach($employees as $employee){
-            if(sizeof($employee->workshifts) > 0){
-                $workshift = $employee->workshifts->first();
-                $shifts = $workshift->workshift_schedules;
-                
-                for($day = 1 ; $day <= 31 ; $day++){
-                    $date = Carbon::create(2019, 11, $day, 0, 0, 0, 'Asia/Jakarta');
-    
-                    $attendance = EmployeeAttendance::create([
-                        'employee_id' => $employee->id,
-                        'date' => $date,
-                        'in' => $in,
-                        'out' => $out
-                    ]);
-    
-                    $shift = $shifts->where('days', $days[$date->dayOfWeek])->first();
-                    if($shift){
-                        $status = Status::ofAttendance()->where('code','absence')->first();
-                        $attendance->statuses()->attach($status->id);
-                    }else{
-                        $status = null;
-                    }
 
+        $attendance = EmployeeAttendance::whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)->first();
 
-                }
+        if(empty($attendance)){
+            foreach($employees as $employee){
+                EmployeeAttendance::create([
+                    'employee_id' => $employee->id,
+                    'date' => Carbon::today(),
+                    'in' => $in,
+                    'out' => $out
+                ]);
             }
-
+        }else{
+            return;
         }
+
+       
+
+        // code yang dijalankan untuk trial
+        // $employees = Employee::get(); //todo where active and approved, tapi fitur belom ada
+        // // create attendance with null;
+        // $in = '00:00:00';
+        // $out = '00:00:00';
+        
+        // foreach($employees as $employee){
+        //     if(sizeof($employee->workshifts) > 0){
+        //         $workshift = $employee->workshifts->first();
+        //         $shifts = $workshift->workshift_schedules;
+                
+        //         for($day = 1 ; $day <= 31 ; $day++){
+        //             $date = Carbon::create(2019, 11, $day, 0, 0, 0, 'Asia/Jakarta');
+    
+        //             $attendance = EmployeeAttendance::create([
+        //                 'employee_id' => $employee->id,
+        //                 'date' => $date,
+        //                 'in' => $in,
+        //                 'out' => $out
+        //             ]);
+    
+        //             $shift = $shifts->where('days', $days[$date->dayOfWeek])->first();
+        //             if($shift){
+        //                 $status = Status::ofAttendance()->where('code','absence')->first();
+        //                 $attendance->statuses()->attach($status->id);
+        //             }else{
+        //                 $status = null;
+        //             }
+
+
+        //         }
+        //     }
+
+        // }
     }
 }
