b7d3e2ce54db3f2e326fe34eed6bbe28275b3216
audhy <audhy.vk01@gmail.com>
2019-12-26 08:44:41 +0700

    workpackage permision create and edit

--- resources/views/frontend/workpackage/create.blade.php
+++ resources/views/frontend/workpackage/create.blade.php
@@ -1,3 +1,4 @@
+@can('heavy-maintenace_workpackage_create')
 @extends('frontend.master')
 
 @section('content')
@@ -149,3 +150,4 @@
     <script src="{{ asset('js/frontend/workpackage/create.js') }}"></script>
     <script src="{{ asset('js/frontend/workpackage/form-reset.js') }}"></script>
 @endpush
+@endcan
