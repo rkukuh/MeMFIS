088a03cd379f1c4da43f2a54abe9e87a76508ef3
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-01-16 20:18:16 +0700

    add general ledger in sidebar

--- resources/views/frontend/include/_sidebar.blade.php
+++ resources/views/frontend/include/_sidebar.blade.php
@@ -558,6 +558,12 @@
                     </a>
                 </li>
                 <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
+                    <a href="/general-ledger" class="m-menu__link m-menu__toggle">
+                        <i class="m-menu__link-icon flaticon-list-3"></i>
+                        <span class="m-menu__link-text">General Ledger</span>
+                    </a>
+                </li>
+                <li class="m-menu__item" aria-haspopup="true" data-menu-submenu-toggle="hover">
                     <a href="/bond" class="m-menu__link m-menu__toggle">
                         <i class="m-menu__link-icon flaticon-list-3"></i>
                         <span class="m-menu__link-text">Bond</span>
